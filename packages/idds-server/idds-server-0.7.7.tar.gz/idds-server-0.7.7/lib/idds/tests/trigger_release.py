a8210b8c866287249ca65c7ad96c3dab22886a6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:09:56 +0000

    Add package idds-server/0.7.7/idds-server-0.7.7.tar.gz

---
+++ packages/idds-server/idds-server-0.7.7.tar.gz/idds-server-0.7.7/lib/idds/tests/trigger_release.py
@@ -0,0 +1,32 @@
+
+from idds.common.utils import json_dumps                 # noqa F401
+from idds.common.constants import ContentStatus, ContentRelationType          # noqa F401
+from idds.core.requests import get_requests              # noqa F401
+from idds.core.messages import retrieve_messages         # noqa F401
+from idds.core.transforms import get_transforms, release_inputs          # noqa F401
+from idds.core.workprogress import get_workprogresses    # noqa F401
+from idds.core.processings import get_processings        # noqa F401
+from idds.core import transforms as core_transforms      # noqa F401
+from idds.core.catalog import get_contents, update_contents  # noqa F401
+from idds.orm.contents import get_input_contents             # noqa F401
+
+
+request_id = 221
+contents = get_contents(request_id=request_id, status=ContentStatus.Available)
+ret_contents = {}
+for content in contents:
+    if content['content_relation_type'] == ContentRelationType.Output:   # InputDependency
+        if content['coll_id'] not in ret_contents:
+            ret_contents[content['coll_id']] = []
+        ret_contents[content['coll_id']].append(content)
+
+for ret_content in ret_contents:
+    print(ret_content)
+    break
+
+updated_contents = core_transforms.release_inputs_by_collection(ret_contents)
+for update_content in updated_contents:
+    print(update_content)
+    # break
+
+update_contents(updated_contents)
