3392e6152be91d6c779d4028a48bb5859b19bcdf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:12:02 +0000

    Add package mygpio/0.0.1/mygpio-0.0.1.tar.gz

---
+++ packages/mygpio/mygpio-0.0.1.tar.gz/mygpio-0.0.1/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+with open('README.md', 'r') as f:
+	long_description = f.read()
+
+setup(
+	name='mygpio',
+	version='0.0.1',
+	description='GPIO Library which extends GPIO functions',
+	long_description=long_description,
+	long_description_content_type='text/markdown',
+	url='',
+	author='Governor Baloyi',
+	author_email='governor.baloyi@gmail.com',
+	install_requires=[], # list of specifiers
+	py_modules=['mygpio'],
+	package_dir={'': 'mygpio'},
+	extras_require={
+		"dev": [
+			"pytest>=3.7",
+		],
+	},
+	classifiers=[ 
+		# https://pypi.org/classifiers
+	],
+)
