c64b6e1d1082575bde7faa298184c20f7229b1f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:14 +0000

    Add package py42/1.18.1/py42-1.18.1-py2.py3-none-any.whl

---
+++ packages/py42/py42-1.18.1-py2.py3-none-any.whl/py42/clients/__init__.py
@@ -0,0 +1,15 @@
+from collections import namedtuple
+
+Clients = namedtuple(
+    "Clients",
+    [
+        "alerts",
+        "archive",
+        "authority",
+        "detectionlists",
+        "securitydata",
+        "auditlogs",
+        "cases",
+        "loginconfig",
+    ],
+)
