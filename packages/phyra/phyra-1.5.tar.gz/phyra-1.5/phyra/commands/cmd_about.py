49ce8293a9c6aea9f669a20667413aafe1a05810
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:55 +0000

    Add package phyra/1.5/phyra-1.5.tar.gz

---
+++ packages/phyra/phyra-1.5.tar.gz/phyra-1.5/phyra/commands/cmd_about.py
@@ -0,0 +1,10 @@
+import click
+
+@click.command()
+def cli():
+    '''About Phyra CLI'''
+    print('=== PHYRA CLI ===')
+    print('- Developer : LyQuid')
+    print('- Email : lyquidpersonal@gmail.com')
+    print('- Open source : https://github.com/EterNomm/Phyra-CLI')
+    print('- Pypi Web : https://pypi.org/project/phyra')
