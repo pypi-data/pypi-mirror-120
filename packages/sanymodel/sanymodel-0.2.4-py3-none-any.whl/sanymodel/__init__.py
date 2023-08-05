b015cee4b4143172b88959afe740e700e2b78347
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:12:44 +0000

    Add package sanymodel/0.2.4/sanymodel-0.2.4-py3-none-any.whl

---
+++ packages/sanymodel/sanymodel-0.2.4-py3-none-any.whl/sanymodel/__init__.py
@@ -0,0 +1,12 @@
+from ._version import __version__
+from .modeltools import ModelTool, Result, Tag, Logger, Figure
+
+__all__ = [
+    "__version__",
+    "ModelTool",
+    "Result",
+    "Tag",
+    "Logger",
+    "Figure"
+
+]
