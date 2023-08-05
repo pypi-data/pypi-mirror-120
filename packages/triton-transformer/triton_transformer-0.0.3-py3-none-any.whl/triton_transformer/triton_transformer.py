f24149be18160b532dcf367b4175ce7893fd9148
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:12:19 +0000

    Add package triton-transformer/0.0.3/triton_transformer-0.0.3-py3-none-any.whl

---
+++ packages/triton-transformer/triton_transformer-0.0.3-py3-none-any.whl/triton_transformer/triton_transformer.py
@@ -0,0 +1,114 @@
+import torch
+from torch import nn, einsum
+import torch.nn.functional as F
+from einops import rearrange
+
+import triton
+import triton.language as tl
+
+# helpers classes
+
+class Attention(nn.Module):
+    def __init__(
+        self,
+        dim,
+        dim_head = 64,
+        heads = 8
+    ):
+        super().__init__()
+        self.heads = heads
+        self.scale = dim_head ** -0.5
+        inner_dim = dim_head * heads
+
+        self.norm = nn.LayerNorm(dim)
+        self.to_qkv = nn.Linear(dim, inner_dim * 3, bias = False)
+        self.to_out = nn.Linear(inner_dim, dim)
+
+    def forward(self, x, mask = None):
+        h = self.heads
+        x = self.norm(x)
+
+        q, k, v = self.to_qkv(x).chunk(3, dim = -1)
+        q, k, v = map(lambda t: rearrange(t, 'b n (h d) -> (b h) n d', h = h), (q, k, v))
+
+        q = q * self.scale
+        sim = einsum('b i d, b j d -> b i j', q, k)
+
+        if mask is not None:
+            mask_value = -torch.finfo(sim.dtype).max
+            sim = sim.masked_fill(mask, mask_value)
+
+        attn = sim.softmax(dim = -1)
+        out = einsum('b i j, b j d -> b i d', attn, v)
+        out = rearrange(out, '(b h) n d -> b n (h d)', h = h)
+        return self.to_out(out)
+
+def FeedForward(dim, mult = 4):
+    return nn.Sequential(
+        nn.LayerNorm(dim),
+        nn.Linear(dim, dim * mult),
+        nn.GELU(),
+        nn.Linear(dim * mult, dim)
+    )
+
+# main class
+
+class Transformer(nn.Module):
+    def __init__(
+        self,
+        *,
+        dim,
+        num_tokens,
+        max_seq_len,
+        depth,
+        causal = False,
+        heads = 8,
+        dim_head = 64
+    ):
+        super().__init__()
+        self.token_emb = nn.Embedding(num_tokens, dim)
+        self.pos_emb = nn.Embedding(max_seq_len, dim)
+
+        self.layers = nn.ModuleList([])
+        for _ in range(depth):
+            self.layers.append(nn.ModuleList([
+                Attention(dim, heads = heads, dim_head = dim_head),
+                FeedForward(dim)
+            ]))
+
+        self.to_logits = nn.Sequential(
+            nn.LayerNorm(dim),
+            nn.Linear(dim, num_tokens)
+        )
+
+        # mask
+
+        self.causal = causal
+        mask = torch.ones(max_seq_len, max_seq_len, dtype = torch.bool).triu(1) if causal else None
+        self.register_buffer('mask', mask, persistent = False)
+
+    def forward(self, x, mask = None):
+        n, device = x.shape[1], x.device
+
+        # embed token and add positional embedding
+
+        x = self.token_emb(x)
+        pos_emb = self.pos_emb(torch.arange(n, device = device))
+        x = x + rearrange(pos_emb, 'n d -> () n d')
+
+        # generate mask, depending on whether autoregressive or not
+
+        if self.causal:
+            mask = self.mask[:n, :n]
+            mask = rearrange(mask, 'i j -> () i j')
+        elif mask is not None:
+            mask = rearrange(mask, 'b i -> b i ()') * rearrange(mask, 'b j -> b () j')
+            mask = ~mask
+
+        # go through layers
+
+        for attn, ff in self.layers:
+            x = attn(x, mask = mask) + x
+            x = ff(x) + x
+
+        return self.to_logits(x)
