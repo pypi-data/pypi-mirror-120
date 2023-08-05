7a7537438e008bbedaa5e65450dfacc3e480a56e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:10:24 +0000

    Add package mlplatform-lib/8.4.0.0.10.0.3/mlplatform_lib-8.4.0.0.10.0.3.tar.gz

---
+++ packages/mlplatform-lib/mlplatform_lib-8.4.0.0.10.0.3.tar.gz/mlplatform_lib-8.4.0.0.10.0.3/mlplatform_lib/dataclass/__init__.py
@@ -0,0 +1,19 @@
+from .data_object_out_col import DataObjectOutCol
+from .data_object import DataObject
+from .insert_tuple_object import InsertTupleObject
+from .column_of_table_from_default_source import ColumnOfTableFromDefaultSource
+from .table_from_default_datasource_info import TableFromDefaultDataSourceInfo
+from .table_column_info import TableColumnInfo
+from .table_desc_info import TableDescInfo
+from .data_object_info_list import DataObjectInfoList
+
+__all__ = [
+    "DataObjectOutCol",
+    "DataObject",
+    "InsertTupleObject",
+    "ColumnOfTableFromDefaultSource",
+    "TableFromDefaultDataSourceInfo",
+    "TableColumnInfo",
+    "TableDescInfo",
+    "DataObjectInfoList"
+]
