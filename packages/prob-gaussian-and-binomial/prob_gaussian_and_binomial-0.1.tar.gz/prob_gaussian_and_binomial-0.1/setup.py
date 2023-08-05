85acc4950ccae3e30259a6a132b869036348079c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:11:59 +0000

    Add package prob-gaussian-and-binomial/0.1/prob_gaussian_and_binomial-0.1.tar.gz

---
+++ packages/prob-gaussian-and-binomial/prob_gaussian_and_binomial-0.1.tar.gz/prob_gaussian_and_binomial-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='prob_gaussian_and_binomial',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['prob_gaussian_and_binomial'],
+      author = 'Douglas Onkeo',
+      author_email = 'douglasonkeo456@gmail.com',
+      zip_safe=False)
