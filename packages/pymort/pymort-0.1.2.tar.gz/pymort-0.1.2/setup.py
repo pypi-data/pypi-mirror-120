5bbf9b24a77c015da47f9836d5c39e542d4c4db0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:53 +0000

    Add package pymort/0.1.2/pymort-0.1.2.tar.gz

---
+++ packages/pymort/pymort-0.1.2.tar.gz/pymort-0.1.2/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['pymort', 'pymort.SOA_Tables_20210915']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'pymort',
+    'version': '0.1.2',
+    'description': '',
+    'long_description': None,
+    'author': 'Matthew Caseres',
+    'author_email': 'matthewcaseres@outlook.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
