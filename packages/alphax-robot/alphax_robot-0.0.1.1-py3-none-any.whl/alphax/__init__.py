ea51cad4f3a564fab69c3ff1b8bd7201deabe028
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:10:29 +0000

    Add package alphax-robot/0.0.1.1/alphax_robot-0.0.1.1-py3-none-any.whl

---
+++ packages/alphax-robot/alphax_robot-0.0.1.1-py3-none-any.whl/alphax/__init__.py
@@ -0,0 +1,14 @@
+__version__ = "0.0.1.1"
+__name__ = "alphax"
+
+def add_numbers(num1, num2):
+    return num1 + num2
+
+def subtract_numbers(num1, num2):
+    return num1 - num2
+
+def multiply_numbers(num1, num2):
+    return num1 * num2
+
+def divide_numbers(num1, num2):
+    return num1 / num2
