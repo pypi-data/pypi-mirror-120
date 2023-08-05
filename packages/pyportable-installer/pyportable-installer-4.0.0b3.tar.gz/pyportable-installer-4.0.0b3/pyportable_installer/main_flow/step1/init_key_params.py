2f7321cd50e3d63f2c781cc47278e3bac1e886e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:05:50 +0000

    Add package pyportable-installer/4.0.0b3/pyportable-installer-4.0.0b3.tar.gz

---
+++ packages/pyportable-installer/pyportable-installer-4.0.0b3.tar.gz/pyportable-installer-4.0.0b3/pyportable_installer/main_flow/step1/init_key_params.py
@@ -0,0 +1,119 @@
+from os.path import exists
+
+from embed_python_manager import EmbedPythonManager
+from lk_logger import lk
+
+from ...compilers.accessory import where_python_installed
+from ...global_conf import gconf
+from ...path_model import prj_model
+from ...typehint import *
+
+
+def init_key_params(conf: TConf, **kwargs):
+    # pyproj_file
+    gconf.pyproj_file = kwargs['pyproj_file']
+    # pyproj_dir
+    gconf.pyproj_dir = kwargs['pyproj_dir']
+    
+    # --------------------------------------------------------------------------
+    # attachments
+    
+    gconf.attachments_exclusions = conf['build']['attachments_exclusions']
+    assert type(gconf.attachments_exclusions) is tuple
+    gconf.attachments_exist_scheme = conf['build']['attachments_exist_scheme']
+    
+    # --------------------------------------------------------------------------
+    
+    pyversion = conf['build']['venv']['python_version']
+    assert pyversion, '`conf["build"]["venv"]["python_version"]` not defined'
+    pyversion = 'python' + pyversion.replace('.', '')
+    gconf.python_version = pyversion
+    
+    # --------------------------------------------------------------------------
+    
+    name = conf['build']['compiler']['name']  # type: TCompilerName
+    mode = conf['build']['venv']['mode']  # type: TVenvMode
+    enable_venv = conf['build']['venv']['enable_venv']  # type: bool
+    
+    is_full_python_required = False
+    if name in ('cython', 'mypyc', 'nuitka'):
+        is_full_python_required = True
+    elif name == 'pyportable_crypto':
+        if conf['build']['compiler']['options'][name]['key'] != '_trial':
+            is_full_python_required = True
+    
+    is_embed_python_required = False
+    if (
+            (name in ('pyarmor', 'pyc')) or
+            (enable_venv and mode == 'pip') or
+            (enable_venv and mode == 'source_venv' and
+             conf['build']['venv']['options']['source_venv']['copy_venv'])
+    ):
+        is_embed_python_required = True
+    
+    if name in ('pyarmor', 'pyc') or \
+            (enable_venv and mode in ('pip', 'source_venv')):
+        is_embed_python_required = True
+    
+    is_pip_required = False
+    if enable_venv and mode == 'pip':
+        is_pip_required = True
+
+    lk.logt('[I5239]',
+            is_full_python_required,
+            is_embed_python_required,
+            is_pip_required)
+    
+    # --------------------------------------------------------------------------
+    
+    if is_full_python_required:
+        # noinspection PyTypedDict
+        options = conf['build']['compiler']['options'][name]
+        if options['python_path'] in ('_auto_detect', 'auto_detect', ''):
+            gconf.full_python = _get_full_python(pyversion)
+        else:
+            gconf.full_python = options['python_path']
+        assert exists(gconf.full_python)
+    
+    # --------------------------------------------------------------------------
+    
+    if is_embed_python_required:
+        if is_pip_required:
+            gconf.embed_python = _get_embed_python(pyversion, True)
+        else:
+            gconf.embed_python = _get_embed_python_from_local(pyversion) or \
+                                 _get_embed_python(pyversion, False)
+        assert exists(gconf.embed_python)
+
+
+def _get_full_python(pyversion):
+    try:
+        return where_python_installed(pyversion) + '/python.exe'
+    except FileNotFoundError as e:
+        lk.logt("[E5951]", 'Cannot find installed python in your computer. '
+                           'Please pass in your python path manually (for '
+                           'example "C:\\Program Files\\Python38") ...')
+        path = input(f'Input path here ({pyversion}): ')
+        if not exists(path):
+            raise e
+        else:
+            return path
+
+
+def _get_embed_python_from_local(pyversion):
+    if pyversion == 'python39' and \
+            exists(f'{prj_model.prj_root}/venv/python39._pth.bak'):
+        return f'{prj_model.prj_root}/venv/python.exe'
+    else:
+        return ''
+
+
+def _get_embed_python(pyversion, add_pip_suits: bool):
+    manager = EmbedPythonManager(pyversion)
+    manager.change_source('npm_taobao_org.yml')
+    manager.deploy(
+        add_pip_suits=add_pip_suits,
+        add_pip_scripts=False,
+        add_tk_suits=False  # FIXME: check tkinter requirement
+    )
+    return manager.python
---
+++ packages/pyportable-installer/pyportable-installer-4.0.0b3.tar.gz/pyportable-installer-4.0.0b3/pyportable_installer/main_flow/step3/step3_3/create_launcher.py
@@ -0,0 +1,296 @@
+import pickle
+from os.path import dirname
+from shutil import copyfile
+from uuid import uuid1
+
+from lk_logger import lk
+from lk_utils import ropen
+from lk_utils import wopen
+from lk_utils.filesniff import get_filename
+from lk_utils.subproc import run_new_thread
+
+from ....bat_2_exe import bat_2_exe
+from ....path_model import *
+from ....typehint import TBuildConf
+
+thread_of_bat_2_exe = None
+_is_depsland_mode = False  # TODO: downgrade it to local variable
+
+
+def create_launcher(build: TBuildConf):
+    global _is_depsland_mode
+    _is_debug_mode = False
+    if build['venv']['enable_venv']:
+        if build['venv']['mode'] == 'depsland':
+            _is_depsland_mode = True
+        elif build['venv']['mode'] == '_no_venv':
+            _is_debug_mode = True
+    #   these will effect `_create_launcher` function.
+    
+    deduplicator = LauncherNamesDeduplicator(build['launcher_name'])
+    fixed_kwargs_for_creating_launcher = {
+        'add_pywin32_support': build[
+            'experimental_features']['add_pywin32_support'],
+        'enable_console'     : build['enable_console'],
+        'enable_venv'        : build['venv']['enable_venv'],
+        'generate_exe'       : False if _is_depsland_mode or
+                                        _is_debug_mode else True,
+        'module_paths'       : build['module_paths'],
+        'module_paths_scheme': build['module_paths_scheme'],
+        'venv_python'        : 'python' if _is_debug_mode else '',
+    }
+    
+    for i, t in enumerate(build['target']):
+        if i == 0:
+            _create_launcher(
+                name=build['launcher_name'], icon=build['icon'],
+                target=t, is_main_entry=True,
+                **fixed_kwargs_for_creating_launcher
+            )
+            
+            if _is_depsland_mode:
+                options = build['venv']['options']['depsland']
+                _create_depsland_setup(
+                    build['launcher_name'], build['icon'],
+                    options['venv_name'], options['venv_id'],
+                    options['requirements'], build['enable_console'],
+                    offline=options['offline'], local=options['local'],
+                    pyversion='python' + build[
+                        'venv']['python_version'].replace('.', '')
+                )
+        else:
+            name = get_filename(t['file'], suffix=False)
+            name = deduplicator.optimize_name(name, t['function'])
+            _create_launcher(
+                name=name, icon='',
+                target=t, is_main_entry=False,
+                **fixed_kwargs_for_creating_launcher
+            )
+
+
+def _create_launcher(
+        name, icon, target, is_main_entry: bool,
+        module_paths=None, module_paths_scheme='translate',
+        enable_venv=True, **options
+):
+    """
+    
+    Args:
+        name:
+        icon:
+        target:
+        is_main_entry:
+        **options:
+
+    Keyword Args:
+        add_pywin32_support: bool[False]
+        enable_console: bool[True]
+        enable_venv:
+        generate_exe: bool[True]
+        module_paths: list[str]
+        module_paths_scheme: Literal['translate', 'leave as-is']
+        venv_python: str
+    """
+    launcher_name = name
+    conf_name = 'main' if is_main_entry else str(uuid1())
+    
+    abs_paths = {
+        'target_file': src_2_dst(target['file']),
+        'target_dir' : src_2_dst(dirname(target['file'])),
+        'launch_file': dst_model.pylauncher,
+        'launch_dir' : dst_model.src_root,
+        'conf_dir'   : dst_model.pylauncher_conf,
+        'conf_file'  : f'{dst_model.pylauncher_conf}/{conf_name}.pkl',
+        'bat_file'   : f'{dst_model.dst_root}/{launcher_name}.bat',
+        'exe_file'   : f'{dst_model.dst_root}/{launcher_name}.exe',
+    }
+    
+    if _is_depsland_mode:
+        abs_paths['bat_file'] = f'{dst_model.build}/{launcher_name}.bat'
+        abs_paths['exe_file'] = ''
+    
+    # if not exists(d := abs_paths['conf_dir']):
+    #     mkdir(d)
+    
+    _rel0 = lambda p: relpath(p, dst_model.dst_root)  # starts from dst root
+    _rel1 = lambda p: relpath(p, dst_model.src_root)  # strats from pylauncher dir
+    rel_paths = {
+        'lib_dir'    : _rel1(dst_model.lib),  # '../lib'
+        'launch_file': _rel0(abs_paths['launch_file']),  # 'src/pylauncher.py'
+        'target_dir' : _rel1(abs_paths['target_dir']),  # 'hello_world'
+        'conf_file'  : _rel1(abs_paths['conf_file']),  # '.pylauncher_conf'
+        'venv_dir'   : _rel0(dst_model.venv),  # 'venv'
+        'venv_python': options.get('venv_python') or
+                       _rel0(dst_model.python)  # 'venv/python.exe'
+    }
+    del _rel0, _rel1
+    
+    # --------------------------------------------------------------------------
+    
+    def _generate_target_conf():
+        with open(abs_paths['conf_file'], 'wb') as f:
+            target_dir = rel_paths['target_dir']
+            target_pkg = ''
+            # target_pkg = target_dir.replace('/', '.')
+            target_mod = '{}.{}'.format(
+                target_dir.replace('/', '.'),
+                get_filename(abs_paths['target_file'], suffix=False)
+            )
+            
+            pickle.dump({
+                'TARGET_DIR'   : target_dir,
+                'TARGET_PKG'   : target_pkg,
+                'TARGET_MOD'   : target_mod,
+                'TARGET_FUNC'  : target['function'],
+                'TARGET_ARGS'  : target['args'],
+                'TARGET_KWARGS': target['kwargs'],
+            }, f)
+    
+    def _generate_pylauncher():
+        if module_paths_scheme == 'translate':
+            _ext_paths = list(map(
+                lambda d: relpath(
+                    d if not d.startswith('src:') else src_2_dst(d[4:]),
+                    #   FIXME: this is a temp measure. see source point at
+                    #       `pyportable_installer/main_flow/step1/indexing_paths.py
+                    #        > vars:module_paths`
+                    start=abs_paths['launch_dir']
+                ),
+                module_paths
+            ))
+        else:  # leave module paths as is. (usually this is used in debug mode)
+            _ext_paths = list(map(
+                lambda d: d[4:] if d.startswith('src:') else d,
+                module_paths
+            ))
+        
+        with ropen(prj_model.pylauncher) as f:
+            template = f.read()
+            code = template.format(
+                # see `~/template/pylauncher.txt > docs:placeholders`
+                PROJ_LIB_DIR=rel_paths['lib_dir'],
+                ADD_PYWIN32_SUPPORT=str(
+                    options.get('add_pywin32_support', False)),
+                MODULE_PATHS=str(_ext_paths),
+                # DEFAULT_CONF='default.pkl',
+            )
+        
+        with wopen(abs_paths['launch_file']) as f:
+            f.write(code)
+    
+    def _generate_bat():
+        with ropen(prj_model.launch_bat) as f:
+            template = f.read()
+            
+            if _is_depsland_mode:
+                code = template.format(
+                    PYTHON='{PYTHON}',  # remain placeholder for the client
+                    #   side, this value will be filled when user runs depsland
+                    #   installer.
+                    #   see `~/template/setup_for_depsland.txt > section
+                    #   :generate launcher`
+                    PYLAUNCHER=rel_paths['launch_file'],
+                    PYCONF='%*' if is_main_entry else rel_paths['conf_file']
+                )
+                #   '%*' supports passing multiple arguments to python. for
+                #   example:
+                #       example.bat:
+                #           python test.py %*
+                #       test.py
+                #           import sys
+                #           print(sys.argv)
+                #       cmd:
+                #           example.bat hello world 1 2 3
+                #       output in console:
+                #           ['test.py', 'hello', 'world', '1', '2', '3']
+            elif enable_venv:
+                code = template.format(
+                    PYTHON=rel_paths['venv_python'].replace('/', '\\'),
+                    PYLAUNCHER=rel_paths['launch_file'],
+                    PYCONF='%*' if is_main_entry else rel_paths['conf_file']
+                )
+            else:
+                code = template.format(
+                    PYTHON='python',  # use system python which is defined in
+                    #   global environment variable (PATH).
+                    PYLAUNCHER=rel_paths['launch_file'],
+                    PYCONF='%*' if is_main_entry else rel_paths['conf_file']
+                )
+        
+        with wopen(abs_paths['bat_file']) as f:
+            f.write(code)
+    
+    def _generate_exe():
+        if options.get('generate_exe', True) is False:
+            return
+        
+        def _run(bat_file, exe_file, icon_file, *options):
+            lk.loga('converting bat to exe ... '
+                    '(it may take several seconds ~ one minute)')
+            bat_2_exe(bat_file, exe_file, icon_file, *options)
+            lk.loga('convertion bat-to-exe done')
+        
+        # this is a time-consuming operation (persists 1-10 seconds), we put it
+        # in a sub thread_of_bat_2_exe.
+        global thread_of_bat_2_exe
+        thread_of_bat_2_exe = run_new_thread(
+            _run, abs_paths['bat_file'], abs_paths['exe_file'], icon, '/x64',
+            '' if options.get('enable_console', True) else '/invisible'
+        )
+        #   the thread_of_bat_2_exe will be recycled in `..step3_4.cleanup`.
+    
+    _generate_target_conf()
+    _generate_pylauncher()
+    _generate_bat()
+    _generate_exe()
+    
+    return abs_paths['exe_file'] or abs_paths['bat_file']
+
+
+def _create_depsland_setup(launcher_name, icon,
+                           venv_name, venv_id, requirements,
+                           enable_console=True, **kwargs):
+    with ropen(prj_model.setup_for_depsland) as f:
+        template = f.read()
+        
+        code = template.format(
+            LAUNCHER=launcher_name,
+            VENV_NAME=venv_name,
+            VENV_ID=venv_id,
+            PYVERSION=kwargs.get('pyversion', 'python39'),
+            REQUIREMENTS=requirements,
+            OFFLINE=kwargs.get('offline', False),
+            # # LOCAL_DIR=kwargs.get('local', ''),
+            LOCAL_DIR=relpath(kwargs.get('local', None), dst_model.build),
+            INVISIBLE='' if enable_console else '/invisible'
+        )
+    
+    with wopen(dst_model.setup_py) as f:
+        f.write(code)
+    
+    copyfile(
+        prj_model.setup_for_depsland_bat,
+        dst_model.setup_bat,
+    )
+    
+    if icon:
+        copyfile(icon, f'{dst_model.build}/launcher.ico')
+
+
+class LauncherNamesDeduplicator:
+    """ Make sure there's no duplicate name generated. """
+    
+    def __init__(self, *top_names):
+        self._names_counter = {}  # type: dict[str, int]
+        self._names_counter.update({n: 0 for n in top_names})
+    
+    def optimize_name(self, name, function=''):
+        if name in self._names_counter:
+            if function:
+                name += ' ({})'.format(function)
+                return self.optimize_name(name, '')
+            self._names_counter[name] += 1
+            name += ' ({})'.format(self._names_counter[name])
+        else:
+            self._names_counter[name] = 0
+        return name
