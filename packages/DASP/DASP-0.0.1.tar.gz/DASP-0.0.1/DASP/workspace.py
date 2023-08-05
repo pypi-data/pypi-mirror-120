36db700cd7f307c826657f1ae72c0bde1da3cd86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:56 +0000

    Add package DASP/0.0.1/DASP-0.0.1.tar.gz

---
+++ packages/DASP/DASP-0.0.1.tar.gz/DASP-0.0.1/DASP/workspace.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2021/9/16 15:13
+# @Author  : wangzhimin！
+# @FileName: workspace.py
+# @Blog    ：https://blog.csdn.net/qq_42197919?spm=1001.2101.3001.5343
+
+def aaa():
+    return 1;
