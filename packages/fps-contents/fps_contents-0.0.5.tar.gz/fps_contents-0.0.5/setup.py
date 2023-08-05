6c6a8e7502a59a028babe456d094a3243d2185c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:08 +0000

    Add package fps-contents/0.0.5/fps_contents-0.0.5.tar.gz

---
+++ packages/fps-contents/fps_contents-0.0.5.tar.gz/fps_contents-0.0.5/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages  # type: ignore
+
+setup(
+    name="fps_contents",
+    version="0.0.5",
+    packages=find_packages(),
+    install_requires=["fps", "aiofiles", "fps-auth"],
+    entry_points={"fps_router": ["fps-contents = fps_contents.routes"]},
+)
