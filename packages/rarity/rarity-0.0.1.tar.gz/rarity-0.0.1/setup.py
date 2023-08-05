eac2eee78f4bf221de4d1feb491cf3f3ca98147f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:12:22 +0000

    Add package rarity/0.0.1/rarity-0.0.1.tar.gz

---
+++ packages/rarity/rarity-0.0.1.tar.gz/rarity-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='rarity',
+    version='0.0.1',
+    description='',
+    long_description='',
+    url='',
+    author='siewlin',
+    author_email='siewlin@aisingapore.org',
+    packages=[],
+    classifiers=['Development Status :: 1 - Planning'],
+)
