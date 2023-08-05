7dc74564bc3a13ba1498d69667b69c716563488b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:15 +0000

    Add package tars/0.4.1/tars-0.4.1-py3-none-any.whl

---
+++ packages/tars/tars-0.4.1-py3-none-any.whl/tars/strategies/trend_following_macd_strategy.py
@@ -0,0 +1,64 @@
+import pandas as pd
+from ta.trend import MACD
+
+from .abstract_strategy import AbstractStrategy
+from ..evaluators import TraderEvaluator
+from ..markets import CryptoMarket
+
+
+class TrendFollowingMACD(AbstractStrategy):
+    """
+    Follow the moving average convergence divergence (MACD) index is the goal
+    of this strategy. When the signal line is below the MACD line it sells, when
+    it is the opposite, it buys.
+
+    :param trader: Trader
+        The Trader handling a portfolio
+    :param pair: str
+        The pair e.g. XETHZUSD to buy and hold
+    :param volume: float
+        The volume of the pair's quote buy
+    :param validate: boolean
+        Safety Boolean to make sure not to trade real money by default
+
+    :ivar evaluator: AbstractEvaluator
+        Evaluator allows for the evaluation of a strategy
+    :ivar market: AbstractMarket
+        Market object to get information from
+    """
+
+    def __init__(self, trader, pair, volume, validate=True):
+        self.name = 'Trend Following with MACD'
+        self.trader = trader
+        self.pair = pair
+        self.volume = volume
+        self.validate = validate
+        self.evaluator = TraderEvaluator(self.trader)
+        self.market = CryptoMarket()
+
+    def run(self):
+        """ Run the strategy """
+        # Checkpoint
+        balance = self.trader.portfolio.get_trade_balance().loc['eb'].ZUSD   
+        self.evaluator.add_checkpoint(pd.Timestamp.utcnow(), balance)
+
+        # Run strategy
+
+        ## process data
+        market = CryptoMarket()
+        df0 = market.get_ohlc_data(pair=self.pair)[0]['close'].iloc[::-1]
+
+        macd = MACD(df0)
+        line = macd.macd()[-1]
+        signal = macd.macd_signal()[-1]
+
+        if signal < line:
+            self.trader.add_order(pair=self.pair, type='buy',
+                                  ordertype='market', volume=self.volume,
+                                  validate=self.validate)
+        elif signal >= line:
+            self.trader.add_order(pair=self.pair, type='sell',
+                                  ordertype='market', volume=self.volume,
+                                  validate=self.validate)
+        else:
+            pass
