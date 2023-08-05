9bbf9fb631e4a333a1b1de99262621cba25800b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:16:33 +0000

    Add package kiwi/9.23.57/kiwi-9.23.57.tar.gz

---
+++ packages/kiwi/kiwi-9.23.57.tar.gz/kiwi-9.23.57/kiwi/version.py
@@ -0,0 +1,22 @@
+# Copyright (c) 2015 SUSE Linux GmbH.  All rights reserved.
+#
+# This file is part of kiwi.
+#
+# kiwi is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# kiwi is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with kiwi.  If not, see <http://www.gnu.org/licenses/>
+#
+"""
+Global version information used in kiwi and the package
+"""
+__version__ = '9.23.57'
+__githash__ = 'd1f8789af43b153548bc4512b3693c1537d721a1'
