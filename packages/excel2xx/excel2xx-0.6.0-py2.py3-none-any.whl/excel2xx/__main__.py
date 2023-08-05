df0d51d6bf1b626599cbda591cac2447e998ea98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:12:53 +0000

    Add package excel2xx/0.6.0/excel2xx-0.6.0-py2.py3-none-any.whl

---
+++ packages/excel2xx/excel2xx-0.6.0-py2.py3-none-any.whl/excel2xx/__main__.py
@@ -0,0 +1,9 @@
+# encoding: utf-8
+from __future__ import unicode_literals, print_function
+import excel2xx.main
+import excel2xx.fields
+
+if __name__ == '__main__':
+    excel2xx.setUnits("K,M,G,T,P", size=1024)
+    exit(excel2xx.main.main_docopt())
+    pass
