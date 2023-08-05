48dac4d782613c0f274bec87a9c1870068e6f2d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:54 +0000

    Add package classixclustering/0.0.5/ClassixClustering-0.0.5.tar.gz

---
+++ packages/classixclustering/ClassixClustering-0.0.5.tar.gz/ClassixClustering-0.0.5/classix/__init__.py
@@ -0,0 +1,11 @@
+from . import aggregation_cm
+from . import amalgamation_cm
+
+try:
+    from .aggregation_cm import aggregate
+    from .amalgamation_cm import amalgamate
+except ModuleNotFoundError:
+    from .aggregation import aggregate
+    from .amalgamation import amalgamate
+    print("Cython fail.")
+from .classix import Clustering as classix
