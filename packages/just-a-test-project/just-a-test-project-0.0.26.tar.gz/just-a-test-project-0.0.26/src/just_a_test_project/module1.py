c429efd697be68531942758aa507dd6dad66a6e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:09:25 +0000

    Add package just-a-test-project/0.0.26/just-a-test-project-0.0.26.tar.gz

---
+++ packages/just-a-test-project/just-a-test-project-0.0.26.tar.gz/just-a-test-project-0.0.26/src/just_a_test_project/module1.py
@@ -0,0 +1,16 @@
+def public_func1() -> None:
+    ...
+
+
+def public_func2() -> None:
+    """This is public function"""
+    ...
+
+
+def _private_func1() -> None:
+    ...
+
+
+def _private_func2() -> None:
+    """This is private function"""
+    ...
