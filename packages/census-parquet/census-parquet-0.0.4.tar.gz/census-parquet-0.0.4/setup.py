b555acdc163179c07d8d01923e7892a64d9b6945
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:05:56 +0000

    Add package census-parquet/0.0.4/census-parquet-0.0.4.tar.gz

---
+++ packages/census-parquet/census-parquet-0.0.4.tar.gz/census-parquet-0.0.4/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name='census-parquet',
+    version='0.0.4',
+    packages=['census_parquet'],
+    license='MIT',
+    description='Tools for generating Parquet files from US Census 2020',
+    author='makepath',
+    url='https://github.com/makepath/census-parquet',
+    entry_points={
+        'console_scripts': ['run_census_parquet=census_parquet.cli:start']
+    },
+    install_requires=[
+        'click',
+        'dask_geopandas',
+        'openpyxl',
+        'pyarrow',
+    ],
+)
