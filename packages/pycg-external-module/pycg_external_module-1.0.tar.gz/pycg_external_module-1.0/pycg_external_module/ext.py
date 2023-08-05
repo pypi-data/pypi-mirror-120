8dd2e5782e7cacd5fb30724519c8f2510a717f91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:25 +0000

    Add package pycg-external-module/1.0/pycg_external_module-1.0.tar.gz

---
+++ packages/pycg-external-module/pycg_external_module-1.0.tar.gz/pycg_external_module-1.0/pycg_external_module/ext.py
@@ -0,0 +1,6 @@
+class Cls:
+    def __init__(self):
+        pass
+
+    def fun(self):
+        pass
---
+++ packages/pycg-external-module/pycg_external_module-1.0.tar.gz/pycg_external_module-1.0/setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+setuptools.setup(
+      name='pycg_external_module',
+      version='1.0',
+      description='External module for PyCG micro-benchmark',
+      author='Ashwin Prasad',
+      author_email='ashwin.prasad@upb.de',
+      license='GPL-3.0',
+      packages=setuptools.find_packages(),
+      zip_safe=False
+)
