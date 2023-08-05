d37f8e84303882368066574d805dbbc593ed9840
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:13 +0000

    Add package fund-distributions/0.2/fund_distributions-0.2.tar.gz

---
+++ packages/fund-distributions/fund_distributions-0.2.tar.gz/fund_distributions-0.2/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='fund_distributions',
+      version='0.2',
+      description='Gaussian  and Binomial distributions',
+      packages=['fund_distributions'],
+      author='Eduardo Chiarotti',
+      zip_safe=False)
