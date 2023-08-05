9db157d0e2a71e5af8c36fe23045252e09131495
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:54 +0000

    Add package d2b/1.2.1/d2b-1.2.1.tar.gz

---
+++ packages/d2b/d2b-1.2.1.tar.gz/d2b-1.2.1/src/d2b/__init__.py
@@ -0,0 +1,30 @@
+from d2b import cli
+from d2b import commands
+from d2b import d2b
+from d2b import defaults
+from d2b import hookspecs
+from d2b import plugins
+from d2b.d2b import __version__
+from d2b.d2b import Acquisition
+from d2b.d2b import D2B
+from d2b.d2b import Description
+from d2b.d2b import FilenameEntities
+from d2b.d2b import Matcher
+from d2b.d2b import Participant
+
+
+__all__ = (
+    "cli",
+    "commands",
+    "d2b",
+    "defaults",
+    "hookspecs",
+    "plugins",
+    "__version__",
+    "Acquisition",
+    "D2B",
+    "Description",
+    "FilenameEntities",
+    "Matcher",
+    "Participant",
+)
