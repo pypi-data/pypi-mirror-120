d176db277a4a825a23ef98e42dbeeeae01454d59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:13:03 +0000

    Add package census-parquet/0.0.3/census-parquet-0.0.3.tar.gz

---
+++ packages/census-parquet/census-parquet-0.0.3.tar.gz/census-parquet-0.0.3/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='census-parquet',
+    version='0.0.3',
+    packages=['census_parquet'],
+    license='MIT',
+    description='Tools for generating Parquet files from Census 2020',
+    author='makepath',
+    url='https://github.com/makepath/census-parquet',
+    install_requires=[
+        'dask_geopandas',
+        'openpyxl',
+        'pyarrow',
+    ],
+)
