c49db1a9521bf60cd32fd5952c89749c48914287
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:12:17 +0000

    Add package rivery-cli/0.3.5/rivery_cli-0.3.5-py3-none-any.whl

---
+++ packages/rivery-cli/rivery_cli-0.3.5-py3-none-any.whl/rivery_cli/globals/global_settings.py
@@ -0,0 +1,8 @@
+__version__ = "0.3.5"
+AVAILABE_RIVER_TYPES = ['logic']
+
+SCHEMA_VALIDATION_PATH = 'converters/schemas'
+
+DEFAULT_MODELS = 'models'
+DEFAULT_SQLS = 'sqls'
+DEFAULT_MAPPING = 'maps'
