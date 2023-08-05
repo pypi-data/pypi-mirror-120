e3249af3afc218a83befa5e958aaa0419e19498f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:54 +0000

    Add package dsnd-probability-acm/0.1/dsnd_probability_acm-0.1.tar.gz

---
+++ packages/dsnd-probability-acm/dsnd_probability_acm-0.1.tar.gz/dsnd_probability_acm-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="dsnd_probability_acm",
+    version="0.1",
+    description="Gaussian distributions",
+    packages=["dsnd_probability_acm"],
+    zip_safe=False,
+    install_requires=["matplotlib"],
+)
