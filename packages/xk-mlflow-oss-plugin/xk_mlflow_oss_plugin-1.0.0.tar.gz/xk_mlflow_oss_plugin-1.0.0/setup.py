bcb783b6b52d080a58fe08e25cd531118c0a4aae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:15 +0000

    Add package xk-mlflow-oss-plugin/1.0.0/xk_mlflow_oss_plugin-1.0.0.tar.gz

---
+++ packages/xk-mlflow-oss-plugin/xk_mlflow_oss_plugin-1.0.0.tar.gz/xk_mlflow_oss_plugin-1.0.0/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='xk_mlflow_oss_plugin',
+    version='1.0.0',
+    description='Plugin that provides Aliyun oss Artifact Store functionality for MLflow in Xkool',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='Liangyi Murong',
+    author_email='mrly@xkool.org',
+    url="https://github.com/mrly16/mlflow-aliyunstore/archive/refs/tags/1.0.0.tar.gz",
+    packages=find_packages(),
+    install_requires=[
+        'mlflow',
+        'oss2'
+    ],
+    entry_points={
+        "mlflow.artifact_repository": [
+            "oss=aliyunstoreplugin.store.artifact.aliyun_oss_artifact_repo:AliyunOssArtifactRepository"
+        ]
+    },
+    license="Apache License 2.0",
+)
