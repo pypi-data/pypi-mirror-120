5c3542d47644eaf857c8af07e4ad03171e0ce613
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:14:07 +0000

    Add package nexoclom/3.0.3/nexoclom-3.0.3.tar.gz

---
+++ packages/nexoclom/nexoclom-3.0.3.tar.gz/nexoclom-3.0.3/nexoclom/solarsystem/__init__.py
@@ -0,0 +1,7 @@
+from .SSObject import SSObject
+from .planet_dist import planet_dist
+from .planet_geometry import planet_geometry
+
+__name__ = 'solarsystem'
+__author__ = 'Matthew Burger'
+__email__ = 'mburger@stsci.edu'
