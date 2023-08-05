87129eb0622db017fd872f41866ffabe080445d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:45 +0000

    Add package mabelbeta/0.5.0a33/mabelbeta-0.5.0a33.tar.gz

---
+++ packages/mabelbeta/mabelbeta-0.5.0a33.tar.gz/mabelbeta-0.5.0a33/mabel/data/__init__.py
@@ -0,0 +1,6 @@
+from .writers.batch_writer import BatchWriter
+from .writers.stream_writer import StreamWriter
+from .writers.writer import Writer
+from .readers.reader import Reader
+
+from .internals.dictset import STORAGE_CLASS, DictSet
