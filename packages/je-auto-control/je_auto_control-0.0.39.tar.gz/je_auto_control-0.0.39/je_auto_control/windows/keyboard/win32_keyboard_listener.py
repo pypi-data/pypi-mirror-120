4b32d5dbcb9b5f0122318cb82db646a7a9d4aa6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:14:18 +0000

    Add package je-auto-control/0.0.39/je_auto_control-0.0.39.tar.gz

---
+++ packages/je-auto-control/je_auto_control-0.0.39.tar.gz/je_auto_control-0.0.39/je_auto_control/windows/keyboard/win32_keyboard_listener.py
@@ -0,0 +1,14 @@
+import ctypes
+import sys
+
+if sys.platform not in ["win32", "cygwin", "msys"]:
+    raise Exception("should be only loaded on windows")
+
+
+def check_key_is_press(keycode):
+    if type(keycode) is int:
+        temp = ctypes.windll.user32.GetKeyState(keycode)
+    else:
+        temp = ctypes.windll.user32.GetKeyState(ord(keycode))
+    if temp > 1:
+        return True
