d11929f73aa6ac9f811875eb1d9e4b84ad5340ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:10:50 +0000

    Add package imap-tools/0.47.0/imap_tools-0.47.0-py3-none-any.whl

---
+++ packages/imap-tools/imap_tools-0.47.0-py3-none-any.whl/imap_tools/__init__.py
@@ -0,0 +1,12 @@
+# Lib author: Vladimir Kaukin <KaukinVK@ya.ru>
+# Project home page: https://github.com/ikvk/imap_tools
+# License: Apache-2.0
+
+from .query import AND, OR, NOT, Header, UidRange, A, O, N, H, U
+from .mailbox import BaseMailBox, MailBox, MailBoxUnencrypted
+from .message import MailMessage, MailAttachment
+from .folder import MailBoxFolderManager
+from .consts import MailMessageFlags, MailBoxFolderStatusOptions
+from .errors import *
+
+__version__ = '0.47.0'
