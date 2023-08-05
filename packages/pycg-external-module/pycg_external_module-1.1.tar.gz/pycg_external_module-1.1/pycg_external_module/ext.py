d63ac2cb134b90ffa8d0f7152770808644df0a52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:05:54 +0000

    Add package pycg-external-module/1.1/pycg_external_module-1.1.tar.gz

---
+++ packages/pycg-external-module/pycg_external_module-1.1.tar.gz/pycg_external_module-1.1/pycg_external_module/ext.py
@@ -0,0 +1,13 @@
+class Cls:
+    def __init__(self):
+        pass
+
+    def fun(self):
+        pass
+
+class Parent:
+    def __init__(self):
+        pass
+
+    def parent_fun(self):
+        pass
