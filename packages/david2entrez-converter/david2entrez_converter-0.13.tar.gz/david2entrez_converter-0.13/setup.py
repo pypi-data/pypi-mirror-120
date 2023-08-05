b5d6da959d8b9c9067c6cc31e736cb4787ddf754
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:11:42 +0000

    Add package david2entrez-converter/0.13/david2entrez_converter-0.13.tar.gz

---
+++ packages/david2entrez-converter/david2entrez_converter-0.13.tar.gz/david2entrez_converter-0.13/setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+setup(
+  name = 'david2entrez_converter',
+  packages = ['david2entrez_converter'],
+  version = '0.13',
+  license='MIT',
+  description = 'Convert the HGNC gene names to ENTREZ id available in the DAVID bioinformatics database.',
+  author = 'Benjamin Pham',
+  author_email = 'bkpham@ucsd.edu',
+  url = 'https://github.com/user/b-k-pham',
+  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',
+  keywords = ['DAVID','Gene','Conversion'],
+  install_requires=[
+          'pandas',
+          'numpy'
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License', 
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
