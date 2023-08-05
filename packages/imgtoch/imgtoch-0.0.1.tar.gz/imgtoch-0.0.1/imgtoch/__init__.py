2aa3766ef85a7e9717c477b245b43896b741b726
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:22 +0000

    Add package imgtoch/0.0.1/imgtoch-0.0.1.tar.gz

---
+++ packages/imgtoch/imgtoch-0.0.1.tar.gz/imgtoch-0.0.1/imgtoch/__init__.py
@@ -0,0 +1,12 @@
+# coding: utf-8
+
+from .__utils__ import makeImage
+
+NAME = "imgtoch"
+VERSIONNUM = 0,0,1
+VERSION = ".".join(map(str, VERSIONNUM))
+AUTHOR = "hrpzcf"
+EMAIL = "hrpzcf@foxmail.com"
+WEBSITE = "https://gitee.com/hrpzcf/imgtoch"
+
+__all__ = ["makeImage"]
