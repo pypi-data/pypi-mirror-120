5789b5a30ac072fb4a4f282858c1b6343c30c001
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:15 +0000

    Add package sdgs-tools/0.7.8/sdgs-tools-0.7.8.tar.gz

---
+++ packages/sdgs-tools/sdgs-tools-0.7.8.tar.gz/sdgs-tools-0.7.8/sdgs_tools/dashboard/import_individu/enums/keterbukaan_desa.py
@@ -0,0 +1,16 @@
+from enum import Enum
+from sdgs_tools.dashboard.utils import make_str_to_enum
+
+MAPPING = {
+    "Baik": "1",
+    "Cukup": "2",
+    "Buruk": "3",
+}
+
+
+class KeterbukaanDesa(Enum):
+    BAIK = "1"
+    CUKUP = "2"
+    BURUK = "3"
+
+    from_str = classmethod(make_str_to_enum(MAPPING))
---
+++ packages/sdgs-tools/sdgs-tools-0.7.8.tar.gz/sdgs-tools-0.7.8/sdgs_tools/dashboard/import_individu/penyakit_diderita.py
@@ -0,0 +1,87 @@
+import attr
+from typing import Dict, Optional
+
+from .enums import YaTidak
+
+
+MAPPING = {
+    "Mutaber Diare": "mutaber_diare",
+    "Demam Berdarah": "demam_berdarah",
+    "Campak": "campak",
+    "Malaria": "malaria",
+    "Flu Burung Sars": "flu_burung_sars",
+    "Covid19": "covid19",
+    "Hepatitis B": "hepatitis_b",
+    "Hepatitis E": "hepatitis_e",
+    "Difteri": "difteri",
+    "Chikungunya": "chikungunya",
+    "Leptospirosis": "leptospirosis",
+    "Kolera": "kolera",
+    "Gizi Buruk": "gizi_buruk",
+    "Jantung": "jantung",
+    "TBC Paru-Paru": "tbc_paru_paru",
+    "Kanker": "kanker",
+    "Diabetes": "diabetes",
+    "Lumpuh": "lumpuh",
+    "Lainnya": "lainnya",
+}
+
+
+@attr.dataclass
+class PenyakitDiderita:
+    # P404
+    mutaber_diare: YaTidak = YaTidak.TIDAK
+    demam_berdarah: YaTidak = YaTidak.TIDAK
+    campak: YaTidak = YaTidak.TIDAK
+    malaria: YaTidak = YaTidak.TIDAK
+    flu_burung_sars: YaTidak = YaTidak.TIDAK
+    covid19: YaTidak = YaTidak.TIDAK
+    hepatitis_b: YaTidak = YaTidak.TIDAK
+    hepatitis_e: YaTidak = YaTidak.TIDAK
+    difteri: YaTidak = YaTidak.TIDAK
+    chikungunya: YaTidak = YaTidak.TIDAK
+    leptospirosis: YaTidak = YaTidak.TIDAK
+    kolera: YaTidak = YaTidak.TIDAK
+    gizi_buruk: YaTidak = YaTidak.TIDAK
+    jantung: YaTidak = YaTidak.TIDAK
+    tbc_paru_paru: YaTidak = YaTidak.TIDAK
+    kanker: YaTidak = YaTidak.TIDAK
+    diabetes: YaTidak = YaTidak.TIDAK
+    lumpuh: YaTidak = YaTidak.TIDAK
+    lainnya: YaTidak = YaTidak.TIDAK
+
+    @staticmethod
+    def todict(data: Dict[str, YaTidak]) -> Dict[str, str]:
+        return {
+            "1": "1" if data["mutaber_diare"] else "2",
+            "2": "1" if data["demam_berdarah"] else "2",
+            "3": "1" if data["campak"] else "2",
+            "4": "1" if data["malaria"] else "2",
+            "5": "1" if data["flu_burung_sars"] else "2",
+            "6": "1" if data["covid19"] else "2",
+            "7": "1" if data["hepatitis_b"] else "2",
+            "8": "1" if data["hepatitis_e"] else "2",
+            "9": "1" if data["difteri"] else "2",
+            "10": "1" if data["chikungunya"] else "2",
+            "11": "1" if data["leptospirosis"] else "2",
+            "12": "1" if data["kolera"] else "2",
+            "13": "1" if data["gizi_buruk"] else "2",
+            "14": "1" if data["jantung"] else "2",
+            "15": "1" if data["tbc_paru_paru"] else "2",
+            "16": "1" if data["kanker"] else "2",
+            "17": "1" if data["diabetes"] else "2",
+            "18": "1" if data["lumpuh"] else "2",
+            "19": "1" if data["lainnya"] else "2",
+        }
+
+    @classmethod
+    def from_str(cls, value: Optional[str], t=None):
+        if not value:
+            return attr.asdict(cls())
+        data: Dict[str, YaTidak] = dict()
+        for key, name in MAPPING.items():
+            if value in key:
+                data[name] = YaTidak.YA
+            else:
+                data[name] = YaTidak.TIDAK
+        return data
