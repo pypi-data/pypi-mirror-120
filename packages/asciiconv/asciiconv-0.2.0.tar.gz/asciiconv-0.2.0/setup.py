7eec433a787400214d8c9c882cd48a6cce718985
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:23 +0000

    Add package asciiconv/0.2.0/asciiconv-0.2.0.tar.gz

---
+++ packages/asciiconv/asciiconv-0.2.0.tar.gz/asciiconv-0.2.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+ 
+classifiers = [
+  'Development Status :: 3 - Alpha',
+  'Intended Audience :: Education',
+  'Operating System :: Microsoft :: Windows :: Windows 10',
+  'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+  'Programming Language :: Python :: 3'
+]
+
+setup(
+    name='asciiconv',
+    version='0.2.0',
+    description='Convert text to be ASCII compatiable and back',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='',  
+    author='Synergetic00',
+    license='GPLv3', 
+    classifiers=classifiers,
+    keywords='ascii', 
+    packages=find_packages(),
+    install_requires=[''] 
+)
