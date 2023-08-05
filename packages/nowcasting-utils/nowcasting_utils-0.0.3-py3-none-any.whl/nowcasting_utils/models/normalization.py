91392528b48eb0c91d8ce967f7c23262de159f1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:31 +0000

    Add package nowcasting-utils/0.0.3/nowcasting_utils-0.0.3-py3-none-any.whl

---
+++ packages/nowcasting-utils/nowcasting_utils-0.0.3-py3-none-any.whl/nowcasting_utils/models/normalization.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+
+def metnet_normalization(data: np.ndarray) -> np.ndarray:
+    """
+    Perform the normalization used in the MetNet paper on the GOES data
+    This involves subtracting by the median, dividing by the interquartile range,
+    then squashing to [-1,1] with the hyperbolic tangent
+    Args:
+        data: input image data
+
+    Returns:
+        Normalized image data
+    """
+    # Ensure no NaNs
+    data = np.nan_to_num(data)
+    # Get IQR
+    iqr = np.subtract(*np.percentile(data, [75, 25]))
+    data = (data - np.median(data)) / iqr
+    #
+    # Now hyperbolic tangent
+    # data = np.tanh(data)
+    # TODO tanh seems to give an issue of not forcing between -1 and 1, but gives between ~-0.76 and 0.76
+    return np.clip(data, -1.0, 1.0)
+
+
+def standard_normalization(data: np.ndarray, std: np.ndarray, mean: np.ndarray) -> np.ndarray:
+    return (data - mean) / std
