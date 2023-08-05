13c6735e920372a53390b74ae85553ddcc2d1d7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:03 +0000

    Add package cscli/0.2.2/cscli-0.2.2.tar.gz

---
+++ packages/cscli/cscli-0.2.2.tar.gz/cscli-0.2.2/cscli/__init__.py
@@ -0,0 +1,17 @@
+"""Top-level package for cscli."""
+
+__author__ = """Matt Krueger"""
+__email__ = "mkrueger@rstms.net"
+__version__ = "0.2.2"
+__description__ = "CloudSigma API command line interface"
+
+from .api_client import CloudSigmaClient
+
+MIN_CPU = 1
+MIN_MHZ = 1000
+MIN_RAM = "256M"
+MIN_DISK = "512M"
+
+PASSWORD_LEN = 24
+
+__all__ = [CloudSigmaClient]
