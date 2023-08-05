b6b083950e72341f6bcbc6bdd4ca92f139897997
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:09:41 +0000

    Add package firstpackie/1.0.0/firstpackie-1.0.0.tar.gz

---
+++ packages/firstpackie/firstpackie-1.0.0.tar.gz/firstpackie-1.0.0/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name="firstpackie",
+    version="1.0.0",
+    description="Say hello!",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/taslim-a-hussain/firstpackie",
+    author="Taslim A Hussain",
+    author_email="taslim@outlook.co.nz",
+    py_modules=["firstpackie"],
+    package_dir={"": "src"},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent"
+        ]
+)
