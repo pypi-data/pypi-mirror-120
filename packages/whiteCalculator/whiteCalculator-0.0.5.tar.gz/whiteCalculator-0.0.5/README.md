0537d883bb6b4d660e796fd8552d4d4c8aa9d9f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:13:14 +0000

    Add package whiteCalculator/0.0.5/whiteCalculator-0.0.5.tar.gz

---
+++ packages/whiteCalculator/whiteCalculator-0.0.5.tar.gz/whiteCalculator-0.0.5/README.md
@@ -0,0 +1,71 @@
+> # Calculator Module for Python
+> By White Night
+> 
+> [![PYPI](https://img.shields.io/pypi/v/whiteCalculator?style=for-the-badge)](https://pypi.org/project/whiteCalculator/)
+> [![PYPI](https://img.shields.io/pypi/pyversions/whiteCalculator?style=for-the-badge)](https://pypi.org/project/whiteCalculator/)
+> [![PYPI](https://img.shields.io/pypi/wheel/whiteCalculator?style=for-the-badge)](https://pypi.org/project/whiteCalculator/)
+> [![GITHUB](https://img.shields.io/github/contributors/WhiteNightAWA/whiteCalculator?style=for-the-badge)](https://github.com/WhiteNightAWA/whiteCalculator/)
+> [![License](https://img.shields.io/github/license/WhiteNightAWA/whiteCalculator?style=for-the-badge)](https://github.com/WhiteNightAWA/whiteCalculator/)
+> [![GITHUB](https://img.shields.io/github/languages/code-size/WhiteNightAWA/whiteCalculator?style=for-the-badge)](https://github.com/WhiteNightAWA/whiteCalculator/)
+> [![GITHUB](https://img.shields.io/github/issues/WhiteNightAWA/whiteCalculator?style=for-the-badge)](https://github.com/WhiteNightAWA/whiteCalculator/)
+> [![GITHUB](https://img.shields.io/github/issues-pr/WhiteNightAWA/whiteCalculator?style=for-the-badge)](https://github.com/WhiteNightAWA/whiteCalculator/)
+
+## Install
+```shell
+# Linux/macOS
+python3 -m pip install -U whiteCalculator
+
+# Windows
+py -3 -m pip install -U whiteCalculator
+```
+
+
+# QuickStart
+### Calculator
+```python
+>>> from whiteCalculator import Calculator
+>>> c = Calculator()
+>>> print(c.run("1+8(5^2)"))
+201
+>>> print(c.run("9Ans"))
+1809
+```
+### Formula
+```python
+>>> from whiteCalculator import Formula
+>>> Formula.PythagorasTheorem.getA(b=4, c=5)
+3
+>>> Formula.PythagorasTheorem.getA(b="2^2", c="√(25)")
+3
+```
+#### Formula List
+`Pythagoras Theorem`, `Lens Formula`
+
+
+## Errors
+```python
+>>> from whiteCalculator import Calculator
+
+>>> c2 = Calculator() # Default is True
+>>> c2.run("9/0")
+Error: division by zero
+
+>>> c1 = Calculator(skipError=False)
+>>> c1.run("9/0")
+Traceback (most recent call last):
+    ...
+ZeroDivisionError: division by zero
+```
+
+## You can use:
+- `sin` / `asin` / `sinh`
+- `cos` / `acos` / `cosh`
+- `tan` / `atan` / `tanh`
+- `ln` / `log`
+- `×` / `•`
+- `^` / *`*` / `power`
+- `√` / `sqrt`
+- `π` / `pi`
+- `%`
+- `÷`
+- `Ans`
---
+++ packages/whiteCalculator/whiteCalculator-0.0.5.tar.gz/whiteCalculator-0.0.5/src/whiteCalculator/calculator.py
@@ -0,0 +1,129 @@
+import asyncio
+from .maths import *
+from .timeout import timeout
+
+
+functionList = [
+    "tan", "cos", "sin", "log", "ln", "sqrt", "sinh", "cosh", "tanh", "asin", "acos", "atan", "power", "abs"
+]
+symbolList = ["pi", "**", "Ans"] + functionList
+
+replaceList = {
+    "^": "**",
+    "π": "pi",
+    "√": "sqrt",
+    "%": "/100",
+    "×": "*",
+    "•": "*",
+    "÷": "/"
+}
+
+
+class Calculator:
+
+    def __init__(self, skipError=True):
+        self.ans = 0
+        self.equation = ""
+        self.equationList = []
+        self.ERROR = None
+        self.skipError = skipError
+
+    def checkBrackets(self):
+        leftBrackets = self.equation.count("(")
+        rightBrackets = self.equation.count(")")
+        if leftBrackets > rightBrackets:
+            addBrackets = leftBrackets - rightBrackets
+            self.equation += ")" * addBrackets
+
+    def getList(self):
+        last, tryAdd = "", ""
+        self.equationList.clear()
+        for symbol in self.equation:
+            if symbol.isnumeric() or symbol == ".":
+                last += symbol
+                tryAdd = ""
+            elif symbol.isalpha():
+                tryAdd += symbol
+                if last != "":
+                    self.equationList.append(last)
+                last = ""
+            else:
+                if last != "":
+                    self.equationList.append(last)
+
+                self.equationList.append(symbol)
+                last, tryAdd = "", ""
+            if tryAdd in symbolList:
+                self.equationList.append(tryAdd)
+                tryAdd = ""
+        if last != "":
+            self.equationList.append(last)
+
+    def fixList(self):
+        last = "+"
+        pointer = 0
+        added = 0
+        newEquationList = self.equationList.copy()
+        for symbol in self.equationList:
+            if symbol not in [")", "+", "-", "*", "/"]:
+                if last not in ["+", "-", "*", "/", "("] + functionList:
+                    newEquationList.insert(pointer + added, "*")
+                    added += 1
+            last = symbol
+            pointer += 1
+        self.equationList = newEquationList
+
+    def fix(self):
+        last, curr = '+', ''
+        result = ''
+        for i, char in enumerate(self.equation):
+            if char.isnumeric() or char in "(+-*/)":
+                if char.isnumeric():
+                    if last == ')' or (curr and curr not in functionList):
+                        result += '*'
+                elif char == '(':
+                    if (curr not in functionList) if curr else i and last not in "(+-*/":
+                        result += '*'
+                curr = ''
+            elif char.isalpha():
+                if last == ')' or last.isnumeric():
+                    result += '*'
+                curr += char
+            else:
+                raise ValueError()
+            last = char
+            result += char
+        self.equation = result
+
+    def getAns(self):
+        self.ERROR = None
+        Ans = self.ans
+        try:
+            self.ans = eval(self.equation)
+        except Exception as error:
+            if self.skipError:
+                self.ERROR = error
+            else:
+                raise error
+
+    def replaces(self):
+        for x in replaceList:
+            self.equation = self.equation.replace(x, replaceList[x])
+
+    def run(self, equation: str):
+        self.equation = equation.replace(" ", "")
+        self.checkBrackets()
+        self.replaces()
+        self.getList()
+        self.fixList()
+        self.equation = "".join(self.equationList)
+        oldAns, count = self.ans, 0
+        with timeout(seconds=10):
+            self.getAns()
+        if isinstance(self.ans, float):
+            if self.ans.is_integer():
+                self.ans = int(self.ans)
+        if self.ERROR is None:
+            return self.ans
+        else:
+            return "Error: " + str(self.ERROR)
