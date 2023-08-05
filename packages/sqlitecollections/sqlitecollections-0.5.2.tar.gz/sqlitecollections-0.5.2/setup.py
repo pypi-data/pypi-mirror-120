02caa2dabf6337a15cea1e77144e74e03b6d2913
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:11 +0000

    Add package sqlitecollections/0.5.2/sqlitecollections-0.5.2.tar.gz

---
+++ packages/sqlitecollections/sqlitecollections-0.5.2.tar.gz/sqlitecollections-0.5.2/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+from sqlitecollections import (
+    __author__,
+    __description__,
+    __email__,
+    __long_description__,
+    __package_name__,
+    __version__,
+)
+
+setup(
+    name=__package_name__,
+    version=__version__,
+    author=__author__,
+    author_email=__email__,
+    license="MIT",
+    url="https://github.com/osoken/sqlitecollections",
+    description=__description__,
+    long_description=__long_description__,
+    packages=[__package_name__],
+    install_requires=[],
+    extras_require={
+        "dev": ["flake8", "pytest", "black", "mypy", "tox", "isort"],
+        "docs": ["mkdocs", "mkdocs-material", "mdx_include", "mkdocs-macros-plugin"],
+    },
+)
