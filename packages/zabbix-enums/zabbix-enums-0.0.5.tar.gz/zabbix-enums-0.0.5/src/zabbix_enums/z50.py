fe784abeecfd475a278377bf97f684beb7059e93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:14 +0000

    Add package zabbix-enums/0.0.5/zabbix-enums-0.0.5.tar.gz

---
+++ packages/zabbix-enums/zabbix-enums-0.0.5.tar.gz/zabbix-enums-0.0.5/src/zabbix_enums/z50.py
@@ -0,0 +1,34 @@
+from ._common_event import *
+from ._common_host import *
+from ._common_host_group import *
+from ._common_host_interface import *
+from ._common_item import *
+from ._common_problem import *
+from ._common_snmp import *
+from ._common_trigger import *
+
+
+class ItemType(IntEnum):
+    ZABBIX_AGENT = 0
+    ZABBIX_TRAPPER = 2
+    SIMPLE_CHECK = 3
+    ZABBIX_INTERNAL = 5
+    ZABBIX_AGENT_ACTIVE = 7
+    ZABBIX_AGGREGATE = 8
+    WEB_ITEM = 9
+    EXTERNAL_CHECK = 10
+    DATABASE_MONITOR = 11
+    IPMI_AGENT = 12
+    SSH_AGENT = 13
+    TELNET_AGENT = 14
+    CALCULATED = 15
+    JMX_AGENT = 16
+    SNMP_TRAP = 17
+    DEPENDENT_ITEM = 18
+    HTTP_AGENT = 19
+    SNMP_AGENT = 20
+
+
+class MacroType(IntEnum):
+    TEXT = 0
+    SECRET = 1
