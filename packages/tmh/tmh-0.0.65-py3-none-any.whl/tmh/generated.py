f864d2425b4222549a49a0991c6efb63d10f674c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:19 +0000

    Add package tmh/0.0.65/tmh-0.0.65-py3-none-any.whl

---
+++ packages/tmh/tmh-0.0.65-py3-none-any.whl/tmh/generated.py
@@ -0,0 +1,10 @@
+"""
+
+print "What is your name?"
+name = raw_input()
+if name == "Will":
+    print "Hello, master."
+elif name == "Bob":
+    print "Hello, Bob."
+else: 
+    print "Hello, %s." % name
