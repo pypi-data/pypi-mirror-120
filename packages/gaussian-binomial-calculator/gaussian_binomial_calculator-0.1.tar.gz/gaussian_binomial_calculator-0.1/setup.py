60a01eafdec644d49042ccdb9711b4114b9f5691
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:05:55 +0000

    Add package gaussian-binomial-calculator/0.1/gaussian_binomial_calculator-0.1.tar.gz

---
+++ packages/gaussian-binomial-calculator/gaussian_binomial_calculator-0.1.tar.gz/gaussian_binomial_calculator-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='gaussian_binomial_calculator',
+      version='0.1',
+      description='Gaussian and Binomial distributions calculator',
+      packages=['gaussian_binomial_calculator'],
+      author = 'Danilo Salazar',
+      author_email = 'danilo@origo.ec',
+      zip_safe=False)
