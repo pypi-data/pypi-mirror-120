a84303fb514dbad441309690526854a567ac9172
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:05:49 +0000

    Add package cicdtest/2.32/cicdtest-2.32.tar.gz

---
+++ packages/cicdtest/cicdtest-2.32.tar.gz/cicdtest-2.32/buildpan/installer.py
@@ -0,0 +1,19 @@
+from buildpan import yaml_reader, platform_installer
+
+
+def installer():
+    try:
+        print("called")
+        platform_name = yaml_reader.yaml_reader.platform_name
+        node_ver = yaml_reader.yaml_reader.platform_ver
+        print(platform_name)
+
+        if platform_name == "node":
+            platform_installer.node_installer(node_ver)
+        elif platform_name == "":
+            print("Please provide platform name")
+        else:
+            print("This name is not supported")
+    
+    except Exception as e:
+        print(e)
