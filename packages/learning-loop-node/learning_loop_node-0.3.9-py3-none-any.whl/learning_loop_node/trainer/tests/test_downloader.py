b4535b74ab69deb0b43eaddd85bb547698566d5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:12:16 +0000

    Add package learning-loop-node/0.3.9/learning_loop_node-0.3.9-py3-none-any.whl

---
+++ packages/learning-loop-node/learning_loop_node-0.3.9-py3-none-any.whl/learning_loop_node/trainer/tests/test_downloader.py
@@ -0,0 +1,73 @@
+from learning_loop_node.trainer.capability import Capability
+from learning_loop_node.trainer.downloader_factory import DownloaderFactory
+import pytest
+from learning_loop_node.tests import test_helper
+from learning_loop_node.trainer.tests import trainer_test_helper
+from learning_loop_node.trainer.downloader import DataDownloader
+from learning_loop_node import node, node_helper
+from learning_loop_node.context import Context
+from icecream import ic
+import asyncio
+
+
+@pytest.fixture(autouse=True, scope='module')
+def create_project_for_module():
+    # TODO can we use the 'create_project' fixture here?
+    test_helper.LiveServerSession().delete(f"/api/zauberzeug/projects/pytest?keep_images=true")
+    project_configuration = {'project_name': 'pytest', 'inbox': 0, 'annotate': 0, 'review': 0, 'complete': 3, 'image_style': 'beautiful',
+                             'categories': 2, 'thumbs': False, 'tags': 0, 'trainings': 1, 'detections': 3, 'annotations': 0, 'skeleton': False}
+    assert test_helper.LiveServerSession().post(f"/api/zauberzeug/projects/generator",
+                                                json=project_configuration).status_code == 200
+    yield
+    test_helper.LiveServerSession().delete(f"/api/zauberzeug/projects/pytest?keep_images=true")
+
+
+@pytest.fixture
+def downloader() -> DataDownloader:
+    context = Context(organization='zauberzeug', project='pytest')
+    return DownloaderFactory.create(context=context, capability=Capability.Box)
+
+
+@pytest.mark.asyncio
+async def test_download_model(data_folder):
+    _, _, trainings_folder = trainer_test_helper.create_needed_folders(data_folder)
+    model_id = await trainer_test_helper.assert_upload_model()
+
+    await node_helper.download_model(trainings_folder, Context(organization='zauberzeug', project='pytest'), model_id, 'mocked')
+
+    files = test_helper.get_files_in_folder(data_folder)
+    assert len(files) == 3, str(files)
+
+    assert files[0] == data_folder + "/zauberzeug/pytest/trainings/some_uuid/file_1.txt"
+    assert files[1] == data_folder + "/zauberzeug/pytest/trainings/some_uuid/file_2.txt"
+    assert files[2] == data_folder + "/zauberzeug/pytest/trainings/some_uuid/metadata.json"
+
+    assert open(files[0], 'r').read() == 'content of file one'
+    assert open(files[1], 'r').read() == 'content of file two'
+
+
+@pytest.mark.asyncio
+async def test_download_basic_data(downloader: DataDownloader):
+    basic_data = await downloader.download_basic_data()
+
+    assert len(basic_data.image_ids) == 3
+    assert len(basic_data.box_categories) == 2
+
+
+@pytest.mark.asyncio
+async def test_download_images(downloader: DataDownloader, data_folder:str):
+    _, image_folder, _ = trainer_test_helper.create_needed_folders(data_folder)
+
+    basic_data = await downloader.download_basic_data()
+    await downloader.download_images(basic_data.image_ids, image_folder)
+    files = test_helper.get_files_in_folder(data_folder)
+
+    assert len(files) == 3
+
+
+@pytest.mark.asyncio
+async def test_download_training_data(downloader: DataDownloader):
+    basic_data = await downloader.download_basic_data()
+    image_data = await downloader.download_image_data(basic_data.image_ids)
+
+    assert len(image_data) == 3
