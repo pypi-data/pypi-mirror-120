76654894496c64269551e4533facff9335bedcd7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:20 +0000

    Add package rustplus/2.3.1/rustplus-2.3.1-py3-none-any.whl

---
+++ packages/rustplus/rustplus-2.3.1-py3-none-any.whl/rustplus/objects/storage_item.py
@@ -0,0 +1,11 @@
+class Storage_Item:
+
+    def __init__(self, name : str, itemId : int, quantity : int, isBlueprint : bool) -> None:
+        
+        self.name = name
+        self.itemId = itemId
+        self.quantity = quantity
+        self.isBlueprint = isBlueprint
+
+    def __repr__(self) -> str:
+        return "Storage Item(name = {} | itemId = {} | quantity = {} | isBlueprint = {})".format(self.name, self.itemId, self.quantity, self.isBlueprint)
