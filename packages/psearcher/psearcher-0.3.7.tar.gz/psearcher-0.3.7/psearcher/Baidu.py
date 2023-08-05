254f640a245e38627341ec0aa9259e7aea21f816
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:12 +0000

    Add package psearcher/0.3.7/psearcher-0.3.7.tar.gz

---
+++ packages/psearcher/psearcher-0.3.7.tar.gz/psearcher-0.3.7/psearcher/Baidu.py
@@ -0,0 +1,34 @@
+from psearcher.BaseEngine import BaseEngine
+
+
+class Baidu(BaseEngine):
+    url = 'http://www.baidu.com/s'
+    keyTag = 'word'
+    pageTag = 'pn'
+    startIndex = 0
+    indexGap = 10
+
+    def get_url_and_parmas(self):
+        url = self.url
+        params = {
+            self.keyTag: self.keyWord,
+            self.pageTag: self.startIndex + self.indexGap*self.nextPage,
+        }
+        return url, params
+
+    def parser(self, soup):
+        results = []
+        tags = soup.find_all(class_="result")
+        print(len(tags))
+        for tag in tags:
+            print(tag)
+            link = tag.h3.a.get("href")
+            title = tag.h3.text
+            describe = tag.text
+            result = {
+                "link": link,
+                "describe": describe,
+                "title": title,
+            }
+            results.append(result)
+        return results
