a7af337a1ae59587702d131ea0aec551e7ecd0d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:48 +0000

    Add package pandoraPlugintools/0.0.2/pandoraPlugintools-0.0.2.tar.gz

---
+++ packages/pandoraPlugintools/pandoraPlugintools-0.0.2.tar.gz/pandoraPlugintools-0.0.2/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+import os
+
+# Setting up
+setup(
+    name="pandoraPlugintools",
+    version="0.0.2",
+    author="PandoraFMS projects department",
+    author_email="<projects@pandorafms.com>",
+    description="A pluguin tool set of functions for pandorafms",
+    url = 'https://github.com/projects-pandorafms/pandoraPlugintools',
+    packages=find_packages(),
+    zip_safe=False,
+    install_requires=['datetime', 'psutil', 'requests', 'requests_ntlm'],
+    keywords=['python', 'pandora', 'pandorafms', 'plugintool', 'plugintools'],
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+    ]
+)
