221be18460e4075bd32b16a4a3b343f6a883300f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:55 +0000

    Add package pyspark-scout-regiment/0.0.9/pyspark-scout-regiment-0.0.9.tar.gz

---
+++ packages/pyspark-scout-regiment/pyspark-scout-regiment-0.0.9.tar.gz/pyspark-scout-regiment-0.0.9/scoutregiment/scoutregiment_forest.py
@@ -0,0 +1,78 @@
+import datetime
+import doctest
+import functools
+import importlib
+import inspect
+import io
+import itertools
+import json
+import logging
+import math
+import os
+import pickle
+import re
+import subprocess
+import sys
+import time
+import types
+import urllib.request as urllib2
+from pprint import pprint
+
+import numpy as np
+import pandas as pd
+import pyspark
+import pyspark.ml
+import rich
+import rich.console
+import rich.syntax
+import yaml
+from munch import Munch
+from objprint import op
+from pyspark.context import SparkContext
+from pyspark.ml import Pipeline, Transformer
+from pyspark.ml.classification import LogisticRegression
+from pyspark.ml.evaluation import BinaryClassificationEvaluator, Evaluator
+from pyspark.ml.feature import (CountVectorizer, HashingTF,
+                                QuantileDiscretizer, StandardScaler, Tokenizer,
+                                VectorAssembler)
+from pyspark.ml.linalg import DenseVector, Vectors, VectorUDT
+from pyspark.ml.param import Params
+from pyspark.ml.param.shared import (HasInputCol, HasOutputCol, Param, Params,
+                                     TypeConverters)
+from pyspark.ml.tuning import CrossValidator, ParamGridBuilder
+from pyspark.ml.util import DefaultParamsReadable, DefaultParamsWritable
+from pyspark.sql import DataFrame, GroupedData, Row, SparkSession
+from pyspark.sql import functions as fn
+from pyspark.sql.functions import expr as sql_
+from pyspark.sql.types import *
+from termcolor import colored
+
+if '.' not in sys.path and './' not in sys.path: 
+    sys.path.insert(0, ".")
+def f2_download(url):
+    g = globals()
+    def download(filepath):
+        file_uri = f"{url}/{filepath}"
+        filename = filepath.split("/")[-1]
+        module_name = filename.replace(".py", "")
+        with open(f"./{filename}", 'w') as fw:
+            with urllib2.urlopen(file_uri) as f:
+                fw.write(f.read().decode())
+        g[module_name] = __import__(module_name)
+        if module_name in g:
+            importlib.reload(g[module_name])
+    return download
+
+def donwload_2f(url):
+    g = globals()
+    def download(filepath):
+        file_uri = f"{url}/{filepath}"
+        filename = filepath.split("/")[-1]
+        module_name = filename.replace(".py", "")
+        with open(f"./{filename}", 'w') as fw:
+            with urllib2.urlopen(file_uri) as f:
+                fw.write(f.read().decode())
+        g[module_name] = __import__(module_name)
+        if module_name in g:
+            importlib.reload(g[module_name])
+    return download
---
+++ packages/pyspark-scout-regiment/pyspark-scout-regiment-0.0.9.tar.gz/pyspark-scout-regiment-0.0.9/setup.py
@@ -0,0 +1,12 @@
+import setuptools
+setuptools.setup(
+    name='pyspark-scout-regiment',
+    version='v0.0.9',
+    description='pyspark scout regiment',
+    py_modules=['scoutregiment'],
+    author='chenjianwei',
+    author_email='1572069841@qq.com',
+    url='https://github.com/JianweiChen/pyspark-scout-regiment',
+    packages=setuptools.find_packages(),
+    license='MIT'
+)
