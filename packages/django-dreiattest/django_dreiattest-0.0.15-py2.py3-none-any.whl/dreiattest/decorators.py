cd5bf34b910738f36098ef7f16c4d2ed36ad66b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:23 +0000

    Add package django-dreiattest/0.0.15/django_dreiattest-0.0.15-py2.py3-none-any.whl

---
+++ packages/django-dreiattest/django_dreiattest-0.0.15-py2.py3-none-any.whl/dreiattest/decorators.py
@@ -0,0 +1,43 @@
+from base64 import b64decode
+from functools import wraps
+from hashlib import sha256
+
+from cryptography.exceptions import InvalidSignature
+from django.core.handlers.wsgi import WSGIRequest
+from django.http import JsonResponse
+
+from dreiattest.device_session import device_session_from_request
+from dreiattest.exceptions import InvalidHeaderException
+from dreiattest.helpers import request_hash
+from dreiattest.models import Key
+from . import settings as dreiattest_settings
+
+
+def signature_required():
+    """ Check that the given request has a valid signature from a known device session. """
+
+    def decorator(func):
+        @wraps(func)
+        def inner(request: WSGIRequest, *args, **kwargs):
+            try:
+                session = device_session_from_request(request, create=False)
+                public_key = Key.objects.filter(device_session=session).order_by('-id').first()
+                if not public_key:
+                    raise InvalidHeaderException
+
+                nonce_header = request.META.get(dreiattest_settings.DREIATTEST_NONCE_HEADER).encode("utf-8")
+                signature_header = b64decode(request.META.get(dreiattest_settings.DREIATTEST_SIGNATURE_HEADER, ''))
+
+                expected_client_data_hash = request_hash(request).digest()
+                client_data_with_nonce = sha256(expected_client_data_hash + nonce_header).digest()
+
+                public_key.verify(signature_header, client_data_with_nonce)
+            except InvalidHeaderException as exception:
+                return JsonResponse({'error': 'Invalid or missing json payload.'}, status=403)
+            except InvalidSignature as exception:
+                return JsonResponse({'error': 'Invalid signature.'}, status=403)
+            return func(request, *args, **kwargs)
+
+        return inner
+
+    return decorator
