7f1cd3697e3969564be9b49e2378e15e58b88a1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:33 +0000

    Add package classixclustering/0.0.4/ClassixClustering-0.0.4.tar.gz

---
+++ packages/classixclustering/ClassixClustering-0.0.4.tar.gz/ClassixClustering-0.0.4/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+from Cython.Build import cythonize
+import numpy
+
+with open("README.md", 'r') as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="ClassixClustering",
+    packages=["ClassixClustering"],
+    version="0.0.4",
+    setup_requires=["cython>=0.29.4", "numpy>=1.20.0", "scipy>1.6.0"],
+    install_requires=["numpy>=1.20.0", "tqdm", "pandas", "matplotlib"],
+    ext_modules=cythonize(["ClassixClustering/*.pyx"], include_path=["ClassixClustering"]),
+    package_data={"ClassixClustering": ["aggregation_cm.pyx", "amalgamation_cm.pyx"]},
+    include_dirs=[numpy.get_include()],
+    long_description=long_description,
+    author="Stefan Guettel, Xinye Chen",
+    author_email="stefan.guettel@manchester.ac.uk",
+    description="Fast and explainable clustering",
+    long_description_content_type='text/markdown',
+    url="https://github.com/nla-group/CLASSIX",
+    license='BSD 3-Clause'
+)
