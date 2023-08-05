2b4ec44c5b96a4daf74d382d8e7638795d7d8542
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:03 +0000

    Add package stockwell/1.0.3/stockwell-1.0.3-cp38-cp38-win_amd64.whl

---
+++ packages/stockwell/stockwell-1.0.3-cp38-cp38-win_amd64.whl/stockwell/__init__.py
@@ -0,0 +1,26 @@
+
+
+""""""  # start delvewheel patch
+def _delvewheel_init_patch_0_0_14():
+    import os
+    import sys
+    libs_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, 'stockwell.libs'))
+    if sys.version_info[:2] >= (3, 8):
+        if os.path.exists(os.path.join(sys.base_prefix, 'conda-meta')):
+            os.environ['CONDA_DLL_SEARCH_MODIFICATION_ENABLE']='1'
+        os.add_dll_directory(libs_dir)
+    else:
+        from ctypes import WinDLL
+        with open(os.path.join(libs_dir, '.load-order-stockwell-1.0.3')) as file:
+            load_order = file.read().split()
+        for lib in load_order:
+            WinDLL(os.path.join(libs_dir, lib))
+
+
+_delvewheel_init_patch_0_0_14()
+del _delvewheel_init_patch_0_0_14
+# end delvewheel patch
+
+from .lib import st
+from .lib import sine
+from .version import __version__
