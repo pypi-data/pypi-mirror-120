b62bfcc2f094cfb33356069b8841f2958a614719
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:54 +0000

    Add package infercode/0.0.28/infercode-0.0.28.tar.gz

---
+++ packages/infercode/infercode-0.0.28.tar.gz/infercode-0.0.28/setup.py
@@ -0,0 +1,41 @@
+from setuptools import setup, find_packages
+
+install_requires=[
+    "bidict==0.21.2",
+    "coloredlogs == 15.0.1",
+    "dpu_utils==0.2.19",
+    "keras_radam==0.15.0",
+    "numpy==1.19.4",
+    "protobuf==3.14.0",
+    "scikit_learn==0.24.0",
+    "scipy==1.5.2",
+    "sentencepiece==0.1.95",
+    "tensorflow==2.4.0",
+    "tqdm==4.55.1",
+    "tree_sitter_parsers>=0.0.3",
+    "utils==1.0.1"
+]
+
+setup(
+  name = 'infercode',
+  version = "0.0.28",
+  py_modules = ['infercode'],
+  description = 'Map any code snippet into vector',
+  author = 'Nghi D. Q. Bui and Yijun Yu',
+  author_email = 'bdqnghi@gmail.com',
+  license="MIT",
+  url = 'https://github.com/bdqnghi/infercode/',
+  classifiers = [
+    'Development Status :: 3 - Alpha',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python :: 3',
+    'Intended Audience :: Developers',
+  ],
+  package_dir={"infercode": "infercode"},
+  packages=find_packages(where=".", exclude=["tests", "logo", "datasets"]),
+  package_data={'infercode': ['configs/*.ini', 'sentencepiece_vocab/*', 'sentencepiece_vocab/node_types/*' , 'sentencepiece_vocab/subtrees/*', 'sentencepiece_vocab/tokens/*' ]},
+  install_requires=install_requires,
+  include_package_data=True,
+  scripts=['./scripts/infercode'],
+)
