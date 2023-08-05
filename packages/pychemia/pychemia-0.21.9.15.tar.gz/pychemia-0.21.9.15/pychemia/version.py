c7cebe6b487f2509298200b980bf2bc23c4c41d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:00 +0000

    Add package pychemia/0.21.9.15/pychemia-0.21.9.15.tar.gz

---
+++ packages/pychemia/pychemia-0.21.9.15.tar.gz/pychemia-0.21.9.15/pychemia/version.py
@@ -0,0 +1,17 @@
+
+# THIS FILE IS GENERATED FROM PYCHEMIA SETUP.PY
+short_version = '0.21.9.15'
+version = '0.21.9.15'
+full_version = '0.21.9.15.dev0+e74341d'
+git_revision = 'e74341d856af842fca859e4a5f64fdef36584b70'
+name = 'pychemia'
+description = 'Python Framework for Materials Discovery and Design'
+url = 'https://materialsdiscovery.github.io/PyChemia'
+author = 'Guillermo Avendaño Franco'
+email = 'gufranco@mail.wvu.edu'
+status = 'Development'
+copyright = 'Copyright 2021'
+date = 'September 15, 2021'
+release = False
+if not release:
+    version = full_version
