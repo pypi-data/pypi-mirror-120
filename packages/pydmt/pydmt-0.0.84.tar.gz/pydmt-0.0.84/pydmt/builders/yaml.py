efd6a0baaac5ae2229759ccab11317b47da638d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:10:23 +0000

    Add package pydmt/0.0.84/pydmt-0.0.84.tar.gz

---
+++ packages/pydmt/pydmt-0.0.84.tar.gz/pydmt-0.0.84/pydmt/builders/yaml.py
@@ -0,0 +1,22 @@
+import json
+import yaml
+
+from jsonschema import validate
+
+from pydmt.utils.filesystem import mkdir_touch
+
+from pydmt.builders.one_source_one_target import OneSourceOneTarget
+
+METADATA = "metadata"
+SCHEMA_FILE = "schema_file"
+
+
+class YamlValidate(OneSourceOneTarget):
+    def build(self):
+        with open(self.source, "rt") as input_handle:
+            data = yaml.load(input_handle, yaml.SafeLoader)
+            if METADATA in data:
+                schema_file = data[METADATA][SCHEMA_FILE]
+                schema = json.load(schema_file, Loader=yaml.SafeLoader)
+                validate(data, schema)
+        mkdir_touch(self.target)
