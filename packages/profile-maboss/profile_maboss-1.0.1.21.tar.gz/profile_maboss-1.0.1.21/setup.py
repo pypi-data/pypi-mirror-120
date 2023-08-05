cf15c9c21e6d2b2b3fa8e752e458f9b73e04f4ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:07:57 +0000

    Add package profile-maboss/1.0.1.21/profile_maboss-1.0.1.21.tar.gz

---
+++ packages/profile-maboss/profile_maboss-1.0.1.21.tar.gz/profile_maboss-1.0.1.21/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(name='profile_maboss',
+    version="1.0.1.21",
+    author="Rémi Trimbour",
+    author_email="remi.trimbour@ens.fr",
+    description="A package allowing personalisation of MaBoSS models with the PROFILE methodology defined in [paper_ref]",
+	install_requires = ['maboss'],
+	packages=['profile_maboss'],
+)
