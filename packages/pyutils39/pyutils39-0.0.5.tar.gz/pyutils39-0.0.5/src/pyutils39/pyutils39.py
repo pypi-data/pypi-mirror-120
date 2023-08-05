7f173de401727a4cc26b7ddae013299e34687276
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:05:52 +0000

    Add package pyutils39/0.0.5/pyutils39-0.0.5.tar.gz

---
+++ packages/pyutils39/pyutils39-0.0.5.tar.gz/pyutils39-0.0.5/src/pyutils39/pyutils39.py
@@ -0,0 +1,29 @@
+from tkinter import Tk, messagebox
+def reverse(data):
+    x = str(data)
+    datalen = len(data)
+    x = x[datalen::-1]
+    return x
+def consoleask(question):
+    while True:
+        print(str(question)+' (Y/N)')
+        x = input()
+        if x.lower().startswith('y'):
+            return True
+            break
+        elif x.lower().startswith('n'):
+            return False
+            break
+def error(message,title='Error'):
+    title = str(title)
+    message = str(message)
+    Tk().withdraw()
+    messagebox.showerror(title,message)
+def toplevelerror(message,title='Error'):
+    title = str(title)
+    message = str(message)
+    root = Tk()
+    root.wm_attributes("-topmost",True)
+    root.withdraw()
+    messagebox.showerror(title,message)
+        
