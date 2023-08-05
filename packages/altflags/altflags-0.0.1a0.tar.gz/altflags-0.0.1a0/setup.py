9d9c77684a28cfc1d81609cc34611b63e57d2dda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:55 +0000

    Add package altflags/0.0.1a0/altflags-0.0.1a0.tar.gz

---
+++ packages/altflags/altflags-0.0.1a0.tar.gz/altflags-0.0.1a0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name="altflags",
+    author="Oddity",
+    version="0.0.1a",
+    license="MIT",
+    description="Python alternative (binary) flags",
+    python_requires=">=3.8.0",
+    packages=["altflags"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
