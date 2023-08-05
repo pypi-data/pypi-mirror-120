bfd6caa91d774aad7d5aaf92def5c30264c25c82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:07 +0000

    Add package feincms3-language-sites/0.0.3/feincms3_language_sites-0.0.3.tar.gz

---
+++ packages/feincms3-language-sites/feincms3_language_sites-0.0.3.tar.gz/feincms3_language_sites-0.0.3/feincms3_language_sites/middleware.py
@@ -0,0 +1,47 @@
+from django.conf import settings
+from django.core.exceptions import ImproperlyConfigured
+from django.http import Http404, HttpResponsePermanentRedirect
+from django.utils import translation
+
+
+def site_middleware(get_response):
+    from .models import site_for_host
+
+    def middleware(request):
+        request.site = site_for_host(request.get_host())
+        if request.site is None:
+            raise Http404("No configuration found for %r" % request.get_host())
+
+        translation.activate(request.site["language_code"])
+        request.LANGUAGE_CODE = translation.get_language()
+        response = get_response(request)
+        response.setdefault("Content-Language", translation.get_language())
+        return response
+
+    return middleware
+
+
+def redirect_to_site_middleware(get_response):
+    def middleware(request):
+        if not hasattr(request, "site"):
+            raise ImproperlyConfigured(
+                'No "site" attribute on request. Insert site_middleware'
+                " before redirect_to_site_middleware."
+            )
+
+        # Host matches, and either no HTTPS enforcement or already HTTPS
+        if request.get_host() == request.site["host"] and (
+            not settings.SECURE_SSL_REDIRECT or request.is_secure()
+        ):
+            return get_response(request)
+
+        return HttpResponsePermanentRedirect(
+            "http%s://%s%s"
+            % (
+                "s" if (settings.SECURE_SSL_REDIRECT or request.is_secure()) else "",
+                request.site["host"],
+                request.get_full_path(),
+            )
+        )
+
+    return middleware
