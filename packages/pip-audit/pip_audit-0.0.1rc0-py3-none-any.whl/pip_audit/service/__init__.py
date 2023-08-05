49d067292dc83bce714d5a0ad5842ffc35633e33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:08 +0000

    Add package pip-audit/0.0.1rc0/pip_audit-0.0.1rc0-py3-none-any.whl

---
+++ packages/pip-audit/pip_audit-0.0.1rc0-py3-none-any.whl/pip_audit/service/__init__.py
@@ -0,0 +1,8 @@
+from .interface import (  # noqa: F401
+    Dependency,
+    ServiceError,
+    VersionRange,
+    VulnerabilityResult,
+    VulnerabilityService,
+)
+from .osv import OsvService  # noqa: F401
