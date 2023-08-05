6bf949c3b6897829593ad4a06e95b3f62a1c0227
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:05 +0000

    Add package myosutils/1.0.0/myosutils-1.0.0-py3-none-any.whl

---
+++ packages/myosutils/myosutils-1.0.0-py3-none-any.whl/myosutils.py
@@ -0,0 +1,20 @@
+import contextlib
+import os
+import termios
+
+@contextlib.contextmanager
+def raw_mode(file):
+	old_attrs = termios.tcgetattr(file.fileno())
+	new_attrs = old_attrs[:]
+	new_attrs[3] = new_attrs[3] & ~(termios.ECHO | termios.ICANON)
+	try:
+		termios.tcsetattr(file.fileno(), termios.TCSADRAIN, new_attrs)
+		yield
+	finally:
+		termios.tcsetattr(file.fileno(), termios.TCSADRAIN, old_attrs)
+		
+def clear_terminal():
+	if os.name is 'posix':
+		_ = os.system('clear')
+	else:
+		_ = os.sysem('cls')
