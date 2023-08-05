6d55190d3692abb6fdf28460b023d2518b148009
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:17 +0000

    Add package LobbyBot/2.0.0/LobbyBot-2.0.0.tar.gz

---
+++ packages/LobbyBot/LobbyBot-2.0.0.tar.gz/LobbyBot-2.0.0/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+name = 'LobbyBot'
+
+
+setuptools.setup(
+    name=name,
+    version="2.0.0",
+    author="Pirxcy",
+    description="24/7 Fortnite Lobbybot With Online Dashboard",
+    long_description="24/7 Fortnite Lobbybot With Online Dashboard",
+    long_description_content_type="text/markdown",
+    url="https://github.com/PirxcyFinal/pirxcybotv2",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'sanic',
+        'fortnitepy',
+        'BenBotAsync',
+        'PirxcyPinger'
+    ],
+)
