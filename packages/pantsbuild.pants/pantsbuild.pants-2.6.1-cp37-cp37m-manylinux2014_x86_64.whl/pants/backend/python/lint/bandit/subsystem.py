3e9541aa0245d17b20db943ae4b68d9055417f7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:22 +0000

    Add package pantsbuild.pants/2.6.1/pantsbuild.pants-2.6.1-cp37-cp37m-manylinux2014_x86_64.whl

---
+++ packages/pantsbuild.pants/pantsbuild.pants-2.6.1-cp37-cp37m-manylinux2014_x86_64.whl/pants/backend/python/lint/bandit/subsystem.py
@@ -0,0 +1,73 @@
+# Copyright 2020 Pants project contributors (see CONTRIBUTORS.md).
+# Licensed under the Apache License, Version 2.0 (see LICENSE).
+
+from __future__ import annotations
+
+from typing import cast
+
+from pants.backend.python.subsystems.python_tool_base import PythonToolBase
+from pants.backend.python.target_types import ConsoleScript
+from pants.core.util_rules.config_files import ConfigFilesRequest
+from pants.option.custom_types import file_option, shell_str
+
+
+class Bandit(PythonToolBase):
+    options_scope = "bandit"
+    help = "A tool for finding security issues in Python code (https://bandit.readthedocs.io)."
+
+    default_version = "bandit>=1.7.0,<1.8"
+    default_extra_requirements = [
+        "setuptools<45; python_full_version == '2.7.*'",
+        "setuptools; python_version > '2.7'",
+        "stevedore<3",  # stevedore 3.0 breaks Bandit.
+    ]
+    default_main = ConsoleScript("bandit")
+
+    @classmethod
+    def register_options(cls, register):
+        super().register_options(register)
+        register(
+            "--skip",
+            type=bool,
+            default=False,
+            help=f"Don't use Bandit when running `{register.bootstrap.pants_bin_name} lint`",
+        )
+        register(
+            "--args",
+            type=list,
+            member_type=shell_str,
+            help=(
+                f"Arguments to pass directly to Bandit, e.g. "
+                f'`--{cls.options_scope}-args="--skip B101,B308 --confidence"`'
+            ),
+        )
+        register(
+            "--config",
+            type=file_option,
+            default=None,
+            advanced=True,
+            help=(
+                "Path to a Bandit YAML config file "
+                "(https://bandit.readthedocs.io/en/latest/config.html)."
+            ),
+        )
+
+    @property
+    def skip(self) -> bool:
+        return cast(bool, self.options.skip)
+
+    @property
+    def args(self) -> tuple[str, ...]:
+        return tuple(self.options.args)
+
+    @property
+    def config(self) -> str | None:
+        return cast("str | None", self.options.config)
+
+    @property
+    def config_request(self) -> ConfigFilesRequest:
+        # Refer to https://bandit.readthedocs.io/en/latest/config.html. Note that there are no
+        # default locations for Bandit config files.
+        return ConfigFilesRequest(
+            specified=self.config, specified_option_name=f"{self.options_scope}.config"
+        )
