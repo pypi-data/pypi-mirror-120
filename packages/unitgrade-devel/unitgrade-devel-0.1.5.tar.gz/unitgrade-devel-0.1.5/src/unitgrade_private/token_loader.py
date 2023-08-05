00a8c4b73c91bb42e59f0bb1ea184e5d46bf4c51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:31 +0000

    Add package unitgrade-devel/0.1.5/unitgrade-devel-0.1.5.tar.gz

---
+++ packages/unitgrade-devel/unitgrade-devel-0.1.5.tar.gz/unitgrade-devel-0.1.5/src/unitgrade_private/token_loader.py
@@ -0,0 +1,38 @@
+import glob
+import os
+import pickle
+import bz2
+import zipfile
+import io
+
+def unpack_sources_from_token(token_file, destination=None):
+    with open(token_file, 'rb') as f:
+        rs = pickle.load(f)
+    if destination is None:
+        destination = os.path.dirname(token_file )
+    destination = os.path.normpath(destination)
+
+    for k, data in rs['sources'].items():
+
+        out = destination +"/" + os.path.basename(token_file)[:-6] + f"_{k}/"
+
+        if not os.path.exists(out):
+            zip = zipfile.ZipFile(io.BytesIO(data['zipfile']))
+            zip.extractall(out)
+
+
+if __name__ == "__main__":
+    import irlc
+    import irlc.assignments.assignment_fruit as fa
+    fa.__file__
+
+
+    dn = os.path.dirname(fa.__file__ )
+
+    l = glob.glob(dn+"/*fruit*.token")
+    token_file = l[0]
+    print(token_file)
+
+    rs = unpack_sources_from_token(token_file)
+
+    a = 3455
