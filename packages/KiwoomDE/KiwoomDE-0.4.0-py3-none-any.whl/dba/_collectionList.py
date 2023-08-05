beea5482e7b074779e7f6f43bd8cb7d90cea57d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:49 +0000

    Add package KiwoomDE/0.4.0/KiwoomDE-0.4.0-py3-none-any.whl

---
+++ packages/KiwoomDE/KiwoomDE-0.4.0-py3-none-any.whl/dba/_collectionList.py
@@ -0,0 +1,151 @@
+# -*- coding: utf-8 -*-
+from datetime import datetime
+import math
+import os, sys
+import re
+import pprint
+pp = pprint.PrettyPrinter(indent=2)
+
+
+import pandas as pd
+
+
+from idebug import *
+from kiwoomde.database import *
+
+
+from kiwoomde import config
+
+
+
+def _parse_collection_list(names):
+    # pp.pprint(sorted(names))
+    pat = f'_*(Schema|Test)*_*([a-zA-Z0-9]+)_*([a-z\d]+)*$'
+    data = []
+    for name in names:
+        m = re.search(pat, name)
+        # print(m.groups(), name)
+        li = list(m.groups())
+        li.append(name)
+        data.append(li)
+
+    df = pd.DataFrame(data, columns=['collmode','modelName','param','collName'])
+    df = df.fillna(value={'collmode':'Real'})
+    return df.sort_values(['collmode','modelName','param']).reset_index(drop=True)
+
+
+@funcIdentity
+def CollectionList(modelName):
+    # 모델명에 해당하는 컬렉션리스트를 반환
+    pat = f'_*([Schema|Test]*)_*({modelName})_*([\d|a-z]+)*$'
+    names = db.list_collection_names(filter={'name':{'$regex':pat}})
+    df = _parse_collection_list(names)
+    logger.info(f"{modelName}-> len:{len(df)}")
+    return df
+
+
+@funcIdentity
+def AllCollectionList():
+    names = db.list_collection_names(filter=None)
+    df = _parse_collection_list(names)
+    logger.info(f"len:{len(df)}")
+    return df
+
+
+@funcIdentity
+def SchemaCollectionList():
+    df = AllCollectionList()
+    df = df.query('collmode == "Schema"').reset_index(drop=True)
+    names = list(df.collName)
+    logger.info(f"len:{len(df)}")
+    return df
+
+
+@funcIdentity
+def RealCollectionList():
+    df = AllCollectionList()
+    df = df.query('collmode == "Real"').reset_index(drop=True)
+    names = list(df.collName)
+    logger.info(f"len:{len(df)}")
+    return df
+
+
+@funcIdentity
+def TestCollectionList():
+    df = AllCollectionList()
+    df = df.query('collmode == "Test"').reset_index(drop=True)
+    names = list(df.collName)
+    logger.info(f"len:{len(df)}")
+    return df
+
+
+@funcIdentity
+def RealtimeCollectionList():
+    df = CollectionList('Realtime')
+    logger.info(f"len:{len(df)}")
+    return df
+
+
+@funcIdentity
+def TRCollectionList():
+    names = db.list_collection_names(filter={'name':{'$regex':'op[tw]\d+', '$options':'i'}})
+    df = _parse_collection_list(names)
+    logger.info(f"len:{len(df)}")
+    return df
+
+
+@funcIdentity
+def AssetCollectionList():
+    filter = {'name':{'$regex':'opw0', '$options':'i'}}
+    names = db.list_collection_names(filter=filter)
+    df = _parse_collection_list(names)
+    logger.info(f"len:{len(df)}")
+    return df
+
+
+@funcIdentity
+def StaticSchemaModels(n=2):
+    if n == 0:
+        names = db.list_collection_names(filter={'name':{'$regex':'^[A-Z]'}})
+        p = re.compile('^[opt|opw|config]', flags=re.I)
+        names = [name for name in names if p.search(name) is not None]
+    elif n == 1:
+        names = ['TRList','TRInput','TRItem','RTList','RealtimeFID']
+    elif n == 2:
+        names = []
+        with os.scandir(config.SchemaCSVPath) as it:
+            for entry in it:
+                if not entry.name.startswith('.') and entry.is_file():
+                    # print(entry.name, entry)
+                    root, ext = os.path.splitext(entry.name)
+                    names.append(root)
+
+    return sorted(names)
+
+
+@funcIdentity
+def DevGuideModels():
+    # 텍스트파일이므로 확장자명은 필요없다
+    names = []
+    with os.scandir(config.DevGuideTextPath) as it:
+        for entry in it:
+            if not entry.name.startswith('.') and entry.is_file():
+                # print(entry.name, entry)
+                root, ext = os.path.splitext(entry.name)
+                names.append(root)
+
+    return sorted(names)
+
+
+@funcIdentity
+def ManDataModels():
+    # 파일타입이 CSV/JSON/등등 을 모두 커버해야하므로, 확장자명까지 함께 남긴다
+    names = []
+    with os.scandir(config.ManDataJSONPath) as it:
+        for entry in it:
+            if not entry.name.startswith('.') and entry.is_file():
+                # print(entry.name, entry)
+                root, ext = os.path.splitext(entry.name)
+                names.append(root)
+
+    return sorted(names)
---
+++ packages/KiwoomDE/KiwoomDE-0.4.0-py3-none-any.whl/models/base.py
@@ -0,0 +1,78 @@
+# -*- coding: utf-8 -*-
+import re
+import os
+import pprint
+pp = pprint.PrettyPrinter(indent=2)
+
+
+import pandas as pd
+
+
+from idebug import *
+
+
+from kiwoomde import config
+from kiwoomde.base import BaseClass, BaseDataClass
+from kiwoomde.database import *
+from kiwoomde.models._util import AutoFilter, FidAutoFilter, FidAutoProjection, assign_dtype
+
+
+class KWModel(DataModel):
+    # Kiwoom Base Model
+
+    def __init__(self, modelName, collmode='real'):
+        super().__init__(modelName, collmode)
+
+    def _extend_cidmodel(self, **cid):
+        # eXtended-CID-Model 로 변환 (extend a model to cid-model)
+        # modelName에 cid를 추가하여 collName을 만듦으로써, 컬렉션을 확장하는 모델
+        if len(cid) > 0:
+            cid.update({'active':True})
+            self.cidentify(**cid)
+            self.collName = f"{self.modelName}__{self.cid}"
+            self.apply_collmode(self.collmode)
+        else:
+            logger.critical(f'{self.modelName} 모델은 반드시 종목을 특정해야 한다')
+            raise
+        return self
+
+    @funcIdentity
+    def cidentify(self, **cid):
+        # 빅히트/하이브 처럼 2개 이상 존재할 경우를 고려하라. 종목코드는 같고, cid가 다르다.
+        # 나중에 업데이트된 것을 골라야 한다.
+        self.Company = SelectorModel('Company').select(**cid)
+        self.cid = self.Company._id
+        self.issueName = self.Company.name
+        self.issueCode = self.Company.code
+        return self
+
+    def reindex(self, df, columnPattern):
+        cols = list(df.columns)
+        cols = [c for c in cols if re.search(columnPattern, string=c) is not None]
+        return df.reindex(columns=cols)
+
+    def _dropna(self, data):
+        if len(data) > 0:
+            df = pd.DataFrame(data)
+            len0 = f"({len(df)}, {len(df.columns)})"
+
+            subset = list(df.columns)
+            if 'dt' in subset:
+                subset.remove('dt')
+            df = df.dropna(axis=0, how='all', subset=subset)
+            df = df.dropna(axis=1, how='all')
+
+            len1 = f"({len(df)}, {len(df.columns)})"
+            logger.debug(f"{len0} --> {len1}")
+            return df.to_dict('records')
+        else:
+            return []
+
+    def select(self, filter=None, projection=None, **kw):
+        af = AutoFilter(self.schema).search(filter)
+        data = self.load(af.filter, projection, **kw)
+        try:
+            self.selected = BaseDataClass(dataname=f'{self.modelName}-Selected', **data[0])
+        except Exception as e:
+            logger.exception(e)
+        return self
