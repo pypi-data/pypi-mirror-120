da76aafb016b0b0cacabaa1b458b70707184edfb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:12:22 +0000

    Add package shiprocket/0.0.1/shiprocket-0.0.1.tar.gz

---
+++ packages/shiprocket/shiprocket-0.0.1.tar.gz/shiprocket-0.0.1/setup.py
@@ -0,0 +1,25 @@
+import os
+from setuptools import setup, find_packages
+
+README = open(os.path.join(os.path.dirname(__file__), 'README.md')).read()
+setup(
+    name='shiprocket',
+    version='0.0.1',
+    packages=find_packages(),
+    url='https://github.com/pupattan/shiprocket',
+    license='MIT',
+    include_package_data=True,
+    keywords="python shiprocket ecommerce shipping",
+    author='Pulak Pattanayak',
+    long_description=README,
+    long_description_content_type='text/markdown',
+    author_email='pkbsdmp@gmail.com',
+    description='Python APIs for shiprocket.in',
+    classifiers =[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development'
+    ]
+)
