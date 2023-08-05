9565815132680f935503123762adb9b8f26f0a13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:05:58 +0000

    Add package goddy/0.0.2/goddy-0.0.2.tar.gz

---
+++ packages/goddy/goddy-0.0.2.tar.gz/goddy-0.0.2/setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Author: Goddy <goddy@mykg.ai> 2021/9/16
+# Desc:
+
+from setuptools import setup, find_packages
+
+setup(
+    name="goddy",
+    version="0.0.2",
+    keyword=("goddy"),
+    description="A test project for goddy.",
+    long_description="A test project for goddy wu.",
+    license="MIT Licence",
+
+    url="https://goddywu.github.io/",
+    author="goddy",
+    author_email="goddy@mykg.ai",
+
+    packages=['goddy'],
+    include_package_data=True,
+    platform="any",
+    install_requires=[],
+)
