23f4793685fb025b4907ff584b494bde5e1c0810
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:12:59 +0000

    Add package wxproject/1.0/wxproject-1.0.tar.gz

---
+++ packages/wxproject/wxproject-1.0.tar.gz/wxproject-1.0/setup.py
@@ -0,0 +1,11 @@
+#coding:utf-8
+from distutils.core import setup
+
+setup(
+    name = 'wxproject',
+    version = '1.0',
+    description= '测试发布',
+    author ='wx',
+    author_email='wx@faw.com.cn',
+    py_modules =['mypackwx.mypyfile']
+)
