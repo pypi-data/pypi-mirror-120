beaeb312b0bd3a92b62e25a2f44bb6fc30484cc0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:14:16 +0000

    Add package GB-distributions-class-probability/0.3/GB_distributions_class_probability-0.3.tar.gz

---
+++ packages/GB-distributions-class-probability/GB_distributions_class_probability-0.3.tar.gz/GB_distributions_class_probability-0.3/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='GB_distributions_class_probability',
+      version= '0.3',
+      description= 'Gaussian and Binomial distributions',
+      packages= ['GB_distributions_class_probability'],
+      author= 'Raviteja Mangina',
+      author_email= 'mangina.raviteja@gmail.com',
+      zip_safe=False)
