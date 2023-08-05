b53f26f6cb8495965de61357d0e69ba14663efc1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:11:28 +0000

    Add package tiggy/0.0/tiggy-0.0.tar.gz

---
+++ packages/tiggy/tiggy-0.0.tar.gz/tiggy-0.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+  name = 'tiggy',
+  packages = find_packages(),
+  version = '0.0',
+  license='GPL v3.0',
+  description = 'tiggy',
+  install_requires=[
+      'numpy',
+  ],
+)
