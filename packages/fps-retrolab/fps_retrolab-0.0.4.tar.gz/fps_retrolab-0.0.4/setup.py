fc105e41314fcbec9154b3998c6abc73942acd9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:08 +0000

    Add package fps-retrolab/0.0.4/fps_retrolab-0.0.4.tar.gz

---
+++ packages/fps-retrolab/fps_retrolab-0.0.4.tar.gz/fps_retrolab-0.0.4/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages  # type: ignore
+
+setup(
+    name="fps_retrolab",
+    version="0.0.4",
+    packages=find_packages(),
+    install_requires=["fps", "retrolab", "fps-contents", "fps-kernels", "fps-auth"],
+    entry_points={"fps_router": ["fps-retrolab = fps_retrolab.routes"]},
+)
