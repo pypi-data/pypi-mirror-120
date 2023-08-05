64a4b97cea4d136da7385336c561aea8f9cb8664
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:22 +0000

    Add package gnista-cli/0.1.4/gnista_cli-0.1.4-py3-none-any.whl

---
+++ packages/gnista-cli/gnista_cli-0.1.4-py3-none-any.whl/gnista_cli/__main__.py
@@ -0,0 +1,36 @@
+import logging
+import sys
+import time
+import structlog
+
+from .main import main
+
+logging.basicConfig(
+    format="%(message)s",
+    stream=sys.stdout,
+    level=logging.INFO,
+)
+
+structlog.configure(
+    processors=[
+        structlog.stdlib.add_logger_name,
+        structlog.stdlib.add_log_level,
+        structlog.stdlib.PositionalArgumentsFormatter(),
+        structlog.processors.TimeStamper(fmt="%Y-%m-%d %H:%M.%S"),
+        structlog.processors.StackInfoRenderer(),
+        structlog.processors.format_exc_info,
+        structlog.dev.ConsoleRenderer(),
+    ],
+    context_class=dict,
+    logger_factory=structlog.stdlib.LoggerFactory(),
+    wrapper_class=structlog.stdlib.BoundLogger,
+    cache_logger_on_first_use=True,
+)
+
+if __name__ == "__main__":
+    try:
+        main()
+    # pylint:disable=W0703
+    except Exception as ex:
+        print(ex)
+        time.sleep(20)
