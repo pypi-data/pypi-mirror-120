3cfba8f0d003bcc7a9e8e88a23c3b32025574225
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:08 +0000

    Add package fps-yjs/0.0.3/fps_yjs-0.0.3.tar.gz

---
+++ packages/fps-yjs/fps_yjs-0.0.3.tar.gz/fps_yjs-0.0.3/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages  # type: ignore
+
+setup(
+    name="fps_yjs",
+    version="0.0.3",
+    packages=find_packages(),
+    install_requires=["fps", "fps-contents", "fps-kernels", "fps-auth"],
+    entry_points={"fps_router": ["fps-yjs = fps_yjs.routes"]},
+)
