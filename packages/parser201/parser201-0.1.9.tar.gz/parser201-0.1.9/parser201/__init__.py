ab1cb239ee3dfa1905b035d0447b25b2fbf8e6b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:19 +0000

    Add package parser201/0.1.9/parser201-0.1.9.tar.gz

---
+++ packages/parser201/parser201-0.1.9.tar.gz/parser201-0.1.9/parser201/__init__.py
@@ -0,0 +1,9 @@
+"""Top-level package for parser201."""
+
+from .parser201 import LogParser
+
+__author__ = """Peter Nardi"""
+__email__ = 'pete@nardi.com'
+__version__ = '0.1.9'
+
+parser201.__module__ = 'LogParser'
