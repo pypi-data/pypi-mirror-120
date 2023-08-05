779a62653e6801e682be5fbbdd09db3fb352fb8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:55 +0000

    Add package luanaBanana-distributions/0.1/luanaBanana_distributions-0.1.tar.gz

---
+++ packages/luanaBanana-distributions/luanaBanana_distributions-0.1.tar.gz/luanaBanana_distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='luanaBanana_distributions',
+      version='0.1',
+      description='Gaussian luanaBanana_distributions',
+      packages=['luanaBanana_distributions'],
+      zip_safe=False)
