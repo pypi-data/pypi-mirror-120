85ff06d8a3337bbb96af8f266742c1935cef1cc2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:13:03 +0000

    Add package felupe/0.0.11/felupe-0.0.11.tar.gz

---
+++ packages/felupe/felupe-0.0.11.tar.gz/felupe-0.0.11/felupe/__init__.py
@@ -0,0 +1,24 @@
+from .__about__ import __version__
+from . import math
+from . import mesh
+from . import quadrature
+from . import doftools
+from . import element
+
+# from . import field
+from . import tools
+
+# from . import region
+from . import constitution
+from . import solve
+from . import utils
+
+from .region import Region
+from .form import IntegralForm, IntegralFormMixed, IntegralFormAxisymmetric
+from .field import Field, FieldAxisymmetric, FieldMixed
+from .doftools import Boundary
+
+
+__all__ = [
+    "__version__",
+]
