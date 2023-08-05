57b509b0e2e03b9c5697b2e18c368656bf73a2d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:05:44 +0000

    Add package tars/0.3.3/tars-0.3.3-py3-none-any.whl

---
+++ packages/tars/tars-0.3.3-py3-none-any.whl/tars/strategies/__init__.py
@@ -0,0 +1,7 @@
+from .abstract_strategy import AbstractStrategy
+from .buy_and_hold_strategy import BuyAndHold
+from .random_investment_strategy import RandomInvestment
+from .sequential_investment_strategy import SequentialInvestment
+from .trend_following_strategy import TrendFollowing
+from .trend_following_macd_strategy import TrendFollowingMACD
+from .prediction_strategy import PredictionStrategy
