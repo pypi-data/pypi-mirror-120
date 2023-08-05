70095835f22b4714c6fb6595840714beae89ddc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:09:39 +0000

    Add package seven-cloudapp-frame/1.0.0.160/seven_cloudapp_frame-1.0.0.160.tar.gz

---
+++ packages/seven-cloudapp-frame/seven_cloudapp_frame-1.0.0.160.tar.gz/seven_cloudapp_frame-1.0.0.160/seven_cloudapp_frame/handlers/w_client/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+:Author: HuangJianYi
+:Date: 2021-07-15 11:59:42
+@LastEditTime: 2021-09-14 10:01:04
+@LastEditors: HuangJianYi
+:description: 
+"""
+__all__ = ["w_pay"]
