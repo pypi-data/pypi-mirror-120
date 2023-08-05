44ecda9f47849abfcefa2384d29a0c01b7671d52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:05:47 +0000

    Add package aqtinstall/2.0.0rc4/aqtinstall-2.0.0rc4.tar.gz

---
+++ packages/aqtinstall/aqtinstall-2.0.0rc4.tar.gz/aqtinstall-2.0.0rc4/tests/test_connection.py
@@ -0,0 +1,45 @@
+import re
+import sys
+
+import pytest
+
+import aqt
+
+
+@pytest.mark.remote_data
+def test_cli_unknown_version(capsys):
+    wrong_version = "5.16.0"
+    wrong_url_ending = "mac_x64/desktop/qt5_5160/Updates.xml"
+    cli = aqt.installer.Cli()
+    assert cli.run(["install-qt", "mac", "desktop", wrong_version]) == 1
+    out, err = capsys.readouterr()
+    sys.stdout.write(out)
+    sys.stderr.write(err)
+    assert not out
+
+    """
+    Expected result when no redirect occurs:
+
+    aqtinstall(aqt) v.* on Python 3.*
+    Specified Qt version is unknown: 5.16.0.
+    Failed to retrieve file at https://download.qt.io/online/qtsdkrepository/mac_x64/desktop/qt5_5160/Updates.xml
+    Server response code: 404, reason: Not Found
+
+    Expected result when redirect occurs:
+
+    aqtinstall(aqt) v.* on Python 3.*
+    Specified Qt version is unknown: 5.16.0.
+    Connection to the download site failed and fallback to mirror site.
+    Failed to retrieve file at .*/mac_x64/desktop/qt5_5160/Updates.xml
+    Server response code: 404, reason: Not Found
+    Connection to the download site failed. Aborted...
+    """
+
+    matcher = re.compile(
+        r"^aqtinstall\(aqt\) v.* on Python 3.*\n"
+        r".*Specified Qt version is unknown: " + re.escape(wrong_version) + r"\.\n"
+        r".*Failed to retrieve file at .*" + re.escape(wrong_url_ending) + r"\n"
+        r".*Server response code: 404, reason: Not Found.*"
+    )
+
+    assert matcher.match(err)
