9a36c047e45cd5ba58b3d66017b2100821911540
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:40:38 +0000

    Add package NlpToolkit-Corpus/1.0.12/NlpToolkit-Corpus-1.0.12.tar.gz

---
+++ packages/NlpToolkit-Corpus/NlpToolkit-Corpus-1.0.12.tar.gz/NlpToolkit-Corpus-1.0.12/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='NlpToolkit-Corpus',
+    version='1.0.12',
+    packages=['Corpus'],
+    url='https://github.com/StarlangSoftware/Corpus-Py',
+    license='',
+    author='olcaytaner',
+    author_email='olcay.yildiz@ozyegin.edu.tr',
+    description='Corpus library',
+    install_requires=['NlpToolkit-Dictionary']
+)
