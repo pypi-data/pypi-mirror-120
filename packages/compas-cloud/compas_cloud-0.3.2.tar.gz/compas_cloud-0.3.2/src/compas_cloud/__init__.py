005b4df7c76b35157a855c49fdbe8669612eb709
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:12:14 +0000

    Add package compas-cloud/0.3.2/compas_cloud-0.3.2.tar.gz

---
+++ packages/compas-cloud/compas_cloud-0.3.2.tar.gz/compas_cloud-0.3.2/src/compas_cloud/__init__.py
@@ -0,0 +1,14 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import compas
+
+from .proxy import Proxy # noqa F401
+
+if not compas.IPY:
+    from .sessions import Sessions # noqa F401
+
+__version__ = '0.3.2'
+
+__all__ = [name for name in dir() if not name.startswith('_')]
