b679dfb3eb01fdf12a2a636301d0dc1ae2fa2a36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:54:13 +0000

    Add package ligeor/1.0b0/ligeor-1.0b0.tar.gz

---
+++ packages/ligeor/ligeor-1.0b0.tar.gz/ligeor-1.0b0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(name='ligeor',
+      version='1.0b',
+      description='LIght curve GEOmetRy of eclipsing binaries',
+      url='https://github.com/gecheline/ligeor',
+      author='Angela Kochoska',
+      author_email='a.kochoska@gmail.com',
+      license='MIT License',
+      packages=find_packages(),
+      zip_safe=False)
