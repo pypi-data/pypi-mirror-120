e8ff50f32c17d0b9f394c193fbd510ae69d308c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:20 +0000

    Add package obsidianpy/1.0.6.1/obsidianpy-1.0.6.1.tar.gz

---
+++ packages/obsidianpy/obsidianpy-1.0.6.1.tar.gz/obsidianpy-1.0.6.1/obsidian/__init__.py
@@ -0,0 +1,14 @@
+"""Made possible by cloudwithax 2021"""
+
+__version__ = "1.0"
+__title__ = "obsidian.py"
+__author__ = "cloudwithax"
+
+
+from .exceptions import *
+from .events import *
+from .filters import *
+from .objects import *
+from .pool import NodePool
+from .node import Node
+from .player import Player
