66c87ac701cfa2cd04537b6ec3683dd046980ed1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:30:54 +0000

    Add package minos-microservice-networks/0.0.16/minos_microservice_networks-0.0.16-py3-none-any.whl

---
+++ packages/minos-microservice-networks/minos_microservice_networks-0.0.16-py3-none-any.whl/minos/networks/handlers/services.py
@@ -0,0 +1,52 @@
+import logging
+from typing import (
+    Any,
+    Optional,
+)
+
+from aiomisc import (
+    Service,
+)
+from dependency_injector.wiring import (
+    Provide,
+)
+
+from .consumers import (
+    Consumer,
+)
+
+logger = logging.getLogger(__name__)
+
+
+class ConsumerService(Service):
+    """Minos QueueDispatcherService class."""
+
+    dispatcher: Consumer = Provide["consumer"]
+
+    def __init__(self, dispatcher: Optional[Consumer] = None, **kwargs):
+        super().__init__(**kwargs)
+
+        if dispatcher is not None:
+            self.dispatcher = dispatcher
+
+    async def start(self) -> None:
+        """Method to be called at the startup by the internal ``aiomisc`` loigc.
+
+        :return: This method does not return anything.
+        """
+        await self.dispatcher.setup()
+
+        try:
+            self.start_event.set()
+        except RuntimeError:
+            logger.warning("Runtime is not properly setup.")
+
+        await self.dispatcher.dispatch()
+
+    async def stop(self, exception: Exception = None) -> Any:
+        """Stop the service execution.
+
+        :param exception: Optional exception that stopped the execution.
+        :return: This method does not return anything.
+        """
+        await self.dispatcher.destroy()
---
+++ packages/minos-microservice-networks/minos_microservice_networks-0.0.16-py3-none-any.whl/minos/networks/snapshots.py
@@ -0,0 +1,50 @@
+from typing import (
+    Optional,
+)
+
+from aiomisc.service.periodic import (
+    PeriodicService,
+)
+from dependency_injector.wiring import (
+    Provide,
+)
+
+from minos.common import (
+    MinosSnapshot,
+)
+
+
+class SnapshotService(PeriodicService):
+    """Minos Snapshot Service class."""
+
+    snapshot: MinosSnapshot = Provide["snapshot"]
+
+    def __init__(self, snapshot: Optional[MinosSnapshot] = None, interval: float = 60, **kwargs):
+        super().__init__(interval=interval, **kwargs)
+
+        if snapshot is not None:
+            self.snapshot = snapshot
+
+    async def start(self) -> None:
+        """Start the service execution.
+
+        :return: This method does not return anything.
+        """
+        await self.snapshot.setup()
+        await super().start()
+
+    async def callback(self) -> None:
+        """Callback implementation to be executed periodically.
+
+        :return: This method does not return anything.
+        """
+        await self.snapshot.synchronize()
+
+    async def stop(self, err: Exception = None) -> None:
+        """Stop the service execution.
+
+        :param err: Optional exception that stopped the execution.
+        :return: This method does not return anything.
+        """
+        await super().stop(err)
+        await self.snapshot.destroy()
