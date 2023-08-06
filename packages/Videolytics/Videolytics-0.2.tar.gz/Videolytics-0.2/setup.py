75dccf3450d03f0017648d2236096acb9d61a168
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:23:52 +0000

    Add package Videolytics/0.2/Videolytics-0.2.tar.gz

---
+++ packages/Videolytics/Videolytics-0.2.tar.gz/Videolytics-0.2/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+    name='Videolytics',  
+    version='0.2',
+    author="Samran Elahi",
+    author_email="m.samranelahi@gmail.com",
+    description="Efficiently process videos",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Samran-Elahi/Videolytics",
+    project_urls={
+        "Bug Tracker": "https://github.com/Samran-Elahi/Videolytics/issues",
+    },
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+ )
