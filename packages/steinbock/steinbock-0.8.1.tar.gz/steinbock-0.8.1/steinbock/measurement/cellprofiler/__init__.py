6c548cb0f9f9995eb67717cd0346c4c2ea7b1ce2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:41:26 +0000

    Add package steinbock/0.8.1/steinbock-0.8.1.tar.gz

---
+++ packages/steinbock/steinbock-0.8.1.tar.gz/steinbock-0.8.1/steinbock/measurement/cellprofiler/__init__.py
@@ -0,0 +1,6 @@
+from steinbock.measurement.cellprofiler.cellprofiler import (
+    create_and_save_measurement_pipeline,
+    measure_objects,
+)
+
+__all__ = ["create_and_save_measurement_pipeline", "measure_objects"]
