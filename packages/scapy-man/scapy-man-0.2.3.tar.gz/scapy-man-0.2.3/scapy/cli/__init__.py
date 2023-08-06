33da4c7e731ba4b2e90570b26f688d03887496d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:04 +0000

    Add package scapy-man/0.2.3/scapy-man-0.2.3.tar.gz

---
+++ packages/scapy-man/scapy-man-0.2.3.tar.gz/scapy-man-0.2.3/scapy/cli/__init__.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""CLI App module."""
+
+import click
+
+from .delpoy import deploy
+from .download import get
+from .generator import gen
+from .install import install
+from .paths import path
+
+cli = click.Group()
+cli.add_command(deploy)
+cli.add_command(gen)
+cli.add_command(path)
+cli.add_command(install)
+cli.add_command(get)
