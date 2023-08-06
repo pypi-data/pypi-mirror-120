3f830c0469f8c74c240b4a1a633d14828e73b303
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:10:07 +0000

    Add package darmo/0.1.6/darmo-0.1.6.tar.gz

---
+++ packages/darmo/darmo-0.1.6.tar.gz/darmo-0.1.6/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+exec(open('darmo/version.py').read())
+
+setup(
+    name='darmo',
+    version=__version__,
+    author='Chakkrit Termritthikun',
+    author_email='chakkritt60@nu.ac.th',
+    packages=['darmo'],
+    url='https://github.com/jitdee-ai/darmo',
+    description='darts model pre-trained',
+    install_requires=['torch >= 1.0', 'torchvision', 'ofa==0.0.4.post2007200808', 'timm==0.4.12'],
+    python_requires='>=3.6',
+    package_data={
+        'darmo': ['config/*.*'],
+    },
+)
+
