0f63156dab6fb936c27b20bc316b70cd4ac95225
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:10:19 +0000

    Add package datazen/1.7.10/datazen-1.7.10.tar.gz

---
+++ packages/datazen/datazen-1.7.10.tar.gz/datazen-1.7.10/datazen/__init__.py
@@ -0,0 +1,20 @@
+# =====================================
+# generator=datazen
+# version=1.7.9
+# hash=7370e5c6c93e3fd5bdc9211e4fd1c673
+# =====================================
+
+"""
+datazen - Useful defaults and other package metadata.
+"""
+
+PKG_NAME = "datazen"
+VERSION = "1.7.10"
+DESCRIPTION = "Compile and render schema-validated configuration data."
+
+DEFAULT_TYPE = "yaml"
+DEFAULT_MANIFEST = f"manifest.{DEFAULT_TYPE}"
+DEFAULT_DIR = f"{PKG_NAME}-out"
+CACHE_SUFFIX = "_cache"
+GLOBAL_KEY = "global"
+ROOT_NAMESPACE = "__root__"
