e05378227e927d4d8d42b91fb1f2c49978b145fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:41:38 +0000

    Add package focal-loss-torch/0.0.9/focal_loss_torch-0.0.9-py3-none-any.whl

---
+++ packages/focal-loss-torch/focal_loss_torch-0.0.9-py3-none-any.whl/focal_loss/focal_loss.py
@@ -0,0 +1,28 @@
+import torch
+import torch.nn as nn
+import numpy as np
+
+
+class FocalLoss(nn.Module):
+    def __init__(self, alpha=1, gamma=2, reduction: str = 'mean'):
+        super().__init__()
+        if reduction not in ['mean', 'none', 'sum']:
+            raise NotImplementedError('Reduction {} not implemented.'.format(reduction))
+        self.reduction = reduction
+        self.alpha = alpha
+        self.gamma = gamma
+
+    def forward(self, x, target):
+        eps = np.finfo(float).eps
+        p_t = torch.where(target == 1, x, 1-x)
+        fl = - 1 * (1 - p_t) ** self.gamma * torch.log(p_t + eps)
+        fl = torch.where(target == 1, fl * self.alpha, fl * (1 - self.alpha))
+        return self._reduce(fl)
+
+    def _reduce(self, x):
+        if self.reduction == 'mean':
+            return x.mean()
+        elif self.reduction == 'sum':
+            return x.sum()
+        else:
+            return x
