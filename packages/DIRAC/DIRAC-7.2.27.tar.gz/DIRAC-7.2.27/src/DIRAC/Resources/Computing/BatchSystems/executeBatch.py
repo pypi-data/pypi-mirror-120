3085ae11f5ac32423d0e44cb797621800a6956cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:30:32 +0000

    Add package DIRAC/7.2.27/DIRAC-7.2.27.tar.gz

---
+++ packages/DIRAC/DIRAC-7.2.27.tar.gz/DIRAC-7.2.27/src/DIRAC/Resources/Computing/BatchSystems/executeBatch.py
@@ -0,0 +1,48 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+###########################################################
+#
+#  Simple executor script for Batch class methods.
+#
+#  The script is concatenated on the fly with the required
+#  batch system class definition
+#
+#  15.11.2014
+#  Author: A.T.
+#
+###########################################################
+
+executeBatchContent = """
+if __name__ == "__main__":
+
+  import sys
+  import json
+  try:
+    from six.moves.urllib.parse import quote as urlquote
+    from six.moves.urllib.parse import unquote as urlunquote
+  except ImportError:
+    try:
+      from urllib import unquote as urlunquote
+      from urllib import quote as urlquote
+    except ImportError:
+      from urllib.parse import quote as urlquote
+      from urllib.parse import unquote as urlunquote
+
+
+  arguments = sys.argv[1]
+  inputDict = json.loads(urlunquote(arguments))
+
+  method = inputDict.pop('Method')
+  batchSystem = inputDict.pop('BatchSystem')
+  batch = locals()[batchSystem]()
+
+  try:
+    result = getattr(batch, method)(**inputDict)
+  except Exception as x:
+    result = 'Exception: %s' % str(x)
+
+  resultJson = urlquote(json.dumps(result))
+  print("============= Start output ===============")
+  print(resultJson)
+"""
