84ed37b0511f93f2a2eb769c2d9b372f1e08a715
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:27 +0000

    Add package cubedtube/0.2.0.dev2/cubedtube-0.2.0.dev2.tar.gz

---
+++ packages/cubedtube/cubedtube-0.2.0.dev2.tar.gz/cubedtube-0.2.0.dev2/setup.py
@@ -0,0 +1,52 @@
+import os.path
+import setuptools
+import json
+
+DIR = os.path.dirname(__file__)
+print('working from ', DIR)
+with open(os.path.join(DIR, "README.md"), "r", encoding="utf-8") as fp:
+    long_description = fp.read()
+
+with open(os.path.join(DIR, 'versions.json')) as fp:
+    versions = json.load(fp)
+VERSION = versions['current']
+
+
+setuptools.setup(
+    name="cubedtube", # Replace with your own username
+    version=VERSION,
+    author="stevarino",
+    author_email="stevarino@hermit.tube",
+    description="A video viewing webapp that organizes videos into channels and series",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/stevarino/cubed-tube",
+    project_urls={
+        "Bug Tracker": "https://github.com/stevarino/cubed-tube/issues",
+    },
+    packages=setuptools.find_packages(),
+    package_data={
+        'cubed_tube.frontend.templates': ['*', '**/*'],
+        'cubed_tube.actions': ['*.yaml']
+    },
+    install_requires=[
+        'Authlib<=0.15.4',  # Authlib 1.0a has signature changes
+        'awscli>=1.19.97',
+        'boto3>=1.17.97',
+        'Flask>=1.1.2',
+        'gunicorn>=20.0.0',
+        'Jinja2>=3.0',
+        'peewee>=3.13.3',
+        'prometheus-client>=0.11.0',
+        'pymemcache>=3.5.0',
+        'PyYAML>=5.3.1',
+        'requests>=2.0',
+        'jsonpath-ng==1.5.3',
+    ],
+    classifiers=[
+    ],
+    entry_points = {
+        'console_scripts': ['cubedtube=cubed_tube.main:main'],
+    },
+    python_requires='>=3.6',
+)
