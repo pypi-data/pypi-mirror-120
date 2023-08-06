f8c25ac0cb8aa694575d1712d9783171354950d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:30:11 +0000

    Add package AstroCabTools/1.3.0/AstroCabTools-1.3.0.tar.gz

---
+++ packages/AstroCabTools/AstroCabTools-1.3.0.tar.gz/AstroCabTools-1.3.0/astrocabtools/mrs_subviz/src/utils/constants.py
@@ -0,0 +1,6 @@
+
+__all__ = ["SUBBANDDICT", "INDEXDICT", "COLORDICT"]
+
+SUBBANDDICT = {0:"1 Short",1:"1 Medium",2:"1 Long",3:"2 Short",4:"2 Medium",5:"2 Long",6:"3 Short",7:"3 Medium",8:"3 Long",9:"4 Short",10:"4 Medium",11:"4 Long"}
+INDEXDICT = {"1S":0,"1M":1,"1L":2,"2S":3,"2M":4, "2L":5,"3S":6,"3M":7,"3L":8,"4S":9,"4M":10,"4L":11}
+COLORDICT = {0:("1S","#e5be1b"),1:("1M", "#e4e51b"),2:("1L", "#9ce51b"),3:("2S", "#5fe51b"),4:("2M", "#23872a"),5:("2L", "#21d69b"),6:("3S", "#10bcee"),7:("3M", "#109aee"),8:("3L", "#1057ee"),9:("4S", "#9310ee"),10:("4M", "#ee10b8"),11:("4L", "#ee1010")}
