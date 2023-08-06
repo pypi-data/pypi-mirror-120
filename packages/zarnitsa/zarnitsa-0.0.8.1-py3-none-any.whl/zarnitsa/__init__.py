803c0437644e897f60ccb8aead145e6f93271f41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:48 +0000

    Add package zarnitsa/0.0.8.1/zarnitsa-0.0.8.1-py3-none-any.whl

---
+++ packages/zarnitsa/zarnitsa-0.0.8.1-py3-none-any.whl/zarnitsa/__init__.py
@@ -0,0 +1,6 @@
+from .DataAugmenter import AbstractDataAugmenter
+
+from .stats.DataAugmenterInternally import DataAugmenterInternally
+from .stats.DataAugmenterExternally import DataAugmenterExternally
+from .nlp.DataAugmenterNLP import DataAugmenterNLP
+from .series.DataAugmenterTimeSeries import DataAugmenterTimeSeries
