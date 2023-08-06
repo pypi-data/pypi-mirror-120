d37f1544c258be4394270bea2100bcee44ce0548
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:29:16 +0000

    Add package wiktextract/1.99.6/wiktextract-1.99.6-py3-none-any.whl

---
+++ packages/wiktextract/wiktextract-1.99.6-py3-none-any.whl/wiktextract/__init__.py
@@ -0,0 +1,22 @@
+# This file defines the public exports from the wiktextract module.
+#
+# Copyright (c) 2018-2021 Tatu Ylonen.  See LICENSE and https://ylonen.org
+
+from .wiktionary import (parse_wiktionary, reprocess_wiktionary,
+                         extract_namespace)
+from .config import WiktionaryConfig
+from .page import clean_value, parse_page
+from .parts_of_speech import PARTS_OF_SPEECH
+from .thesaurus import extract_thesaurus_data
+from .categories import extract_categories
+
+__all__ = (
+    "WiktionaryConfig",
+    "parse_wiktionary",
+    "reprocess_wiktionary",
+    "PARTS_OF_SPEECH",
+    "parse_page",
+    "extract_thesaurus_data",
+    "extract_namespace",
+    "extract_categories",
+)
