4cc067c7c982e98fe04b1e54d3865968ed563d11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:40:43 +0000

    Add package datacorecommon/0.0.3/datacorecommon-0.0.3.tar.gz

---
+++ packages/datacorecommon/datacorecommon-0.0.3.tar.gz/datacorecommon-0.0.3/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+version = '0.0.3'
+
+setup(
+    name='datacorecommon',
+    packages=find_packages(),
+    version=version,
+    license='MIT',
+    author='Data core team',
+    description='Wrapper functions for PySpark',
+    keywords=['pyspark', 'databricks'],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.8',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Development Status :: 3 - Alpha',
+        'Topic :: Software Development',
+    ],
+    python_requires='>=3.6',
+    install_requires=['delta'],
+    zip_safe=False
+)
