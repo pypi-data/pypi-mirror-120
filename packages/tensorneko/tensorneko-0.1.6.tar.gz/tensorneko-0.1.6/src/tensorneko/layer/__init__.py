3d6bc7b354b116928464a411c6d99c5889eadebb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:15 +0000

    Add package tensorneko/0.1.6/tensorneko-0.1.6.tar.gz

---
+++ packages/tensorneko/tensorneko-0.1.6.tar.gz/tensorneko-0.1.6/src/tensorneko/layer/__init__.py
@@ -0,0 +1,25 @@
+# import from other libraries
+from torch.nn import MultiheadAttention as _Attention
+
+Attention = _Attention
+
+# import from modules
+from .concatenate import Concatenate
+from .conv2d import Conv2d
+from .linear import Linear
+from .log import Log
+from .patching import Patching, PatchEmbedding2d
+from .positional_embedding import PositionalEmbedding
+from .reshape import Reshape
+
+__all__ = [
+    "Attention",
+    "Concatenate",
+    "Conv2d",
+    "Linear",
+    "Log",
+    "Patching",
+    "PatchEmbedding2d",
+    "PositionalEmbedding",
+    "Reshape",
+]
