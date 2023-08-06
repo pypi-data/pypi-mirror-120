ca3faffb8d9c65e02e35ca7c851e8671f654d115
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:41:37 +0000

    Add package minorimpact/0.0.6/minorimpact-0.0.6.tar.gz

---
+++ packages/minorimpact/minorimpact-0.0.6.tar.gz/minorimpact-0.0.6/README.md
@@ -0,0 +1,29 @@
+# python-minorimpact
+
+
+
+## Build
+
+    $ python setup.py sdist
+
+
+### PyPi Upload
+
+Old method:
+
+    $ python setup.py sdist upload
+
+New method:
+
+    $ twine upload dist/*
+
+## Installation
+
+### Testing
+
+    $ pip3 install dist/*
+
+### Generally
+
+    $ pip install minorimpact
+
---
+++ packages/minorimpact/minorimpact-0.0.6.tar.gz/minorimpact-0.0.6/setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+import minorimpact
+from setuptools import find_packages, setup
+
+setup(
+    name='minorimpact',
+    packages=find_packages(include=['minorimpact']),
+    version=minorimpact.__version__,
+    description='Personal utility library',
+    author='Patrick Gillan',
+    author_email = 'pgillan@minorimpact.com',
+    license='GPLv3',
+    install_requires=[],
+    setup_requires=[],
+    tests_require=[],
+)
