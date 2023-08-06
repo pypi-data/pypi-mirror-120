f5d13496d160890a197d4f7e09fe65f10dd0967d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:29:42 +0000

    Add package commodutil/1.1.1/commodutil-1.1.1.tar.gz

---
+++ packages/commodutil/commodutil-1.1.1.tar.gz/commodutil-1.1.1/commodutil/dates.py
@@ -0,0 +1,32 @@
+import re
+import datetime 
+
+curmon = datetime.datetime.now().month
+curyear = datetime.datetime.now().year
+curmonyear = datetime.datetime(curyear, curmon, 1)
+curmonyear_str = '%s-%s' % (curyear, curmon) # get pandas time filtering
+
+nextyear = curyear + 1
+
+
+def find_year(df, use_delta=False):
+    """
+    Given a dataframe find the years in the column headings. Return a dict of colname to year
+    eg { 'Q1 2016' : 2016, 'Q1 2017' : 2017
+    """
+    res = {}
+    for colname in df:
+        colregex = re.findall('\d\d\d\d', str(colname))
+        colyear = None
+        if len(colregex) >= 1:
+            colyear = int(colregex[0])
+
+        if colyear:
+            res[colname] = colyear
+            if colyear and use_delta:
+                delta = colyear - curyear
+                res[colname] = delta
+        else:
+            res[colname] = colname
+
+    return res
