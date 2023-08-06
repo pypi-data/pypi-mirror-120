9bd5c188718ad666be192de6effbfed89e40e0b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:40:46 +0000

    Add package drawline/0.0.1/drawline-0.0.1.tar.gz

---
+++ packages/drawline/drawline-0.0.1.tar.gz/drawline-0.0.1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+with open("readme.md", "r") as fh:
+    long_description = fh.read()
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+
+setup(
+    name='drawline',
+    version='0.0.1',
+    packages=['drawline'],
+    url='https://github.com/bendangnuksung/drawline',
+    license='MIT License',
+    author='bendangnuksung',
+    author_email='bendangnuksungimsong@gmail.com',
+    description='Clean draw of  rectangle and polygon contours',
+    install_requires=required,
+    classifiers=[
+            "Programming Language :: Python :: 3",
+            "Operating System :: OS Independent",
+        ],
+
+)
