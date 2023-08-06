9ec867a80dba593a93001e9d1f795e6aa1906221
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:38 +0000

    Add package initcommerce-utils/0.2.1/initcommerce_utils-0.2.1-py3-none-any.whl

---
+++ packages/initcommerce-utils/initcommerce_utils-0.2.1-py3-none-any.whl/initcommerce/common/settings.py
@@ -0,0 +1,8 @@
+from pydantic import BaseSettings as PydanticSettings
+
+
+class BaseSettings(PydanticSettings):
+    class Config:
+        env_file = ".env"
+        env_file_encoding = "utf-8"
+        case_sensitive = True
