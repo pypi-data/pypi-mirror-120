63ca1172064d44ec0d223f553bb17d92eb690070
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:40:04 +0000

    Add package sardana-nxsrecorder/3.7.1/sardana-nxsrecorder-3.7.1.tar.gz

---
+++ packages/sardana-nxsrecorder/sardana-nxsrecorder-3.7.1.tar.gz/sardana-nxsrecorder-3.7.1/.ci/run.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+if [[ $1 == "2" ]]; then
+    echo "run python-sardana-nxsrecorder"
+    docker exec  ndts python test
+else
+    echo "run python3-sardana-nxsrecorder"
+    docker exec  ndts python3 test
+fi
+if [ "$?" -ne "0" ]
+then
+    exit -1
+fi
