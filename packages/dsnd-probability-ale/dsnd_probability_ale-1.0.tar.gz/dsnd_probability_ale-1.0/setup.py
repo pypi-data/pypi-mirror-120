0da87b892ac56a873ee6aef4dd0f4bdc933b1450
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:28:32 +0000

    Add package dsnd-probability-ale/1.0/dsnd_probability_ale-1.0.tar.gz

---
+++ packages/dsnd-probability-ale/dsnd_probability_ale-1.0.tar.gz/dsnd_probability_ale-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='dsnd_probability_ale',
+      version='1.0',
+      description='Gaussian and Binomialdistributions',
+      packages=['distributions'],
+      author = 'Alejandro Rios',
+      author_email = 'aarc.88@gmail.com',
+      zip_safe=False)
