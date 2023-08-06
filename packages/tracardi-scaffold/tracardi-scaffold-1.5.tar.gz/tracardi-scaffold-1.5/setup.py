9cb89b34f5548310f7d97d3bfbe1916e382b31d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:11:11 +0000

    Add package tracardi-scaffold/1.5/tracardi-scaffold-1.5.tar.gz

---
+++ packages/tracardi-scaffold/tracardi-scaffold-1.5.tar.gz/tracardi-scaffold-1.5/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+setuptools.setup(
+    name='tracardi-scaffold',
+    version='1.5',
+    # scripts=['./cmd/tracardi-scaffold'],
+    entry_points={
+        'console_scripts': ['mybinary=tracardi_plugin_scaffold.create_plugin:cli'],
+    },
+    author='Me',
+    description='This runs my tracardi plugin scaffold.',
+    packages=['tracardi_plugin_scaffold'],
+    install_requires=[
+        'setuptools',
+        'jinja2'
+    ],
+    python_requires='>=3.5',
+    include_package_data=True
+)
