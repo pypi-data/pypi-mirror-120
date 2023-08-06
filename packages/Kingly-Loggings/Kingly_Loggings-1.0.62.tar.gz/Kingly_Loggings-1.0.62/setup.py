ffe3bdcef55d309dbf29b302cccb9f2a499e55dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:43:13 +0000

    Add package Kingly-Loggings/1.0.62/Kingly_Loggings-1.0.62.tar.gz

---
+++ packages/Kingly-Loggings/Kingly_Loggings-1.0.62.tar.gz/Kingly_Loggings-1.0.62/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Kingly_Loggings',
+    version='1.0.62',
+    author='CHK141',
+    author_email='chk141@139.com',
+    # description='自用Logger类,   仅支持Python3',
+    long_description=open('README.md', 'r').read(),
+    long_description_content_type='text/markdown',
+    # packages=['Loggings'],
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    python_requires=">=3",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'loguru', 'pathlib'
+    ],
+)
+
