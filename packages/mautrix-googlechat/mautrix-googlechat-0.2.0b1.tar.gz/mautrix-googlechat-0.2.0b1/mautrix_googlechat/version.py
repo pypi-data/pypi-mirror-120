224919221af7077975c5a01e1ee38bdebcee8d84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:29:26 +0000

    Add package mautrix-googlechat/0.2.0b1/mautrix-googlechat-0.2.0b1.tar.gz

---
+++ packages/mautrix-googlechat/mautrix-googlechat-0.2.0b1.tar.gz/mautrix-googlechat-0.2.0b1/mautrix_googlechat/version.py
@@ -0,0 +1,6 @@
+# Generated in setup.py
+
+git_tag = 'v0.2.0b1'
+git_revision = '82b2832a'
+version = '0.2.0b1'
+linkified_version = '[0.2.0b1](https://github.com/mautrix/googlechat/releases/tag/v0.2.0b1)'
