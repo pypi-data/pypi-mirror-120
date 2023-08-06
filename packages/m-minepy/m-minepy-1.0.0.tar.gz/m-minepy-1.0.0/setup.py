a9dad2325a1399dce20cc04b9a7bdfc40f004d14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:23:46 +0000

    Add package m-minepy/1.0.0/m-minepy-1.0.0.tar.gz

---
+++ packages/m-minepy/m-minepy-1.0.0.tar.gz/m-minepy-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+   name='m-minepy',
+   version='1.0.0',
+   description='A useful module',
+   author='Man minepy',
+   author_email='minepymail@minepy.com',
+   packages=['minepy'],  #same as name
+)
