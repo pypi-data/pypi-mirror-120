de642761fd2c4a8e5cd681c3ab9fdf14744cb03b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:16 +0000

    Add package dhparser/0.9.8/DHParser-0.9.8.tar.gz

---
+++ packages/dhparser/DHParser-0.9.8.tar.gz/DHParser-0.9.8/tests/run.py
@@ -0,0 +1,142 @@
+#!/usr/bin/env python3
+
+"""Runs the dhparser test-suite with several installed interpreters"""
+
+import concurrent.futures
+import doctest
+import os
+import subprocess
+import sys
+import time
+import threading
+
+scriptdir = os.path.dirname(os.path.realpath(__file__))
+sys.path.append(os.path.join(scriptdir, '../'))
+
+from DHParser.configuration import get_config_value
+from DHParser.toolkit import instantiate_executor
+
+
+lock = None  # threading.Lock() initialized in __main__
+
+
+def run_cmd(parameters: list):
+    try:
+        subprocess.run(parameters)
+        return True
+    except FileNotFoundError:
+        return False
+
+
+def run_doctests(module):
+    with lock:
+        namespace = {}
+        print('DOCTEST ' + module)
+        exec('import DHParser.' + module, namespace)
+        mod = getattr(namespace['DHParser'], module)
+        result = doctest.testmod(mod)
+        return result.failed
+
+
+def run_unittests(command):
+    # print('>>>>>>>> ', command)
+    args = command.split(' ')
+    filename = args[1]
+    print('\nUNITTEST ' + args[0] + ' ' + filename)
+    subprocess.run(args)
+    print('COMPLETED ' + args[0] + ' ' + filename + '\n')
+
+
+if __name__ == "__main__":
+    lock = threading.Lock()
+    found = []
+    if run_cmd(['pypy3', '-V']):
+        found.append('pypy3 ')
+    elif run_cmd(['pypy36', '-V']):
+        found.append('pypy36 ')
+    elif run_cmd(['pypy', '-V']):
+        found.append('pypy ')
+    if run_cmd(['python', '-V']):
+        output = subprocess.run(['python', '-V'], capture_output=True).stdout
+        if output.find(b'Python 3') >= 0:
+            found.append('python ')
+        elif run_cmd(['python3', '-V']):
+            found.append('python3')
+    elif run_cmd(['python3', '-V']):
+        found.append('python3')
+    # if run_cmd(['python3.5', '-V']):
+    #     found.append('python3.5 ')
+    # elif run_cmd(['~/.local/bin/python3.5', '-V']):
+    #     found.append('~/.local/bin/python3.5 ')
+    if run_cmd(['python3.6', '-V']):
+        found.append('python3.6 ')
+    elif run_cmd(['~/.local/bin/python3.6', '-V']):
+        found.append('~/.local/bin/python3.6 ')
+    if run_cmd(['python3.7', '-V']):
+        found.append('python3.7 ')
+    elif run_cmd(['~/.local/bin/python3.7', '-V']):
+        found.append('~/.local/bin/python3.7 ')
+    if run_cmd(['python3.9', '-V']):
+        found.append('python3.8 ')
+    elif run_cmd(['~/.local/bin/python3.8', '-V']):
+        found.append('~/.local/bin/python3.8 ')
+    elif run_cmd(['~/.local/bin/python3.9', '-V']):
+        found.append('~/.local/bin/python3.9 ')
+    elif run_cmd(['~/.local/bin/python3.10', '-V']):
+        found.append('~/.local/bin/python3.10 ')        
+    print('Interpreters found: ' + ''.join(found))
+
+    arguments = [arg for arg in sys.argv[1:] if arg[:1] != '-']
+
+    if len(arguments) > 1:
+        # use interpreters from command line
+        interpreters = []
+        for interpreter in arguments:
+            interpreter = interpreter.strip() + ' '
+            if interpreter not in found:
+                print('Interpreter ' + arguments[1] + ' not found.')
+                sys.exit(1)
+            else:
+                interpreters.append(interpreter)
+    else:
+        interpreters = found
+
+    cwd = os.getcwd()
+    os.chdir(os.path.join(scriptdir, '..'))
+
+    timestamp = time.time()
+
+    run_doctests('toolkit')
+
+    with instantiate_executor(get_config_value('test_parallelization'),
+                              concurrent.futures.ProcessPoolExecutor) as pool:
+        results = []
+
+        # doctests
+        for filename in os.listdir('DHParser'):
+            if filename.endswith('.py') and filename not in \
+                    ("foreign_typing.py", "shadow_cython.py", "versionnumber.py",
+                     "__init__.py"):
+                results.append(pool.submit(run_doctests, filename[:-3]))
+
+        # unit tests
+        for interpreter in interpreters:
+            if run_cmd([interpreter.strip(), '--version']):
+                for filename in os.listdir('tests'):
+                    if filename.endswith('.py') and (filename.startswith('test_') or
+                                                     filename.startswith('notest')):
+                        command = interpreter + os.path.join('tests', filename)
+                        results.append(pool.submit(run_unittests, command))
+
+        done, not_done = concurrent.futures.wait(results, timeout=120)
+        assert not not_done, str(not_done)
+
+    elapsed = time.time() - timestamp
+    print('\n Test-Duration: %.2f seconds' % elapsed)
+
+    print('\nPlease note, the following phenomena are not bugs:')
+    print('  1. Some doctests may fail on Windows, due to different file-separators.')
+    print('  2. Some tests end with OSError("handle already closed") on pypy3.6, 3.7. '
+          'This seems to be a python < 3.9 bug. See: pypy3 scratch/process_pool_doc_examples.py')
+
+    os.chdir(cwd)
---
+++ packages/dhparser/DHParser-0.9.8.tar.gz/DHParser-0.9.8/tests/test_dhparser.py
@@ -0,0 +1,117 @@
+#!/usr/bin/env python3
+
+"""test_dhparser.py - tests of the dhparser.py command line tool
+
+
+Author: Eckhart Arnold <arnold@badw.de>
+
+Copyright 2017 Bavarian Academy of Sciences and Humanities
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+import os
+import platform
+import random
+import shutil
+import subprocess
+import sys
+import time
+
+scriptpath = os.path.dirname(__file__) or '.'
+sys.path.append(os.path.abspath(os.path.join(scriptpath, '..')))
+LOG_DIR = os.path.abspath(os.path.join(scriptpath, "LOGS"))
+
+from DHParser.testing import unique_name
+
+
+def system(s: str) -> int:
+    # return os.system(s)
+    return subprocess.call(s, shell=True)
+
+
+class TestDHParser:
+    def test_selftest(self):
+        if os.getcwd().replace('\\', '/').rstrip('/').endswith('/DHParser'):
+            sys.path.append('./DHParser/scripts/')
+        else:
+            sys.path.append('../DHParser/scripts/')
+        import dhparser
+        assert dhparser.selftest(silent=True)
+
+
+# TODO: make this code multiprocessing safe!
+class TestDHParserCommandLineTool:
+    def setup(self):
+        self.cwd = os.getcwd()
+        os.chdir(scriptpath)
+        # avoid race-condition
+        counter = 10
+        while counter > 0:
+            try:
+                self.dirname = unique_name('test_dhparser_data')
+                os.mkdir(self.dirname)
+                counter = 0
+            except FileExistsError:
+                self.dirname = ''
+                time.sleep(random.random())
+                counter -= 1
+        assert self.dirname
+        self.nulldevice = " >/dev/null" if platform.system() != "Windows" else " > NUL"
+        self.python = sys.executable + ' '
+
+    def teardown(self):
+        # return
+        name = self.dirname
+        if os.path.exists(name + '/%sServer.py' % name):
+            system(self.python + name + '/%sServer.py --stopserver' % name + self.nulldevice)
+        if os.path.exists(name) and os.path.isdir(name):
+            shutil.rmtree(name)
+        if os.path.exists(name) and not os.listdir(name):
+            os.rmdir(name)
+        cfg_name = '%sServer.py.cfg' % name
+        if os.path.exists(cfg_name):
+            os.remove(cfg_name)
+        os.chdir(self.cwd)
+        if os.path.exists(LOG_DIR) and os.path.isdir(LOG_DIR):
+            for fname in os.listdir(LOG_DIR):
+                os.remove(os.path.join(LOG_DIR, fname))
+            os.rmdir(LOG_DIR)
+        # if os.path.exists('out') and os.path.isdir('out'):
+        #     os.rmdir('out')
+
+    def test_dhparser(self):
+        name = self.dirname
+        # test compiler creation and execution
+        system(self.python + '../DHParser/scripts/dhparser.py ' + name + self.nulldevice)
+        system(self.python + name + '/tst_%s_grammar.py --singlethread ' % name + self.nulldevice)
+        system(self.python + name + '/%sParser.py ' % name + name + '/example.dsl >' + name + '/example.xml')
+        with open(name + '/example.xml', 'r', encoding='utf-8') as f:
+            xml = f.read()
+        assert xml.find('document') >= 0, xml
+        os.remove(name + '/%sParser.py' % name)
+        os.remove(name + '/example.xml')
+        # test server
+        system(self.python + name + '/%sServer.py --stopserver' % name + self.nulldevice)
+        system(self.python + name + '/%sServer.py ' % name + name + '/example.dsl >' + name + '/example.xml')
+        with open(name + '/example.xml', 'r', encoding='utf-8') as f:
+            json = f.read()
+        assert json.find('document') >= 0, name + ' ' + str(len(json))
+        system(self.python + name + '/%sServer.py ' % name + name + '/example.dsl ' + self.nulldevice)
+        system(self.python + name + '/%sServer.py ' % name + name + '/example.dsl ' + self.nulldevice)
+        system(self.python + name + '/%sServer.py --stopserver' % name+ self.nulldevice)
+
+
+if __name__ == "__main__":
+    from DHParser.testing import runner
+    runner("", globals())
