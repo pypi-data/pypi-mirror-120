7429b5620f3e2da3d0976637770db3e747fd8801
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:30:02 +0000

    Add package pynixutil/0.4.0/pynixutil-0.4.0.tar.gz

---
+++ packages/pynixutil/pynixutil-0.4.0.tar.gz/pynixutil-0.4.0/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['pynixutil']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'pynixutil',
+    'version': '0.4.0',
+    'description': 'Utility functions for working with Nix in Python',
+    'long_description': None,
+    'author': 'adisbladis',
+    'author_email': 'adam.hose@tweag.io',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/tweag/pynixutil',
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
