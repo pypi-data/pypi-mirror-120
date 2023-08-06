8a7cc1a909b2c2fcc70fb5fb7e12e8535d49a055
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:02:25 +0000

    Add package cartorio/1.1.2/cartorio-1.1.2.tar.gz

---
+++ packages/cartorio/cartorio-1.1.2.tar.gz/cartorio-1.1.2/setup.py
@@ -0,0 +1,34 @@
+import pathlib
+from setuptools import setup
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# This call to setup() does all the work
+setup(
+    name="cartorio",
+    version="1.1.2",
+    description="Cartorio: A library for logging",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/hsteinshiromoto/cartorio",
+    author="Humberto STEIN SHIROMOTO",
+    author_email="h.stein.shiromoto@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.9",
+    ],
+    packages=["cartorio"],
+    include_package_data=True,
+    install_requires=["typing"],
+    entry_points={
+        "console_scripts": [
+            "cartorio=cartorio.__main__:main",
+        ]
+    },
+)
