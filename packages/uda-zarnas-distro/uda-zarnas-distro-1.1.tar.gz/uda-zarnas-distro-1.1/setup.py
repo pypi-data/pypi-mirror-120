2d2f4afd7df13a10737a33de2dcb83a34ef61d55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:29:04 +0000

    Add package uda-zarnas-distro/1.1/uda-zarnas-distro-1.1.tar.gz

---
+++ packages/uda-zarnas-distro/uda-zarnas-distro-1.1.tar.gz/uda-zarnas-distro-1.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='uda-zarnas-distro',
+      version='1.1',
+      description='Gaussian & Binomial distributions',
+      packages=['uda-zarnas-distro'],
+      author = 'Zarana Nakrani',
+      author_email = 'zarnasnakrani17@gmail.com',
+      zip_safe=False)
---
+++ packages/uda-zarnas-distro/uda-zarnas-distro-1.1.tar.gz/uda-zarnas-distro-1.1/uda-zarnas-distro/__init__.py
@@ -0,0 +1,4 @@
+from distributions.Gaussiandistribution import Gaussian
+from distributions.Binomialdistribution import Binomial
+
+# TODO: import the Binomial class from the Binomialdistribution module
