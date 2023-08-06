f4b0edb14fdb4d3a2c7fdfc61ce33c1dbd96897c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:31:19 +0000

    Add package random-profile/0.0.8/random_profile-0.0.8.tar.gz

---
+++ packages/random-profile/random_profile-0.0.8.tar.gz/random_profile-0.0.8/setup.py
@@ -0,0 +1,37 @@
+import setuptools
+ 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+ 
+setuptools.setup(
+    #Here is the module name.
+    name="random_profile",
+ 
+    #version of the module
+    version="0.0.8",
+ 
+    #Name of Author
+    author="CodePerfectPlus",
+ 
+    #your Email address
+    author_email="deepak008@live.com",
+ 
+    #Small Description about module
+    description="Generate Random Profile",
+ 
+    long_description=long_description,
+ 
+    #Specifying that we are using markdown file for description
+    long_description_content_type="text/markdown",
+ 
+    #Any link to reach this module, if you have any webpage or github profile
+    url="https://github.com/codePerfectPlus/Random-Profile-Generator",
+    packages=setuptools.find_packages(),
+ 
+    #classifiers like program is suitable for python3, just leave as it is.
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
