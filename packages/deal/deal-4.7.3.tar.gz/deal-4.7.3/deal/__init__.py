6c55c62c02d5a2bc73835f78d45bf4bba7925f4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:27 +0000

    Add package deal/4.7.3/deal-4.7.3.tar.gz

---
+++ packages/deal/deal-4.7.3.tar.gz/deal-4.7.3/deal/__init__.py
@@ -0,0 +1,68 @@
+"""
+**Deal** is a Python library for [design by contract][wiki] (DbC) programming.
+See [documentation][docs] for more details.
+
+[wiki]: https://en.wikipedia.org/wiki/Design_by_contract
+[docs]: https://deal.readthedocs.io/index.html
+"""
+
+# main package info
+__title__ = 'deal'
+__version__ = '4.7.3'
+__author__ = 'Gram Orsinium'
+__license__ = 'MIT'
+
+
+from ._aliases import (
+    chain, ensure, has, implies, inv, post, pre, pure, raises, reason, safe,
+)
+from ._exceptions import (
+    ContractError, InvContractError, MarkerError, OfflineContractError, PostContractError,
+    PreContractError, RaisesContractError, ReasonContractError, SilentContractError,
+)
+from ._imports import activate, module_load
+from ._schemes import Scheme
+from ._state import disable, enable, reset
+from ._testing import TestCase, cases
+
+
+__all__ = [
+    'cases',
+    'Scheme',
+    'TestCase',
+
+    # state
+    'disable',
+    'enable',
+    'reset',
+
+    # decorators
+    'chain',
+    'ensure',
+    'has',
+    'inv',
+    'post',
+    'pre',
+    'raises',
+    'reason',
+
+    # aliases
+    'safe',
+    'pure',
+    'implies',
+
+    # module level
+    'module_load',
+    'activate',
+
+    # exceptions
+    'ContractError',
+    'InvContractError',
+    'MarkerError',
+    'OfflineContractError',
+    'PostContractError',
+    'PreContractError',
+    'RaisesContractError',
+    'ReasonContractError',
+    'SilentContractError',
+]
---
+++ packages/deal/deal-4.7.3.tar.gz/deal-4.7.3/docs/details/api.md
@@ -0,0 +1,49 @@
+# API
+
+## Values
+
+```eval_rst
+.. autofunction:: deal.pre
+.. autofunction:: deal.post
+.. autofunction:: deal.ensure
+.. autofunction:: deal.inv
+```
+
+## Side-effects and exceptions
+
+```eval_rst
+.. autofunction:: deal.has
+.. autofunction:: deal.raises
+.. autofunction:: deal.reason
+```
+
+## Helpers
+
+```eval_rst
+.. autofunction:: deal.chain
+.. autofunction:: deal.pure
+.. autofunction:: deal.safe
+.. autofunction:: deal.implies
+```
+
+## Testing
+
+Keep in mind that sphinx [skipped some of the docstrings](https://github.com/sphinx-doc/sphinx/issues/7787) for `deal.cases`.
+
+```eval_rst
+.. autoclass:: deal.cases
+    :members:
+    :special-members:
+
+.. autoclass:: deal.TestCase
+    :members:
+```
+
+## Other
+
+```eval_rst
+.. autofunction:: deal.module_load
+.. autofunction:: deal.disable
+.. autofunction:: deal.enable
+.. autofunction:: deal.reset
+```
