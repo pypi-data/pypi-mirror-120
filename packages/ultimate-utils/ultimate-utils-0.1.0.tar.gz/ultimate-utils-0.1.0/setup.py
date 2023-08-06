b9805646527e29c8b29995e6f77c183234b51bb7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:28 +0000

    Add package ultimate-utils/0.1.0/ultimate-utils-0.1.0.tar.gz

---
+++ packages/ultimate-utils/ultimate-utils-0.1.0.tar.gz/ultimate-utils-0.1.0/setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup
+from setuptools import find_packages
+
+import pathlib
+
+# The directory containing this file
+# HERE = pathlib.Path(__file__).parent
+HERE = pathlib.Path('~/ultimate-utils/').expanduser()
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+setup(
+    name='ultimate-utils',  # project name
+    version='0.1.0',
+    description='Brandos ultimate utils for science, machine learning and AI',
+    long_description=README,
+    long_description_content_type="text/markdown",
+    #url
+    author='Brando Miranda',
+    author_email='brandojazz@gmail.com',
+    license='MIT',
+    packages=find_packages(),  # imports all modules (folder with __init__.py) & python files in this folder (since defualt args are . and empty exculde i.e. () )
+    install_requires=['dill',
+                      'networkx>=2.5',
+                      'scipy',
+                      'scikit-learn',
+                      'lark-parser',
+                      'torchtext',
+                      'tensorboard',
+                      'pandas',
+                      'progressbar2',
+                      'transformers',
+                      'requests',
+                      'aiohttp',
+                      'numpy',
+                      'plotly',
+                      'wandb',
+                      'matplotlib',
+
+                      # 'pygraphviz'  # removing because it requires user to install graphviz and gives other issues, e.g. if the user does not want to do graph stuff then uutils shouldn't need to force the user to install uutils
+                      ]
+)
---
+++ packages/ultimate-utils/ultimate-utils-0.1.0.tar.gz/ultimate-utils-0.1.0/uutils/torch_uu/tensorboard.py
@@ -0,0 +1,106 @@
+# from torch_uu.utils.tensorboard import SummaryWriter  # https://deeplizard.com/learn/video/psexxmdrufm
+from pathlib import Path
+
+def log_2_tb(tb, args, it, tag1: str, loss: float, tag2: str, acc: float):
+    # tb = SummaryWriter(log_dir=args.current_logs_path)  # uncomment for documentation to work
+    # tag1 = tag1.replace(' ', '_')
+    # tag2 = tag2.replace(' ', '_')
+    tb.add_scalar(tag1, float(loss), it)
+    tb.add_scalar(tag2, float(acc), it)
+
+def log_2_tb_supervisedlearning(tb, args, it, loss, acc, split):
+    """
+    :param tb:
+    :param acc:
+    :param acc_err:
+    :param loss:
+    :param it:
+    :param args:
+    :param split: train, val, test
+    :return:
+    """
+    if not hasattr(args, 'target_type'):
+        tag1 = f'{split}_loss'
+        tag2 = f'{split}_accuracy_R2'
+    elif args.target_type == 'regression':
+        tag1 = f'{split}_loss'
+        tag2 = f'{split}_R2'
+    elif args.target_type == 'classification':
+        tag1 = f'{split}_loss'
+        tag2 = f'{split}_accuracy'
+    else:
+        raise ValueError(f'Error: args.target_type = {args.target_type} not valid.')
+    # tb = SummaryWriter(log_dir=args.current_logs_path)  # uncomment for documentation to work
+    tb.add_scalar(tag1, loss, it)
+    tb.add_scalar(tag2, acc, it)
+
+def log_2_tb_metalearning(tb, args, it, loss, acc, split):
+    """
+    :param tb:
+    :param acc:
+    :param loss:
+    :param it:
+    :param args:
+    :param split: train, val, test
+    :return:
+    """
+    if not hasattr(args, 'target_type'):
+        tag1 = f'{split}_loss'
+        tag2 = f'{split}_accuracy_R2'
+    elif args.target_type == 'regression':
+        tag1 = f'meta-{split}_loss'
+        tag2 = f'meta-{split}_R2'
+    elif args.target_type == 'classification':
+        tag1 = f'meta-{split}_loss'
+        tag2 = f'meta-{split}_accuracy'
+    else:
+        raise ValueError(f'Error: args.target_type = {args.target_type} not valid.')
+    # tb = SummaryWriter(log_dir=args.current_logs_path)  # uncomment for documentation to work
+    tb.add_scalar(tag1, loss, it)
+    tb.add_scalar(tag2, acc, it)
+
+def log_2_tb_metalearning_old(tb, args, it, loss, acc_err, split):
+    """
+    :param acc:
+    :param loss:
+    :param it:
+    :param args:
+    :param split: train, val, test
+    :return:
+    """
+    if args.target_type == 'regression':
+        tag1 = f'meta-{split}_loss'
+        tag2 = f'meta-{split}_regression_accuracy'
+    elif args.target_type == 'classification':
+        tag1 = f'meta-{split}_loss'
+        tag2 = f'meta-{split}_accuracy'
+    else:
+        raise ValueError(f'Error: args.target_type = {args.target_type} not valid.')
+    # tb = SummaryWriter(log_dir=args.current_logs_path)  # uncomment for documentation to work
+    tb.add_scalar(tag1, loss, it)
+    tb.add_scalar(tag2, acc_err, it)
+
+# -- tests
+
+def test():
+    from datetime import datetime
+    print('\n---running tb test, writing ot a tb...')
+    import numpy as np
+    from torch.utils.tensorboard import SummaryWriter  # https://deeplizard.com/learn/video/psexxmdrufm
+
+    current_time: str = datetime.now().strftime('%b%d_%H-%M-%S')
+    path: Path = Path(f'~/data/logs/logs_{current_time}/tb').expanduser()
+    print(f'Test path: {path=}')
+    # path.mkdir(parents=True, exist_ok=True)  # doesn't seem it's needed
+    tb: SummaryWriter = SummaryWriter(log_dir=path)
+    print(f'created {tb=}')
+
+    for i in range(5):
+        loss = i + np.random.normal(loc=0, scale=1)
+        print(f'{i=}: {loss=}')
+        tb.add_scalar('loss', loss, i)
+
+
+if __name__ == '__main__':
+    test()
+    print('Done with tensorboard test!\a')
