e384396f36e411b67e73fb90cef105ab14ddbfe2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:29:59 +0000

    Add package minecraftstats/1.0.9/minecraftstats-1.0.9-py3-none-any.whl

---
+++ packages/minecraftstats/minecraftstats-1.0.9-py3-none-any.whl/minecraftstats/bedwarsstats.py
@@ -0,0 +1,50 @@
+"""All stats for bedwars.
+
+This also includes overall bedwars stats.
+"""
+from pydantic import BaseModel, Field
+
+from .utils import filter_kwargs, StatsModel
+
+__all__ = []
+
+_game_modes = [
+    "eight_one",
+    "eight_two",
+    "four_four",
+    "two_four",
+    "four_three"
+]
+
+class OverallBedwarsStats(StatsModel):
+    """Overall bedwars stats."""
+    _suffix: str = "_bedwars"
+
+    games_played: int = 0
+    beds_lost: int = 0
+    coins: int = 0
+    deaths: int = 0
+    diamonds_collected: int = Field(0, alias="diamond_resources_collected")
+    final_deaths: int = 0
+    gold_collected: int = Field(0, alias="gold_resources_collected")
+    iron_collected: int = Field(0, alias="iron_resources_collected")
+    losses: int = 0
+    total_resources_collected: int = Field(0, alias="resources_collected")
+    void_deaths: int = 0
+    void_final_deaths: int = 0
+    entity_final_deaths: int = Field(0, alias="entity_attack_final_deaths")
+    emeralds_collected: int = Field(0, alias="emerald_resources_collected")
+    entity_deaths: int = Field(0, alias="entity_attack_deaths")
+    entity_kills: int = Field(0, alias="entity_attack_kills")
+    kills: int = 0
+    beds_broken: int = 0
+    fall_damage_deaths: int = Field(0, alias="fall_deaths")
+    current_winstreak: int = Field(0, alias="winstreak")
+    entity_final_kills: int = Field(0, alias="entity_attack_final_kills")
+    final_kills: int = 0
+    wins: int = 0
+    void_kills: int = 0
+    void_final_kills: int = 0
+    fall_damage_kills: int = Field(0, alias="fall_kills")
+    fall_damage_final_deaths: int = Field(0, alias="fall_final_deaths")
+    fall_damage_final_kills: int = Field(0, alias="fall_final_kills")
