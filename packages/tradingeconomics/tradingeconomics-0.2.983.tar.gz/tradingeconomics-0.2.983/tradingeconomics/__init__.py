35c0cafe0088afce3c0205e1f7c2524ae00dc392
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:10:02 +0000

    Add package tradingeconomics/0.2.983/tradingeconomics-0.2.983.tar.gz

---
+++ packages/tradingeconomics/tradingeconomics-0.2.983.tar.gz/tradingeconomics-0.2.983/tradingeconomics/__init__.py
@@ -0,0 +1,54 @@
+"""
+This package allows Trading Economics clients to easily query the Trading Economics API to get data into their Python code.
+"""
+
+import sys
+
+PY3 = sys.version_info[0] == 3
+
+
+if PY3: # Python 3+
+    from .historicalDB import getHistorical
+    from .historical import getHistoricalData, getHistoricalRatings
+    from .calendar import getCalendarData, getCalendarId
+    from .forecasts import getForecastData
+    from .indicators import getIndicatorData, getRatings, getLatestUpdates, getDiscontinuedIndicator
+    from .markets import getMarketsData, getMarketsBySymbol, getMarketsIntraday, getMarketsPeers, getMarketsComponents, getMarketsSearch, getMarketsForecasts, getCurrencyCross
+    from .historicalMarkets import fetchMarkets
+    from .glob import login, subscribe
+    from .stream import run
+    from .earnings import getEarnings, getEarningsType
+    from .news import getNews, getArticles, getArticleId 
+    from .worldBank import getWBCategories, getWBIndicator, getWBCountry, getWBHistorical
+    from .comtrade import getCmtCategories, getCmtCountry, getCmtHistorical, getCmtTwoCountries, getCmtUpdates, getCmtCountryByCategory, getTotalByType, getCmtCountryFilterByType
+    from .federalReserve import getFedRStates, getFedRSnaps, getFedRHistorical, getFedRCounty
+    from .eurostat import getEurostatData
+    from .historicalEurostat import getHistoricalEurostat
+    from .financials import getFinancialsData
+    from .historicalFinancials import getHistoricalFinancials
+
+
+else: # Python 2.X
+    from historicalDB import getHistorical
+    from historical import getHistoricalData, getHistoricalRatings
+    from calendar import getCalendarData, getCalendarId
+    from forecasts import getForecastData
+    from indicators import getIndicatorData, getRatings, getLatestUpdates, getDiscontinuedIndicator
+    from markets import getMarketsData, getMarketsBySymbol, getMarketsIntraday, getMarketsPeers, getMarketsComponents, getMarketsSearch, getMarketsForecasts, getCurrencyCross
+    from historicalMarkets import fetchMarkets
+    from glob import login, subscribe
+    from stream import run
+    from earnings import getEarnings, getEarningsType
+    from news import getNews, getArticles, getArticleId
+    from worldBank import getWBCategories, getWBIndicator, getWBCountry, getWBHistorical
+    from comtrade import getCmtCategories, getCmtCountry, getCmtHistorical, getCmtTwoCountries, getCmtUpdates, getCmtCountryByCategory, getTotalByType, getCmtCountryFilterByType
+    from federalReserve import getFedRStates, getFedRSnaps, getFedRHistorical, getFedRCounty
+    from eurostat import getEurostatData
+    from historicalEurostat import getHistoricalEurostat
+    from financials import getFinancialsData
+    from historicalFinancials import getHistoricalFinancials
+
+
+
+
+    
