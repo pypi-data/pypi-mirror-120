cd088eb6f31349fa4e1675a309c48478a20a603d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:27 +0000

    Add package magnebot/1.3.2/magnebot-1.3.2.tar.gz

---
+++ packages/magnebot/magnebot-1.3.2.tar.gz/magnebot-1.3.2/magnebot/constants.py
@@ -0,0 +1,6 @@
+# The radius of the Magnebot as defined by its longer axis.
+MAGNEBOT_RADIUS: float = 0.22
+# The cell size on the occupancy map. This is slightly bigger than diameter of the Magnebot.
+OCCUPANCY_CELL_SIZE: float = (MAGNEBOT_RADIUS * 2) + 0.05
+# The required version of TDW.
+TDW_VERSION: str = "1.8.25"
