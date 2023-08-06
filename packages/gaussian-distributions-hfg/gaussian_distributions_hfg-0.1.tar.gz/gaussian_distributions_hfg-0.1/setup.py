19b509df11c7788a87da6fb5743a747d46cd62c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:40:12 +0000

    Add package gaussian-distributions-hfg/0.1/gaussian_distributions_hfg-0.1.tar.gz

---
+++ packages/gaussian-distributions-hfg/gaussian_distributions_hfg-0.1.tar.gz/gaussian_distributions_hfg-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='gaussian_distributions_hfg',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['gaussian_distributions_hfg'],
+      zip_safe=False)
