1a4527c1d0423888f1f810c08fbf53007dea0277
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:31:20 +0000

    Add package invenio-subjects-lcsh/2021.9.19/invenio_subjects_lcsh-2021.9.19-py2.py3-none-any.whl

---
+++ packages/invenio-subjects-lcsh/invenio_subjects_lcsh-2021.9.19-py2.py3-none-any.whl/invenio_subjects_lcsh/cli.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2021 Northwestern University.
+#
+# invenio-subjects-lcsh is free software; you can redistribute it and/or
+# modify it under the terms of the MIT License; see LICENSE file for more
+# details.
+
+"""Command line tool."""
+
+from pathlib import Path
+
+import click
+
+from .converter import MeSHConverter
+from .download import download_mesh
+from .reader import MeSHReader
+from .writer import write_yaml
+
+
+@click.command()
+def main():
+    """Generate new subjects_lcsh.yaml file."""
+    filepath = download_mesh()
+
+    reader = MeSHReader(filepath, filter='topics')
+
+    converter = MeSHConverter(reader)
+
+    filepath = write_yaml(converter)
+
+    print(f"MeSH terms written here {filepath}")
---
+++ packages/invenio-subjects-lcsh/invenio_subjects_lcsh-2021.9.19-py2.py3-none-any.whl/invenio_subjects_lcsh/converter.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2021 Northwestern University.
+#
+# invenio-subjects-lcsh is free software; you can redistribute it and/or
+# modify it under the terms of the MIT License; see LICENSE file for more
+# details.
+
+"""MeSH term converter."""
+
+
+class MeSHConverter:
+    """Convert MeSH term into subject dict for YAML writing."""
+
+    def __init__(self, reader):
+        """Constructor."""
+        self._reader = reader
+
+    def generate_id(self, identifier):
+        """Generate URI id."""
+        return f"https://id.nlm.nih.gov/mesh/{identifier}"
+
+    def __iter__(self):
+        """Iterate over converted entries."""
+        for term in self._reader:
+            yield {
+                "id": self.generate_id(term['UI']),
+                "scheme": "MeSH",
+                "subject": term['MH']
+            }
