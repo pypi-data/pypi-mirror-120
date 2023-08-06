6de6b5609a49992f45b755ddac22510e64364ff2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:14:29 +0000

    Add package zWhite/0.0.81/zWhite-0.0.81.tar.gz

---
+++ packages/zWhite/zWhite-0.0.81.tar.gz/zWhite-0.0.81/setup.py
@@ -0,0 +1,6 @@
+import setuptools
+from distutils.core import setup
+
+
+packages = ['zWhite']
+setup(name='zWhite',version='0.0.81', author='liguifa', packages=packages, package_dir={'requests': 'requests'},)
