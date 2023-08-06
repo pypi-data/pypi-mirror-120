1479c4759f7d4b27bac8506ae3120a54d5954f52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:14 +0000

    Add package mupemenet/0.1.9/mupemenet-0.1.9.tar.gz

---
+++ packages/mupemenet/mupemenet-0.1.9.tar.gz/mupemenet-0.1.9/mupemenet/osdependent/PlatformDependentTripleClickListener.py
@@ -0,0 +1,20 @@
+from abc import ABC, abstractmethod
+from mvc.eventbus.requests.BasicRequests import FrPipelineRequest
+
+from pyeventbus3.pyeventbus3 import PyBus
+
+
+
+class PlatformDependentTripleClickListener(ABC):
+
+    @abstractmethod
+    def enter(self, params=None):
+        raise NotImplemented
+
+    @abstractmethod
+    def exit(self):
+        raise NotImplemented
+
+    @staticmethod
+    def triple_button_click_action(event):
+        PyBus.Instance().post(FrPipelineRequest())
