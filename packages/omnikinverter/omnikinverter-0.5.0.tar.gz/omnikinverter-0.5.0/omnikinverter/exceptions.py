03d929500c5bd9b3680ccfa143c2a6981e31d4b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:58 +0000

    Add package omnikinverter/0.5.0/omnikinverter-0.5.0.tar.gz

---
+++ packages/omnikinverter/omnikinverter-0.5.0.tar.gz/omnikinverter-0.5.0/omnikinverter/exceptions.py
@@ -0,0 +1,22 @@
+"""Exceptions for Omnik Inverter."""
+
+
+class OmnikInverterError(Exception):
+    """Generic Omnik Inverter exception."""
+
+
+class OmnikInverterConnectionError(OmnikInverterError):
+    """Omnik Inverter connection exception."""
+
+
+class OmnikInverterWrongSourceError(OmnikInverterError):
+    """Omnik Inverter wrong data source url exception."""
+
+
+class OmnikInverterWrongValuesError(OmnikInverterError):
+    """Omnik Inverter gives wrong values.
+
+    This error appears when your inverter
+    shows the same value for both day and
+    total production.
+    """
