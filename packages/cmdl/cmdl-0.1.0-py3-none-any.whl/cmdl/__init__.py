2cd81ac48fca855047eb97640923aa00eebf0237
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:22 +0000

    Add package cmdl/0.1.0/cmdl-0.1.0-py3-none-any.whl

---
+++ packages/cmdl/cmdl-0.1.0-py3-none-any.whl/cmdl/__init__.py
@@ -0,0 +1,10 @@
+"""
+`cmdl` module by Cypheriel.
+
+Repository: https://github.com/Cypheriel/cmdl
+"""
+
+from ._command_line_parser import CommandLine, Command, Alias
+
+
+__all__ = ["CommandLine", "Command", "Alias"]
