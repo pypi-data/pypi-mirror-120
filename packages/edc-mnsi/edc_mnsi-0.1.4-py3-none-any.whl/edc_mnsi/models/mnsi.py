9c373ed4e8f1cfcc75b9e64087ccba7fedd345f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:25 +0000

    Add package edc-mnsi/0.1.4/edc_mnsi-0.1.4-py3-none-any.whl

---
+++ packages/edc-mnsi/edc_mnsi-0.1.4-py3-none-any.whl/edc_mnsi/models/mnsi.py
@@ -0,0 +1,25 @@
+from django.db import models
+from edc_identifier.model_mixins import UniqueSubjectIdentifierFieldMixin
+from edc_model import models as edc_models
+from edc_model.models import HistoricalRecords
+from edc_sites.models import CurrentSiteManager, SiteModelMixin
+from edc_utils import get_utcnow
+
+from ..model_mixins import MnsiModelMixin
+
+
+class Mnsi(
+    UniqueSubjectIdentifierFieldMixin,
+    MnsiModelMixin,
+    SiteModelMixin,
+    edc_models.BaseUuidModel,
+):
+
+    report_datetime = models.DateTimeField(default=get_utcnow)
+
+    on_site = CurrentSiteManager()
+    objects = models.Manager()
+    history = HistoricalRecords()
+
+    class Meta(MnsiModelMixin.Meta, edc_models.BaseUuidModel.Meta):
+        pass
