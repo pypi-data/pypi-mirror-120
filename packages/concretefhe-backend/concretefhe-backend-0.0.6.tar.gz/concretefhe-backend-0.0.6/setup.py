2977ebc9501ecee6e4512322ac9584e5965134c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:46:50 +0000

    Add package concretefhe-backend/0.0.6/concretefhe-backend-0.0.6.tar.gz

---
+++ packages/concretefhe-backend/concretefhe-backend-0.0.6.tar.gz/concretefhe-backend-0.0.6/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="concretefhe-backend",
+    version="0.0.6",
+    author="Zama Team",
+    author_email="hello@zama.ai",
+    description="Zama Concretefhe-backend",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://zama.ai",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.8",
+)
