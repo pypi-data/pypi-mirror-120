747d0c9e935e6f0ddc955d42e150280df60db59d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:04 +0000

    Add package deepctrl-scrapy-redis/0.8.4/deepctrl-scrapy-redis-0.8.4.tar.gz

---
+++ packages/deepctrl-scrapy-redis/deepctrl-scrapy-redis-0.8.4.tar.gz/deepctrl-scrapy-redis-0.8.4/src/scrapy_redis/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from .connection import (  # NOQA
+    get_redis,
+    get_redis_from_settings,
+)
+
+
+__author__ = 'yichao.wu'
+__email__ = 'yichao.wu@deepctrl.com'
+__version__ = '0.8.1'
