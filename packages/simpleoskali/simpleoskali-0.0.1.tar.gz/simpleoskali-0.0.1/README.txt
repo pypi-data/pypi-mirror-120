7d9cf0ea0828c0accec4ca8e851811577043394f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:43 +0000

    Add package simpleoskali/0.0.1/simpleoskali-0.0.1.tar.gz

---
+++ packages/simpleoskali/simpleoskali-0.0.1.tar.gz/simpleoskali-0.0.1/README.txt
@@ -0,0 +1,2 @@
+c() - clears terminal | 
+pwd() - current location | 
---
+++ packages/simpleoskali/simpleoskali-0.0.1.tar.gz/simpleoskali-0.0.1/simpleoskali/__init__.py
@@ -0,0 +1,10 @@
+import os
+
+# ------ CLEAR THE CONSOLE ------
+def c():
+    os.system('clear')
+
+# ------ WORKING LOCATION ------
+def pwd():
+    os.system('pwd')
+
