3ae5cfbde4813824a1a860aea82c9110f9d84a72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:14:19 +0000

    Add package amaindexsnippetworkflow/0.1.0/amaindexsnippetworkflow-0.1.0.tar.gz

---
+++ packages/amaindexsnippetworkflow/amaindexsnippetworkflow-0.1.0.tar.gz/amaindexsnippetworkflow-0.1.0/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['amaindexsnippetworkflow']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'amaindexsnippetworkflow',
+    'version': '0.1.0',
+    'description': '',
+    'long_description': None,
+    'author': 'Amaindex',
+    'author_email': 'amaindex@outlook.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
