09966f81d353d2b20a5de27e034d42d2c0c07916
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:24:01 +0000

    Add package ipython-sparql-pandas/1.2/ipython-sparql-pandas-1.2.tar.gz

---
+++ packages/ipython-sparql-pandas/ipython-sparql-pandas-1.2.tar.gz/ipython-sparql-pandas-1.2/src/ipython_sparql_pandas/sparql.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+
+from IPython.core.magic import register_cell_magic, needs_local_scope
+from IPython.core.magic_arguments import argument, magic_arguments, parse_argstring
+from SPARQLWrapper import SPARQLWrapper, JSON
+from IPython.core.display import display_javascript, Javascript
+from rdflib import URIRef, Literal
+import pandas as pd
+
+
+
+@register_cell_magic
+@needs_local_scope
+@magic_arguments()
+@argument('endpoint', help='SPARQL endpoint')
+@argument('--save', '-s', help='Save result into variable')
+@argument('--quiet', '-q', action='store_true', help='Don\'t output anything')
+def sparql(line, cell, local_ns=None):
+    
+    args = parse_argstring(sparql, line)
+    sparql_endpoint = args.endpoint
+
+    client = SPARQLWrapper(sparql_endpoint)
+    client.setReturnFormat(JSON)
+    client.setQuery(cell)
+    results = client.query().convert()
+    df = df_results(results)
+
+    if args.save:
+        local_ns[args.save] = df
+    
+    if not args.quiet:
+        return df
+
+def convert_node(obj):
+    if obj['type'] == 'uri':
+        return obj['value']
+    if obj['type'] == 'typed-literal' or 'literal':
+        if 'datatype' in obj:
+            dt = URIRef(obj['datatype'])
+            return Literal(obj['value'], datatype=dt).toPython()
+        elif 'xsd:lang' in obj:
+            lang = obj['xsd:lang']
+            return Literal(obj['value'], lang=lang)
+        else:
+            return obj['value']
+    
+    raise Exception(f'Invalid RDF node type {obj["type"]}')
+
+
+def df_results(result):
+
+    if 'boolean' in result:
+        vars = ['ASK']
+        table = [[result['boolean']]]
+    else:
+        vars = result['head']['vars']
+        table = []
+        for data in result['results']['bindings']:
+            row = []
+            for var in vars:
+                if var in data:
+                    d = convert_node(data[var])
+                else:
+                    d = None
+                row.append(d)
+            table.append(row)
+
+    return pd.DataFrame(table, columns=vars)
+
+def load_ipython_extension(ipython):
+    pass
