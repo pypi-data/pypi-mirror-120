8ce09a7cf4669497442177fa5ba79c4d8e3c81d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:43:00 +0000

    Add package westworld/0.1.2/westworld-0.1.2-py3-none-any.whl

---
+++ packages/westworld/westworld-0.1.2-py3-none-any.whl/westworld/environment/grid.py
@@ -0,0 +1,609 @@
+
+import numpy as np
+import pandas as pd
+import pygame
+import random
+from PIL import Image
+from win32api import GetSystemMetrics
+
+
+# Custom libraries
+from ..algorithms.neighbors import NeighborsFinder
+
+
+class GridEnvironment:
+    def __init__(self,width = None,height = None,cell_size = 10,
+        objects = None,show_grid = False,grid_color = (50,50,50),background_color = (0,0,0),
+        callbacks_step = None,max_spawn_trials = 1000,
+        toroidal = True,
+        ):
+
+        # Init pygame
+        pygame.init()
+        self.screen = pygame.display.set_mode((1,1),0,0)
+
+        # Store parameters
+        self._clock = 0
+        self._cell_size = cell_size
+        self._done = False
+        self.show_grid = show_grid
+        self.grid_color = grid_color
+        self.max_spawn_trials = max_spawn_trials
+        self.width = width if width is not None else ((GetSystemMetrics(0) - 100) // cell_size)
+        self.height = height if height is not None else ((GetSystemMetrics(1) - 100) // cell_size)
+        self.background_color = background_color
+        self.callbacks_step = [] if callbacks_step is None else callbacks_step
+        self.toroidal = toroidal
+
+        # Groups initialization
+        self.group_blocking = pygame.sprite.Group()
+        self.group_layers = pygame.sprite.Group()
+        self.group_triggers = pygame.sprite.Group()
+
+        # Objects initialization
+        self._objects = {}
+        self.add_object(objects)
+
+        # Grid Environment parameters
+        self.setup_screen()
+
+        # Init objects data
+        self.set_data()
+        self.render()
+
+        # Prepare logs
+        self._logs = []
+        
+
+
+    @property
+    def is_grid(self):
+        return self.cell_size > 1
+
+
+    def make_group(self,condition = None):
+        group = pygame.sprite.Group()
+
+        if condition is not None:
+            objs = self.find_objects(condition = condition,return_objects = True)
+            group.add(objs)
+
+        return group
+
+
+
+    @property
+    def cell_size(self):
+        return self._cell_size
+
+    def init_screen(self):
+        pass
+
+
+
+    @property
+    def layers(self):
+        return self.group_layers.sprites()
+
+    def has_layers(self):
+        return len(self.group_layers) > 0
+    
+    def has_blocking(self):
+        return len(self.group_blocking) > 0
+
+    def has_triggers(self):
+        return len(self.group_triggers) > 0
+
+
+    def quit(self):
+        pygame.quit()
+
+
+    def finish_episode(self):
+        self._done = True
+
+
+    @property
+    def clock(self):
+        """Default internal clock for each object
+        """
+        return self._clock
+
+    def clocktick(self):
+        """Helper function to increment internal clock
+        """
+        self._clock += 1
+
+    @property
+    def data(self):
+        return self._data
+
+    @property
+    def done(self):
+        return self._done
+
+
+    @property
+    def objects(self):
+        return list(self._objects.values())
+
+
+    def get_object(self,object_id):
+        if isinstance(object_id,str):
+            return self._objects[object_id]
+        else:
+            return self._objects[object_id.id]
+
+
+    def __getitem__(self,object_id):
+        return self.get_object(object_id)
+
+    def remove_object(self,object_id):
+
+        if isinstance(object_id,str):
+            obj = self._objects.pop(object_id)
+        else:
+            obj = self._objects.pop(object_id.id)
+
+        self.group_blocking.remove(obj)
+        self.group_triggers.remove(obj)
+        self.group_layers.remove(obj)
+
+
+
+    def add_object(self,obj):
+
+        if obj is not None:
+
+            # If we add a list of objects, using recursive function to add each object
+            if isinstance(obj,list):
+                for o in obj:
+                    self.add_object(o)
+                
+            # Add object 
+            else:
+                
+                # Set environment as attribute for easy manipulation
+                obj.bind(self)
+                self._objects[obj.id] = obj
+
+                if obj.layer:
+                    self.group_layers.add(obj)
+
+                if obj.blocking:
+                    self.group_blocking.add(obj)
+
+                if obj.trigger:
+                    self.group_triggers.add(obj)
+                
+
+
+    def find(self,return_pos = False,return_objects = True,return_data = False,**kwargs):
+        # TODO is it faster with numpy or pandas
+        # The loop could be accelerated with numba?
+        # Previously called find_objects
+        ids = []
+
+        if len(kwargs) == 0:
+            ids = [x.id for x in self.objects]
+        else:
+            def helper_check_fn(obj,k,v):
+                if not hasattr(obj,k):
+                    return False
+                else:
+                    return getattr(obj,k) == v
+
+            for obj in self.objects:
+                if all([helper_check_fn(obj,k,v) for k,v in kwargs.items()]):
+                    ids.append(obj.id)
+                
+
+        if return_pos:
+            return [self._objects[k].pos for k in ids]
+        elif return_data:
+            return self.data.loc[ids]
+        elif not return_objects:
+            return ids
+        else:
+            return [self._objects[k] for k in ids]
+
+
+
+
+
+
+
+    #=================================================================================
+    # COLLIDERS
+    #=================================================================================
+
+
+    def wrap_in_toroidal_envs(self,x,y):
+
+        if self.toroidal:
+
+            env_width = self.width
+            env_height = self.height
+
+            # Check with x
+            if x >= env_width:
+                new_x = 0
+            elif x < 0:
+                new_x = env_width - 1
+            else:
+                new_x = x
+
+            # Check with y
+            if y >= env_height: 
+                new_y = 0
+            elif y < 0:
+                new_y = env_height - 1
+            else:
+                new_y = y
+
+            return new_x,new_y
+
+        else:
+            return x,y
+
+
+    def is_object_colliding(self,obj):
+        is_collision,_ = obj.collides()
+        return is_collision
+
+
+    #=================================================================================
+    # SPAWNERS
+    #=================================================================================
+
+
+
+    def spawn(self,spawner,n,allow_overlap = False,**kwargs):
+        """Spawner function
+        """
+
+        # Spawn n elements (works also with n = 1)
+        for i in range(n):
+
+            if not self.is_grid and allow_overlap == False:
+
+                x,y = self.get_random_available_pos(allow_overlap = True)
+                obj = spawner(x,y,**kwargs)
+                self.add_object(obj)
+                is_spawned = not obj.collides()[0]
+                i = 0
+
+                while not is_spawned:
+                    x,y = self.get_random_available_pos(allow_overlap = True)
+                    obj.move_at(x = x,y = y)
+                    is_spawned = not obj.collides()[0]
+                    
+                    i += 1
+                    if i > self.max_spawn_trials:
+                        is_spawned = True
+                        self.remove_object(obj)
+
+            else:
+
+                # Generate a random position considering or not overlaps
+                x,y = self.get_random_available_pos(allow_overlap = allow_overlap)
+
+                # Spawn new object using spawner
+                # Pass also kwargs to spawner
+                obj = spawner(x,y,**kwargs)
+
+                self.add_object(obj)
+
+        # Append to data
+        self.set_data()
+
+
+
+
+    #=================================================================================
+    # MESHES AND ARRAYS
+    #=================================================================================
+
+
+    def get_grid(self):
+        return np.zeros((self.width,self.height)).T
+
+
+    def get_navigation_mesh(self,obj = None):
+
+        # Prepare empty mesh        
+        mesh = self.get_grid()
+
+        # Get all blocking objects
+        object_id = "" if obj is None else obj.id
+        positions = [x.pos_array for x in self.objects if (x.id != object_id and x.blocking)]
+        positions = [y for x in positions for y in x]
+
+        # Update mesh with blocking positions
+        if len(positions) > 0:
+            mesh[tuple(np.array(positions).T)] = 1
+
+
+        # Update navigation mesh with obstacles layer if any
+        if self.has_layers:
+            all_meshes = [mesh]
+            all_meshes.extend([layer.get_navigation_mesh() for layer in self.layers])
+            mesh = np.int8(np.sum(all_meshes,axis = 0) > 0)
+
+        return mesh
+
+    def get_available_mesh(self):
+
+        # Prepare empty mesh        
+        mesh = self.get_grid()
+
+        # Get all blocking objects
+        positions = [y for x in self.objects for y in x.pos_array]
+
+        if len(positions) > 0:
+
+            # Update mesh with blocking positions
+            mesh[tuple(np.array(positions).T)] = 1
+
+        return mesh
+
+
+    def get_available_pos(self):
+
+        # Get mesh
+        mesh = self.get_available_mesh()
+
+        # Return list of pos
+        return np.array(np.where(mesh == 0)).T
+
+
+    def get_random_available_pos(self,allow_overlap = False):
+
+        if allow_overlap:
+            x = np.random.randint(0,self.width)
+            y = np.random.randint(0,self.height)
+        else:
+            # Warning x,y are reversed in numpy
+            pos = self.get_available_pos()
+            y,x = random.choice(pos)
+        return x,y
+
+
+
+    #=================================================================================
+    # DATA MANIPULATION
+    #=================================================================================
+
+
+    def _prepare_data(self):
+        # TODO this function is not optimized at all, 
+        # It takes a few ms for only a few agents in the environment
+        # Iteration is quite fast, it's transforming to dataframe that takes time
+        # Yet we can probably use dataframes for faster computing later on, maybe interesting to use faster equivalents
+        # Such a numpy arrays, jax arrays or torch tensors
+        data = [obj.get_data() for obj in self.objects]
+        if len(data) == 0:
+            return None
+        else:
+            data = pd.DataFrame(data).set_index("id")
+            return data
+
+
+    def set_data(self):
+
+        # Update data
+        self._data = self._prepare_data()
+
+        # Update also neighbors finder
+        # self.neighbors_finder = NeighborsFinder(self._data)
+
+
+
+
+
+
+
+    #=================================================================================
+    # ENV LIFECYCLE
+    #=================================================================================
+
+    def callback_step(self):
+        pass
+
+    def post_step(self):
+        pass
+
+    def compute_reward(self):
+        return 0
+
+    def step(self):
+
+        # Iterate for each object
+        for agent in self.objects:
+
+            # Only step with non static objects: ie agents
+            if agent.stationary == False:
+                agent.step()
+                agent.clocktick()
+
+        # Reinitialize data
+        # TODO this can be optional for performance
+        # Actually it's only required for computations such as pathfinding
+        self.set_data()
+
+        # Step callback
+        for fn in self.callbacks_step:
+            fn(self)
+
+
+        self.post_step()
+
+        # Prepare reward and done
+        # TODO add reward
+        reward = self.compute_reward()
+        done = self.done
+
+        # Increment internal clock
+        self.clocktick()
+
+        # Prepare step data
+        step_data = self.get_logs()
+        self.reset_logs()
+
+        return reward,done,step_data
+
+
+    def log(self,d):
+        d = {"step":self.clock,**d}
+        self._logs.append(d)
+
+    def get_logs(self):
+        return self._logs
+
+    def reset_logs(self):
+        self._logs = []
+
+
+    #=================================================================================
+    # RENDERERS
+    #=================================================================================
+
+
+
+
+    def _init_screen_from_layer(self):
+
+        # Init grid map from layers
+        layer = self.layers[0]
+        w,h = layer.get_size()
+        self.width = w//self.cell_size
+        self.height = h//self.cell_size
+
+
+
+
+    def setup_screen(self):
+        """Setup the first PyGame Screen
+        """
+
+        if self.has_layers():
+            self._init_screen_from_layer()
+        
+
+        # Init pygame window
+        pygame.init()
+        pygame.display.set_caption("Westworld environment")
+
+        # Create window
+        self.screen = pygame.display.set_mode((
+            self.width * self.cell_size,
+            self.height * self.cell_size
+            ),pygame.RESIZABLE)
+
+        # Fill with black
+        self.reset_screen()
+
+
+    def render_text(self,text,font = "Arial",size = 30,position = (5,5),screen = None,color = (0,0,0)):
+
+        font = pygame.font.SysFont(font, size)
+        text_surface = font.render(f"{text}", False,color)
+
+        if screen is None: screen = self.screen
+        screen.blit(text_surface,position)
+
+
+
+
+    def reset_screen(self):
+        """Reset PyGame screen by filling with BLACK color
+        """
+        self.screen.fill(self.background_color)
+
+
+    def render_grid(self,color = (50,50,50)):
+
+        for x in range(self.width):
+            for y in range(self.height):
+                rect = pygame.Rect(x*self.cell_size, y*self.cell_size,self.cell_size, self.cell_size)
+                pygame.draw.rect(self.screen, color, rect, 1)
+
+
+    def prerender(self):
+        pass
+
+
+    def postrender(self):
+        pass
+
+
+    def render(self,screen = None):
+        """Render the environment
+        TODO: could be without PyGame
+        """
+
+        if screen is None:
+            # Reset current screen to black
+            self.reset_screen()
+            screen = self.screen
+
+        # Show grid if necessary
+        if self.show_grid:
+            self.render_grid(self.grid_color)
+
+        # Render each object
+        for el in self.objects:
+            el.prerender()
+            el.render(screen = screen)
+            el.postrender()
+
+        # Update the PyGame renderer
+        pygame.display.update()
+
+
+
+    def get_frame(self):
+        """Get the current frame as numpy array
+
+        Returns:
+            np.ndarray: The current frame as numpy array
+        """
+        return pygame.surfarray.array3d(self.screen).swapaxes(0,1)
+
+
+    def get_img(self):
+        """Get the current frame as PIL image
+
+        Returns:
+            PIL.Image: The current frame as PIL Image
+        """
+        img = self.get_frame()
+        return Image.fromarray(img)
+
+
+    def save_img(self,filepath):
+        """Save the current frame in a picture file (eg .png)
+
+        Args:
+            filepath (str): The output filename
+        """
+        img = self.get_img()
+        img.save(filepath)
+
+
+        
+
+
+
+
+        
+
+        
+
+
+
+
+
+        
+
+
---
+++ packages/westworld/westworld-0.1.2-py3-none-any.whl/westworld/objects/base_object.py
@@ -0,0 +1,271 @@
+
+
+import uuid
+import pygame
+from pygame.sprite import Sprite
+
+from ..exceptions import ObjectNotBoundError
+
+
+
+class BaseObject(Sprite):
+    """Default Object serving as base for each agent evolving in environments
+    """
+    def __init__(self):
+
+        # Init base sprite class
+        super().__init__()
+
+        # Create id and clock
+        self.id = str(uuid.uuid1())
+        self._clock = 0
+
+        # Init function to be overridden
+        self.init()
+
+
+    @property
+    def clock(self):
+        """Default internal clock for each object
+        """
+        return self._clock
+
+    def clocktick(self):
+        """Helper function to increment internal clock
+        """
+        self._clock += 1
+
+
+    def kill(self):
+        if self.id in self.env._objects.keys():
+            self.env.remove_object(self)
+
+
+    @property
+    def env(self):
+        """Environment linked to the object
+        """
+        if self.is_bound():
+            return self._env
+        else:
+            raise ObjectNotBoundError("The object must be attached to an environment")
+
+
+    def is_bound(self):
+        return hasattr(self,"_env")
+
+
+    def bind(self,env):
+        """Default setter function
+        """
+        self._env = env
+        self.post_bind()
+
+    def post_bind(self):
+        pass
+
+
+    def init(self):
+        """Default init function 
+        Can be overriden in special objects
+        """
+        pass
+
+
+    @property
+    def on_grid(self):
+        return self.env.is_grid
+
+    @property
+    def stationary(self):
+        return True
+
+    @property
+    def blocking(self):
+        return True
+
+    @property
+    def trigger(self):
+        return False
+
+    @property
+    def collectible(self):
+        return False
+
+    @property
+    def radius(self):
+        return 1
+
+
+
+    @property
+    def layer(self):
+        return False
+
+
+    @property
+    def obstacle(self):
+        return self.stationary and self.blocking
+
+
+
+    def collides(self) -> list:
+        """Returns if an object collides with any objects in the environment
+        Needs to be binded to an environment of course
+        The function will computes collisions against:
+            - Rectangular or circle collisions with the blocking sprite group
+            - Mask collisions with the layer sprite group
+            - Rectangular collisions with the trigger sprite group
+
+        TODO:
+        Could be improved to account for trigger layers
+
+        Returns:
+            list: List of objects in collisions, empty if no collision
+        """
+        
+
+        collisions = []
+
+        # Find all rect collisions
+        if self.env.has_blocking():
+            c = self.collides_group(self.env.group_blocking,method = self.collision_method)
+            collisions.extend(c)
+
+        # Find all mask collisions
+        if self.env.has_layers():
+            c = self.collides_group(self.env.group_layers,method = "mask")
+            collisions.extend(c)
+
+        if self.env.has_triggers():
+            triggers = self.collides_group(self.env.group_triggers,method = "rect")
+            for trigger in triggers:
+                trigger.on_collision(self)
+
+        return collisions
+
+
+
+
+    def collides_group(self,group:pygame.sprite.Group, method:str = "rect", ratio:float = None) -> list:
+        """Collision helper functions, computes if an object enters a collision with a sprite group
+        Three methods are available (rect, circle and mask)
+
+        Args:
+            group (pygame.sprite.Group): The target sprite group on which to test collisions
+            method (str, optional): Collision method, either rectangular, circular or using a pixel perfect mask. Defaults to "rect".
+            ratio (float, optional): Apply a ratio around the center of the object, useful to get collisions in a circle range. Defaults to None.
+
+        Returns:
+            list: List of objects in collisions, empty if no collision
+        """
+
+        # Verify method is in acceptable values
+        assert method in ["rect","circle","mask"]
+
+        # Rectangular collider
+        if method == "rect":
+            if ratio is None:
+                collisions = pygame.sprite.spritecollide(self,group,False)
+            else:
+                collisions = pygame.sprite.spritecollide(self,group,False,pygame.sprite.collide_rect_ratio(ratio))
+
+        # Circle collider
+        elif method == "circle":
+            # Warning, to check ratio works with rect but for circle 
+            # We need to add the cell_size, maybe works as a radius and not a ratio
+
+            if ratio is  None:
+                collisions = pygame.sprite.spritecollide(self,group,False,pygame.sprite.collide_circle)
+            else:
+                ratio = ratio * self.env.cell_size
+                collisions = pygame.sprite.spritecollide(self,group,False,pygame.sprite.collide_circle_ratio(ratio))
+
+        # Mask collider
+        elif method == "mask":
+            collisions = pygame.sprite.spritecollide(self,group,False,pygame.sprite.collide_mask)
+
+        # Filter to remove self object from collisions 
+        collisions = [x for x in collisions if x.id != self.id]
+
+        return collisions
+
+
+    def render_circle(self,x = None,y = None,radius = None,color = None,screen = None,thickness = 0,cell_size = None):
+
+        # Default arguments
+        if x is None: x = self.x
+        if y is None: y = self.y
+        if color is None: color = self.color
+        if screen is None: screen = self.env.screen
+        if cell_size is None: cell_size = self.env.cell_size
+        if radius is None: radius = self.radius 
+
+        # When in a grid, the center is normalized on cell size
+        if cell_size is not None:
+            x = int((x + 0.5) * cell_size)
+            y = int((y + 0.5) * cell_size)
+            radius = int(radius * cell_size)
+
+        # Draw a circle on the grid using pygame
+        pygame.draw.circle(screen,color,(x,y),radius,thickness)
+
+    
+    def render_image(self):
+        pass
+
+    
+    def render_text(self,text,font = "Arial",size = 30,screen = None,color = (255,255,255)):
+        
+        x = self.x
+        y = self.y
+        cell_size = self.env.cell_size
+        
+        position = (
+            int(x* cell_size),
+            int(y* cell_size),
+        )
+
+        self.env.render_text(text,font,size,position,screen,color = color)
+
+
+
+    def render_rect(self,x = None,y = None,width = None,height = None,radius = None,color = None,center = False,screen = None,cell_size = None,thickness = 0):
+
+        # Default arguments
+        if x is None: x = self.x
+        if y is None: y = self.y
+        if color is None: color = self.color
+        if screen is None: screen = self.env.screen
+        if cell_size is None: cell_size = self.env.cell_size
+        if width is None: width = self.width
+        if height is None: height = self.height
+        if radius is not None:
+            width = radius * 2
+            height = radius * 2
+
+        # Center the square on x,y or not
+        if not center:
+            dimensions = (x*cell_size,y*cell_size,width * cell_size,height*cell_size)
+        else:
+            dimensions = (
+                int((x + 0.5 - 0.5 * width) * cell_size),
+                int((y + 0.5 - 0.5 * height) * cell_size),
+                width * cell_size,
+                height * cell_size,
+            )
+
+
+        # Draw rectangle using pygame
+        pygame.draw.rect(screen,color,dimensions,thickness)
+
+
+    def render_img(self,image = None,x = None,y = None,screen = None,cell_size = None):
+
+        if x is None: x = self.x 
+        if y is None: y = self.y
+        if cell_size is None: cell_size = self.env.cell_size
+        if image is None: image = self.image
+        if screen is None: screen = self.env.screen
+        pos = (x * cell_size,y * cell_size)
+        screen.blit(image,pos)
