ea23074e3997885254e8c220cb0149b6d405169c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:38 +0000

    Add package aehmc/0.0.2/aehmc-0.0.2.tar.gz

---
+++ packages/aehmc/aehmc-0.0.2.tar.gz/aehmc-0.0.2/aehmc/hmc.py
@@ -0,0 +1,134 @@
+from typing import Callable
+
+import aesara.tensor as aet
+import numpy as np
+from aesara.ifelse import ifelse
+from aesara.tensor.random.utils import RandomStream
+from aesara.tensor.var import TensorVariable
+
+import aehmc.integrators as integrators
+import aehmc.metrics as metrics
+import aehmc.trajectory as trajectory
+
+
+def kernel(
+    srng: RandomStream,
+    logprob_fn: TensorVariable,
+    step_size: TensorVariable,
+    inverse_mass_matrix: TensorVariable,
+    num_integration_steps: TensorVariable,
+    divergence_threshold: int = 1000,
+):
+    """Build a HMC kernel.
+
+    Parameters
+    ----------
+    srng
+        RandomStream object.
+    logprob_fn
+        A function that returns the value of the log-probability density
+        function of a chain at a given position.
+    step_size
+        The step size used in the symplectic integrator
+    inverse_mass_matrix
+        One or two-dimensional array used as the inverse mass matrix that
+        defines the euclidean metric.
+    num_integration_steps
+        The number of times we apply the symplectic integrator to integrate the trajectory.
+    divergence_threshold
+        The difference in energy above which we say the transition is
+        divergent.
+
+    Returns
+    -------
+    A kernel that takes the current state of the chain and that returns a new
+    state.
+
+
+    """
+
+    def potential_fn(x):
+        return -logprob_fn(x)
+
+    momentum_generator, kinetic_energy_fn, _ = metrics.gaussian_metric(
+        inverse_mass_matrix
+    )
+    symplectic_integrator = integrators.velocity_verlet(potential_fn, kinetic_energy_fn)
+    proposal_generator = hmc_proposal(
+        symplectic_integrator,
+        kinetic_energy_fn,
+        step_size,
+        num_integration_steps,
+        divergence_threshold,
+    )
+
+    def step(
+        q: TensorVariable,
+        potential_energy: TensorVariable,
+        potential_energy_grad: TensorVariable,
+    ):
+        """Perform a single step of the HMC algorithm."""
+        p = momentum_generator(srng)
+        (
+            q_new,
+            p_new,
+            potential_energy_new,
+            potential_energy_grad_new,
+        ) = proposal_generator(srng, q, p, potential_energy, potential_energy_grad)
+        return q_new, potential_energy_new, potential_energy_grad_new
+
+    return step
+
+
+def hmc_proposal(
+    integrator: Callable,
+    kinetic_energy: Callable[[TensorVariable], TensorVariable],
+    step_size: TensorVariable,
+    num_integration_steps: TensorVariable,
+    divergence_threshold: int,
+):
+    """Builds a function that returns a HMC proposal."""
+
+    integrate = trajectory.static_integration(
+        integrator, step_size, num_integration_steps
+    )
+
+    def propose(
+        srng: RandomStream,
+        q: TensorVariable,
+        p: TensorVariable,
+        potential_energy: TensorVariable,
+        potential_energy_grad: TensorVariable,
+    ):
+        """Use the HMC algorithm to propose a new state."""
+
+        new_q, new_p, new_potential_energy, new_potential_energy_grad = integrate(
+            q, p, potential_energy, potential_energy_grad
+        )
+
+        # flip the momentum to keep detailed balance
+        flipped_p = -1.0 * new_p
+
+        # compute transition-related quantities
+        energy = potential_energy + kinetic_energy(p)
+        new_energy = new_potential_energy + kinetic_energy(flipped_p)
+        delta_energy = energy - new_energy
+        delta_energy = aet.where(aet.isnan(delta_energy), -np.inf, delta_energy)
+        # is_transition_divergence = aet.abs(delta_energy) > divergence_threshold
+
+        p_accept = aet.clip(aet.exp(delta_energy), 0, 1.0)
+        do_accept = srng.bernoulli(p_accept)
+        (
+            final_q,
+            final_p,
+            final_potential_energy,
+            final_potential_energy_grad,
+        ) = ifelse(
+            do_accept,
+            (new_q, flipped_p, new_potential_energy, new_potential_energy_grad),
+            (q, p, potential_energy, potential_energy_grad),
+        )
+
+        return final_q, final_p, final_potential_energy, final_potential_energy_grad
+
+    return propose
---
+++ packages/aehmc/aehmc-0.0.2.tar.gz/aehmc-0.0.2/aehmc/metrics.py
@@ -0,0 +1,104 @@
+from typing import Callable, Tuple
+
+import aesara.tensor as aet
+import aesara.tensor.slinalg as slinalg
+from aesara.tensor.random.utils import RandomStream
+from aesara.tensor.shape import shape_tuple
+from aesara.tensor.var import TensorVariable
+
+
+def gaussian_metric(
+    inverse_mass_matrix: TensorVariable,
+) -> Tuple[Callable, Callable, Callable]:
+    r"""Hamiltonian dynamic on euclidean manifold with normally-distributed momentum.
+
+    The gaussian euclidean metric is a euclidean metric further characterized
+    by setting the conditional probability density :math:`\pi(momentum|position)`
+    to follow a standard gaussian distribution. A Newtonian hamiltonian
+    dynamics is assumed.
+
+    Arguments
+    ---------
+    inverse_mass_matrix
+        One or two-dimensional array corresponding respectively to a diagonal
+        or dense mass matrix. The inverse mass matrix is multiplied to a
+        flattened version of the Pytree in which the chain position is stored
+        (the current value of the random variables). The order of the variables
+        should thus match JAX's tree flattening order, and more specifically
+        that of `ravel_pytree`.
+        In particular, JAX sorts dictionaries by key when flattening them. The
+        value of each variables will appear in the flattened Pytree following
+        the order given by `sort(keys)`.
+
+    Returns
+    -------
+    momentum_generator
+        A function that generates a value for the momentum at random.
+    kinetic_energy
+        A function that returns the kinetic energy given the momentum.
+    is_turning
+        A function that determines whether a trajectory is turning back on
+        itself given the values of the momentum along the trajectory.
+
+    References
+    ----------
+    .. [1]: Betancourt, Michael. "A general metric for Riemannian manifold
+            Hamiltonian Monte Carlo." International Conference on Geometric Science of
+            Information. Springer, Berlin, Heidelberg, 2013.
+
+    """
+    shape = shape_tuple(inverse_mass_matrix)[0]
+
+    if inverse_mass_matrix.ndim == 1:
+        mass_matrix_sqrt = aet.sqrt(aet.reciprocal(inverse_mass_matrix))
+        dot, matmul = lambda x, y: x * y, lambda x, y: x * y
+    elif inverse_mass_matrix.ndim == 2:
+        tril_inv = slinalg.cholesky(inverse_mass_matrix)
+        identity = aet.eye(shape)
+        mass_matrix_sqrt = slinalg.solve_lower_triangular(tril_inv, identity)
+        dot, matmul = aet.dot, aet.dot
+    else:
+        raise ValueError(
+            f"Expected a mass matrix of dimension 1 (diagonal) or 2, got {inverse_mass_matrix.ndim}"
+        )
+
+    def momentum_generator(srng: RandomStream) -> TensorVariable:
+        norm_samples = srng.normal(0, 1, size=shape)
+        momentum = dot(mass_matrix_sqrt, norm_samples)
+        return momentum
+
+    def kinetic_energy(momentum: TensorVariable) -> TensorVariable:
+        velocity = matmul(inverse_mass_matrix, momentum)
+        kinetic_energy = 0.5 * aet.dot(velocity, momentum)
+        return kinetic_energy
+
+    def is_turning(
+        momentum_left: TensorVariable,
+        momentum_right: TensorVariable,
+        momentum_sum: TensorVariable,
+    ) -> bool:
+        """Generalized U-turn criterion.
+
+        Parameters
+        ----------
+        momentum_left
+            Momentum of the leftmost point of the trajectory.
+        momentum_right
+            Momentum of the rightmost point of the trajectory.
+        momentum_sum
+            Sum of the momenta along the trajectory.
+
+        .. [1]: Betancourt, Michael J. "Generalizing the no-U-turn sampler to Riemannian manifolds." arXiv preprint arXiv:1304.1920 (2013).
+        .. [2]: "NUTS misses U-turn, runs in cicles until max depth", Stan Discourse Forum
+                https://discourse.mc-stan.org/t/nuts-misses-u-turns-runs-in-circles-until-max-treedepth/9727/46
+        """
+        velocity_left = matmul(inverse_mass_matrix, momentum_left)
+        velocity_right = matmul(inverse_mass_matrix, momentum_right)
+
+        rho = momentum_sum - (momentum_right + momentum_left) / 2
+        turning_at_left = aet.dot(velocity_left, rho) <= 0
+        turning_at_right = aet.dot(velocity_right, rho) <= 0
+
+        return turning_at_left | turning_at_right
+
+    return momentum_generator, kinetic_energy, is_turning
