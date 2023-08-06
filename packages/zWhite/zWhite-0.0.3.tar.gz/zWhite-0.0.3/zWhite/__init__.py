0ffa4f1a3cf7408e3c8e04fd846dfd1cbd564bfb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:02:30 +0000

    Add package zWhite/0.0.3/zWhite-0.0.3.tar.gz

---
+++ packages/zWhite/zWhite-0.0.3.tar.gz/zWhite-0.0.3/zWhite/__init__.py
@@ -0,0 +1,6 @@
+from . import application
+from . import controller
+# from .logger import logger, ErrorLog
+from .config import configuration
+from .filter import filter
+from .plugin import plugin
---
+++ packages/zWhite/zWhite-0.0.3.tar.gz/zWhite-0.0.3/zWhite/config.py
@@ -0,0 +1,41 @@
+class configuration:
+    @staticmethod
+    def init(config, appPath):
+        configuration.config = config
+        configuration.default = deflautConfig()
+        configuration.appPath = appPath
+
+    @staticmethod
+    def __getattr__(name):
+        value = None
+        if hasattr(configuration.config, name):
+            value = getattr(configuration.config, name).format(appPath=configuration.appPath)
+        elif hasattr(configuration.default, name):
+            value = getattr(configuration.default, name).format(appPath=configuration.appPath)
+        return value
+
+class deflautConfig:
+
+    middleware_list = []
+
+    middleware_path = "{appPath}/middleware"
+
+    controller_path = "{appPath}/controller"
+
+    plugin_path = "{appPath}/plugin"
+
+    plugin_list = []
+
+    filter_path = "{appPath}/filter"
+
+    filter_list = []
+
+    core = "*"
+
+    cookie_id = "alauda_app_id"
+
+    encrypt = "keyskeyskeyskeys"
+
+    router_controller = "home"
+
+    router_action = "index"
