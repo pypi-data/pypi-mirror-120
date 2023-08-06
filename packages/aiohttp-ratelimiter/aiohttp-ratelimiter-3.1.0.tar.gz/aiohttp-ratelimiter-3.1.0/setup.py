190d2de9dd06b2e14f310153650bf3f88ba369a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:51:48 +0000

    Add package aiohttp-ratelimiter/3.1.0/aiohttp-ratelimiter-3.1.0.tar.gz

---
+++ packages/aiohttp-ratelimiter/aiohttp-ratelimiter-3.1.0.tar.gz/aiohttp-ratelimiter-3.1.0/setup.py
@@ -0,0 +1,51 @@
+from setuptools import setup, find_packages
+import pip
+
+def install(package):
+    if hasattr(pip, 'main'):
+        pip.main(['install', package])
+    else:
+        pip._internal.main(['install', package])
+
+install("Cython")
+
+def get_long_description():
+    with open("README.md", encoding="utf-8") as file:
+        return file.read()
+
+
+VERSION = "3.1.0"
+
+classifiers = [
+    "Development Status :: 4 - Beta",
+    "Intended Audience :: Developers",
+    "License :: OSI Approved :: MIT License",
+    "Programming Language :: Python :: 3",
+]
+from Cython.Build import cythonize
+
+def cython():
+    try:
+        r = cythonize("aiohttplimiter/utils.pyx")
+        return r
+    except ValueError:
+        return cythonize("utils.pyx")
+
+
+setup(
+    name="aiohttp-ratelimiter",
+    version=VERSION,
+    description="A simple ratelimiter for aiohttp.web",
+    long_description=get_long_description(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/Nebulizer1213/aiohttp-ratelimiter",
+    author="George Luca",
+    author_email="fixingg@gmail.com",
+    license="MIT",
+    classifiers=classifiers,
+    keywords="",
+    packages=find_packages(),
+    install_requires=["aiohttp"],
+    ext_modules=cython(),
+    package_data={"aiohttplimiter": ["*.pyi"]}
+)
