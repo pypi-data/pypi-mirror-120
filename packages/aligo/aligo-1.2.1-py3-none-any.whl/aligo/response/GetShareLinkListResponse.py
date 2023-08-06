efe01bc01f01b99739a7684d8402818ab6097a55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:40:58 +0000

    Add package aligo/1.2.1/aligo-1.2.1-py3-none-any.whl

---
+++ packages/aligo/aligo-1.2.1-py3-none-any.whl/aligo/response/GetShareLinkListResponse.py
@@ -0,0 +1,12 @@
+"""..."""
+from dataclasses import dataclass, field
+from typing import List
+
+from aligo.types import DataClass, ShareLinkSchema
+
+
+@dataclass
+class GetShareLinkListResponse(DataClass):
+    """..."""
+    items: List[ShareLinkSchema] = field(default_factory=list)
+    next_marker: str = ''
