15b24ee21ae561ee79e1ca00d628b5d6ea524b69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:29:26 +0000

    Add package upbge-stubs/0.3.0.18.dev1251995463/upbge-stubs-0.3.0.18.dev1251995463.tar.gz

---
+++ packages/upbge-stubs/upbge-stubs-0.3.0.18.dev1251995463.tar.gz/upbge-stubs-0.3.0.18.dev1251995463/bpy/__init__.pyi
@@ -0,0 +1,19 @@
+from . import utils
+
+from . import types
+
+from . import props
+
+from . import path
+
+from . import ops
+
+from . import msgbus
+
+from . import data
+
+from . import context
+
+from . import app
+
+import typing
---
+++ packages/upbge-stubs/upbge-stubs-0.3.0.18.dev1251995463.tar.gz/upbge-stubs-0.3.0.18.dev1251995463/gpu_extras/presets.pyi
@@ -0,0 +1,37 @@
+"""
+
+
+gpu_extras submodule (gpu_extras.presets)
+*****************************************
+
+:func:`draw_circle_2d`
+
+:func:`draw_texture_2d`
+
+"""
+
+import gpu
+
+import typing
+
+import mathutils
+
+def draw_circle_2d(position: mathutils.Vector, color: typing.Tuple[typing.Any, ...], radius: float) -> None:
+
+  """
+
+  Draw a circle.
+
+  """
+
+  ...
+
+def draw_texture_2d(texture: gpu.types.GPUTexture, position: mathutils.Vector, width: float, height: float) -> None:
+
+  """
+
+  Draw a 2d texture.
+
+  """
+
+  ...
