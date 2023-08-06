e280b25d0d4c256f49ef4a658c42e3b4d5668ad2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:11:05 +0000

    Add package stadtradeln-data/0.0.2rc2/stadtradeln-data-0.0.2rc2.tar.gz

---
+++ packages/stadtradeln-data/stadtradeln-data-0.0.2rc2.tar.gz/stadtradeln-data-0.0.2rc2/setup.py
@@ -0,0 +1,49 @@
+import os
+from setuptools import setup, find_packages
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name='stadtradeln-data',
+    version="0.0.2-rc-2",
+    setup_requires=['setuptools'],
+    use_scm_version=False,
+    install_requires=[
+        'tqdm',
+        'requests',
+        'click',
+        'pytest',
+        'numpy',
+        'pandas'
+    ],
+    python_requires='>=3.6',
+    package_dir={'': 'src'},
+    packages=find_packages('src'),
+    license="MIT",
+    url="https://github.com/Duam/stadtradeln-data",
+    author="Paul Daum",
+    author_email="paul.daum@posteo.de",
+    description="A python package for downloading, extracting and clipping bicycle traffic count data "
+                "from the STADTRADELN database",
+    long_description=read('README.md'),
+    long_description_content_type='text/markdown',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: MIT License",
+        "Intended Audience :: Science/Research",
+        "Natural Language :: English",
+        "Operating System :: Unix",
+        "Topic :: Scientific/Engineering :: Information Analysis",
+        "Typing :: Typed"
+    ],
+    entry_points={
+        'console_scripts': [
+            'stadtradeln-data-manager = stadtradeln_data_manager.__main__:cli'
+        ]
+    },
+)
---
+++ packages/stadtradeln-data/stadtradeln-data-0.0.2rc2.tar.gz/stadtradeln-data-0.0.2rc2/src/stadtradeln_data_tools/dataset_downloader.py
@@ -0,0 +1,64 @@
+import requests
+import pathlib
+from tqdm import tqdm
+from dataclasses import dataclass
+from stadtradeln_data_tools.status import Status
+from stadtradeln_data_tools.constants import default_cache_dir
+
+
+@dataclass
+class DownloadResult:
+    status: Status
+    filepath: str
+
+
+def download_dataset(
+        url: str,
+        destination_path: str = default_cache_dir,
+        overwrite: bool = False,
+        verify_ca_certificate: bool = False,
+) -> DownloadResult:
+    """Downloads a whole (zipped) STADTRADELN dataset from the database of the
+    Bundesministerium für Verkehr und digitale Infrastructure (BmVI)
+    and stores it locally.
+    :param url: The URL pointing to the desired dataset.
+    :param destination_path: The download destination directory.
+    :param overwrite: If set, overwrites the local file.
+    :param verify_ca_certificate: Verifies the CA-certificate if True.
+        Download may fail if your CA-certificates are not set up properly.
+        See https://stackoverflow.com/questions/63210851/python-requests-throwing-sslerror-after-downloading-certificate
+    :returns: An enum telling you if the download was successful or not and (if successful) the resulting filepath.
+    """
+    destination_path = pathlib.Path(destination_path)
+    filename = pathlib.Path(url).name
+    filepath = pathlib.Path(destination_path, filename)
+
+    if not destination_path.is_dir():
+        destination_path.mkdir(parents=True)
+
+    if filepath.is_file() and not overwrite:
+        return DownloadResult(Status.FILE_ALREADY_EXISTS, filepath)
+
+    # Get download metadata
+    try:
+        response = requests.get(url, verify=verify_ca_certificate, stream=True)
+    except requests.exceptions.SSLError:
+        return DownloadResult(Status.SSL_ERROR, "")
+
+    # Create the progress bar
+    total_size_in_bytes = int(response.headers.get('content-length', 0))
+    block_size = 10 * 1024  # 10kB
+    progress_bar = tqdm(total=total_size_in_bytes, unit='iB', ncols=100, unit_scale=True)
+
+    # Download dataset
+    with open(filepath, "w+b") as file:
+        for data in response.iter_content(block_size):
+            progress_bar.update(len(data))
+            file.write(data)
+    progress_bar.close()
+
+    # Check download
+    if total_size_in_bytes != 0 and progress_bar.n != total_size_in_bytes:
+        return DownloadResult(Status.FAILURE, "")
+
+    return DownloadResult(Status.SUCCESS, filepath)
