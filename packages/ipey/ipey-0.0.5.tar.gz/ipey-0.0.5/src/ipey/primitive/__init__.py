55035d6df4bccf865b8bc410d518082373655f56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:28:52 +0000

    Add package ipey/0.0.5/ipey-0.0.5.tar.gz

---
+++ packages/ipey/ipey-0.0.5.tar.gz/ipey-0.0.5/src/ipey/primitive/__init__.py
@@ -0,0 +1,11 @@
+from .primitive import Primitive
+from .rectangle import Rectangle
+from .line import Line
+from .polygon import Polygon
+from .spline import Spline, SplineType
+from .splinegon import Splinegon
+from .group import Group
+from .glyph import Glyph
+from .circle import Circle
+from .label import Label
+from .minipage import Minipage
