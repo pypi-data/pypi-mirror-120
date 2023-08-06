c186520f9916cf9bd22343d7841c605c216a54be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:41:00 +0000

    Add package veritastool/0.1/veritastool-0.1.tar.gz

---
+++ packages/veritastool/veritastool-0.1.tar.gz/veritastool-0.1/setup.py
@@ -0,0 +1,5 @@
+from setuptools import setup
+
+setup(name='veritastool',version='0.1',description ='veritastool',author='MAS Veritas',
+packages=['veritastool','veritastool.fairness','veritastool.custom','veritastool.config'],
+zip_safe=False)
