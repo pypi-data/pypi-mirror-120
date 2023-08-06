8c1bbec959c269b7197868ed2d93f67043014089
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:39:31 +0000

    Add package geodesicDome/2.0.12/geodesicDome-2.0.12.tar.gz

---
+++ packages/geodesicDome/geodesicDome-2.0.12.tar.gz/geodesicDome-2.0.12/setup.py
@@ -0,0 +1,14 @@
+from setuptools import find_packages, setup
+setup(
+    name='geodesicDome',
+    packages=find_packages(include=['geodesicDome']),
+    version='2.0.12',
+    
+    long_description_content_type="text/markdown",
+    install_requires=["numpy", "numba"],
+    description='Geodesic Dome',
+    author='Sam, Lewis, James, Kevin, Jacob, Manan',
+    license='MIT',
+    
+   
+)
