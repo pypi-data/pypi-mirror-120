01f5c1116849fd8dee51da04a59cab7837ccbd73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:39:54 +0000

    Add package blocktorch/1.33.2/blocktorch-1.33.2-py3-none-any.whl

---
+++ packages/blocktorch/blocktorch-1.33.2-py3-none-any.whl/blocktorch/pipelines/components/estimators/regressors/rf_regressor.py
@@ -0,0 +1,57 @@
+"""Random Forest Regressor."""
+from sklearn.ensemble import RandomForestRegressor as SKRandomForestRegressor
+from skopt.space import Integer
+
+from blocktorch.model_family import ModelFamily
+from blocktorch.pipelines.components.estimators import Estimator
+from blocktorch.problem_types import ProblemTypes
+
+from .blockwise_voting_regressor import BlockwiseVotingRegressor
+
+class RandomForestRegressor(Estimator):
+    """Random Forest Regressor.
+
+    Args:
+        n_estimators (float): The number of trees in the forest. Defaults to 100.
+        max_depth (int): Maximum tree depth for base learners. Defaults to 6.
+        n_jobs (int or None): Number of jobs to run in parallel. -1 uses all processes. Defaults to -1.
+        random_seed (int): Seed for the random number generator. Defaults to 0.
+    """
+
+    name = "Random Forest Regressor"
+    hyperparameter_ranges = {
+        "n_estimators": Integer(10, 1000),
+        "max_depth": Integer(1, 32),
+    }
+    """{
+        "n_estimators": Integer(10, 1000),
+        "max_depth": Integer(1, 32),
+    }"""
+    model_family = ModelFamily.RANDOM_FOREST
+    """ModelFamily.RANDOM_FOREST"""
+    supported_problem_types = [
+        ProblemTypes.REGRESSION,
+        ProblemTypes.TIME_SERIES_REGRESSION,
+    ]
+    """[
+        ProblemTypes.REGRESSION,
+        ProblemTypes.TIME_SERIES_REGRESSION,
+    ]"""
+
+    def __init__(
+        self, n_estimators=100, max_depth=6, n_jobs=-1, random_seed=0, **kwargs
+    ):
+        parameters = {
+            "n_estimators": n_estimators,
+            "max_depth": max_depth,
+            "n_jobs": n_jobs,
+        }
+        parameters.update(kwargs)
+
+        rf_regressor = SKRandomForestRegressor(random_state=random_seed, **parameters)
+        rf_regressor = BlockwiseVotingRegressor(
+            rf_regressor,
+        )
+        super().__init__(
+            parameters=parameters, component_obj=rf_regressor, random_seed=random_seed
+        )
---
+++ packages/blocktorch/blocktorch-1.33.2-py3-none-any.whl/blocktorch/utils/update_checker.py
@@ -0,0 +1,10 @@
+"""Check if blocktorch has updated since the user installed."""
+from pkg_resources import iter_entry_points
+
+for entry_point in iter_entry_points("alteryx_open_src_initialize"):
+    try:
+        method = entry_point.load()
+        if callable(method):
+            method("blocktorch")
+    except Exception:
+        pass
