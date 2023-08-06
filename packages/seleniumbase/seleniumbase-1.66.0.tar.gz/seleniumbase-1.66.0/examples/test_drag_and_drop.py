ad988045dfca826647c06e87b60c6c65bd088ddb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:32:06 +0000

    Add package seleniumbase/1.66.0/seleniumbase-1.66.0.tar.gz

---
+++ packages/seleniumbase/seleniumbase-1.66.0.tar.gz/seleniumbase-1.66.0/examples/test_drag_and_drop.py
@@ -0,0 +1,23 @@
+"""
+Testing Drag & Drop on different pages.
+"""
+from seleniumbase import BaseCase
+
+
+class DragAndDropTests(BaseCase):
+    def test_drag_and_drop(self):
+        self.open("https://seleniumbase.io/other/drag_and_drop")
+        self.assert_element_not_visible("#div1 img#drag1")
+        self.drag_and_drop("#drag1", "#div1")
+        self.assert_element("#div1 img#drag1")
+        self.sleep(0.8)
+
+    def test_w3schools_drag_and_drop(self):
+        url = "://w3schools.com/html/tryit.asp?filename=tryhtml5_draganddrop"
+        self.open(url)
+        self.remove_elements("#tryitLeaderboard")
+        self.switch_to_frame("iframeResult")
+        self.assert_element_not_visible("#div1 img#drag1")
+        self.drag_and_drop("#drag1", "#div1")
+        self.assert_element("#div1 img#drag1")
+        self.sleep(0.8)
