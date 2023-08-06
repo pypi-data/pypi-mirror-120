e1cb0a48de22bac9c5a75b1d050e743c4d1a43b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:31:17 +0000

    Add package OpenSeesPySubStepping/0.1.1/OpenSeesPySubStepping-0.1.1.tar.gz

---
+++ packages/OpenSeesPySubStepping/OpenSeesPySubStepping-0.1.1.tar.gz/OpenSeesPySubStepping-0.1.1/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="OpenSeesPySubStepping",
+    version="0.1.1",
+    author="Konstantinos Mixios",
+    author_email="k.mixios@gmail.com",
+    description="Substepping method for LoadControl and Displacement Control Analysis",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=setuptools.find_packages(),
+    python_requires='>=3.7.9',
+)
