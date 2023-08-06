111f359f35ae638a38d9920f69baee079891f8f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:41:02 +0000

    Add package distributions-RCR/0.1/distributions_RCR-0.1.tar.gz

---
+++ packages/distributions-RCR/distributions_RCR-0.1.tar.gz/distributions_RCR-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_RCR',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_RCR'],
+      zip_safe=False)
