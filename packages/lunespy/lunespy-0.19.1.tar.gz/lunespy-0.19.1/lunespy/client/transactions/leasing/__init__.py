8c2977370900749f88ee63d6bdb3c0e402d37393
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:46 +0000

    Add package lunespy/0.19.1/lunespy-0.19.1.tar.gz

---
+++ packages/lunespy/lunespy-0.19.1.tar.gz/lunespy-0.19.1/lunespy/client/transactions/leasing/__init__.py
@@ -0,0 +1,14 @@
+from lunespy.client.transactions import BaseTransaction
+
+class Leasing(BaseTransaction):
+    def __init__(self):
+        pass
+
+    def ready(self):
+        pass
+
+    def transaction(self):
+        pass
+
+    def send(self):
+        pass
