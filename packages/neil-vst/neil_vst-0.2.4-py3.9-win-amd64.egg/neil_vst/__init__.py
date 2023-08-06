323be0bef2f82f7cc390848dfd03d5452e563106
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:03:28 +0000

    Add package neil-vst/0.2.4/neil_vst-0.2.4-py3.9-win-amd64.egg

---
+++ packages/neil-vst/neil_vst-0.2.4-py3.9-win-amd64.egg/neil_vst/__init__.py
@@ -0,0 +1,8 @@
+
+__version__ = '0.2.4'
+
+from .vst_host import VstHost
+from .vst_plugin import VstPlugin
+from .vst_chain_worker import VstChainWorker
+from .vst_exceptions import VSTPluginLoadException
+from .logger import NLogger
