ed33950bf840795df894172b40c7172e5bf06874
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:26 +0000

    Add package ebayoauth/0.0.1/ebayoauth-0.0.1.tar.gz

---
+++ packages/ebayoauth/ebayoauth-0.0.1.tar.gz/ebayoauth-0.0.1/README.md
@@ -0,0 +1,5 @@
+# ebay-oauth-python-client
+
+eBay OAuth Python Client
+
+eBay OAuth client library is a simple and easy-to-use library for integrating with eBay OAuth and designed to be used for OAuth 2.0 specification supported by eBay. There are multiple standard clients that can be used with eBay OAuth, such as Spring OAuth client. However, this library in addition to functioning as a simple eBay OAuth client, helps with additional features such as cached App tokens.
---
+++ packages/ebayoauth/ebayoauth-0.0.1.tar.gz/ebayoauth-0.0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'ebayoauth',
+    packages = find_packages(),
+    version = '0.0.1',  
+    description = 'eBay OAuth Client',
+    author = '',
+    author_email = '',
+    url = 'https://github.com/aissa-aouli/ebay-oauth-python-client.git',
+    download_url = 'https://github.com/aissa-aouli/ebay-oauth-python-client/archive/refs/tags/0.0.1.tar.gz',
+    keywords = ['ebay', 'oauth', 'api'],
+    classifiers = [],
+)
