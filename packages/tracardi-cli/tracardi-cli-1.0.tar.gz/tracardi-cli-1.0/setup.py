d6f19bbf76dea24ea602f751c510cd42faaf6e67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:24:45 +0000

    Add package tracardi-cli/1.0/tracardi-cli-1.0.tar.gz

---
+++ packages/tracardi-cli/tracardi-cli-1.0.tar.gz/tracardi-cli-1.0/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+setuptools.setup(
+    name='tracardi-cli',
+    version='1.0',
+    entry_points={
+        'console_scripts': ['tracardi=tracardi_cli.create_plugin:cli'],
+    },
+    author='Me',
+    description='This is tracardi CLI.',
+    packages=['tracardi_cli'],
+    install_requires=[
+        'setuptools',
+        'argparse',
+        'jinja2'
+    ],
+    python_requires='>=3.5',
+    include_package_data=True
+)
