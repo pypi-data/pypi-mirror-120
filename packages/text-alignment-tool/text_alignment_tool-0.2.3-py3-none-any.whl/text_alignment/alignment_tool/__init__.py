8bdec88d7d41ceb9854e840bc5641ea7596081b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:30:20 +0000

    Add package text-alignment-tool/0.2.3/text_alignment_tool-0.2.3-py3-none-any.whl

---
+++ packages/text-alignment-tool/text_alignment_tool-0.2.3-py3-none-any.whl/text_alignment/alignment_tool/__init__.py
@@ -0,0 +1,14 @@
+from aligner import (
+    TextAlignmentException,
+    AlignedIndices,
+    AlignmentTextDataObject,
+    AlignmentOperation,
+)
+from alignment_tool import (
+    TextAlignmentTool,
+    get_text_for_range,
+    get_text_chunk_string,
+    get_text_chunks_string,
+    compare_parallel_text_chunks,
+    print_parallel_text_chunks,
+)
