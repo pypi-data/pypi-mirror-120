a54f9a06c3630a0e785890b8d1250d2cde5535e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:19 +0000

    Add package abcd-yt9898/1.0.0/abcd_yt9898-1.0.0.tar.gz

---
+++ packages/abcd-yt9898/abcd_yt9898-1.0.0.tar.gz/abcd_yt9898-1.0.0/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="abcd_yt9898",
+    version="1.0.0",
+    author="tongye",
+    author_email="791161009@qq.com",
+    description="test install requires",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://blog.xiaoqying.com",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=["numpy","matplotlib"],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)
---
+++ packages/abcd-yt9898/abcd_yt9898-1.0.0.tar.gz/abcd_yt9898-1.0.0/src/abcd_yt9898/addone.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+def add(number):
+	return number+1
+
+def list():
+	data = np.arange(10).reshape(2,5)
+	print(data)
+
