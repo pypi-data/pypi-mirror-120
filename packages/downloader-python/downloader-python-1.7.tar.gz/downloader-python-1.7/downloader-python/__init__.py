217ba36f2befc6d576b3bff2a75c4751a1884a82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:44:28 +0000

    Add package downloader-python/1.7/downloader-python-1.7.tar.gz

---
+++ packages/downloader-python/downloader-python-1.7.tar.gz/downloader-python-1.7/downloader-python/__init__.py
@@ -0,0 +1,1 @@
+print("Can't import,you can found this path:'<python_path>/Lib/site-pakages/downloader-python/main.py',and then,use python to run it.")
---
+++ packages/downloader-python/downloader-python-1.7.tar.gz/downloader-python-1.7/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+with open('README.md','r',encoding='utf-8') as f:
+    long_description = f.read()
+setup(
+    name='downloader-python',
+    version='1.7',
+    url='https://github.com',
+    long_description=long_description,
+    author='mc_creater',
+    author_email='guoxiuchen20170402@163.com',
+    license='MIT',
+    packages=['downloader-python','command','pyim','exes','doge'],
+    classifiers=['Programming Language :: Python :: 3.6',
+                 'Programming Language :: Python :: 3.7',
+                 'Programming Language :: Python :: 3.8',
+                 'Programming Language :: Python :: 3.9',],
+    install_requires=['PyQt5','requests','bs4','lxml'],
+    include_package_data=True
+
+)
