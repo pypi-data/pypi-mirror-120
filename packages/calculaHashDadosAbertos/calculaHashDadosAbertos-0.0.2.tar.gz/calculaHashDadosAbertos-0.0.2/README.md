1ed224bb6b2d7a17c60c5ee913912efcfba6c13b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:11:27 +0000

    Add package calculaHashDadosAbertos/0.0.2/calculaHashDadosAbertos-0.0.2.tar.gz

---
+++ packages/calculaHashDadosAbertos/calculaHashDadosAbertos-0.0.2.tar.gz/calculaHashDadosAbertos-0.0.2/README.md
@@ -0,0 +1,21 @@
+# calculaHashDadosAbertos
+The library can calculate hash files from dados abertos website in MD5 format
+Example of PyPI package.
+## Getting Started
+#### Dependencies
+You need Python 3.7 or later to use **calculaHashDadosAbertos**. You can find it at [python.org](https://www.python.org/).
+
+```
+pip install setuptools
+pip install wheel
+pip install twine
+```
+#### Installation
+Clone this repo to your local machine using:
+```
+git clone https://github.com/masuta16/calculaHash.git
+```
+## Features
+- File structure for PyPI packages
+- Setup with package informations
+- License example
---
+++ packages/calculaHashDadosAbertos/calculaHashDadosAbertos-0.0.2.tar.gz/calculaHashDadosAbertos-0.0.2/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    readme = fh.read()
+
+setup(name='calculaHashDadosAbertos',
+    version='0.0.2',
+    url='https://github.com/masuta16/calculaHash',
+    license='MIT License',
+    author='Israel Andrade',
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    author_email='israndade16@hotmail.com',
+    keywords='Pacote',
+    description=u'Retorna dados de hash dos ultimos n dias da biblioteca de dados abertos do governo',
+    packages=['calculaHashDadosAbertos'],
+    install_requires=['requests','pandas','datetime'],)
+
