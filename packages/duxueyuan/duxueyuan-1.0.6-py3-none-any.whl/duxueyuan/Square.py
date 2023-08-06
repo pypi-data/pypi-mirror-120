060b81c9a4628a27c5e6b6452626406f463b725c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:22:40 +0000

    Add package duxueyuan/1.0.6/duxueyuan-1.0.6-py3-none-any.whl

---
+++ packages/duxueyuan/duxueyuan-1.0.6-py3-none-any.whl/duxueyuan/Square.py
@@ -0,0 +1,12 @@
+class Square:
+	def __init__(self):
+		self.data = 3
+
+	def square(self, x):
+		return x * x 
+
+	def squarefuture(self, x):
+		return x * x * x 
+
+	def get_data(self):
+		return self.data 
