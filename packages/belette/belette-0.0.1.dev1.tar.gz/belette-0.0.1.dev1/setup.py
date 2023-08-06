52ab0f544e231c7ba849e5a36c451fea6e9040b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:38:49 +0000

    Add package belette/0.0.1.dev1/belette-0.0.1.dev1.tar.gz

---
+++ packages/belette/belette-0.0.1.dev1.tar.gz/belette-0.0.1.dev1/setup.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['belette']
+
+package_data = \
+{'': ['*'], 'belette': ['licenses/*', 'templates/*']}
+
+install_requires = \
+['conan>=1.35.1,<2.0.0', 'docopt>=0.6.2,<0.7.0']
+
+entry_points = \
+{'console_scripts': ['belette = belette.weasel:main',
+                     'weasel = belette.weasel:main']}
+
+setup_kwargs = {
+    'name': 'belette',
+    'version': '0.0.1.dev1',
+    'description': 'Generates licensing information for dependencies for Conan projects',
+    'long_description': None,
+    'author': 'Alazar Technologies Inc.',
+    'author_email': 'support@alazartech.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
