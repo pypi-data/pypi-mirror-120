003b3500ccb5c6b58050632d0007b2a0146f34e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:40:26 +0000

    Add package amaindexsnippetworkflow/0.1.1/amaindexsnippetworkflow-0.1.1-py3-none-any.whl

---
+++ packages/amaindexsnippetworkflow/amaindexsnippetworkflow-0.1.1-py3-none-any.whl/amaindexsnippetworkflow/hello.py
@@ -0,0 +1,11 @@
+class Hello:
+    who: str
+
+    def __init__(self, who):
+        self.who = who
+
+    def say(self):
+        print(f"Hello, {self.who}")
+
+    def get_who(self):
+        return self.who
