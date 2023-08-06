17ac6441c4595352ca15d40792b26ddd0e65f6ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:48:27 +0000

    Add package concretefhe-hnumpy/0.0.10/concretefhe-hnumpy-0.0.10.tar.gz

---
+++ packages/concretefhe-hnumpy/concretefhe-hnumpy-0.0.10.tar.gz/concretefhe-hnumpy-0.0.10/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="concretefhe-hnumpy",
+    version="0.0.10",
+    author="zama",
+    author_email="hello@zama.ai",
+    description="Concrete hnumpy for the Concrete Framework",
+    packages=("concretefhe.hnumpy",),
+    namespace_packages=("concretefhe",),
+    package_dir={"concretefhe.hnumpy": "hnumpy"},
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://zama.ai",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.8",
+)
