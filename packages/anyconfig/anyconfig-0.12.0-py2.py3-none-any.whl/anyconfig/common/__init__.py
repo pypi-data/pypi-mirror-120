b44802d84038e37b0b2d7e8d1de24baad6fb43f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:44 +0000

    Add package anyconfig/0.12.0/anyconfig-0.12.0-py2.py3-none-any.whl

---
+++ packages/anyconfig/anyconfig-0.12.0-py2.py3-none-any.whl/anyconfig/common/__init__.py
@@ -0,0 +1,22 @@
+#
+# Copyright (C) 2021 Satoru SATOH <satoru.satoh@gmail.com>
+# SPDX-License-Identifier: MIT
+#
+r"""misc global constants, variables, classes and so on.
+"""
+from .datatypes import (
+    InDataT, InDataExT, PrimitiveT
+)
+from .errors import (
+    UnknownParserTypeError, UnknownProcessorTypeError, UnknownFileTypeError,
+    ValidationError
+)
+
+
+__all__ = [
+    'InDataT', 'InDataExT', 'PrimitiveT',
+    'UnknownParserTypeError', 'UnknownProcessorTypeError',
+    'UnknownFileTypeError', 'ValidationError',
+]
+
+# vim:sw=4:ts=4:et:
---
+++ packages/anyconfig/anyconfig-0.12.0-py2.py3-none-any.whl/anyconfig/ioinfo/constants.py
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2018 - 2021 Satoru SATOH <satoru.satoh @ gmmail.com>
+# SPDX-License-Identifier: MIT
+#
+r"""ioinfo.constants to provide global constant variables.
+"""
+import os.path
+
+
+GLOB_MARKER: str = '*'
+PATH_SEP: str = os.path.sep
+
+# vim:sw=4:ts=4:et:
