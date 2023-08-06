9d03003c0a5b4bd5ad82eb89a7931d831d4536f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:52 +0000

    Add package cicdtest/2.84/cicdtest-2.84-py3-none-any.whl

---
+++ packages/cicdtest/cicdtest-2.84-py3-none-any.whl/ci_commands/start.py
@@ -0,0 +1,36 @@
+"""
+    Title: start.py
+    Author: Kushagra A.
+    Language: Python
+    Date Created: 10-09-2021
+    Date Modified: 11-09-2021
+    Description:
+        ###############################################################
+        ## Create a cron process   ## 
+         ###############################################################
+ """
+
+
+import click
+from click.decorators import command
+from crontab import CronTab
+import os
+import sys, subprocess
+
+@click.command()
+def start():
+    '''
+    For initiating the cron process 
+
+    \f
+    '''
+    client_os = sys.platform
+    
+    if client_os == "linux":
+        path = subprocess.run("whereis buildpan", shell = True)
+        print(path)
+    # cron_job = CronTab(user=True)
+    # cron_path = os.environ.get("cron_path")
+    # job = cron_job.new(command=f'{cron_path} pull')
+    # job.minute.every(1)
+    # cron_job.write()
