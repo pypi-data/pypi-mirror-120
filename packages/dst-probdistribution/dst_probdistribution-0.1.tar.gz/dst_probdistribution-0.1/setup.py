f7a6859f5e58f5e26b43e516341cbeb8ad1179d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:40 +0000

    Add package dst-probdistribution/0.1/dst_probdistribution-0.1.tar.gz

---
+++ packages/dst-probdistribution/dst_probdistribution-0.1.tar.gz/dst_probdistribution-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='dst_probdistribution',
+      version='0.1',
+      description='Gaussian & Binomial distributions',
+      packages=['dst_probdistribution'],
+      author='Hillary',
+      author_email='hillarywang2005@gmail.com',
+      zip_safe=False)
