408f0be961dd6ef51025dac29e97aa477094020c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:55 +0000

    Add package novelsave-sources/0.2.1/novelsave-sources-0.2.1.tar.gz

---
+++ packages/novelsave-sources/novelsave-sources-0.2.1.tar.gz/novelsave-sources-0.2.1/novelsave_sources/__init__.py
@@ -0,0 +1,9 @@
+__version__ = '0.2.1'
+
+from .exceptions import (
+    SourcesException, BadResponseException, UnknownSourceException,
+    UnavailableException, ChapterException,
+)
+from .models import Novel, Volume, Chapter, Metadata
+from .sources import sources, meta_sources
+from .utils.parsers import parse_source, parse_metasource
