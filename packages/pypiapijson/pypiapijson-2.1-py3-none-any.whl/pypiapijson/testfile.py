e4a5ffb49732c68b72a7129aca4d56b0c7fd6bcb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:31:38 +0000

    Add package pypiapijson/2.1/pypiapijson-2.1-py3-none-any.whl

---
+++ packages/pypiapijson/pypiapijson-2.1-py3-none-any.whl/pypiapijson/testfile.py
@@ -0,0 +1,4 @@
+import pypiapijson as  __init__;print(__init__.get("flask"));print(__init__.getbyv("pypiapijson",1));print(__init__.status())
+asyncs = __init__.asyncs
+async def test():import asyncs;print(asyncs.get("flask"))
+import asyncio;print(asyncio.run(test()))
