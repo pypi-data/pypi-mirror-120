89b2889de246f7debc7e8d13dad54251afdd35ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:40 +0000

    Add package 2ppy/0.1.10/2ppy-0.1.10-py3-none-any.whl

---
+++ packages/2ppy/2ppy-0.1.10-py3-none-any.whl/tuprolog/__main__.py
@@ -0,0 +1,16 @@
+from tuprolog import Info
+from tuprolog.core import *
+from tuprolog.core.comparators import *
+from tuprolog.core.exception import *
+from tuprolog.core.formatters import *
+from tuprolog.core.operators import *
+from tuprolog.core.visitors import *
+from tuprolog.core.parsing import *
+from tuprolog.unify import *
+from tuprolog.theory import *
+from tuprolog.theory.parsing import *
+from tuprolog.solve import *
+
+import code
+
+code.interact(local=locals())
