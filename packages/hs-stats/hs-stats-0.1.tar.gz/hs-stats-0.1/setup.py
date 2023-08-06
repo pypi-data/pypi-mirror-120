28c877203f2f9d3dc7891c7a369bf887abd2aa59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:35 +0000

    Add package hs-stats/0.1/hs-stats-0.1.tar.gz

---
+++ packages/hs-stats/hs-stats-0.1.tar.gz/hs-stats-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='hs-stats',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['hs-stats'],
+      author='Hério Sousa',
+      author_email='heriosousa@hotmail.com',
+      zip_safe=False)
