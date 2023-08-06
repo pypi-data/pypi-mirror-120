f68243570e51d73b8da496ef918370fe54b2e4ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:25 +0000

    Add package nexx-probability/0.1/nexx_probability-0.1.tar.gz

---
+++ packages/nexx-probability/nexx_probability-0.1.tar.gz/nexx_probability-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='nexx_probability',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['nexx_probability'],
+      author = 'Narayan Soni',
+      zip_safe=False)
