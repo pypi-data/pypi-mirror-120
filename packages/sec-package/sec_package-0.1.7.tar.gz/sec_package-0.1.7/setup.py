8ddb1d4004798d3c69e6a6ca4aa2028392771491
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:03:13 +0000

    Add package sec-package/0.1.7/sec_package-0.1.7.tar.gz

---
+++ packages/sec-package/sec_package-0.1.7.tar.gz/sec_package-0.1.7/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['sec_package']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['black>=21.9b0,<22.0', 'flake8>=3.9.2,<4.0.0']
+
+setup_kwargs = {
+    'name': 'sec-package',
+    'version': '0.1.7',
+    'description': '',
+    'long_description': None,
+    'author': 'sven',
+    'author_email': 'sven.lauenroth@icloud.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
