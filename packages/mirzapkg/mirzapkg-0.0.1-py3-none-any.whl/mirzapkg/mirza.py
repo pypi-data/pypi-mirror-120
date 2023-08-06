9d21efab2a928afe59c4495932a567994c838132
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:23:24 +0000

    Add package mirzapkg/0.0.1/mirzapkg-0.0.1-py3-none-any.whl

---
+++ packages/mirzapkg/mirzapkg-0.0.1-py3-none-any.whl/mirzapkg/mirza.py
@@ -0,0 +1,11 @@
+import sys
+
+#doool is dooling on the dool for sake of dool
+def main():
+ 
+     print (sys.argv[1:].split())
+     exit()
+     #yyhg
+     
+if __name__ == "__main__":
+    main()
