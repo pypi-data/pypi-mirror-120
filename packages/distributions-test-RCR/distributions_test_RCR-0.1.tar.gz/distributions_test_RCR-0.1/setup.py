63c1ac3082df176c783279097919560db0677a0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:40:29 +0000

    Add package distributions-test-RCR/0.1/distributions_test_RCR-0.1.tar.gz

---
+++ packages/distributions-test-RCR/distributions_test_RCR-0.1.tar.gz/distributions_test_RCR-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_test_RCR',
+      version='0.1',
+      description='Gaussian distributions, binomial distributions',
+      packages=['distributions_test_RCR'],
+      zip_safe=False)
