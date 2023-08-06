9b16b09530adfeb5855219bc3b929e5ed328e8c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:41:05 +0000

    Add package pyhoma/0.5.21/pyhoma-0.5.21-py3-none-any.whl

---
+++ packages/pyhoma/pyhoma-0.5.21-py3-none-any.whl/pyhoma/exceptions.py
@@ -0,0 +1,30 @@
+class BadCredentialsException(Exception):
+    pass
+
+
+class InvalidCommandException(Exception):
+    pass
+
+
+class NotAuthenticatedException(Exception):
+    pass
+
+
+class TooManyExecutionsException(Exception):
+    pass
+
+
+class TooManyRequestsException(Exception):
+    pass
+
+
+class MaintenanceException(Exception):
+    pass
+
+
+class InvalidEventListenerIdException(Exception):
+    pass
+
+
+class NoRegisteredEventListenerException(Exception):
+    pass
