7021502142349376d485f58169a3ad17614f8ca3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:44 +0000

    Add package oneneuron-pypi-keithferns98/0.0.2/oneneuron_pypi-keithferns98-0.0.2.tar.gz

---
+++ packages/oneneuron-pypi-keithferns98/oneneuron_pypi-keithferns98-0.0.2.tar.gz/oneneuron_pypi-keithferns98-0.0.2/setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+Project_Name="oneneuron_pypi"
+User_Name="keithferns98"
+setuptools.setup(
+    name=f"{Project_Name}-{User_Name}",
+    version="0.0.2",
+    author=User_Name,
+    author_email="keithfernandes311@gmail.com",
+    description="Perceptron package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/keithferns98/oneneuron_pypi",
+    project_urls={
+        "Bug Tracker": "https://github.com/keithferns98/oneneuron_pypi/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.7",
+    install_requires=[
+        "numpy",
+        "tqdm"
+        
+    ]
+)
