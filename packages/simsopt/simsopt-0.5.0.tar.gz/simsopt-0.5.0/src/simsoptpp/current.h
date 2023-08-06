0b4a73db409035a16587c7b6101da77021fd0e40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:33:40 +0000

    Add package simsopt/0.5.0/simsopt-0.5.0.tar.gz

---
+++ packages/simsopt/simsopt-0.5.0.tar.gz/simsopt-0.5.0/src/simsoptpp/current.h
@@ -0,0 +1,30 @@
+#pragma once 
+#include <memory>
+
+template<class Array>
+class CurrentBase {
+    public:
+        virtual double get_value();
+};
+
+template<class Array>
+class Current : public CurrentBase<Array>{
+    private:
+        double value;
+    public:
+        Current(double value) : value(value) {}
+        inline void set_dofs(Array& dofs) { value=dofs.data()[0]; };
+        inline Array get_dofs() { return Array({value}); };
+        double get_value() override { return value; }
+};
+
+template<class Array>
+class ScaledCurrent : public CurrentBase<Array> {
+    private:
+        const std::shared_ptr<CurrentBase<Array>> current;
+
+    public:
+        const double scale;
+        ScaledCurrent(std::shared_ptr<CurrentBase<Array>> current, double scale) : current(current), scale(scale) {}
+        double get_value() override { return scale * (current->get_value()); }
+};
