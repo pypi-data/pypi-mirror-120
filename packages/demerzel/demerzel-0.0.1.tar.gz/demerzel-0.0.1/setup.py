da818ae3c94b0ed5b223abd3c36653dab09ef997
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:14 +0000

    Add package demerzel/0.0.1/demerzel-0.0.1.tar.gz

---
+++ packages/demerzel/demerzel-0.0.1.tar.gz/demerzel-0.0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+
+setup(
+    name="demerzel",
+    version="0.0.1",
+    description="Simple workflow scheduling and monitoring",
+    author="Wayde Gilliam",
+    author_email="waydegilliam@gmail.com",
+    url="https://github.com/waydegg/demerzel",
+    license="MIT",
+    packages=["demerzel"],
+    python_requires=">=3.9"
+)
