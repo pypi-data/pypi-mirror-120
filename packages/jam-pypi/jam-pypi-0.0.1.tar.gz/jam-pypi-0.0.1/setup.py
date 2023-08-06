8c08787c0b192c808a104450155842ab4d4449bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:27 +0000

    Add package jam-pypi/0.0.1/jam-pypi-0.0.1.tar.gz

---
+++ packages/jam-pypi/jam-pypi-0.0.1.tar.gz/jam-pypi-0.0.1/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['jam_pypi']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['numpy>=1.21.2,<2.0.0']
+
+setup_kwargs = {
+    'name': 'jam-pypi',
+    'version': '0.0.1',
+    'description': 'Tesing repo for publish via ci',
+    'long_description': None,
+    'author': 'qsh.27',
+    'author_email': 'qsh.zh27@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<3.9',
+}
+
+
+setup(**setup_kwargs)
