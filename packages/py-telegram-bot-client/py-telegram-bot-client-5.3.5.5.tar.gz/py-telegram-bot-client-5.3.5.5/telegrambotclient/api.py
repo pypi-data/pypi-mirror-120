67694a1852b878436827a7b9ad570b9ce0783b37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:40:52 +0000

    Add package py-telegram-bot-client/5.3.5.5/py-telegram-bot-client-5.3.5.5.tar.gz

---
+++ packages/py-telegram-bot-client/py-telegram-bot-client-5.3.5.5.tar.gz/py-telegram-bot-client-5.3.5.5/telegrambotclient/api.py
@@ -0,0 +1,355 @@
+try:
+    import ujson as json
+except ImportError:
+    import json
+
+import socket
+from typing import Any, Callable, Dict, List, Tuple, Union
+
+import urllib3
+
+from telegrambotclient.base import (BotCommand, BotCommandScope,
+                                    InlineQueryResult, InputFile, InputMedia,
+                                    LabeledPrice, Message,
+                                    PassportElementError, TelegramBotException,
+                                    TelegramObject, Update, logger)
+from telegrambotclient.utils import exclude_none, pretty_format
+
+DEFAULT_API_HOST = "https://api.telegram.org"
+
+
+class TelegramBotAPIException(TelegramBotException):
+    __slots__ = ("_error", )
+
+    def __init__(self, error: TelegramObject) -> None:
+        super().__init__(
+            error.get("description", "unknown telegrambot api error"))
+        self._error = error
+
+    @property
+    def error(self):
+        return self._error
+
+    def __repr__(self) -> str:
+        pretty_format(self._error)
+
+
+class TelegramBotAPI:
+
+    __version__ = "5.3"
+    _api_url = "/bot{0}/{1}"
+    _download_url = "/file/bot{0}/{1}"
+    __slots__ = ("_api_caller", )
+
+    def __init__(self,
+                 api_host: str = DEFAULT_API_HOST,
+                 maxsize: int = 10,
+                 block: bool = True,
+                 **pool_kwargs):
+        class TelegramBotAPICaller:
+            __slots__ = ("_pool", "_api_host")
+            _json_header = {"Content-Type": "application/json"}
+
+            def __init__(self, api_host: str, maxsize: int, block: bool,
+                         **other_pool_kwargs):
+                self._api_host = api_host.lower(
+                ) if api_host else DEFAULT_API_HOST
+                other_pool_kwargs.get("headers", {}).update({
+                    "connection":
+                    "keep-alive",
+                    "user-agent":
+                    "telegram-bot-client: A Telegram Bot API Python Provider",
+                })
+                other_pool_kwargs["socket_options"] = (
+                    other_pool_kwargs.get("socket_options", []) + [
+                        (socket.IPPROTO_TCP, socket.TCP_NODELAY,
+                         1),  # from urllib3.connection.default_socket_options
+                        (socket.SOL_SOCKET, socket.SO_KEEPALIVE, 1),
+                    ])
+                if api_host.startswith("https://"):
+                    self._pool = urllib3.HTTPSConnectionPool(
+                        host=self._api_host[8:],
+                        maxsize=maxsize,
+                        block=block,
+                        **other_pool_kwargs)
+                elif api_host.startswith("http://"):
+                    self._pool = urllib3.HTTPConnectionPool(
+                        host=self._api_host[7:],
+                        maxsize=maxsize,
+                        block=block,
+                        **other_pool_kwargs)
+                else:
+                    raise TelegramBotException(
+                        "Telegram Bot API Host only supports https:// and http://"
+                    )
+
+            @property
+            def api_host(self):
+                return self._api_host
+
+            @staticmethod
+            def __format_response(response):
+                if response.status == 500:
+                    raise TelegramBotException(response.data)
+                json_response = json.loads(response.data.decode("utf-8"))
+                if response.status == 200:
+                    result = json_response["result"]
+                    if isinstance(result, dict):
+                        return TelegramObject(**result)
+                    return result
+                raise TelegramBotAPIException(TelegramObject(**json_response))
+
+            def call(self,
+                     api_url: str,
+                     data: Dict = None,
+                     files: List = None) -> Any:
+                if data is None:
+                    data = {}
+                if not files:
+                    return self.__format_response(
+                        self._pool.request(
+                            "POST",
+                            api_url,
+                            body=json.dumps(data).encode("utf-8"),
+                            headers=self._json_header,
+                        ))
+                for _ in files:
+                    data[_[0]] = _[1]
+                return self.__format_response(
+                    self._pool.request("POST", api_url, fields=data))
+
+        self._api_caller = TelegramBotAPICaller(api_host=api_host,
+                                                maxsize=maxsize,
+                                                block=block,
+                                                **pool_kwargs)
+
+    @property
+    def host(self):
+        return self._api_caller.api_host
+
+    @property
+    def download_url(self):
+        return self._download_url
+
+    @staticmethod
+    def __prepare_request_data(api_name, **kwargs) -> Tuple[str, Dict, List]:
+        api_data = exclude_none(**kwargs)
+        attached_files = []
+        for field in tuple(api_data.keys()):
+            value = api_data[field]
+            if isinstance(value, TelegramObject):
+                api_data[field] = value.param
+                continue
+            if isinstance(value, InputFile):
+                if field == "thumb":
+                    attached_files.append((value.attach_key, value.file_tuple))
+                    api_data["thumb"] = value.attach_str
+                else:
+                    attached_files.append((field, value.file_tuple))
+                    del api_data[field]
+        return api_name.replace("_", "").lower(), api_data, attached_files
+
+    def __call_api(
+        self,
+        token: str,
+        api_name: str,
+        data: Dict = None,
+        files: List = None,
+    ):
+        return self._api_caller.call(self._api_url.format(token, api_name),
+                                     data, files)
+
+    def __getattr__(self, api_name: str) -> Callable:
+        def bot_api_method(token: str, **kwargs):
+            real_api_name, form_data, attached_files = self.__prepare_request_data(
+                api_name, **kwargs)
+            return self.__call_api(token,
+                                   real_api_name,
+                                   data=form_data,
+                                   files=attached_files)
+
+        return bot_api_method
+
+    def get_updates(self, token: str, **kwargs):
+        if "allowed_updates" in kwargs:
+            kwargs["allowed_updates"] = json.dumps(kwargs["allowed_updates"])
+        return tuple(
+            Update(**raw_update)
+            for raw_update in self.getUpdates(token, **kwargs))
+
+    def set_webhook(self, token: str = None, **kwargs) -> bool:
+        if "allowed_updates" in kwargs:
+            kwargs["allowed_updates"] = json.dumps(kwargs["allowed_updates"])
+        return self.setwebhook(token, **kwargs)
+
+    def send_media_group(self, token: str, chat_id: Union[int, str], media,
+                         **kwargs) -> Message:
+        if 2 <= len(media) <= 10:
+            files = []
+            media_group = []
+            for input_media in media:
+                if isinstance(input_media, InputMedia):
+                    files.extend(input_media.attached_files)
+                    media_group.append(input_media.media_data)
+            real_api_name, form_data, attached_files = self.__prepare_request_data(
+                "sendMediaGroup",
+                chat_id=chat_id,
+                media=json.dumps(media_group),
+                **kwargs)
+            return self.__call_api(
+                token,
+                real_api_name,
+                data=form_data,
+                files=attached_files + files if attached_files else files,
+            )
+        raise TelegramBotException("'media' must include 2-10 items")
+
+    def edit_message_media(self,
+                           token: str,
+                           chat_id: Union[int, str] = None,
+                           message_id: int = None,
+                           inline_message_id: int = None,
+                           media: InputMedia = None,
+                           **kwargs) -> Message:
+        if not media:
+            raise TelegramBotException("'media' is required")
+        real_api_name, form_data, attached_files = self.__prepare_request_data(
+            "editMessageMedia",
+            chat_id=chat_id,
+            message_id=message_id,
+            inline_message_id=inline_message_id,
+            media=json.dumps(media.media_data),
+            **kwargs)
+        return self.__call_api(
+            token,
+            real_api_name,
+            data=form_data,
+            files=attached_files +
+            media.attached_files if attached_files else media.attached_files,
+        )
+
+    def set_my_commands(self,
+                        token: str,
+                        commands: Tuple[BotCommand],
+                        scope: BotCommandScope = None,
+                        language_code: str = None) -> bool:
+        data = {
+            "commands": json.dumps(commands),
+            "scope": json.dumps(scope) if scope else None,
+            "language_code": language_code
+        }
+        return self.__call_api(token,
+                               "setmycommands",
+                               data=exclude_none(**data))
+
+    def delete_my_commands(self,
+                           token: str,
+                           scope: BotCommandScope = None,
+                           language_code: str = None):
+        data = {
+            "scope": json.dumps(scope) if scope else None,
+            "language_code": language_code,
+        }
+        return self.__call_api(token,
+                               "deletemycommands",
+                               data=exclude_none(**data))
+
+    def get_my_commands(self,
+                        token: str,
+                        scope: BotCommandScope = None,
+                        language_code: str = None):
+        data = {
+            "scope": json.dumps(scope) if scope else None,
+            "language_code": language_code,
+        }
+        return self.__call_api(token,
+                               "getmycommands",
+                               data=exclude_none(**data))
+
+    def send_poll(self, token: str, chat_id: Union[int, str], question: str,
+                  options: Tuple[str], **kwargs) -> Message:
+        real_api_name, form_data, attached_files = self.__prepare_request_data(
+            "sendPoll",
+            chat_id=chat_id,
+            question=question,
+            options=json.dumps(options),
+            **kwargs)
+        return self.__call_api(token,
+                               real_api_name,
+                               data=form_data,
+                               files=attached_files)
+
+    def answer_inline_query(self, token: str, inline_query_id: str,
+                            results: Union[List[InlineQueryResult],
+                                           Tuple[InlineQueryResult]],
+                            **kwargs) -> bool:
+        real_api_name, form_data, attached_files = self.__prepare_request_data(
+            "answerInlineQuery",
+            inline_query_id=inline_query_id,
+            results=json.dumps(results),
+            **kwargs)
+        return self.__call_api(token,
+                               real_api_name,
+                               data=form_data,
+                               files=attached_files)
+
+    def send_invoice(self, token: str, chat_id: int, title: str,
+                     description: str, payload: str, provider_token: str,
+                     currency: str, prices: Tuple[LabeledPrice],
+                     **kwargs) -> Message:
+        provider_data = kwargs.get("provider_data", None)
+        if provider_data:
+            kwargs["provider_data"] = json.dumps(provider_data)
+        suggested_tip_amounts = kwargs.get("suggested_tip_amounts", None)
+        if suggested_tip_amounts:
+            kwargs["suggested_tip_amounts"] = json.dumps(suggested_tip_amounts)
+        real_api_name, form_data, attached_files = self.__prepare_request_data(
+            "sendInvoice",
+            chat_id=chat_id,
+            title=title,
+            description=description,
+            payload=payload,
+            provider_token=provider_token,
+            currency=currency,
+            prices=json.dumps(prices),
+            **kwargs)
+        return self.__call_api(token,
+                               real_api_name,
+                               data=form_data,
+                               files=attached_files)
+
+    def answer_shipping_query(self,
+                              token: str,
+                              shipping_query_id: str,
+                              ok: bool = True,
+                              **kwargs) -> bool:
+
+        if ok:
+            if "shipping_options" not in kwargs:
+                raise TelegramBotException(
+                    "'shipping_options' is required when ok is True")
+            kwargs["shipping_options"] = json.dumps(kwargs["shipping_options"])
+        else:
+            if "error_message" not in kwargs:
+                raise TelegramBotException(
+                    "'error_message' is required when ok is False")
+        real_api_name, form_data, attached_files = self.__prepare_request_data(
+            "answerShippingQuery",
+            shipping_query_id=shipping_query_id,
+            ok=ok,
+            **kwargs)
+        return self.__call_api(token,
+                               real_api_name,
+                               data=form_data,
+                               files=attached_files)
+
+    def set_passport_data_errors(self, token: str, user_id: int,
+                                 errors: Tuple[PassportElementError]) -> bool:
+        real_api_name, form_data, attached_files = self.__prepare_request_data(
+            "setPassportDataErrors",
+            user_id=user_id,
+            errors=json.dumps(errors))
+        return self.__call_api(token,
+                               real_api_name,
+                               data=form_data,
+                               files=attached_files)
---
+++ packages/py-telegram-bot-client/py-telegram-bot-client-5.3.5.5.tar.gz/py-telegram-bot-client-5.3.5.5/telegrambotclient/storage.py
@@ -0,0 +1,369 @@
+try:
+    import ujson as json
+except ImportError:
+    import json
+
+import os
+import sqlite3
+import time
+from typing import Any, Dict, Mapping
+
+from telegrambotclient.utils import pretty_format
+
+
+class TelegramStorage:
+    def set_field(self, key: str, field: str, value, expires: int) -> bool:
+        raise NotImplementedError()
+
+    def get_field(self, key: str, field: str, expires: int) -> Any:
+        raise NotImplementedError()
+
+    def update_fields(self, key: str, field_mapping: Mapping,
+                      expires: int) -> bool:
+        raise NotImplementedError()
+
+    def delete_fields(self, key: str, expires: int, *field) -> bool:
+        raise NotImplementedError()
+
+    def delete_key(self, key: str) -> bool:
+        raise NotImplementedError()
+
+    def dict(self, key: str) -> Dict:
+        raise NotImplementedError()
+
+
+class MemoryStorage(TelegramStorage):
+    __slots__ = ("_data", )
+
+    def __init__(self):
+        self._data = {}
+
+    def set_field(self, key: str, field: str, value, expires: int) -> bool:
+        if value is None:
+            self.delete_fields(key, expires, field)
+            return True
+        current_time = int(time.time())
+        if key not in self._data or self._data[key].get("expires",
+                                                        0) < current_time:
+            self._data[key] = {
+                "expires": current_time + expires,
+                "data": {
+                    field: value
+                },
+            }
+            return True
+        data = self._data[key]
+        data["expires"] = current_time + expires
+        data["data"][field] = value
+        return True
+
+    def get_field(self, key: str, field: str, expires: int) -> Any:
+        current_time = int(time.time())
+        if key in self._data:
+            if self._data[key].get("expires", 0) < current_time:
+                self.delete_key(key)
+                return None
+            data = self._data[key]
+            data["expires"] = current_time + expires
+            return data["data"].get(field, None)
+        return None
+
+    def update_fields(self,
+                      key: str,
+                      field_mapping: Mapping,
+                      expires: int = 1800) -> bool:
+        current_time = int(time.time())
+        if key in self._data and self._data[key].get("expires",
+                                                     0) < current_time:
+            self.delete_key(key)
+        data = self._data.get(key, {})
+        if "data" in data:
+            data["data"].update(field_mapping)
+        else:
+            data["data"] = field_mapping
+        data["expires"] = int(time.time()) + expires
+        self._data[key] = data
+        return True
+
+    def delete_fields(self, key: str, expires: int, *fields) -> bool:
+        current_time = int(time.time())
+        if key in self._data:
+            if self._data[key].get("expires", 0) < current_time:
+                self.delete_key(key)
+                return True
+            data = self._data[key]
+            data["expires"] = current_time + expires
+            for field in fields:
+                if field in data["data"]:
+                    del data["data"][field]
+            self._data[key] = data
+            return True
+        return False
+
+    def delete_key(self, key: str) -> bool:
+        if key in self._data:
+            del self._data[key]
+        return True
+
+    def dict(self, key: str) -> Dict:
+        if key not in self._data or self._data[key].get("expires", 0) < int(
+                time.time()):
+            self.delete_key(key)
+            return {}
+        return self._data[key]["data"]
+
+
+class SQLiteStorage(TelegramStorage):
+    __slots__ = ("_db_conn", )
+
+    def __init__(self, db_file: str = None):
+        if db_file is None:
+            self._db_conn = sqlite3.connect(
+                "file:memory?cache=shared&mode=memory", uri=True)
+        else:
+            db_path = os.path.dirname(db_file)
+            if not os.path.exists(db_path):
+                os.mkdir(db_path)
+            self._db_conn = sqlite3.connect(db_file)
+        self._db_conn.row_factory = sqlite3.Row
+        with self._db_conn:
+            self._db_conn.execute("""
+                CREATE TABLE IF NOT EXISTS `t_storage` (
+                    `key`        TEXT NOT NULL UNIQUE,
+                    `data`       TEXT NOT NULL,
+                    `expires`    INTEGER NOT NULL,
+                    PRIMARY KEY(`key`)
+                    )
+                """)
+
+    def __del__(self):
+        self._db_conn.close()
+
+    def set_field(self, key: str, field: str, value: str,
+                  expires: int) -> bool:
+        with self._db_conn:
+            cur = self._db_conn.execute(
+                "SELECT data, expires from t_storage WHERE key=?", (key, ))
+            row_data = cur.fetchone()
+            current_time = int(time.time())
+            if row_data and row_data["expires"] >= current_time:
+                data = json.loads(row_data["data"])
+                data[field] = value
+                cur = self._db_conn.execute(
+                    "UPDATE t_storage SET data=?, expires=? WHERE key=?",
+                    (json.dumps(data), current_time + expires, key),
+                )
+                return bool(cur.rowcount)
+
+            cur = self._db_conn.execute(
+                """
+                INSERT OR REPLACE INTO t_storage (
+                    key,
+                    data,
+                    expires
+                ) VALUES (?, ?, ?)
+                """,
+                (key, json.dumps({field: value}), current_time + expires),
+            )
+            return bool(cur.lastrowid)
+
+    def get_field(self, key: str, field: str, expires: int) -> Any:
+        with self._db_conn:
+            cur = self._db_conn.execute(
+                "SELECT data, expires from t_storage WHERE key=?", (key, ))
+            row_data = cur.fetchone()
+            current_time = int(time.time())
+            if row_data and row_data["expires"] >= current_time:
+                self._db_conn.execute(
+                    "UPDATE t_storage SET expires=? WHERE key=?",
+                    (current_time + expires, key),
+                )
+                return json.loads(row_data["data"]).get(field, None)
+            return None
+
+    def update_fields(self, key: str, field_mapping: Mapping,
+                      expires: int) -> bool:
+        with self._db_conn:
+            cur = self._db_conn.execute(
+                "SELECT data, expires from t_storage WHERE key=?", (key, ))
+            row_data = cur.fetchone()
+            current_time = int(time.time())
+            if row_data and row_data["expires"] >= current_time:
+                data = json.loads(row_data["data"])
+                data.update(field_mapping)
+                cur = self._db_conn.execute(
+                    "UPDATE t_storage SET data=?, expires=? WHERE key=?",
+                    (json.dumps(data), current_time + expires, key),
+                )
+                return bool(cur.rowcount)
+
+            cur = self._db_conn.execute(
+                """
+                INSERT OR REPLACE INTO t_storage (
+                    key,
+                    data,
+                    expires
+                ) VALUES (?, ?, ?)
+                """,
+                (key, json.dumps(field_mapping), current_time + expires),
+            )
+            return bool(cur.lastrowid)
+
+    def delete_fields(self, key: str, expires: int, *fields) -> bool:
+        with self._db_conn:
+            cur = self._db_conn.execute(
+                "SELECT data, expires from t_storage WHERE key=?", (key, ))
+            row_data = cur.fetchone()
+            current_time = int(time.time())
+            if row_data and row_data["expires"] >= current_time:
+                data = json.loads(row_data["data"])
+                for field in fields:
+                    if field in data:
+                        del data[field]
+                cur = self._db_conn.execute(
+                    "UPDATE t_storage SET data=?, expires=? WHERE key=?",
+                    (json.dumps(data), current_time + expires, key),
+                )
+                return bool(cur.rowcount)
+            return False
+
+    def delete_key(self, key: str):
+        with self._db_conn:
+            self._db_conn.execute("DELETE from t_storage WHERE key=? ",
+                                  (key, ))
+
+    def dict(self, key: str) -> Dict:
+        cur = self._db_conn.execute(
+            "SELECT data, expires from t_storage WHERE key=?", (key, ))
+        row_data = cur.fetchone()
+        if row_data and row_data.get("expires", 0) >= int(time.time()):
+            return json.loads(row_data["data"])
+        else:
+            self.delete_key(key)
+            return {}
+
+
+class RedisStorage(TelegramStorage):
+    __slots__ = ("_redis", )
+
+    def __init__(self, redis_client):
+        self._redis = redis_client
+
+    def set_field(self, key: str, field: str, value, expires: int) -> bool:
+        self._redis.expire(key, expires)
+        return bool(self._redis.hset(key, field, json.dumps(
+            (value, )))) if value else bool(self._redis.hdel(key, field))
+
+    def get_field(self, key: str, field: str, expires: int) -> Any:
+        if self._redis.exists(key) != 1:
+            return None
+        self._redis.expire(key, expires)
+        value = self._redis.hget(key, field)
+        return json.loads(value)[0] if value else None
+
+    def delete_fields(self, key: str, expires: int, *fields) -> bool:
+        if self._redis.exists(key) != 1:
+            return True
+        self._redis.expire(key, expires)
+        return bool(self._redis.hdel(key, *fields))
+
+    def update_fields(self, key: str, field_mapping: Mapping,
+                      expires: int) -> bool:
+        data = self._redis.hgetall(key)
+        data.update({
+            field: json.dumps((value, ))
+            for field, value in field_mapping.items()
+        })
+        self._redis.expire(key, expires)
+        return bool(self._redis.hset(key, mapping=data))
+
+    def delete_key(self, key: str) -> bool:
+        return bool(self._redis.delete(key))
+
+    def dict(self, key: str) -> Dict:
+        return {
+            field: json.loads(value)[0]
+            for field, value in self._redis.hgetall(key).items()
+        }
+
+    def __del__(self):
+        self._redis.close()
+
+
+class TelegramSession:
+    __slots__ = ("_user_id", "_storage", "_session_id", "_expires",
+                 "_local_data")
+    _session_key_format = "bot:session:{0}:{1}"
+
+    def __init__(self,
+                 bot_id: int,
+                 user_id: int,
+                 storage: TelegramStorage,
+                 expires: int = 1800) -> None:
+        self._user_id = user_id
+        self._storage = storage
+        self._session_id = self._session_key_format.format(bot_id, user_id)
+        self._expires = expires
+        self._local_data = {}
+
+    @property
+    def id(self):
+        return self._session_id
+
+    def get(self, field: str, default=None) -> Any:
+        try:
+            return self.__getitem__(field)
+        except KeyError:
+            return default
+
+    def __getitem__(self, field: str) -> Any:
+        if field in self._local_data:
+            return self._local_data[field]
+        value = self._storage.get_field(self._session_id, field, self._expires)
+        if value:
+            self._local_data[field] = value
+            return value
+        raise KeyError("'{0}' is not found".format(field))
+
+    def set(self, field: str, value):
+        self._local_data[field] = value
+
+    def __setitem__(self, field: str, value):
+        self.set(field, value)
+
+    def delete(self, *fields) -> bool:
+        for field in fields:
+            if field in self._local_data:
+                del self._local_data[field]
+        return self._storage.delete_fields(self._session_id, self._expires,
+                                           *fields)
+
+    def __delitem__(self, field: str):
+        self.delete(field)
+
+    def pop(self, field: str, default=None):
+        value = self.get(field, default)
+        del self[field]
+        return value
+
+    def __contains__(self, field: str):
+        return self.get(field) is not None
+
+    def save(self) -> bool:
+        return self._storage.update_fields(self._session_id,
+                                           self._local_data,
+                                           expires=self._expires)
+
+    def clear(self) -> bool:
+        self._local_data = {}
+        return self._storage.delete_key(self._session_id)
+
+    @property
+    def data(self) -> Dict:
+        data = self._storage.dict(self._session_id)
+        data.update(self._local_data)
+        return data
+
+    def __str__(self):
+        return "Session(id={0}, data={1})".format(self._session_id,
+                                                  pretty_format(self.data))
