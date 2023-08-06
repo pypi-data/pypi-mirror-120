14ddb24f8af68694ad3b739880a3b0ee27bb3b32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:49 +0000

    Add package pyqrp/0.3/pyqrp-0.3.tar.gz

---
+++ packages/pyqrp/pyqrp-0.3.tar.gz/pyqrp-0.3/src/pyqrp/__init__.py
@@ -0,0 +1,5 @@
+from os.path import dirname, basename, isfile, join
+import glob
+__all__ = [basename(f)[:-3]
+           for f in glob.glob(join(dirname(__file__), "*.py"))
+           if isfile(f) and not f.endswith('__init__.py')]
