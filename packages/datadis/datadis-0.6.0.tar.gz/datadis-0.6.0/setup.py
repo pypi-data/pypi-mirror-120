73ec9611c6b1cf0852040ab92894e9317cc46c46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:53 +0000

    Add package datadis/0.6.0/datadis-0.6.0.tar.gz

---
+++ packages/datadis/datadis-0.6.0.tar.gz/datadis-0.6.0/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['datadis']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['requests>=2.26.0,<3.0.0']
+
+setup_kwargs = {
+    'name': 'datadis',
+    'version': '0.6.0',
+    'description': 'Datadis API client',
+    'long_description': '# Datadis\nAPI client for https://datadis.es\n\n[![Semantic Release](https://github.com/MrMarble/datadis/actions/workflows/release.yml/badge.svg)](https://github.com/MrMarble/datadis/actions/workflows/release.yml)\n![PyPI - Downloads](https://img.shields.io/pypi/dm/datadis)\n[![Quality Gate Status](https://sonarcloud.io/api/project_badges/measure?project=MrMarble_datadis&metric=alert_status)](https://sonarcloud.io/dashboard?id=MrMarble_datadis)\n\n',
+    'author': 'Alvaro Tinoco',
+    'author_email': 'alvarotinocomarmol@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
