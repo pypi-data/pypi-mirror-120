6c12736349ef688c0fa178e1be51675cd193b032
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:41:51 +0000

    Add package fgroups/0.0/fgroups-0.0.tar.gz

---
+++ packages/fgroups/fgroups-0.0.tar.gz/fgroups-0.0/setup.py
@@ -0,0 +1,17 @@
+# encoding: utf8
+
+from setuptools import setup
+
+setup(
+	author='Cris Torres',
+	author_email='cristobal_javier@hotmail.com',
+	description='Finite Groups Computations',
+	install_requires=['finite-groups'],
+	long_description='''Use `finite-groups <https://pypi.org/project/finite-groups/>`_ instead.''',
+	name='fgroups',
+	platforms=['all'],
+	py_modules=['wheel-platform-tag-is-broken-on-empty-wheels-see-issue-141'],
+	url='https://pypi.org/project/finite-groups/',
+	version="0.0",
+	zip_safe=False,
+)
