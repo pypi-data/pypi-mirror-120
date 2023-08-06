b8b69cec24a426f00ce2d9fc9caebc7dbc031bcd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:41:30 +0000

    Add package potluck-eval/0.9.30/potluck_eval-0.9.30-py3-none-any.whl

---
+++ packages/potluck-eval/potluck_eval-0.9.30-py3-none-any.whl/potluck/tests/__main__.py
@@ -0,0 +1,11 @@
+"""
+Runs tests via pytest. Invoke using `python -m potluck.tests`.
+
+tests/__main__.py
+"""
+
+import sys
+
+import pytest
+
+sys.exit(pytest.main(["--pyargs", "potluck.tests"] + sys.argv[1:]))
