44e4478c5e76e987cc340b09f2410c7aab747cc1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:41:29 +0000

    Add package webresource/1.0b8/webresource-1.0b8.tar.gz

---
+++ packages/webresource/webresource-1.0b8.tar.gz/webresource-1.0b8/webresource/__init__.py
@@ -0,0 +1,14 @@
+from webresource._api import (  # noqa
+    config,
+    LinkResource,
+    Resource,
+    ResourceCircularDependencyError,
+    ResourceConflictError,
+    ResourceError,
+    ResourceGroup,
+    ResourceMissingDependencyError,
+    ResourceRenderer,
+    ResourceResolver,
+    ScriptResource,
+    StyleResource
+)
