3eea8656b2c2b162b0ed7cef794a56c8992405dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:31:22 +0000

    Add package slot-filling-ZC/0.2.2/slot-filling-ZC-0.2.2.tar.gz

---
+++ packages/slot-filling-ZC/slot-filling-ZC-0.2.2.tar.gz/slot-filling-ZC-0.2.2/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="slot-filling-ZC",
+    version="0.2.2",
+    author="Zichu",
+    author_email="zichuu@gwu.edu",
+    description="train(train_file,test_file,vocab_intent_file,vocab_slot_file)",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="",
+    packages=setuptools.find_packages(include=['slot_ZC','slot_ZC.*']),
+    package_data={'slot_ZC': ['config.json']},
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
