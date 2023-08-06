47f329b676e9e20fb57c17eab59a1f340a39a838
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:03:21 +0000

    Add package proto-formatter/0.1.8/proto-formatter-0.1.8.tar.gz

---
+++ packages/proto-formatter/proto-formatter-0.1.8.tar.gz/proto-formatter-0.1.8/src/proto_formatter/constant.py
@@ -0,0 +1,10 @@
+class Constant(object):
+    MULTIPLE_COMENT_START_SYMBOL = '/*'
+    MULTIPLE_COMENT_END_SYMBOL = '*/'
+    SINGLE_COMMENT_SYMBOL = '//'
+    BRACE_LEFT = '{'
+    BRACE_RIGHT = '}'
+    EQUAL_SIGN = '='
+    SEMICOLON = ';'
+    ANGLE_BRACKET_LEFT = '<'
+    ANGLE_BRACKET_RIGHT = '>'
