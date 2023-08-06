fc5a4b6355f4a9d7459008e1be290b533c14caf7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:23:50 +0000

    Add package mminepy/1.1.0/mminepy-1.1.0.tar.gz

---
+++ packages/mminepy/mminepy-1.1.0.tar.gz/mminepy-1.1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+   name='mminepy',
+   version='1.1.0',
+   description='A useful module',
+   author='Man minepy',
+   author_email='minepymail@minepy.com',
+   packages=['minepy'],  #same as name
+)
