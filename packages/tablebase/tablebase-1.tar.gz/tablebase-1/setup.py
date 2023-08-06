4d0fa49a3a032ee90358e76bcb022d099dfd616e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:41 +0000

    Add package tablebase/1/tablebase-1.tar.gz

---
+++ packages/tablebase/tablebase-1.tar.gz/tablebase-1/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+setup(
+      name='tablebase',
+      version='1',
+      description='To make tables in python as easy as printing to the console',
+      long_description="To make tables in python as easy as printing to the console",
+      author='Maximilian Lange',
+      license='MIT',
+      author_email='maxhlange@gmail.com',
+      url='https://pypi.org/project/tablebase/',
+      py_modules=['tablebase'],
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Environment :: Console',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: MIT License',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python',
+          'Topic :: Terminals'
+      ])
