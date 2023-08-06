3072c56c5b4e555f490e922cc1a4e869e1fd4c5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:02:22 +0000

    Add package apigeecli/0.50.0/apigeecli-0.50.0-py3-none-any.whl

---
+++ packages/apigeecli/apigeecli-0.50.0-py3-none-any.whl/apigee/exceptions.py
@@ -0,0 +1,46 @@
+import builtins
+import functools
+import inspect
+import json
+import logging
+import sys
+
+from apigee import APIGEE_CLI_EXCEPTIONS_LOG_FILE, console
+from apigee.utils import remove_file_above_size, touch
+
+
+class InvalidApisError(Exception):
+    pass
+
+
+class NotYetImplementedError(Exception):
+    pass
+
+
+def setup_global_logger():
+    remove_file_above_size(APIGEE_CLI_EXCEPTIONS_LOG_FILE, size_kb=1000)
+    f_handler = logging.FileHandler(APIGEE_CLI_EXCEPTIONS_LOG_FILE)
+    f_handler.setLevel(logging.WARNING)
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    f_handler.setFormatter(formatter)
+    logging.getLogger("").addHandler(f_handler)
+    # logging.StreamHandler(stream=None)
+
+
+def exception_handler(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        try:
+            result = func(*args, **kwargs)
+            return result
+        except Exception as e:
+            logging.error('Exception occurred', exc_info=True)
+            frm = inspect.trace()[-1]
+            mod = inspect.getmodule(frm[0])
+            modname = mod.__name__ if mod else frm[1]
+            sys.exit(f'An exception of type {modname}.{type(e).__name__} occurred. Arguments:\n{e}')
+        except KeyboardInterrupt:
+            console.echo()
+            sys.exit(130)
+
+    return wrapper
