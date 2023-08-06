406e43136a5f142e2f8c409c4325e38e969cb317
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:12:02 +0000

    Add package viadot/0.2.5/viadot-0.2.5-py3-none-any.whl

---
+++ packages/viadot/viadot-0.2.5-py3-none-any.whl/viadot/sources/__init__.py
@@ -0,0 +1,7 @@
+from .azure_blob_storage import AzureBlobStorage
+from .azure_data_lake import AzureDataLake
+from .azure_sql import AzureSQL
+from .supermetrics import Supermetrics
+
+# APIS
+from .uk_carbon_intensity import UKCarbonIntensity
