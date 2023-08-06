36f832eed4e9646a8454ecdb091a7f171452b0ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:32:10 +0000

    Add package cicdtest/3.38/cicdtest-3.38.tar.gz

---
+++ packages/cicdtest/cicdtest-3.38.tar.gz/cicdtest-3.38/ci_commands/test.py
@@ -0,0 +1,10 @@
+import click
+from click.decorators import command
+import os
+
+@click.command()
+def test():
+    path = os.getenv.get('file_path')
+    print(path)
+    with open(f"{path}/test.txt", "a") as file:
+        file.write("hi" + "\n")
