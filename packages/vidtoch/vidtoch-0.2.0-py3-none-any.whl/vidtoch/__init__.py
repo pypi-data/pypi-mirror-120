798de6e0ca158b31605c63173b6ae6d418b62d0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:02:27 +0000

    Add package vidtoch/0.2.0/vidtoch-0.2.0-py3-none-any.whl

---
+++ packages/vidtoch/vidtoch-0.2.0-py3-none-any.whl/vidtoch/__init__.py
@@ -0,0 +1,12 @@
+# coding: utf-8
+
+from .__utils__ import makeVideo
+
+NAME = "vidtoch"
+VERSIONNUM = 0, 2, 0
+VERSION = ".".join(map(str, VERSIONNUM))
+AUTHOR = "hrpzcf"
+EMAIL = "hrpzcf@foxmail.com"
+WEBSITE = "https://gitee.com/hrpzcf/vidtoch"
+
+__all__ = ["makeVideo"]
