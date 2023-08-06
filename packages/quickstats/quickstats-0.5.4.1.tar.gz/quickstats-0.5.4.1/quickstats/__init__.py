ff9a6c665fb28f4a1c6a64868ae307f6bde6368c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:43:21 +0000

    Add package quickstats/0.5.4.1/quickstats-0.5.4.1.tar.gz

---
+++ packages/quickstats/quickstats-0.5.4.1.tar.gz/quickstats-0.5.4.1/quickstats/__init__.py
@@ -0,0 +1,17 @@
+from quickstats._version import __version__
+
+import pathlib
+
+import ROOT
+ROOT.gROOT.SetBatch(True) 
+ROOT.PyConfig.IgnoreCommandLineOptions = True
+macro_path = pathlib.Path(__file__).parent.absolute()
+
+MAX_WORKERS = 8
+
+from quickstats.utils.io import VerbosePrint
+
+_PRINT_ = VerbosePrint("INFO")
+
+from quickstats.main import *
+
