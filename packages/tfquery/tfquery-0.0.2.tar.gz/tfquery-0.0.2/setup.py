3f0d6446955c764fa06f0c1acdf245509f1e149b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:49 +0000

    Add package tfquery/0.0.2/tfquery-0.0.2.tar.gz

---
+++ packages/tfquery/tfquery-0.0.2.tar.gz/tfquery-0.0.2/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+
+setup(name='tfquery',
+      version='0.0.2',
+      author='Mazin Ahmed',
+      author_email='mazin@mazinahmed.net',
+      packages=['tfquery'],
+      entry_points={'console_scripts': ['tfquery=tfquery.__main__:main']},
+      url='http://github.com/mazen160/tfquery',
+      license='LICENSE.md',
+      description='tfquery: Run SQL queries on your Terraform infrastructure. Query resources and analyze its configuration using a SQL-powered framework.',
+      long_description=open('README.md').read(),
+      long_description_content_type = "text/markdown")
