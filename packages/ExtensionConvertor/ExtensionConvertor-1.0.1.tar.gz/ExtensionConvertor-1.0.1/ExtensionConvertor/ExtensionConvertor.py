da76bbd049747dc702a1d9b100978a17661e7c84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:52:54 +0000

    Add package ExtensionConvertor/1.0.1/ExtensionConvertor-1.0.1.tar.gz

---
+++ packages/ExtensionConvertor/ExtensionConvertor-1.0.1.tar.gz/ExtensionConvertor-1.0.1/ExtensionConvertor/ExtensionConvertor.py
@@ -0,0 +1,45 @@
+from os.path import dirname, abspath, exists
+from typing import Optional, List
+
+
+def _split(text: str) -> List[str]:
+    return text.split(".")
+
+
+def _join(texts: List[str]) -> str:
+    return ".".join(texts)
+
+
+class ExtensionConvertor:
+    """ExtensionConvertor can replace file extension to a new specified one.
+
+    Examples:
+    ---------
+        >>> conv = ExtensionConvertor("hoge.jpg")
+        >>> conv.replace_extension("pdf")
+        hoge.pdf
+        >>> conv.replace_extension("pdf", "_sub")
+        hoge_sub.pdf
+        >>> conv.add_post_text("_hoge")
+        hoge_hoge.jpg
+    """
+    def __init__(self, base_filename: str, to_abs: bool = True):
+        assert exists(base_filename)
+        self.BASE_FILENAME = abspath(base_filename) if to_abs else base_filename
+
+    def replace_extension(self, new_ext: str, post_text: Optional[str] = None) -> str:
+        split = _split(self.BASE_FILENAME)
+        base = _join(split[:-1]) + "{}.{}"
+        return base.format("", new_ext) if post_text is None else base.format(post_text, new_ext)
+
+    def add_post_text(self, post_text: str) -> str:
+        split = _split(self.BASE_FILENAME)
+        return _join(split[:-1]) + post_text + f".{split[-1]}"
+
+    @property
+    def folder(self) -> str:
+        return dirname(self.BASE_FILENAME)
+
+    @property
+    def extension(self) -> str:
+        return _split(self.BASE_FILENAME)[-1]
