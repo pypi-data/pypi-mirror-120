53c38c01a0eaf5c4eafb906a43d1c7f407d5c3c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:03 +0000

    Add package cicdtest/2.46/cicdtest-2.46.tar.gz

---
+++ packages/cicdtest/cicdtest-2.46.tar.gz/cicdtest-2.46/buildpan/yaml_reader.py
@@ -0,0 +1,48 @@
+"""
+    Title: yaml_reader
+    Author: Kushagra A.
+    Language: Python
+    Date Created: 13-09-2021
+    Date Modified: 13-09-2021
+    Description:
+        ###############################################################
+        ## Reads a yaml file on a specific repository   ## 
+         ###############################################################
+ """
+
+
+import yaml
+
+
+def yaml_reader(path):
+    
+    try:
+        a_yaml_file = open(f"{path}/buildpan.yaml")
+        parsed_yaml_file = yaml.load(a_yaml_file, Loader=yaml.FullLoader)
+
+        # tool version 
+        yaml_reader.version = parsed_yaml_file["version"]
+
+        # reading Project id 
+        yaml_reader.project_id = parsed_yaml_file['projotectDetail']["projectid"]
+        print("1")
+
+        # Redeaing Platform and and its version 
+        platform = parsed_yaml_file['projotectDetail']["platform"]
+        temp_index=platform.find(":")
+        yaml_reader.platform_ver = platform[temp_index + 1:].strip()
+        yaml_reader.platform_name = platform[:temp_index].strip()
+        print("2")
+
+        # jobs
+        yaml_reader.jobs = parsed_yaml_file["jobs"]
+        yaml_reader.deploy = parsed_yaml_file["jobs"]["deploy"]
+        print("3")
+
+        # workflow
+        yaml_reader.workflow = parsed_yaml_file["workflows"]
+        print("4")
+    
+    except Exception as e:
+
+        print("Invalid format = ",e)
