57c76da0507de092962fafbc504379679c93e442
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:38 +0000

    Add package concurrentbuffer/0.0.3/concurrentbuffer-0.0.3.tar.gz

---
+++ packages/concurrentbuffer/concurrentbuffer-0.0.3.tar.gz/concurrentbuffer-0.0.3/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='concurrentbuffer',
+    version='0.0.3',
+    author='Mart van Rijthoven',
+    author_email='mart.vanrijthoven@gmail.com',
+    packages=['concurrentbuffer'],
+    url='http://pypi.python.org/pypi/concurrentbuffer/',
+    license='LICENSE.txt',
+    install_requires=['numpy>=1.18.1'],
+    long_description='',
+)
