995f2bff9499796ef6a9245b0ddb8623cdd5ee03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:14:35 +0000

    Add package livemark/0.70.2/livemark-0.70.2.tar.gz

---
+++ packages/livemark/livemark-0.70.2.tar.gz/livemark-0.70.2/livemark/errors.py
@@ -0,0 +1,9 @@
+# NOTE:
+# It would be greate to create an error reference available in the docs
+# So every CLI error will link the correspoinding error in the docs
+
+
+class Error(Exception):
+    """Livemark error"""
+
+    pass
