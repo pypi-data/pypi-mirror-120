7c97737c1a27f502c2d9cdf62d7d47c75dbb7072
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:44 +0000

    Add package fastapi-helpers/0.0.2/fastapi_helpers-0.0.2.tar.gz

---
+++ packages/fastapi-helpers/fastapi_helpers-0.0.2.tar.gz/fastapi_helpers-0.0.2/fastapi_helpers/workers/__init__.py
@@ -0,0 +1,23 @@
+from sqlalchemy.pool import NullPool
+from fastapi_helpers.db import DbConfig
+from fastapi_helpers.logging import DefaultLogger
+
+class Worker:
+
+    db_config: DbConfig
+    logger: DefaultLogger
+
+    @classmethod
+    def use_db_connection(cls, func):
+        '''Decorator that reports the execution time.'''
+
+        async def wrap(*args, **kwargs):
+            await cls.db_config.connect_db(NullPool)
+            result = None
+            try:
+                result = await func(*args, **kwargs)
+            except Exception as ex:
+                cls.logger.error(ex)
+            await cls.db_config.disconnect_db()
+            return result
+        return wrap
