52502459369a8fd6234548658715e7d7c4b3ce20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:57 +0000

    Add package novelsave/0.7.5/novelsave-0.7.5-py3-none-any.whl

---
+++ packages/novelsave/novelsave-0.7.5-py3-none-any.whl/novelsave/services/packagers/__init__.py
@@ -0,0 +1,3 @@
+from .epub_packager import EpubPackager
+from .html_packager import HtmlPackager
+from .package_provider import PackagerProvider
---
+++ packages/novelsave/novelsave-0.7.5-py3-none-any.whl/novelsave/services/packagers/package_provider.py
@@ -0,0 +1,24 @@
+from typing import List, Set, Iterable, Tuple
+
+from novelsave.core.services.packagers import BasePackager, BasePackagerProvider
+
+
+class PackagerProvider(BasePackagerProvider):
+
+    def __init__(self, epub: BasePackager, html: BasePackager):
+        self._packagers = (epub, html)
+
+    def keywords(self):
+        return [keyword for p in self._packagers for keyword in p.keywords()]
+
+    def packagers(self) -> Iterable[BasePackager]:
+        return self._packagers
+
+    def filter_packagers(self, keywords: Iterable[str]) -> Set[BasePackager]:
+        keywords = [k.lower() for k in keywords]
+
+        return {
+            compiler
+            for compiler in self._packagers
+            if any(k in keywords for k in compiler.keywords())
+        }
