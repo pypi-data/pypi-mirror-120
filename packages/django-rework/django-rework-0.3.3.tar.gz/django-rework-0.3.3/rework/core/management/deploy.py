40f5f94f9e0f3ce8a98d49a8b79aecb258bfa24c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:46:41 +0000

    Add package django-rework/0.3.3/django-rework-0.3.3.tar.gz

---
+++ packages/django-rework/django-rework-0.3.3.tar.gz/django-rework-0.3.3/rework/core/management/deploy.py
@@ -0,0 +1,20 @@
+import os
+import shutil
+
+from ..utils.command import BaseCommand
+
+
+class DeployCommand(BaseCommand):
+    def handle(self, params):
+        if params == ['--init']:
+            try:
+                working_dir = os.path.join(self.base_dir, 'deploy')
+                core_dir = os.path.join(self.core_dir, 'deploy')
+                shutil.copytree(core_dir, working_dir)
+            except Exception as e:
+                print(e)
+        else:
+            pass
+
+    def __call__(self, params):
+        return self.handle(params)
---
+++ packages/django-rework/django-rework-0.3.3.tar.gz/django-rework-0.3.3/rework/core/templates/.gitignore.tpl
@@ -0,0 +1,27 @@
+### Django ###
+*.log
+*.pot
+*.pyc
+__pycache__/
+
+# ide
+.idea/
+
+# hidden files
+.*
+!.gitignore
+!.editorconfig
+
+# celery
+celerybeat.pid
+
+# static generated
+static_root/
+
+# virtual env
+venv
+
+.doc
+
+**/settings/local.py
+**/settings/dev.local.py
