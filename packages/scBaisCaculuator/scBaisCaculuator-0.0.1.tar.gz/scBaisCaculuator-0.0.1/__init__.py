6b794642ac0639e40cc5987085a9f613ff0830c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:13:30 +0000

    Add package scBaisCaculuator/0.0.1/scBaisCaculuator-0.0.1.tar.gz

---
+++ packages/scBaisCaculuator/scBaisCaculuator-0.0.1.tar.gz/scBaisCaculuator-0.0.1/__init__.py
@@ -0,0 +1,11 @@
+def __Add_number(num1, num2):
+    return num1+num2
+
+def __Substract_number(num1, num2):
+    return num1-num2
+
+def __Mutliply_number(num1, num2):
+    return num1 * num2
+
+def __Divide_number(num1, num2):
+    return num1 / num2
