4be4262e1286ef511573fa55fc8e9ec577639f42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:10:16 +0000

    Add package quevedo/1.1.0/quevedo-1.1.0-py3-none-any.whl

---
+++ packages/quevedo/quevedo-1.1.0-py3-none-any.whl/quevedo/cli.py
@@ -0,0 +1,63 @@
+# 2020-04-08 Antonio F. G. Sevilla <afgs@ucm.es>
+# Licensed under the Open Software License version 3.0
+
+import click
+from os import getcwd
+
+from quevedo import web, dataset as ds
+import quevedo.network.cli as network
+from quevedo.extract_graphemes import extract_graphemes
+from quevedo.generate import generate
+from quevedo.run_script import run_script
+from quevedo.migrate import migrate
+from quevedo.split import split
+
+
+@click.group(commands=[
+    ds.config_edit, ds.info, ds.create, ds.add_images,
+    split, extract_graphemes, generate,
+    network.prepare, network.train,
+    network.predict_image, network.test,
+    web.launcher, run_script, migrate,
+], chain=True, invoke_without_command=True)
+@click.option('-D', '--dataset', type=click.Path(), default=getcwd(),
+              help="Path to the dataset to use, by default use current directory")
+@click.option('-N', '--network', help="Neural network configuration to use")
+@click.version_option()
+@click.pass_context
+def cli(ctx, dataset, network):
+    '''Quevedo is a tool for managing datasets of images with compositional
+    semantics.
+
+    This includes file management, annotation of data, and neural network
+    training and use.
+
+    The -D and -N flags are global, and affect all commands used afterwards. For
+    example, to run a full experiment for neural network 'one', run:
+
+        quevedo -D path/to/dataset -N one split -p 80 prepare train test
+    '''
+    dataset = ds.Dataset(dataset)
+    ctx.obj = {
+        'dataset': dataset,
+        'network': network
+    }
+    if ctx.invoked_subcommand is None:
+        ctx.invoke(ds.info)
+        click.echo(cli.get_help(ctx))
+
+
+# Adapted from https://stackoverflow.com/a/58018765
+def get_docs(cmd=cli, parent=None):
+    name = cmd.name
+    if name == 'cli':
+        name = 'quevedo'
+        print("# Command Line Interface\n\n```txt")
+    else:
+        print("\n## `{}`\n\n```txt".format(name))
+    ctx = click.core.Context(cmd, info_name=name, parent=parent)
+    print(cmd.get_help(ctx))
+    print("```")
+    commands = getattr(cmd, 'commands', {})
+    for sub in commands.values():
+        get_docs(sub, ctx)
---
+++ packages/quevedo/quevedo-1.1.0-py3-none-any.whl/quevedo/network/detect.py
@@ -0,0 +1,153 @@
+# 2021-04-21 Antonio F. G. Sevilla <afgs@ucm.es>
+# Licensed under the Open Software License version 3.0
+
+import json
+from pathlib import Path
+from PIL import Image
+from string import Template
+
+from .network import Network
+from quevedo.annotation import Target
+
+
+class DetectNet(Network):
+    '''A neural network for performing grapheme detection within logograms.'''
+
+    target = Target.LOGO
+    names_file_name = 'names'  # Darknet is not very consistent
+    network_type = 'detector'
+
+    def _update_tag_set(self, tag_set, annotation):
+        try:
+            tag_set.update(set(self.get_tag(g.tags)
+                               for g in annotation.graphemes))
+        except KeyError:
+            raise KeyError("Error getting annotations for logogram {} ({})".format(
+                annotation.id, json.dumps(annotation.to_dict())))
+
+    def _prepare_annotation(self, annotation, num, tag_set):
+        # For YOLO, we write an adjacent txt file with the bounding boxes and
+        # the class (its index)
+        link_name = "{}.png".format(num)
+        (self.train_path / link_name).with_suffix(".txt").write_text(
+            "".join("{} {} {} {} {}\n".format(
+                tag_set.index(self.get_tag(g.tags)),
+                *g.box) for g in annotation.graphemes))
+        return link_name
+
+    def _get_net_config(self, num_classes):
+        template = Template((Path(__file__).parent.parent /
+                             'darknet/yolo.cfg').read_text())
+        config = self.config
+        augment = self.config.get('augment', {})
+
+        num_max_batches = config.get('max_batches',
+            num_classes * 1000)
+
+        return template.substitute(
+            height=config.get('height', 416),  # Important! multiple of 32
+            width=config.get('width', 416),    # Important! multiple of 32
+            flip=augment.get('flip', 0),
+            angle=augment.get('angle', 0),
+            exposure=augment.get('exposure', 0),
+            num_classes=num_classes,
+            num_filters=((num_classes + 5) * 3),
+            num_max_batches=num_max_batches,
+            num_steps_1=int(num_max_batches * 80 / 100),
+            num_steps_2=int(num_max_batches * 90 / 100))
+
+    def predict(self, image):
+
+        if not isinstance(image, Image.Image):
+            image = Image.open(image)
+
+        def clamp(val, minim, maxim):
+            return min(maxim, max(minim, val))
+
+        def make_bbox(im_width, im_height, x, y, w, h):
+            x = clamp(x / im_width, 0, 1)
+            y = clamp(y / im_height, 0, 1)
+            w = clamp(w / im_width, 0, 1)
+            h = clamp(h / im_height, 0, 1)
+            return [x, y, w, h]
+
+        width, height = image.size
+
+        return [{
+            'name': self.tag_map[s],
+            'confidence': c,
+            'box': make_bbox(width, height, *b)
+        } for (s, c, b) in self._darknet.detect(image)]
+
+    def test(self, annotation, stats):
+        predictions = self.predict(annotation.image_path)
+        for g in annotation.graphemes:
+            tag = self.get_tag(g.tags)
+            if tag is None:
+                continue
+            logo = {'box': g.box, 'name': tag}
+            if len(predictions) > 0:
+                similarities = sorted(((box_similarity(p, logo), i) for (i, p) in
+                                      enumerate(predictions)), reverse=True)
+                (sim, idx) = similarities[0]
+                if sim > 0.7:
+                    predictions.pop(idx)
+                    # Tag equality is checked in the similarity computation
+                    stats.register(tag, tag)
+                    continue
+            stats.register(None, tag)
+        # Unassigned predictions are false positives
+        for pred in predictions:
+            stats.register(pred['name'], None)
+
+    def auto_annotate(self, a):
+        graphemes = []
+        for pred in self.predict(a.image):
+            g = {'box': pred['box'], 'tags': {}}
+            self.prediction_to_tag(g['tags'], pred['name'])
+            graphemes.append(g)
+        a.update(graphemes=graphemes)
+
+
+# Utilities for YOLO
+
+def safe_divide(a, b):
+    if a == 0:
+        return 0
+    elif b == 0:
+        return 1
+    else:
+        return a / b
+
+
+def box(xc, yc, w, h):
+    return {
+        'l': float(xc) - float(w) / 2,
+        'r': float(xc) + float(w) / 2,
+        'b': float(yc) - float(h) / 2,
+        't': float(yc) + float(h) / 2,
+        'w': float(w),
+        'h': float(h)
+    }
+
+
+def box_similarity(a, b):
+    '''Similarity of grapheme boxes.'''
+    return iou(a['box'], b['box']) if (a['name'] == b['name']) else 0
+
+
+def iou(a, b):
+    '''Intersection over union for boxes in x, y, w, h format'''
+    a = box(*a)
+    b = box(*b)
+    # Intersection
+    il = max(a['l'], b['l'])
+    ir = min(a['r'], b['r'])
+    ix = ir - il if ir > il else 0
+    ib = max(a['b'], b['b'])
+    it = min(a['t'], b['t'])
+    iy = it - ib if it > ib else 0
+    i = ix * iy
+    # Sum (union = sum - inters)
+    s = a['w'] * a['h'] + b['w'] * b['h']
+    return safe_divide(i, (s - i))
