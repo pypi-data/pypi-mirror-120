a9b9a72c04c4f64cfc767c8a536968b9e1e62ba9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:39:52 +0000

    Add package django-koldar-utils/2.93.0/django_koldar_utils-2.93.0-py3-none-any.whl

---
+++ packages/django-koldar-utils/django_koldar_utils-2.93.0-py3-none-any.whl/django_koldar_utils/django/models/AbstractModelMetaclass.py
@@ -0,0 +1,31 @@
+import abc
+from django.db import models
+
+
+class AbstractModelMetaclass(abc.ABCMeta, type(models.Model)):
+    """
+    Class which can be used to provide abc abstract methods and django_toolbox models
+
+    .. code-block::
+
+        class AbstractModel(models.Model, metaclass=AbstractModelMetaclass):
+            # You may have common fields here.
+
+            class Meta:
+                abstract = True
+
+            @abc.abstractmethod
+            def must_implement(self):
+                pass
+
+
+        class MyModel(AbstractModel):
+            code = models.CharField("code", max_length=10, unique=True)
+
+            class Meta:
+                app_label = 'my_app'
+
+    .. seealso:: SO `stackoverflow <https://stackoverflow.com/a/66888176/1887602>`_
+    """
+
+    pass
