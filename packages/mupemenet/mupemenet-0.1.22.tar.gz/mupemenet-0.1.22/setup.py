07216d776a9e65418603eb35f27a4fd6f745e944
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:40:38 +0000

    Add package mupemenet/0.1.22/mupemenet-0.1.22.tar.gz

---
+++ packages/mupemenet/mupemenet-0.1.22.tar.gz/mupemenet-0.1.22/setup.py
@@ -0,0 +1,34 @@
+from setuptools import find_packages, setup
+
+def get_requirements():
+    with open('mupemenet/requirements.txt') as fs:
+        lst = fs.readlines()
+    rez = []
+    for x in lst:
+        y = x.replace("\n", "")
+        if 'tflite_runtime' not in y:
+            rez.append(y)
+    return rez
+
+
+setup(
+    name = 'mupemenet',           
+    version = '0.1.22',         
+    description = 'mupemenet',  
+    author = 'Kelwe Technologies',                  
+    author_email = 'jp@kelwe-tech.com',    
+    url = 'https://kelwe-tech.com',   
+    packages = find_packages(),
+    install_requires=get_requirements(),
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+    ],
+    package_data={
+        'mupemenet': ['requirements.txt'],
+        'mupemenet.config': ['*.json'],
+        'mupemenet.resources': ['*'],
+        'mupemenet.resources.models': ['*'],
+        'mupemenet.resources.certificates': ['*'],
+        'mupemenet.VL53L0X_rasp_python.bin': ['*.so'],
+    },
+)
