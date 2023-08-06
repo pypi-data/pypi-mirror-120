f67f84aa8c93633c344e9b3dc4b422576f2a70cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:25:48 +0000

    Add package mpd-what/1.0.6/mpd_what-1.0.6.tar.gz

---
+++ packages/mpd-what/mpd_what-1.0.6.tar.gz/mpd_what-1.0.6/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+setup(
+    name='mpd_what',
+    version='1.0.6',
+    author='github.com/charmparticle',
+    scripts=['bin/mpd_what'],
+    url='https://github.com/charmparticle/mpd_what',
+    license='GPL3',
+    description='A python album art and music info getter',
+    long_description=open('README.md').read(),
+    install_requires=[
+        "python-mpd2",
+        "pyyaml",
+        "pycurl",
+        "latest-user-agents",
+        "python-magic",
+        "pylast",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)
+
