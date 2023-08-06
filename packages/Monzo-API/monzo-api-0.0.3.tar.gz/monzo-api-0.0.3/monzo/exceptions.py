0c313811925438c180555a39cc629742fb99ece5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:29:56 +0000

    Add package Monzo-API/0.0.3/monzo-api-0.0.3.tar.gz

---
+++ packages/Monzo-API/monzo-api-0.0.3.tar.gz/monzo-api-0.0.3/monzo/exceptions.py
@@ -0,0 +1,14 @@
+class MonzoAuthenticationError(Exception):
+    pass
+
+
+class MonzoHTTPError(Exception):
+    pass
+
+
+class MonzoArgumentError(Exception):
+    pass
+
+
+class MonzoGeneralError(Exception):
+    pass
