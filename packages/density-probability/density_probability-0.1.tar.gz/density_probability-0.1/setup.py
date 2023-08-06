64c04259a6b3c9585585e2b216b61eb7920b6158
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:12:24 +0000

    Add package density-probability/0.1/density_probability-0.1.tar.gz

---
+++ packages/density-probability/density_probability-0.1.tar.gz/density_probability-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='density_probability',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['density_probability'],
+      author = 'Franck Tchamo',
+      author_email = 'francktchamo@gmail.com',
+      zip_safe=False)
