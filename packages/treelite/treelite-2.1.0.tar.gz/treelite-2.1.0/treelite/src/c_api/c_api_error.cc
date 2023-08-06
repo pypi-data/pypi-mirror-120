3e856253a138115e4d16a0b786264555465cd297
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:44:52 +0000

    Add package treelite/2.1.0/treelite-2.1.0.tar.gz

---
+++ packages/treelite/treelite-2.1.0.tar.gz/treelite-2.1.0/treelite/src/c_api/c_api_error.cc
@@ -0,0 +1,27 @@
+/*!
+ * Copyright (c) 2017-2021 by Contributors
+ * \file c_api_error.cc
+ * \author Hyunsu Cho
+ * \brief C error handling
+ */
+#include <treelite/thread_local.h>
+#include <treelite/c_api_error.h>
+#include <string>
+
+namespace {
+
+struct TreeliteAPIErrorEntry {
+  std::string last_error;
+};
+
+using TreeliteAPIErrorStore = treelite::ThreadLocalStore<TreeliteAPIErrorEntry>;
+
+}  // anonymous namespace
+
+const char* TreeliteGetLastError() {
+  return TreeliteAPIErrorStore::Get()->last_error.c_str();
+}
+
+void TreeliteAPISetLastError(const char* msg) {
+  TreeliteAPIErrorStore::Get()->last_error = msg;
+}
