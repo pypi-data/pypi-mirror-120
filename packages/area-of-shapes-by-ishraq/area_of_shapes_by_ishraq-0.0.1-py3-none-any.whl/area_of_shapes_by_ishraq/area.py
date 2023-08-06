4218757be667d7f661a300b081eb8197411b87aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:47 +0000

    Add package area-of-shapes-by-ishraq/0.0.1/area_of_shapes_by_ishraq-0.0.1-py3-none-any.whl

---
+++ packages/area-of-shapes-by-ishraq/area_of_shapes_by_ishraq-0.0.1-py3-none-any.whl/area_of_shapes_by_ishraq/area.py
@@ -0,0 +1,8 @@
+def squareArea(length):
+    return length * length
+
+def rectangleArea(length, width):
+    return length * width
+
+def triangleArea(base, height):
+    return 0.5 * base * height
