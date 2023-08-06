36f6341ebf67c211937b4f5fb05b5b56e0c16e25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:40:39 +0000

    Add package mypr/1.0/mypr-1.0.tar.gz

---
+++ packages/mypr/mypr-1.0.tar.gz/mypr-1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='mypr',
+      version='1.0',
+      description='Python Project',
+      author='sneha',
+      author_email='sneha@gmail.com',
+      url='https://pypi.org/manage/projects/',
+      packages=['my_pr'],
+     )
