7ee60ada6dd97e0d4d4668092e80c8faa5f6195c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:12:28 +0000

    Add package noteout/0.0.3a1/noteout-0.0.3a1-py3-none-any.whl

---
+++ packages/noteout/noteout-0.0.3a1-py3-none-any.whl/noteout/filter_divspans.py
@@ -0,0 +1,20 @@
+""" Panflute filter to drop divs and spans
+"""
+
+from .nb_only import Filter
+
+
+class MetaFilter(Filter):
+
+    metadata_field = 'noteout.filter-divspans'
+
+    @classmethod
+    def get_bad_names(cls, doc):
+        dds = doc.get_metadata(cls.metadata_field)
+        if dds is not None:
+            return {dds} if isinstance(dds, str) else set(dds)
+        return set()
+
+
+if __name__ == "__main__":
+    MetaFilter.main()
