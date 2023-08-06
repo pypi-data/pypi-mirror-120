ab079b4741855c1190da90d5752744b64743ecd1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:36 +0000

    Add package sdgs-tools/0.7.9/sdgs_tools-0.7.9-py3-none-any.whl

---
+++ packages/sdgs-tools/sdgs_tools-0.7.9-py3-none-any.whl/sdgs_tools/dashboard/import_individu/enums/kecepatan_internet.py
@@ -0,0 +1,16 @@
+from enum import Enum
+from sdgs_tools.dashboard.utils import make_str_to_enum
+
+MAPPING = {
+    "Cepat": "1",
+    "Sedang": "2",
+    "Lambat": "3",
+}
+
+
+class KecepatanInternet(Enum):
+    CEPAT = "1"
+    SEDANG = "2"
+    LAMBAT = "3"
+
+    from_str = classmethod(make_str_to_enum(MAPPING))
