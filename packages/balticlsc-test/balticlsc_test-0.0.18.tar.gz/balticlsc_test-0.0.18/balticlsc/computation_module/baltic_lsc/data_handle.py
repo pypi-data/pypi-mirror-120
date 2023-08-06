281734be9e5e45b5c3fd3d5877daa415f9962f4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:50 +0000

    Add package balticlsc-test/0.0.18/balticlsc_test-0.0.18.tar.gz

---
+++ packages/balticlsc-test/balticlsc_test-0.0.18.tar.gz/balticlsc_test-0.0.18/balticlsc/computation_module/baltic_lsc/data_handle.py
@@ -0,0 +1,54 @@
+import abc
+import os
+import uuid
+from os import listdir
+from os.path import isdir, isfile, join, basename
+from shutil import rmtree
+from typing import final
+
+
+class DataHandle(metaclass=abc.ABCMeta):
+    __baltic_data_prefix: final = 'BalticLSC-'
+    __uuid_length: final = 6
+
+    @classmethod
+    def __subclasshook__(cls, subclass):
+        return (hasattr(subclass, 'download') and
+                callable(subclass.download) and
+                hasattr(subclass, 'upload') and
+                callable(subclass.upload) and
+                hasattr(subclass, 'check_connection') and
+                callable(subclass.check_connection) or
+                NotImplemented)
+
+    @abc.abstractmethod
+    def __init__(self, pin_name: str, pins_configuration: []):
+        self._pin_configuration = next(pc for pc in pins_configuration if pc.pin_name == pin_name)
+        self._local_path = os.getenv('LOCAL_TMP_PATH', '/balticLSC_tmp')
+
+    @abc.abstractmethod
+    def download(self, handle: {}) -> str:
+        pass
+
+    @abc.abstractmethod
+    def upload(self, local_path: str) -> {}:
+        pass
+
+    @abc.abstractmethod
+    def check_connection(self, handle: {}):
+        pass
+
+    def _clear_local(self):
+        if isdir(self._local_path):
+            rmtree(self._local_path)
+        elif isfile(self._local_path):
+            os.remove(self._local_path)
+
+    def _add_guids_to_file_names(self, local_path: str):
+        for f in (f for f in listdir(local_path) if isfile(join(local_path, f))):
+            file_name = basename(f)
+            new_file_name =\
+                self.__baltic_data_prefix + str(uuid.uuid4())[:self.__uuid_length] + '-'\
+                + file_name if not file_name.startswith(self.__baltic_data_prefix) else file_name[len(
+                    self.__baltic_data_prefix)+self.__uuid_length:]
+            os.rename(f, join(new_file_name, local_path))
