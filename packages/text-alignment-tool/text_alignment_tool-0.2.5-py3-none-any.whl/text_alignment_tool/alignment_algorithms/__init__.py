e7d2301f63096198c4b37ac8d5eaa1d5290d277c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:28:36 +0000

    Add package text-alignment-tool/0.2.5/text_alignment_tool-0.2.5-py3-none-any.whl

---
+++ packages/text-alignment-tool/text_alignment_tool-0.2.5-py3-none-any.whl/text_alignment_tool/alignment_algorithms/__init__.py
@@ -0,0 +1,10 @@
+from .alignment_algorithm import (
+    AlignmentAlgorithm,
+    TextAlignments,
+    AlignmentKey,
+    AlignmentException,
+)
+from .global_alignment import GlobalAlignmentAlgorithm
+from .local_alignment import LocalAlignmentAlgorithm
+from .rough_alignment import RoughAlignmentAlgorithm
+from .chunk_alignment import ChunkAlignmentAlgorithm
