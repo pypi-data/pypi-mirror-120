4a0e322862c2f49ab9b6dadff02e0b52d2263858
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:46:19 +0000

    Add package downloader-python/1.8rc2/downloader-python-1.8rc2.tar.gz

---
+++ packages/downloader-python/downloader-python-1.8rc2.tar.gz/downloader-python-1.8rc2/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+version = '1.8pre2'
+print(f'version:{version}')
+with open('README.md','r',encoding='utf-8') as f:
+    long_description = f.read()
+setup(
+    name='downloader-python',
+    version=version,
+    url='https://github.com',
+    long_description=long_description,
+    author='mc_creater',
+    author_email='guoxiuchen20170402@163.com',
+    license='MIT',
+    packages=['downloader-python','command','pyim','exes','doge'],
+    classifiers=['Programming Language :: Python :: 3.6',
+                 'Programming Language :: Python :: 3.7',
+                 'Programming Language :: Python :: 3.8',
+                 'Programming Language :: Python :: 3.9',],
+    install_requires=['PyQt5','requests','bs4','lxml'],
+    include_package_data=True
+
+)
