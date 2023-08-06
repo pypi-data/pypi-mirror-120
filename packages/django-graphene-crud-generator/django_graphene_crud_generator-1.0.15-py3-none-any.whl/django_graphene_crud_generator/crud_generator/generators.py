abd41035434e04c5fc0e745afed43b19fa4aed3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:23:52 +0000

    Add package django-graphene-crud-generator/1.0.15/django_graphene_crud_generator-1.0.15-py3-none-any.whl

---
+++ packages/django-graphene-crud-generator/django_graphene_crud_generator-1.0.15-py3-none-any.whl/django_graphene_crud_generator/crud_generator/generators.py
@@ -0,0 +1,25 @@
+from typing import List, Dict
+
+from django.db.models import QuerySet
+from django_koldar_utils.graphql_toolsbox.graphql_types import TGrapheneReturnType, TGrapheneArgument
+
+from django_graphene_crud_generator.ICrudGraphQLGenerator import ICrudGraphQLGenerator
+from django_graphene_crud_generator.crud_generator.CanXPermissionsMixIn import CanXPermissionsMixIn
+from django_graphene_crud_generator.crud_generator.CreateMutationReturnTrueMixIn import CreateMutationReturnTrueMixIn
+from django_graphene_crud_generator.crud_generator.CreateMutationViaSingleInputMixIn import \
+    CreateMutationViaSingleInputMixIn
+from django_graphene_crud_generator.crud_generator.CrudGraphQLViaTokenGenerateMixIn import \
+    CrudGraphQLViaTokenGenerateMixIn
+from django_graphene_crud_generator.crud_generator.FederationNamesMixIn import FederationNamesMixIn
+from django_graphene_crud_generator.crud_generator.ReadStandardMixIn import ReadStandardMixIn
+from django_graphene_crud_generator.crud_generator.contexts import BuildContext, RuntimeContext
+
+
+class StandardFederatedTokenBasedCrudGenerator(
+    CrudGraphQLViaTokenGenerateMixIn, CanXPermissionsMixIn, FederationNamesMixIn,
+    CreateMutationViaSingleInputMixIn, CreateMutationReturnTrueMixIn,
+    ReadStandardMixIn, ICrudGraphQLGenerator):
+    """
+    Build CRUD operations that are designed to work within a graphql federation and accessed via an access token
+    """
+    pass
