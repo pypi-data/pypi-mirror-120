cd89c9c3ae2cb3da407a890126ccb60b15e804e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:28:56 +0000

    Add package pyobjc-framework-Accessibility/8.0b1/pyobjc-framework-Accessibility-8.0b1.tar.gz

---
+++ packages/pyobjc-framework-Accessibility/pyobjc-framework-Accessibility-8.0b1.tar.gz/pyobjc-framework-Accessibility-8.0b1/Lib/Accessibility/__init__.py
@@ -0,0 +1,36 @@
+"""
+Python mapping for the Accessibility framework.
+
+This module does not contain docstrings for the wrapped code, check Apple's
+documentation for details on how to use these functions and classes.
+"""
+
+import sys
+
+import Quartz
+import objc
+from . import _metadata
+from . import _Accessibility
+
+sys.modules["Accessibility"] = mod = objc.ObjCLazyModule(
+    "Accessibility",
+    "com.apple.Accessibility",
+    objc.pathForFramework("/System/Library/Frameworks/Accessibility.framework"),
+    _metadata.__dict__,
+    None,
+    {
+        "__doc__": __doc__,
+        "objc": objc,
+        "__path__": __path__,
+        "__loader__": globals().get("__loader__", None),
+    },
+    (_Accessibility, Quartz),
+)
+
+mod.AXNumericDataAxisDescriptor.__objc_final__ = True
+mod.AXDataPointValue.__objc_final__ = True
+mod.AXDataPoint.__objc_final__ = True
+mod.AXChartDescriptor.__objc_final__ = True
+
+
+del sys.modules["Accessibility._metadata"]
