8121af09fe98639c658ea1c80a857d76dab06ce9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:06 +0000

    Add package cicdtest/2.80/cicdtest-2.80.tar.gz

---
+++ packages/cicdtest/cicdtest-2.80.tar.gz/cicdtest-2.80/buildpan/repo_pull.py
@@ -0,0 +1,45 @@
+"""
+    Title: repo_pull.py
+    Author: Kushagra A.
+    Language: Python
+    Date Created: 31-08-2021
+    Date Modified: 13-09-2021
+    Description:
+        ###############################################################
+        ## Perform a pull operation on a repository   ## 
+         ###############################################################
+ """
+
+import git
+import datetime
+import requests
+from buildpan import setting
+import subprocess
+
+
+info = setting.info
+
+
+fetch_log = info["FETCH_LOG_URL"]
+
+
+def repo_pull(path, project_id, repo_name, username):
+
+    try:
+        
+        # repo = git.Repo(path)
+        # origin = repo.remote(name='origin')
+        # origin.pull()
+        print("in")
+        # repo_name = repo_name.capitalize()
+        print(repo_name)
+        # subprocess.call(["git", "pull", "origin"], cwd=path)
+        subprocess.Popen(["git", "pull", "origin", "main"], stdout=subprocess.PIPE, cwd =path)
+        print("called")
+        curtime = datetime.datetime.now()          
+        requests.post(fetch_log,data={'project_id':project_id,'repo_name':repo_name,'Time ':curtime,'user_name':username,'message':"pull success",'status':'pull operation performed'})
+    
+    except Exception as e:
+        print("e = ",e)
+        # curtime = datetime.datetime.now()          
+        # requests.post(fetch_log,data={'project_id':project_id,'repo_name':repo_name,'Time ':curtime,'user_name':username,'message':"pull failed",'status':'pull operation performed'})
