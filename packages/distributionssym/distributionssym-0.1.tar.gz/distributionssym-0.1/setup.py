a84ec2567dc0677b16c1a7d254592db64edbc477
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:29:58 +0000

    Add package distributionssym/0.1/distributionssym-0.1.tar.gz

---
+++ packages/distributionssym/distributionssym-0.1.tar.gz/distributionssym-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributionssym',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['distributionssym'],
+      zip_safe=False)
