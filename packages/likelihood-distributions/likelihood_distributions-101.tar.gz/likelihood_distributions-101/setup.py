5383f0be5483c462d89ffc5d7dde6506e39de20d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:51 +0000

    Add package likelihood-distributions/101/likelihood_distributions-101.tar.gz

---
+++ packages/likelihood-distributions/likelihood_distributions-101.tar.gz/likelihood_distributions-101/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='likelihood_distributions',
+      version='101',
+      description='Sule package for probability density function',
+      packages=['likelihood_distributions'],
+      author = 'Shamushideen Sule',
+      author_email = 'shaamsule@gmail.com',
+      zip_safe=False)
