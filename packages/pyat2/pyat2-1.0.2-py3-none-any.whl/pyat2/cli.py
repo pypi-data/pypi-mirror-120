01d6878402afa417754e137ca29d470736e8bdbd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:49 +0000

    Add package pyat2/1.0.2/pyat2-1.0.2-py3-none-any.whl

---
+++ packages/pyat2/pyat2-1.0.2-py3-none-any.whl/pyat2/cli.py
@@ -0,0 +1,34 @@
+import argparse
+import datetime
+import os
+import time
+
+
+def next_datetime(current: datetime.datetime, hour: int, **kwargs) -> datetime.datetime:
+    repl = current.replace(hour=hour, **kwargs)
+    while repl <= current:
+        repl = repl + datetime.timedelta(days=1)
+    return repl
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-f', help='command script.')
+    parser.add_argument('time', help='time')
+    args = parser.parse_args()
+    now = datetime.datetime.now()
+    hour, minute = map(int, args.time.split(':'))
+    dd = next_datetime(now, hour, minute=minute)
+    dd = dd.replace(second=0, microsecond=0)
+    print(f'Job will run at {dd.month}-{dd.day} {dd.hour:02d}:{dd.minute:02d}:{dd.second:02d} {dd.year}')
+    ddt = dd.timestamp()
+    while 1:
+        now_t = datetime.datetime.now().timestamp()
+        if now_t >= ddt:
+            os.system(f"sh {args.f} >> ~/pyat.log")
+            break
+        time.sleep(1)
+
+
+if __name__ == '__main__':
+    main()
