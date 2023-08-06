36bd3323b10d4842300060f370ccde8caf5308de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:03:20 +0000

    Add package ailever/0.3.100/ailever-0.3.100-py3-none-any.whl

---
+++ packages/ailever/ailever-0.3.100-py3-none-any.whl/ailever/investment/INVESTMENTInterfaces.py
@@ -0,0 +1,15 @@
+from .assets import Asset
+from .initializer import stock_market
+from .Screener import Screener
+from .portfolio_optimization import PortfolioManagement
+from .fmlops_models import Forecaster
+from .fmlops_loader_system import Loader, PLoader, Preprocessor
+from . import sectors
+
+prllz_loader = PLoader.prllz_loader
+parallelize = PLoader.parallelize
+
+
+def Management(system):
+    from .fmlops_management import *
+    return locals()[system]
