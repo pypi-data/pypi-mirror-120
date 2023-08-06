9793f46fabe6c10891355062fa75cb68646953b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:11:00 +0000

    Add package sha-accounts/0.1.5/sha_accounts-0.1.5-py3-none-any.whl

---
+++ packages/sha-accounts/sha_accounts-0.1.5-py3-none-any.whl/sha_accounts/managers/user_manager.py
@@ -0,0 +1,21 @@
+from django.contrib.auth.models import BaseUserManager
+from django.db import IntegrityError
+from djrest_wrapper.exceptions import DuplicateModelExp
+
+
+class UserManager(BaseUserManager):
+    def create(self, username, email, password, * args, **kwargs):
+        try:
+            new_user = super().create(username=username, email=email, *args, **kwargs)
+            new_user.set_password(password)
+            new_user.save(using=self._db)
+            return new_user
+        except IntegrityError as e:
+            raise DuplicateModelExp('user already exists')
+
+    def create_superuser(self, username, email, password):
+        user = self.create(
+            username=username, email=email, password=password)
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
