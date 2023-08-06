42783607c1b0c8fb874bb88793d0a7bcb8cf97bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:29:33 +0000

    Add package ignition-api/8.0.17.post3/ignition_api-8.0.17.post3-py2-none-any.whl

---
+++ packages/ignition-api/ignition_api-8.0.17.post3-py2-none-any.whl/system/__version__.py
@@ -0,0 +1,13 @@
+# coding=utf-8
+"""Package information."""
+
+__title__ = "ignition-api"
+__description__ = "Ignition Scripting API."
+__url__ = "https://github.com/thecesrom/Ignition/tree/8.0"
+__version__ = "8.0.17"
+__cycle__ = ".post3"
+__build__ = "2020111210"
+__author__ = "César Román"
+__author_email__ = "cesar@thecesrom.dev"
+__license__ = "MIT"
+__copyright__ = "Copyright (C) 2018-2021 César Román"
