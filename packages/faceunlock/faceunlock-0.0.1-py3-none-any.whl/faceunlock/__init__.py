5df211e0fac55512cdadc6b90535b75ba62fa346
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:24:23 +0000

    Add package faceunlock/0.0.1/faceunlock-0.0.1-py3-none-any.whl

---
+++ packages/faceunlock/faceunlock-0.0.1-py3-none-any.whl/faceunlock/__init__.py
@@ -0,0 +1,15 @@
+import face_recognition
+import face_training
+import facedatacollect
+import facedetection
+
+import cv2
+import numpy
+import os
+import PIL
+def help():
+    print('facedetection\nfacedatacollect\nface_training\nface_recognition')
+
+print('if you want to help, write "help"')
+
+name = 'faceunlock'
