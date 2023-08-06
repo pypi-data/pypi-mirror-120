9c17042519dd743a48bde3a536a82446960f70ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:25:27 +0000

    Add package LaoNLP/0.3.1/LaoNLP-0.3.1-py3-none-any.whl

---
+++ packages/LaoNLP/LaoNLP-0.3.1-py3-none-any.whl/laonlp/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+from laonlp.tokenize import *
+from laonlp.corpus import *
+from laonlp.transliterate import *
+
+TONE_MARKS = "່້"+"໊໋"
+CONSONANTS = "ກຂຄງຈສຊຍດຕຖທນບປຜຝພຟມຢຣລວຫອຮ"
+VOWELS_COMBINING = "ັ"+"ິີ"+"ຶືຸ"+"ູົໍ"
+VOWELS = "ະັາ"+"ຳິີ"+"ຶືຸ"+"ູົຼ"+"ຽເແ"+"ໂໃໄ"+"ໍ"
