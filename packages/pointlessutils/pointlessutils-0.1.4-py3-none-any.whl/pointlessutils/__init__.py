fd2a4750e11da875371ab10cfe8c0721124d6188
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:03:29 +0000

    Add package pointlessutils/0.1.4/pointlessutils-0.1.4-py3-none-any.whl

---
+++ packages/pointlessutils/pointlessutils-0.1.4-py3-none-any.whl/pointlessutils/__init__.py
@@ -0,0 +1,11 @@
+from .decorators import always, alwaysv, false, maybe, never, none, true
+
+__all__ = (
+    "none",
+    "true",
+    "false",
+    "maybe",
+    "never",
+    "always",
+    "alwaysv",
+)
