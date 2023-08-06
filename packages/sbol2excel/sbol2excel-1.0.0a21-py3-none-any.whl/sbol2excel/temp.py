8b45116dfce890f75a083f02f294f9bf9145bec0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:51 +0000

    Add package sbol2excel/1.0.0a21/sbol2excel-1.0.0a21-py3-none-any.whl

---
+++ packages/sbol2excel/sbol2excel-1.0.0a21-py3-none-any.whl/sbol2excel/temp.py
@@ -0,0 +1,9 @@
+import sbol2excel.converter as conv
+import os
+
+
+cwd = os.getcwd()
+sbol_doc_path = os.path.join(cwd, 'SBOL2Excel', 'tests', 'test_files',
+                             'test_sbol.xml')
+output_path = os.path.join(cwd, 'test.xlsx')
+conv.converter(sbol_doc_path, output_path)
