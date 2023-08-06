de8103c84fa97f3dad49d992c2c4bab8d24d2a4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:25 +0000

    Add package cubedtube/0.2.0.dev1/cubedtube-0.2.0.dev1-py3-none-any.whl

---
+++ packages/cubedtube/cubedtube-0.2.0.dev1-py3-none-any.whl/cubed_tube/backend/gunicorn_conf.py
@@ -0,0 +1,28 @@
+import os
+import shutil
+
+from cubed_tube.lib import util
+
+creds = util.load_credentials()
+
+if not creds.site_name:
+    raise ValueError('site_name missing from credentials.yaml')
+
+workers = 5
+bind = f'unix:{creds.site_name}.socket'
+accesslog = f'/var/www/{creds.site_name}/logs/access.log'
+errorlog = f'/var/www/{creds.site_name}/logs/error.log'
+loglevel = 'info'
+enable_stdio_inheritance = True
+
+def post_fork(server, worker):
+    server.log.info("Worker spawned (pid: %s)", worker.pid)
+    
+def on_starting(server):
+    server.log.info("on_starting")
+    shutil.rmtree('./prometheus_multiproc')
+    os.mkdir('./prometheus_multiproc')
+
+def worker_exit(server, worker):
+    from prometheus_client import multiprocess
+    multiprocess.mark_process_dead(worker.pid)
