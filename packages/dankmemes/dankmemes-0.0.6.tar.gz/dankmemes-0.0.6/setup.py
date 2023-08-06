9b284ba1bfdde2683375936ca28646b9df03496f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:15 +0000

    Add package dankmemes/0.0.6/dankmemes-0.0.6.tar.gz

---
+++ packages/dankmemes/dankmemes-0.0.6.tar.gz/dankmemes-0.0.6/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+    name="dankmemes",
+    description="dankmemes",
+    version="0.0.6",
+    url="",
+    license="GPLv3+",
+    author="",
+    author_email="",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Programming Language :: Python :: 3.6",
+    ],
+    python_requires = '>=3.6',
+    install_requires=["ptlibs"],
+)
