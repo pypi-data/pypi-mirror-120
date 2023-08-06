d270486d051a3c1da00414c1d4f38ac0d74ee1c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:10:07 +0000

    Add package ihs-probability/0.1/ihs_probability-0.1.tar.gz

---
+++ packages/ihs-probability/ihs_probability-0.1.tar.gz/ihs_probability-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='ihs_probability',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['ihs_probability'],
+      author='Honoré Mbaya',
+      author_email='honormbaya@gmail.com',
+      zip_safe=False)
