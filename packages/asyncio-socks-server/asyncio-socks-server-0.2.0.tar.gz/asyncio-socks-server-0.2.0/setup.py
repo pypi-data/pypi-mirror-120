3745bd7848bcc67b0d3cadc66456423a176ea0af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:24:26 +0000

    Add package asyncio-socks-server/0.2.0/asyncio-socks-server-0.2.0.tar.gz

---
+++ packages/asyncio-socks-server/asyncio-socks-server-0.2.0.tar.gz/asyncio-socks-server-0.2.0/setup.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['asyncio_socks_server']
+
+package_data = \
+{'': ['*']}
+
+entry_points = \
+{'console_scripts': ['asyncio_socks_server = '
+                     'asyncio_socks_server.__main__:main']}
+
+setup_kwargs = {
+    'name': 'asyncio-socks-server',
+    'version': '0.2.0',
+    'description': 'A socks server implemented with asyncio.',
+    'long_description': None,
+    'author': 'Amaindex',
+    'author_email': 'amaindex@outlook.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/Amaindex/asyncio-socks-server',
+    'packages': packages,
+    'package_data': package_data,
+    'entry_points': entry_points,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
