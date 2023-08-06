80665621e551b4e70ac0be5c07e7a5fd5fa1dc90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:42:26 +0000

    Add package vcd/5.0.0/vcd-5.0.0-py3-none-any.whl

---
+++ packages/vcd/vcd-5.0.0-py3-none-any.whl/vcd/__init__.py
@@ -0,0 +1,13 @@
+"""
+VCD (Video Content Description) library v5.0.0
+
+Project website: http://vcd.vicomtech.org
+
+Copyright (C) 2021, Vicomtech (http://www.vicomtech.es/),
+(Spain) all rights reserved.
+
+VCD is a Python library to create and manage VCD content version 5.0.0.
+VCD is distributed under MIT License. See LICENSE.
+
+"""
+
