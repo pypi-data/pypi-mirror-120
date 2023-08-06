0a6caacd790f884cdf8a7f718f31e183aa113a31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:43:38 +0000

    Add package ambulance_game/0.0.2/ambulance_game-0.0.2.tar.gz

---
+++ packages/ambulance_game/ambulance_game-0.0.2.tar.gz/ambulance_game-0.0.2/CHANGELOG.md
@@ -0,0 +1,6 @@
+# v0.0.2
+Improve simulation functionality
+
+# v0.0.1
+
+First release
---
+++ packages/ambulance_game/ambulance_game-0.0.2.tar.gz/ambulance_game-0.0.2/src/ambulance_game/simulation/__init__.py
@@ -0,0 +1,14 @@
+"""
+Simulation module
+"""
+
+from .simulation import (
+    calculate_class_2_individuals_best_response,
+    extract_total_individuals_and_the_ones_within_target_for_both_classes,
+    get_average_simulated_state_probabilities,
+    get_mean_blocking_difference_between_two_systems,
+    get_mean_proportion_of_individuals_within_target_for_multiple_runs,
+    get_multiple_runs_results,
+    get_simulated_state_probabilities,
+    simulate_model,
+)
