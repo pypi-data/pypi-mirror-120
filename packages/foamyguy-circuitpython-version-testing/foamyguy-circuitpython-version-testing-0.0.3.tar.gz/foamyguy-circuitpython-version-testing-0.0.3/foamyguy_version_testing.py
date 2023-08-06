c436157ae7130efc33c07d8f84b87159f165a044
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:14:03 +0000

    Add package foamyguy-circuitpython-version-testing/0.0.3/foamyguy-circuitpython-version-testing-0.0.3.tar.gz

---
+++ packages/foamyguy-circuitpython-version-testing/foamyguy-circuitpython-version-testing-0.0.3.tar.gz/foamyguy-circuitpython-version-testing-0.0.3/foamyguy_version_testing.py
@@ -0,0 +1,29 @@
+# SPDX-FileCopyrightText: 2017 Scott Shawcroft, written for Adafruit Industries
+# SPDX-FileCopyrightText: Copyright (c) 2021 Tim C for foamyguy
+#
+# SPDX-License-Identifier: MIT
+"""
+`foamyguy_version_testing`
+================================================================================
+
+Testing version munging PR
+
+
+* Author(s): Tim C
+
+Implementation Notes
+--------------------
+
+
+**Software and Dependencies:**
+
+* Adafruit CircuitPython firmware for the supported boards:
+  https://github.com/adafruit/circuitpython/releases
+
+changed
+"""
+
+# imports
+
+__version__ = "0.0.3"
+__repo__ = "https://github.com/foamyguy/Foamyguy_CircuitPython_Version_Testing.git"
