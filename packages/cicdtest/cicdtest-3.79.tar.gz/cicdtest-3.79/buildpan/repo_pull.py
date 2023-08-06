8db3086ebc7e76fa3e42d160bd1b7a1ab362ef20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:40:36 +0000

    Add package cicdtest/3.79/cicdtest-3.79.tar.gz

---
+++ packages/cicdtest/cicdtest-3.79.tar.gz/cicdtest-3.79/buildpan/repo_pull.py
@@ -0,0 +1,47 @@
+"""
+    Title: repo_pull.py
+    Author: Kushagra A.
+    Language: Python
+    Date Created: 31-08-2021
+    Date Modified: 23-09-2021
+    Description:
+        ###############################################################
+        ## Perform a pull operation on a repository   ## 
+         ###############################################################
+ """
+
+import datetime
+import requests
+from buildpan import setting, access_token, read_file
+import subprocess
+
+
+info = setting.info
+
+
+fetch_log = info["FETCH_LOG_URL"]
+
+
+def repo_pull(path, project_id, repo_name, username, provider):
+
+    try:
+        curtime = datetime.datetime.now()
+
+        if provider == "github":
+            subprocess.call(["git", "pull", "origin", "main"], cwd=path)
+
+            requests.post(fetch_log + "?" +'project_id='+project_id+'&repo_name='+repo_name+'&Time='+str(curtime)+'&user_name='+username+'&message=pull operation performed'+'&status=success&operation=pull')
+
+        elif provider == "bitbucket":
+            token = read_file.read_file(path, project_id)
+
+            pull = ["git", "-c", f"http.extraHeader=Authorization: Bearer {token}", "pull"]
+
+            subprocess.run(pull, cwd=path)
+
+            requests.post(fetch_log + "?" +'project_id='+project_id+'&repo_name='+repo_name+'&Time='+str(curtime)+'&user_name='+username+'&message=pull operation performed'+'&status=success&operation=pull')
+
+    except Exception as e:
+        print("e = ",e)
+        curtime = datetime.datetime.now()          
+        requests.post(fetch_log+ "?" +'project_id='+project_id+'&repo_name='+repo_name+'&Time='+str(curtime)+'&user_name='+username+'&message=Exception: pull operation performed. '+str(e)+'&status=failed&operation=pull')
