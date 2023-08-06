1feb172dda3220673e45b854bae50f88d40d6bd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:43:54 +0000

    Add package lightwood/1.2.0/lightwood-1.2.0.tar.gz

---
+++ packages/lightwood/lightwood-1.2.0.tar.gz/lightwood-1.2.0/lightwood/data/splitter.py
@@ -0,0 +1,48 @@
+import pandas as pd
+import numpy as np
+from typing import List
+from itertools import product
+
+from lightwood.api.types import TimeseriesSettings
+
+
+def splitter(data: pd.DataFrame, k: int, tss: TimeseriesSettings) -> List[pd.DataFrame]:
+    """
+    Splits a dataframe into k equally-sized subsets.
+    """
+    if not tss.is_timeseries:
+        # shuffle
+        data = data.sample(frac=1).reset_index(drop=True)
+
+        # split
+        subsets = np.array_split(data, k)
+
+    else:
+        if not tss.group_by:
+            subsets = np.array_split(data, k)
+        else:
+            gcols = tss.group_by
+            subsets = grouped_ts_splitter(data, k, gcols)
+
+    return subsets
+
+
+def grouped_ts_splitter(data: pd.DataFrame, k: int, gcols: List[str]):
+    """
+    Splitter for grouped time series tasks, where there is a set of `gcols` columns by which data is grouped.
+    Each group yields a different time series, and the splitter generates `k` subsets from `data`,
+    with equally-sized sub-series for each group.
+    """
+    all_group_combinations = list(product(*[data[gcol].unique() for gcol in gcols]))
+    subsets = [pd.DataFrame() for _ in range(k)]
+    for group in all_group_combinations:
+        subframe = data
+        for idx, gcol in enumerate(gcols):
+            subframe = subframe[subframe[gcol] == group[idx]]
+
+        subset = np.array_split(subframe, k)
+
+        for i in range(k):
+            subsets[i] = pd.concat([subsets[i], subset[i]])
+
+    return subsets
---
+++ packages/lightwood/lightwood-1.2.0.tar.gz/lightwood-1.2.0/lightwood/ensemble/best_of.py
@@ -0,0 +1,42 @@
+from typing import List
+
+import numpy as np
+import pandas as pd
+
+from lightwood.helpers.log import log
+from lightwood.mixer.base import BaseMixer
+from lightwood.ensemble.base import BaseEnsemble
+from lightwood.data.encoded_ds import EncodedDs, ConcatedEncodedDs
+from lightwood.helpers.general import evaluate_accuracy
+
+
+class BestOf(BaseEnsemble):
+    best_index: int
+
+    def __init__(self, target, mixers: List[BaseMixer], data: List[EncodedDs], accuracy_functions) -> None:
+        super().__init__(target, mixers, data)
+        # @TODO: Need some shared accuracy functionality to determine mixer selection here
+        self.maximize = True
+        best_score = -pow(2, 32) if self.maximize else pow(2, 32)
+        ds = ConcatedEncodedDs(data)
+        for idx, mixer in enumerate(mixers):
+            score_dict = evaluate_accuracy(
+                ds.data_frame,
+                mixer(ds)['prediction'],
+                target,
+                accuracy_functions
+            )
+            avg_score = np.mean(list(score_dict.values()))
+            log.info(f'Mixer {type(mixer).__name__} obtained a best-of evaluation score of {round(avg_score,4)}')
+            if self.improves(avg_score, best_score, accuracy_functions):
+                best_score = avg_score
+                self.best_index = idx
+
+        self.supports_proba = self.mixers[self.best_index].supports_proba
+        log.info(f'Picked best mixer: {type(self.mixers[self.best_index]).__name__}')
+
+    def __call__(self, ds: EncodedDs, predict_proba: bool = False) -> pd.DataFrame:
+        return self.mixers[self.best_index](ds, predict_proba=predict_proba)
+
+    def improves(self, new, old, functions):
+        return new > old if self.maximize else new < old
