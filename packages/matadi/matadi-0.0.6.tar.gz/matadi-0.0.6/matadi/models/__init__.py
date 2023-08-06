a914a1c0d70cc4a65da38d344917db64e10dc94c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:43:33 +0000

    Add package matadi/0.0.6/matadi-0.0.6.tar.gz

---
+++ packages/matadi/matadi-0.0.6.tar.gz/matadi-0.0.6/matadi/models/__init__.py
@@ -0,0 +1,13 @@
+from ._isotropic_hyperelasticity import (
+    neo_hooke,
+    mooney_rivlin,
+    yeoh,
+    third_order_deformation,
+    ogden,
+    arruda_boyce,
+)
+
+from ._anisotropic_hyperelasticity import (
+    fiber,
+    fiber_family,
+)
