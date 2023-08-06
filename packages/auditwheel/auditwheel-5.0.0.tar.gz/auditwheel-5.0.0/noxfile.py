e6bf34bb710d4ce0a71b2bb1ce2bef3daf0fa554
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:41 +0000

    Add package auditwheel/5.0.0/auditwheel-5.0.0.tar.gz

---
+++ packages/auditwheel/auditwheel-5.0.0.tar.gz/auditwheel-5.0.0/noxfile.py
@@ -0,0 +1,75 @@
+import os
+import sys
+from pathlib import Path
+from typing import List
+
+import nox
+
+RUNNING_CI = "TRAVIS" in os.environ or "GITHUB_ACTIONS" in os.environ
+
+
+@nox.session(python=["3.6"], reuse_venv=True)
+def lint(session: nox.Session) -> None:
+    """
+    Run linters on the codebase.
+    """
+    session.install("pre-commit")
+    session.run("pre-commit", "run", "--all-files")
+
+
+@nox.session()
+def coverage(session: nox.Session) -> None:
+    """
+    Run coverage using unit tests.
+    """
+    session.install("-r", "test-requirements.txt", "pytest-cov", ".")
+    session.run(
+        "python",
+        "-m",
+        "pytest",
+        "tests/unit",
+        "--cov=auditwheel",
+        "--cov-report=term-missing",
+    )
+
+
+def _docker_images(session: nox.Session) -> List[str]:
+    tmp_dir = Path(session.create_tmp())
+    script = tmp_dir / "list_images.py"
+    images_file = tmp_dir / "images.lst"
+    script.write_text(
+        fr"""
+import sys
+from pathlib import Path
+sys.path.append("./tests/integration")
+from test_manylinux import MANYLINUX_IMAGES
+images = "\n".join(MANYLINUX_IMAGES.values())
+Path(r"{images_file}").write_text(images)
+"""
+    )
+    session.run("python", str(script), silent=True)
+    return images_file.read_text().splitlines()
+
+
+@nox.session(python=["3.6", "3.7", "3.8", "3.9"])
+def tests(session: nox.Session) -> None:
+    """
+    Run tests.
+    """
+    posargs = session.posargs
+    session.install("-r", "test-requirements.txt", "-e", ".")
+    if RUNNING_CI:
+        session.install("pytest-cov", "codecov")
+        posargs.extend(["--cov", "auditwheel", "--cov-branch"])
+        # pull manylinux images that will be used.
+        # this helps passing tests which would otherwise timeout.
+        for image in _docker_images(session):
+            session.run("docker", "pull", image, external=True)
+
+    session.run("pytest", "-s", *posargs)
+    if RUNNING_CI:
+        session.run("auditwheel", "lddtree", sys.executable)
+        try:
+            session.run("codecov")
+        except nox.command.CommandFailed:
+            pass  # Ignore failures from codecov tool
---
+++ packages/auditwheel/auditwheel-5.0.0.tar.gz/auditwheel-5.0.0/src/auditwheel/tools.py
@@ -0,0 +1,111 @@
+import argparse
+import os
+import subprocess
+import zipfile
+from typing import Any, Iterable, List
+
+
+def unique_by_index(sequence: Iterable[Any]) -> List[Any]:
+    """unique elements in `sequence` in the order in which they occur
+
+    Parameters
+    ----------
+    sequence : iterable
+
+    Returns
+    -------
+    uniques : list
+        unique elements of sequence, ordered by the order in which the element
+        occurs in `sequence`
+    """
+    uniques = []
+    for element in sequence:
+        if element not in uniques:
+            uniques.append(element)
+    return uniques
+
+
+def zip2dir(zip_fname: str, out_dir: str) -> None:
+    """Extract `zip_fname` into output directory `out_dir`
+
+    Parameters
+    ----------
+    zip_fname : str
+        Filename of zip archive to write
+    out_dir : str
+        Directory path containing files to go in the zip archive
+    """
+    with zipfile.ZipFile(zip_fname, "r") as z:
+        for name in z.namelist():
+            member = z.getinfo(name)
+            extracted_path = z.extract(member, out_dir)
+            attr = member.external_attr >> 16
+            if member.is_dir():
+                # this is always rebuilt as 755 by dir2zip
+                os.chmod(extracted_path, 0o755)
+            elif attr != 0:
+                attr &= 511  # only keep permission bits
+                attr |= 6 << 6  # at least read/write for current user
+                os.chmod(extracted_path, attr)
+
+
+def dir2zip(in_dir: str, zip_fname: str) -> None:
+    """Make a zip file `zip_fname` with contents of directory `in_dir`
+
+    The recorded filenames are relative to `in_dir`, so doing a standard zip
+    unpack of the resulting `zip_fname` in an empty directory will result in
+    the original directory contents.
+
+    Parameters
+    ----------
+    in_dir : str
+        Directory path containing files to go in the zip archive
+    zip_fname : str
+        Filename of zip archive to write
+    """
+    with zipfile.ZipFile(zip_fname, "w", compression=zipfile.ZIP_DEFLATED) as z:
+        for root, dirs, files in os.walk(in_dir):
+            for dir in dirs:
+                dname = os.path.join(root, dir)
+                out_dname = os.path.relpath(dname, in_dir)
+                z.write(dname, out_dname)
+            for file in files:
+                fname = os.path.join(root, file)
+                out_fname = os.path.relpath(fname, in_dir)
+                z.write(fname, out_fname)
+
+
+def tarbz2todir(tarbz2_fname: str, out_dir: str) -> None:
+    """Extract `tarbz2_fname` into output directory `out_dir`"""
+    subprocess.check_output(["tar", "xjf", tarbz2_fname, "-C", out_dir])
+
+
+class EnvironmentDefault(argparse.Action):
+    """Get values from environment variable."""
+
+    def __init__(self, env, required=True, default=None, **kwargs):
+        self.env_default = os.environ.get(env)
+        self.env = env
+        if self.env_default:
+            default = self.env_default
+        if default:
+            required = False
+        if self.env_default and "choices" in kwargs:
+            choices = kwargs["choices"]
+            if self.env_default not in choices:
+                self.option_strings = kwargs["option_strings"]
+                args = {
+                    "value": self.env_default,
+                    "choices": ", ".join(map(repr, choices)),
+                    "env": self.env,
+                }
+                msg = (
+                    "invalid choice: %(value)r from environment variable "
+                    "%(env)r (choose from %(choices)s)"
+                )
+                raise argparse.ArgumentError(self, msg % args)
+
+        super().__init__(default=default, required=required, **kwargs)
+
+    def __call__(self, parser, namespace, values, option_string=None):
+        setattr(namespace, self.dest, values)
