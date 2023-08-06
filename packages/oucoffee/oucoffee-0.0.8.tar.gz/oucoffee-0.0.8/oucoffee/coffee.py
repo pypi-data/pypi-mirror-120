bc53197ce64097754100805f9c8c34ce680f03d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:04:10 +0000

    Add package oucoffee/0.0.8/oucoffee-0.0.8.tar.gz

---
+++ packages/oucoffee/oucoffee-0.0.8.tar.gz/oucoffee-0.0.8/oucoffee/coffee.py
@@ -0,0 +1,16 @@
+import torch
+
+def print_hi():
+	print('hi')
+
+def print_hello():
+	print('hello')
+
+def synthetic_data(w,b,num_examples):
+    X = torch.normal(0,1,(num_examples,len(w)))
+    y = torch.matmul(X,w)+b
+    y+=torch.normal(0,0.01,y.shape)
+    return X,y.reshap((-1,1))
+
+if __name__=='__main__':
+	print_hi()
