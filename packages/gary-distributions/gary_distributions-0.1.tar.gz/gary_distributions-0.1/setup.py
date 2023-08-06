2637ba2dd171204eddc491dd5ac3ce21287d61a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:52 +0000

    Add package gary-distributions/0.1/gary_distributions-0.1.tar.gz

---
+++ packages/gary-distributions/gary_distributions-0.1.tar.gz/gary_distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='gary_distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['gary_distributions'],
+      zip_safe=False)
