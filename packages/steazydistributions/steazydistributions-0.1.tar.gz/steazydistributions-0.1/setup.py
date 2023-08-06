ff118f8a1080d22cfb9d1cc925bd5899294221ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:38:56 +0000

    Add package steazydistributions/0.1/steazydistributions-0.1.tar.gz

---
+++ packages/steazydistributions/steazydistributions-0.1.tar.gz/steazydistributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='steazydistributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['steazydistributions'],
+      zip_safe=False)
