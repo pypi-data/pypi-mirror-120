1f6c4f7d236ac29d0597513b1a68d636e8282f7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:31:40 +0000

    Add package equadratures/9.1.0.2/equadratures-9.1.0.2-py3.9.egg

---
+++ packages/equadratures/equadratures-9.1.0.2-py3.9.egg/equadratures/__init__.py
@@ -0,0 +1,16 @@
+from equadratures.parameter import Parameter
+from equadratures.poly import Poly
+from equadratures.stats import Statistics
+from equadratures.basis import Basis
+from equadratures.polynet import Polynet
+from equadratures.correlations import Correlations
+from equadratures.optimisation import Optimisation
+from equadratures.subspaces import Subspaces
+from equadratures.weight import Weight
+from equadratures.poly import evaluate_model, evaluate_model_gradients, vector_to_2D_grid
+from equadratures.polytree import PolyTree
+from equadratures.solver import Solver
+from equadratures.scalers import *
+import numpy as np
+import os, sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
