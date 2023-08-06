c6b9284579284525c5524badc842058165101b22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:29:16 +0000

    Add package foodx-devops-tools/0.0.0.post53/foodx_devops_tools-0.0.0.post53.tar.gz

---
+++ packages/foodx-devops-tools/foodx_devops_tools-0.0.0.post53.tar.gz/foodx_devops_tools-0.0.0.post53/tests/ci/unit_tests/deploy_me/deployment/conftest.py
@@ -0,0 +1,51 @@
+#  Copyright (c) 2021 Food-X Technologies
+#
+#  This file is part of foodx_devops_tools.
+#
+#  You should have received a copy of the MIT License along with
+#  foodx_devops_tools. If not, see <https://opensource.org/licenses/MIT>.
+
+import pytest
+
+from foodx_devops_tools.deploy_me._deployment import PipelineCliOptions
+
+
+@pytest.fixture()
+def mock_completion_event(mock_async_method):
+    mock_async_method(
+        "foodx_devops_tools.deploy_me._deployment"
+        ".DeploymentStatus.wait_for_all_completed"
+    )
+
+
+@pytest.fixture()
+def pipeline_parameters():
+    def _apply(
+        enable_validation=True,
+        monitor_sleep_seconds=30,
+        wait_timeout_seconds=10,
+    ):
+        cli_options = PipelineCliOptions(
+            enable_validation=enable_validation,
+            monitor_sleep_seconds=monitor_sleep_seconds,
+            wait_timeout_seconds=wait_timeout_seconds,
+        )
+
+        return cli_options
+
+    return _apply
+
+
+@pytest.fixture()
+def default_override_parameters():
+    def _apply(context):
+        result = {
+            "locations": {
+                "primary": context.data.location_primary,
+                "secondary": context.data.location_secondary,
+            },
+        }
+
+        return result
+
+    return _apply
