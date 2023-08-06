371831e531cf3d722dfca8d6a24101a1e975c0c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:44:54 +0000

    Add package samourai-Adafruit-Blinka/0.0.1a0/samourai-Adafruit-Blinka-0.0.1a0.tar.gz

---
+++ packages/samourai-Adafruit-Blinka/samourai-Adafruit-Blinka-0.0.1a0.tar.gz/samourai-Adafruit-Blinka-0.0.1a0/src/micropython.py
@@ -0,0 +1,30 @@
+"""
+`micropython` - MicroPython Specific Decorator Functions
+=================================================
+
+* Author(s): cefn
+"""
+
+
+__version__ = "0.0.1-alpha.0"
+__repo__ = "https://github.com/adafruit/Adafruit_Blinka.git"
+
+
+def const(x):
+    "Emulate making a constant"
+    return x
+
+
+def native(f):
+    "Emulate making a native"
+    return f
+
+
+def viper(f):
+    "User is attempting to use a viper code emitter"
+    raise SyntaxError("invalid micropython decorator")
+
+
+def asm_thumb(f):
+    "User is attempting to use an inline assembler"
+    raise SyntaxError("invalid micropython decorator")
