cfa5930c4018afe97ae914cb121fe373b228b003
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:23:58 +0000

    Add package example-pkg-chenbenhua/0.0.1/example-pkg-chenbenhua-0.0.1.tar.gz

---
+++ packages/example-pkg-chenbenhua/example-pkg-chenbenhua-0.0.1.tar.gz/example-pkg-chenbenhua-0.0.1/example_pkg/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+# @Department: Information Centre
+# @date: 2021/9/23 14:31
+# @Author：cbh
+# @Version：Python 3.7.0
+# @Filename: __init__.py
+name = "example_pkg"
