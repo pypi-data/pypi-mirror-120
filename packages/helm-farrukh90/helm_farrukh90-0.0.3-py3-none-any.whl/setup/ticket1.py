24ffdd2bc74d8153ad79075ee53831e4ed0a4547
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:11:10 +0000

    Add package helm-farrukh90/0.0.3/helm_farrukh90-0.0.3-py3-none-any.whl

---
+++ packages/helm-farrukh90/helm_farrukh90-0.0.3-py3-none-any.whl/setup/ticket1.py
@@ -0,0 +1,31 @@
+import os 
+import subprocess
+import sys 
+import time
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+def taskchecker():
+    try:
+        output = subprocess.check_output(["hsdfelm", "version"])
+        if output is not None:
+            print(bcolors.OKGREEN +"helm is installed"+ bcolors.ENDC)
+            time.sleep(0.5)
+        else:
+            print(bcolors.FAIL + "did you install helm? "+ bcolors.ENDC)
+            sys.exit(1)
+            time.sleep(0.5)
+    except FileNotFoundError:
+        print(bcolors.FAIL + "Helm is not installed"+ bcolors.ENDC)
+        sys.exit(1)
+        time.sleep(0.5)
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
