7a20276b98396e1681e4332593e52d019188dffe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:52 +0000

    Add package dr-scaffold/2.1.1/dr_scaffold-2.1.1.tar.gz

---
+++ packages/dr-scaffold/dr_scaffold-2.1.1.tar.gz/dr_scaffold-2.1.1/setup.py
@@ -0,0 +1,54 @@
+from setuptools import find_packages, setup
+
+with open("PYPIREADME.rst", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+NAME = "dr_scaffold"
+VERSION = "v2.1.1"
+DESCRIPTION = "a Django package for scaffolding django rest apis using cli"
+LONG_DESCRIPTION = (long_description,)
+LONG_DESCRIPTION_CONTENT_TYPE = "text/x-rst"
+URL = "https://github.com/Abdenasser/dr_scaffold"
+AUTHOR = "abdenasser"
+AUTHOR_EMAIL = "nasser.elidrissi065@gmail.com"
+LICENSE = "MIT"
+PACKAGES = [
+    "dr_scaffold",
+    "dr_scaffold.scaffold_templates",
+    "dr_scaffold.management",
+    "dr_scaffold.management.commands",
+]
+REQUIREMENTS = ["django", "inflect", "isort"]
+CLASSIFIERS = [
+    "Development Status :: 5 - Production/Stable",
+    "Environment :: Web Environment",
+    "Framework :: Django",
+    "Intended Audience :: Developers",
+    "License :: OSI Approved :: MIT License",
+    "Natural Language :: English",
+    "Operating System :: POSIX :: Linux",
+    "Programming Language :: Python",
+    "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3.4",
+    "Programming Language :: Python :: 3.5",
+    "Programming Language :: Python :: 3.6",
+]
+
+EXCLUDE_FROM_PACKAGES = []
+
+setup(
+    name=NAME,
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type=LONG_DESCRIPTION_CONTENT_TYPE,
+    url=URL,
+    author=AUTHOR,
+    author_email=AUTHOR_EMAIL,
+    license=LICENSE,
+    packages=find_packages(exclude=EXCLUDE_FROM_PACKAGES),
+    install_requires=REQUIREMENTS,
+    include_package_data=True,
+    classifiers=CLASSIFIERS,
+    zip_safe=False,
+)
