e95d98e7beb38a6a897d9e2152778d98b7e295f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:29:00 +0000

    Add package vectorian/0.9.10/vectorian-0.9.10-cp39-cp39-macosx_10_9_universal2.whl

---
+++ packages/vectorian/vectorian-0.9.10-cp39-cp39-macosx_10_9_universal2.whl/vectorian/__init__.py
@@ -0,0 +1,20 @@
+import os
+
+from vectorian._version import __version__
+
+def compile(for_debugging=False, force_rebuild=False):
+	if for_debugging:
+		os.environ['DEBUG_VECTORIAN_CORE'] = '1'
+	if force_rebuild:
+		os.environ['DEBUG_VECTORIAN_FORCE_REBUILD'] = '1'
+	import vectorian.core
+
+
+import vectorian.embeddings as embeddings
+import vectorian.importers as importers
+import vectorian.session as session
+import vectorian.metrics as metrics
+import vectorian.alignment as alignment
+
+# "metrics" is the older name, "similarity" the newer name
+similarity = metrics
