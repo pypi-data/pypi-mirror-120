9071a5abbe5b1ae76cfe2d04543e12f8eb02de6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:22:31 +0000

    Add package veritastool/0.3/veritastool-0.3.tar.gz

---
+++ packages/veritastool/veritastool-0.3.tar.gz/veritastool-0.3/setup.py
@@ -0,0 +1,21 @@
+import platform
+from os import path as op
+import io
+from setuptools import setup
+
+here = op.abspath(op.dirname(__file__))
+# get the dependencies and installs
+with io.open(op.join(here, "requirements.txt"), encoding="utf-8") as f:
+    all_reqs = f.read().split("\n")
+    if platform.system() == "Windows":
+        all_reqs.append("pywin32")
+
+install_requires = [x.strip() for x in all_reqs if "git+" not in x]
+dependency_links = [x.strip().replace("git+", "") for x in all_reqs if "git+" not in x]
+
+setup(name='veritastool',version='0.3',description ='veritastool',author='MAS Veritas',
+install_requires=install_requires,
+dependency_links=dependency_links,
+packages=['veritastool','veritastool.fairness','veritastool.custom','veritastool.config'],
+include_package_data=True,
+zip_safe=False)
