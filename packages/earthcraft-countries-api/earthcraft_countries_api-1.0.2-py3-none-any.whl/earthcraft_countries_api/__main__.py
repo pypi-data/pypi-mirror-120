469612a53e54b6deb10197a692f7e1ec7de9d06e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:14 +0000

    Add package earthcraft-countries-api/1.0.2/earthcraft_countries_api-1.0.2-py3-none-any.whl

---
+++ packages/earthcraft-countries-api/earthcraft_countries_api-1.0.2-py3-none-any.whl/earthcraft_countries_api/__main__.py
@@ -0,0 +1,15 @@
+# __main__.py
+
+from configparser import ConfigParser
+from importlib import resources  # Python 3.7+
+import sys
+
+from earthcraft_countries_api import earthcraft_countries_api
+
+
+def main():
+    print("Hi")
+
+
+if __name__ == "__main__":
+    main()
