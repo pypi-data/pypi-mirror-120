9b6661b92396aed0422a8c0aaf8c96e98ccbeec2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:02:22 +0000

    Add package ripple-detector-CNN/0.1.0/ripple_detector_CNN-0.1.0-py3-none-any.whl

---
+++ packages/ripple-detector-CNN/ripple_detector_CNN-0.1.0-py3-none-any.whl/ripple_detector_CNN/__init__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# Time-stamp: "2021-09-18 06:28:56 (ywatanabe)"
+
+from .RippleDetectorCNN import RippleDetectorCNN
+from .ResNet1D.ResNet1D import ResNet1D
+from .define_ripple_candidates import define_ripple_candidates
+
+__copyright__ = "Copyright (C) 2021 Yusuke Watanabe"
+__version__ = "0.1.0"
+__license__ = "GPL3.0"
+__author__ = "ywatanabe1989"
+__author_email__ = "ywata1989@gmail.com"
+__url__ = "https://github.com/ywatanabe1989/ripple_detector_CNN"
+
+__all__ = ["ResNet1D", "RippleDetectorCNN"]
