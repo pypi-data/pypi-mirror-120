3511ef6c59930aeef85d9f8c4b79a88fa8a0a8f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:38:31 +0000

    Add package kslurm/0.1.0/kslurm-0.1.0.tar.gz

---
+++ packages/kslurm/kslurm-0.1.0.tar.gz/kslurm-0.1.0/kslurm/slurm/slurm_args/validators.py
@@ -0,0 +1,8 @@
+from ..job_templates import templates
+from kslurm.exceptions import TemplateError
+
+
+def job_template(arg: str) -> str:
+    if arg in templates():
+        return arg
+    raise TemplateError(f"{arg} is not a valid job-template")
