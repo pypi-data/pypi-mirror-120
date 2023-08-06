c6b2127e4bd35e0eec72833ab1bc0b9a17a2c5a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:55 +0000

    Add package stx-distributions/0.1/stx_distributions-0.1.tar.gz

---
+++ packages/stx-distributions/stx_distributions-0.1.tar.gz/stx_distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='stx_distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['stx_distributions'],
+      zip_safe=False)
