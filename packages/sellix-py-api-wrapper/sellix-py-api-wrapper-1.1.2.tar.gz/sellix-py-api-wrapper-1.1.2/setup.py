1143a808fdab2801df9f0f008d57dd017b88bf06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:12 +0000

    Add package sellix-py-api-wrapper/1.1.2/sellix-py-api-wrapper-1.1.2.tar.gz

---
+++ packages/sellix-py-api-wrapper/sellix-py-api-wrapper-1.1.2.tar.gz/sellix-py-api-wrapper-1.1.2/setup.py
@@ -0,0 +1,28 @@
+import os
+from setuptools import setup, find_packages
+
+VERSION = '1.1.2'
+DESCRIPTION = 'Sellix.IO API wrapper written in Python.'
+LONG_DESRIPTION = 'Documentation can be found on the Github repo.'
+
+# def read(fname):
+#     return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name="sellix-py-api-wrapper",
+    version=VERSION,
+    author="Mlchael",
+    author_email="",
+    description=DESCRIPTION,
+    packages=find_packages(),
+    # long_description=read('README.md'),
+    long_description=LONG_DESRIPTION,
+    install_requires=['requests'],
+    keywords=['python3', 'sellix', 'sellix.io', 'api', 'api wrapper'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
