aff69f545df82b92e80a40953803e9e9f4f72271
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:10:42 +0000

    Add package stactools-noaa-c-cap/0.1.1/stactools_noaa_c_cap-0.1.1-py3-none-any.whl

---
+++ packages/stactools-noaa-c-cap/stactools_noaa_c_cap-0.1.1-py3-none-any.whl/stactools/noaa_c_cap/__init__.py
@@ -0,0 +1,15 @@
+import stactools.core
+
+from stactools.noaa_c_cap.dataset import Dataset
+from stactools.noaa_c_cap.metadata import Metadata
+
+stactools.core.use_fsspec()
+
+
+def register_plugin(registry):
+    from stactools.noaa_c_cap import commands
+    registry.register_subcommand(commands.create_noaa_c_cap_command)
+
+
+__all__ = ['Metadata', 'Dataset']
+__version__ = "0.1.1"
