30d36a73f0164e17e1fd4a3da89754605547434c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:00 +0000

    Add package robotframework-openapidriver/2.0.0a0/robotframework_openapidriver-2.0.0a0-py3-none-any.whl

---
+++ packages/robotframework-openapidriver/robotframework_openapidriver-2.0.0a0-py3-none-any.whl/OpenApiDriver/__init__.py
@@ -0,0 +1,16 @@
+from importlib.metadata import version
+
+from OpenApiDriver.openapidriver import OpenApiDriver
+from OpenApiDriver.dto_base import  (
+    Relation,
+    Dto,
+    IdDependency,
+    IdReference,
+    PropertyValueConstraint,
+    UniquePropertyValueConstraint,
+)
+
+try:
+    __version__ = version("robotframework-openapidriver")
+except:     # pragma: no cover
+    pass
