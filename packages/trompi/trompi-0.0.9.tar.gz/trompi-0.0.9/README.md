286ed08c55dbcbb794fba11eb8cc19c9e3f6579a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:02:25 +0000

    Add package trompi/0.0.9/trompi-0.0.9.tar.gz

---
+++ packages/trompi/trompi-0.0.9.tar.gz/trompi-0.0.9/README.md
@@ -0,0 +1,2 @@
+# trompi
+trompi
---
+++ packages/trompi/trompi-0.0.9.tar.gz/trompi-0.0.9/setup.py
@@ -0,0 +1,12 @@
+import setuptools
+setuptools.setup(
+    name='trompi',
+    version='v0.0.9',
+    description='import lib from file service',
+    py_modules=['trompi'],
+    author='chenjianwei',
+    author_email='1572069841@qq.com',
+    url='https://github.com/JianweiChen/trompi',
+    packages=setuptools.find_packages(),
+    license='MIT'
+)
