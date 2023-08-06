4252e8dc6b2e63305557b608bf9cfea8345ebc55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:54 +0000

    Add package udacityTestDistributionsxx/0.1/udacityTestDistributionsxx-0.1.tar.gz

---
+++ packages/udacityTestDistributionsxx/udacityTestDistributionsxx-0.1.tar.gz/udacityTestDistributionsxx-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='udacityTestDistributionsxx',
+      version='0.1',
+      description='Gaussian distributions and binomial distribution',
+      packages=['udacityTestDistributionsxx'],
+      author='mohit ',
+      author_email='mohitahlawat.2001.ma@gmail.com',
+      zip_safe=False)
