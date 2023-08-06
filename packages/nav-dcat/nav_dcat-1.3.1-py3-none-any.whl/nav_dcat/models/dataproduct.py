6e190221ac4594171c6f4ac09239f096ef5adfb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:10:08 +0000

    Add package nav-dcat/1.3.1/nav_dcat-1.3.1-py3-none-any.whl

---
+++ packages/nav-dcat/nav_dcat-1.3.1-py3-none-any.whl/nav_dcat/models/dataproduct.py
@@ -0,0 +1,24 @@
+from typing import Optional, Sequence, Union, List
+from pydantic import BaseModel
+
+from nav_dcat.fields import common_fields
+from nav_dcat.models.dataset import DatasetDocument
+
+
+class DataproductDocument(BaseModel):
+    title: str
+    description: str
+    repo: str
+    pii: common_fields.Pii
+    modified: Optional[str] = None
+    issued: Optional[str] = None
+    periodicity: Optional[str] = ""
+    contactPoint: Optional[dict] = {}
+    author: Optional[str] = ""
+    long_description: Optional[str] = ""
+    accessRights: Optional[common_fields.AccessRights] = common_fields.AccessRights.INTERNAL.value
+    keyword: Optional[Sequence] = []
+    theme: Optional[Union[str, List[str]]] = ""
+    temporal: Optional[dict] = None
+    language: Optional[str] = "NO"
+    datasets: Optional[List[DatasetDocument]] = []
