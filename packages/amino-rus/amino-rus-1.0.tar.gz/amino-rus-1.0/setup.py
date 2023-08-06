754d53c4d2d75f59023c49f66a9fe5574b2d8d69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:33:56 +0000

    Add package amino-rus/1.0/amino-rus-1.0.tar.gz

---
+++ packages/amino-rus/amino-rus-1.0.tar.gz/amino-rus-1.0/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup
+version  = "1.0"
+long_description = """
+Асинхронная библиотека для взаимодействия с https://aminoapps.com/api и https://service.narvii.com/api/v1, собранная в одном файле. Все функции имеют русскоязычные имена.
+"""
+setup(
+    name = 'amino-rus',
+    version = '1.0',
+    url = 'https://github.com/D4rkwat3r/amino-rus',
+    download_url = 'https://github.com/D4rkwat3r/amino-rus/archive/refs/heads/main.zip',
+    author = 'Darkwater',
+    author_email = 'ktoya170214@gmail.com',
+    description = 'Асинхронная русскоязычная библиотека для взаимодействия с API Amino',
+    long_description = long_description,
+    keywords = [
+        'aminoapps',
+        'amino',
+        'amino-rus',
+        'amino_rus',
+        'amino-bot',
+        'amino_bot',
+        'amino-py',
+        'amino_py',
+        'amino.py',
+        'narvii',
+        'api',
+    ],
+    install_requires = [
+        'setuptools',
+        'aiohttp'
+    ],
+    setup_requires = [
+        'wheel'
+    ],
+    packages = ['амино']
+)
---
+++ packages/amino-rus/amino-rus-1.0.tar.gz/amino-rus-1.0/амино/__init__.py
@@ -0,0 +1,6 @@
+__title__ = 'Amino-rus'
+__author__ = 'Darkwater'
+__version__ = '1.0'
+
+from .Клиент import Клиент
+from .МестныйКлиент import МестныйКлиент
