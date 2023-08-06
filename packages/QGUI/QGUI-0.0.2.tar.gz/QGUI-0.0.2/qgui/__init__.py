6f898d1a4eaf99fe6dfaa5c07f77052a829948d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:10:41 +0000

    Add package QGUI/0.0.2/QGUI-0.0.2.tar.gz

---
+++ packages/QGUI/QGUI-0.0.2.tar.gz/QGUI-0.0.2/qgui/__init__.py
@@ -0,0 +1,8 @@
+# Author: Acer Zhang
+# Datetime: 2021/9/16 
+# Copyright belongs to the author.
+# Please indicate the source for reprinting.
+
+from qgui.factory import CreateQGUI
+
+from qgui.manager import *
