09029451499af31ad373e79effc63846fb31e41b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:25:32 +0000

    Add package LaoNLP/0.3.1/LaoNLP-0.3.1.tar.gz

---
+++ packages/LaoNLP/LaoNLP-0.3.1.tar.gz/LaoNLP-0.3.1/laonlp/translate/mopt_dict.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+import csv
+import os
+
+from laonlp.corpus import mopt_dict
+
+def dictionary(word: str, src: str, target: str):
+    if src == "lao" and target == "eng":
+        _temp = mopt_dict.get_lao_eng()
+        if word not in list(_temp.keys()):
+            return None
+        return _temp[word]
+    elif src == "eng" and target == "lao":
+        _temp = mopt_dict.get_eng_lao()
+        if word not in list(_temp.keys()):
+            return None
+        return _temp[word]
+    else:
+        return word
