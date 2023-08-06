366e5730734646747508a6b6aed86adf3c302b41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:30:26 +0000

    Add package testNew/1.0.0/testNew-1.0.0.tar.gz

---
+++ packages/testNew/testNew-1.0.0.tar.gz/testNew-1.0.0/index_test.py
@@ -0,0 +1,10 @@
+# _*_ coding:utf-8 _*_
+from distutils.core import setup
+setup(
+    name='testNew',
+    version='1.0.0',
+    description='用于测试简单运算',
+    author='Agui',
+    author_email='Agui@outlook.com',
+    py_modules=['test.test']
+)
---
+++ packages/testNew/testNew-1.0.0.tar.gz/testNew-1.0.0/test/test.py
@@ -0,0 +1,5 @@
+# _*_ coding:utf-8 _*_
+def num(number):
+    num = number * 12
+    return num
+print(num(1))
