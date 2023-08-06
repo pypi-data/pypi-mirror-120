23578b06f4fb8d66bb282cb46f7070e27ad60563
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:01:56 +0000

    Add package nans-are-numbers/1.0.3/nans_are_numbers-1.0.3.tar.gz

---
+++ packages/nans-are-numbers/nans_are_numbers-1.0.3.tar.gz/nans_are_numbers-1.0.3/nans_are_numbers/_version.py
@@ -0,0 +1,8 @@
+__version__ = "1.0.3"
+
+"""
+Version: 1.0.3: Fix badge link in README.
+Version: 1.0.2: Fix pypi link.
+Version: 1.0.1: Experimenting with pypi settings.
+Version: 1.0.0: First release.
+"""
