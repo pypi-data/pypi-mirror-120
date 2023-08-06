53a8c7cea68f4fdae2b41c4c9d704a7927c88053
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:22:48 +0000

    Add package pysmarthome-yeelight/1.2.1/pysmarthome-yeelight-1.2.1.tar.gz

---
+++ packages/pysmarthome-yeelight/pysmarthome-yeelight-1.2.1.tar.gz/pysmarthome-yeelight-1.2.1/pysmarthome-yeelight/controller.py
@@ -0,0 +1,47 @@
+from pysmarthome import Model, RgbLampController, hex_to_rgb, int_to_hex_color
+import yeelight
+
+class YeelightController(RgbLampController):
+    model_class = Model.extends(RgbLampController.model_class, name='YeelightsModel')
+    model_class.schema['addr'] = { 'type': 'string', 'default': '' }
+
+
+    def on_load(self, addr='', **data):
+        self.dev = yeelight.Bulb(addr)
+        super().on_load(**data)
+
+
+    def on(self):
+        return True if self.dev.turn_on() == 'ok' else False
+
+
+    def off(self):
+        return True if self.dev.turn_off() == 'ok' else False
+
+
+    def toggle(self):
+        return True if self.dev.toggle() == 'ok' else False
+
+
+    def get_brightness(self):
+        properties = self.dev.get_properties()
+        return int(properties['current_brightness'])
+
+
+    def set_brightness(self, val):
+        self.dev.set_brightness(int(val))
+
+
+    def set_color(self, color):
+        if type(color) == str:
+            color = hex_to_rgb(color)
+        self.dev.set_rgb(*color)
+
+
+    def get_color(self):
+        return int_to_hex_color(self.dev.get_properties()['rgb'])
+
+
+    def get_power(self):
+        properties = self.dev.get_properties()
+        return properties['power']
---
+++ packages/pysmarthome-yeelight/pysmarthome-yeelight-1.2.1.tar.gz/pysmarthome-yeelight-1.2.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pysmarthome-yeelight',
+    description='Yeelight plugin for pysmarthome',
+    version='1.2.1',
+    author='Filipe Alves',
+    author_email='filipe.alvesdefernando@gmail.com',
+    install_requires=[
+        'pysmarthome~=3.0',
+        'yeelight',
+    ],
+    packages=find_packages(),
+    url='https://github.com/filipealvesdef/pysmarthome-yeelight',
+    zip_zafe=False,
+)
