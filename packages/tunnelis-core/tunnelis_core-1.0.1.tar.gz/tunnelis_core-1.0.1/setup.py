b82a32ab1ab8be5da82b16a5b0b4e9175ac00591
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:40:15 +0000

    Add package tunnelis-core/1.0.1/tunnelis_core-1.0.1.tar.gz

---
+++ packages/tunnelis-core/tunnelis_core-1.0.1.tar.gz/tunnelis_core-1.0.1/setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+setuptools.setup(
+    name="tunnelis_core",
+    version="1.0.1",
+    author="Pedro Melo, Rafael Arrais, Sergio Marinho",
+    author_email="pedro.m.melo@inesctec.pt, rafael.l.arrais@inesctec.pt, sergio.d.marinho@inesctec.pt",
+    description="Python package containing Tunnelis messages andd required utility functions.",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3"
+    ],
+    python_requires='>=3.6'
+)
