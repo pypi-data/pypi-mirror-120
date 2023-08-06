a19fdc207ff34b68ea614c5833814eb7ade3ecec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:46:44 +0000

    Add package fairies/0.1.19/fairies-0.1.19.tar.gz

---
+++ packages/fairies/fairies-0.1.19.tar.gz/fairies-0.1.19/fairies/knowledge.py
@@ -0,0 +1,13 @@
+import fairies as fa
+
+def get_tongyin():
+    
+    return fa.same_pinyin.tongyin
+
+def get_tongxing():
+   
+    return fa.same_stroke.tongxing
+
+def get_hanzi():
+
+    return fa.hanzi.hanzi
