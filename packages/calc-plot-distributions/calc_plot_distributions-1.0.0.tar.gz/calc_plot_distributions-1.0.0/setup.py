b83cc5480050546c489f80cb21f5f3234309cdc6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:22:18 +0000

    Add package calc-plot-distributions/1.0.0/calc_plot_distributions-1.0.0.tar.gz

---
+++ packages/calc-plot-distributions/calc_plot_distributions-1.0.0.tar.gz/calc_plot_distributions-1.0.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='calc_plot_distributions',
+      version='1.0.0',
+      description='Gaussian distributions',
+      author='Ezeobi Jeshurun',
+      author_email='oejeshurun@gmail.com',
+      packages=['calc_plot_distributions'],
+      zip_safe=False)
