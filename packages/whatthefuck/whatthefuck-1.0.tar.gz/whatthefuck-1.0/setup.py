7ee59b3db891447e640456228516851fd224fe80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:50 +0000

    Add package whatthefuck/1.0/whatthefuck-1.0.tar.gz

---
+++ packages/whatthefuck/whatthefuck-1.0.tar.gz/whatthefuck-1.0/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("readme.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="whatthefuck",                     # This is the name of the package
+    version="v1.0",                        # The initial release version
+    author = 'Shubham Rath',                     # Full name of the author
+    author_email = 'shubhamrath.iiitb@gmail.com',
+    description = 'Frustration made me develop this script.',
+    long_description=long_description,      # Long description read from the the readme file
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),    # List of all python modules to be installed
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],                                      # Information to filter the project on PyPi website
+    python_requires='>=3.5',                # Minimum version requirement of the package
+    py_modules=["whatthefuck"],             # Name of the python package
+    package_dir={'':'whatthefuck/src'},     # Directory of the source code of the package
+    install_requires=[]                     # Install other dependencies if any
+)
