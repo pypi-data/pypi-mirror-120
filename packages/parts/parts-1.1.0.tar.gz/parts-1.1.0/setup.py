ab4dff444deefeb3f91021be375ec90e99e5efe8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:29:45 +0000

    Add package parts/1.1.0/parts-1.1.0.tar.gz

---
+++ packages/parts/parts-1.1.0.tar.gz/parts-1.1.0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+with open("README.rst", "r") as fh:
+    long_description = fh.read().replace(".. include:: toc.rst\n\n", "")
+
+setup(
+    name="parts",
+    version="1.1.0",
+    packages=["parts",],
+    install_requires=[],
+    license="MIT",
+    url="https://github.com/lapets/parts",
+    author="Andrei Lapets",
+    author_email="a@lapets.io",
+    description="Minimal library that enables partitioning "+\
+                "of iterable objects in a concise manner.",
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
+    test_suite="nose.collector",
+    tests_require=["nose"],
+)
