fb1bb52569aa43c2988ab5e619fccb0c2c11e926
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:30:18 +0000

    Add package clinica/0.5.1/clinica-0.5.1-py2.py3-none-any.whl

---
+++ packages/clinica/clinica-0.5.1-py2.py3-none-any.whl/clinica/pipelines/dwi_preprocessing_using_t1/dwi_preprocessing_using_t1_cli.py
@@ -0,0 +1,61 @@
+from typing import Optional
+
+import click
+
+from clinica.pipelines import cli_param
+
+pipeline_name = "dwi-preprocessing-using-t1"
+
+
+@click.command(name=pipeline_name)
+@cli_param.argument.bids_directory
+@cli_param.argument.caps_directory
+@cli_param.option_group.pipeline_specific_options
+@cli_param.option.low_bval
+@cli_param.option_group.common_pipelines_options
+@cli_param.option.subjects_sessions_tsv
+@cli_param.option.working_directory
+@cli_param.option.n_procs
+def cli(
+    bids_directory: str,
+    caps_directory: str,
+    low_bval: int = 5,
+    subjects_sessions_tsv: Optional[str] = None,
+    working_directory: Optional[str] = None,
+    n_procs: Optional[int] = None,
+) -> None:
+    """Preprocessing of raw DWI datasets using a T1w image.
+
+    https://aramislab.paris.inria.fr/clinica/docs/public/latest/Pipelines/DWI_Preprocessing/
+    """
+    from networkx import Graph
+
+    from clinica.utils.ux import print_end_pipeline
+
+    from .dwi_preprocessing_using_t1_pipeline import DwiPreprocessingUsingT1
+
+    parameters = {"low_bval": low_bval}
+
+    pipeline = DwiPreprocessingUsingT1(
+        bids_directory=bids_directory,
+        caps_directory=caps_directory,
+        tsv_file=subjects_sessions_tsv,
+        base_dir=working_directory,
+        parameters=parameters,
+        name=pipeline_name,
+    )
+
+    exec_pipeline = (
+        pipeline.run(plugin="MultiProc", plugin_args={"n_procs": n_procs})
+        if n_procs
+        else pipeline.run()
+    )
+
+    if isinstance(exec_pipeline, Graph):
+        print_end_pipeline(
+            pipeline_name, pipeline.base_dir, pipeline.base_dir_was_specified
+        )
+
+
+if __name__ == "__main__":
+    cli()
