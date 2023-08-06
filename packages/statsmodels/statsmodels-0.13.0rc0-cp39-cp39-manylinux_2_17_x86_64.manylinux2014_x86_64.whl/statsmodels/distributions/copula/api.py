daef5252a6d366131084a2eb8ed8c1b83342c583
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:32:03 +0000

    Add package statsmodels/0.13.0rc0/statsmodels-0.13.0rc0-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/statsmodels/statsmodels-0.13.0rc0-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/statsmodels/distributions/copula/api.py
@@ -0,0 +1,21 @@
+# pylint: disable=W0611
+# flake8: noqa
+
+from .copulas import CopulaDistribution
+from . import transforms
+from . import depfunc_ev
+
+from statsmodels.distributions.copula.copulas import (
+    CopulaDistribution)
+
+from statsmodels.distributions.copula.archimedean import (
+    ArchimedeanCopula, FrankCopula, ClaytonCopula, GumbelCopula)
+
+from statsmodels.distributions.copula.elliptical import (
+    GaussianCopula, StudentTCopula)
+
+from statsmodels.distributions.copula.extreme_value import (
+    ExtremeValueCopula)
+
+from statsmodels.distributions.copula.other_copulas import (
+    IndependenceCopula)
