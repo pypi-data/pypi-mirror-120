71db167e21da337526e3567d11bf291f901b8883
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:30:47 +0000

    Add package hot-recharge/3.3.1/hot-recharge-3.3.1.tar.gz

---
+++ packages/hot-recharge/hot-recharge-3.3.1.tar.gz/hot-recharge-3.3.1/hotrecharge/__init__.py
@@ -0,0 +1,8 @@
+#  __init__.py
+from hotrecharge import *
+from .HotRecharge import HotRecharge, HRAuthConfig
+from .HotRechargeException import *
+
+__author__ = "Donald Chinhuru"
+__version__ = "3.3.1"
+__name__ = "hot-recharge"
