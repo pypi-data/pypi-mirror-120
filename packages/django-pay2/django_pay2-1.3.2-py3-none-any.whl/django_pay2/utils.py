d9ebd61202e9a5f6a3534759ec78f95a27f49e0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:47 +0000

    Add package django-pay2/1.3.2/django_pay2-1.3.2-py3-none-any.whl

---
+++ packages/django-pay2/django_pay2-1.3.2-py3-none-any.whl/django_pay2/utils.py
@@ -0,0 +1,17 @@
+def clear_none(a_dict):
+    return {key: value for key, value in a_dict.items() if value is not None}
+
+
+class MappingFieldsMixin:
+    mapping_keys = {}
+
+    def to_internal_value(self, data):
+        new_data = {
+            new_key: data.get(old_key) for new_key, old_key in self.mapping_keys.items()
+        }
+        return super().to_internal_value(new_data)
+
+
+def remove_trivial_zeros(num):
+    num = str(num)
+    return num.rstrip("0").rstrip(".")
