cde47047e59a4f946ca5b5c12c5865199bd5a0fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:49 +0000

    Add package generic-dataset/1.4.1/generic-dataset-1.4.1.tar.gz

---
+++ packages/generic-dataset/generic-dataset-1.4.1.tar.gz/generic-dataset-1.4.1/setup.py
@@ -0,0 +1,95 @@
+import os
+import re
+import subprocess
+
+# To use a consistent encoding
+from codecs import open as copen
+
+from setuptools import find_packages, setup
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+# Get the long description from the relevant file
+with copen(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+
+def read(*parts):
+    with copen(os.path.join(here, *parts), 'r') as fp:
+        return fp.read()
+
+
+def find_version(*file_paths):
+    version_file = read(*file_paths)
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
+                              version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
+
+
+__version__ = find_version("generic_dataset", "__version__.py")
+
+test_deps = [
+    "pytest",
+    "pytest-cov",
+    "opencv-python"
+]
+
+extras = {
+    'test': test_deps,
+}
+
+def get_cuda_version():
+
+    res = subprocess.check_output('nvcc --version'.split(' ')).decode()
+    if bool(res.rstrip()):
+        regex = r'release (\S+),'
+        match = re.search(regex, res)
+        if match:
+            return str(match.group(1)).replace('.', '')
+
+    return ''
+
+try:
+    cuda_version = get_cuda_version()
+    if not cuda_version == '':
+        cuda_version = 'cupy-cuda' + cuda_version
+except:
+    cuda_version = ''
+
+setup(
+    name='generic-dataset',
+    version=__version__,
+    description="Gibson dataset",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/micheleantonazzi/generic-dataset",
+    author="Michele Antonazzi",
+    author_email="micheleantonazzi@gmail.com",
+    # Choose your license
+    license='MIT',
+    include_package_data=True,
+    classifiers=[
+        'Environment :: GPU :: NVIDIA CUDA',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3'
+    ],
+    packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
+    tests_require=test_deps,
+    # Add here the package dependencies
+    install_requires=[
+        'opencv-python',
+        'numpy',
+        'pandas',
+        cuda_version,
+        'termcolor',
+        'stub-generator'
+    ],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+    test_deps=test_deps,
+    extras_require=extras,
+)
