880633e73506f61fca36ce665c866ba9101879ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:28:36 +0000

    Add package cegpy/0.1.2/cegpy-0.1.2.tar.gz

---
+++ packages/cegpy/cegpy-0.1.2.tar.gz/cegpy-0.1.2/src/cegpy/__init__.py
@@ -0,0 +1,19 @@
+"""
+CEGpy
+=====
+
+CEGpy is a python package for the creation, manipulation, and study of
+Chain Event Graphs.
+
+See documentation of more information.
+
+"""
+from utilities import *
+from trees import *
+from graphs import *
+import logging
+# from graphs import ctceg
+
+
+logging.basicConfig(level=logging.ERROR)
+logger = logging.getLogger('cegpy')
