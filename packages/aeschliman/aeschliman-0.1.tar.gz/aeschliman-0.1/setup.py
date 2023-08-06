4cfdb825ecc6b00161386f0719996eca4e80442a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:22:04 +0000

    Add package aeschliman/0.1/aeschliman-0.1.tar.gz

---
+++ packages/aeschliman/aeschliman-0.1.tar.gz/aeschliman-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='aeschliman',
+      version='0.1',
+      description='Gaussian/Binomial distributions',
+      packages=['aeschliman'],
+      author = 'Trevor Aeschliman',
+      author_email = 'trevor@dive-data.com',
+      zip_safe = False)
