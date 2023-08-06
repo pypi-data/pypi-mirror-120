eabbe811491426f40bdba1eb50e4f152688b8bbc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:31:20 +0000

    Add package zarnitsa/0.0.15/zarnitsa-0.0.15.tar.gz

---
+++ packages/zarnitsa/zarnitsa-0.0.15.tar.gz/zarnitsa-0.0.15/zarnitsa/__init__.py
@@ -0,0 +1,6 @@
+from zarnitsa.DataAugmenter import AbstractDataAugmenter
+
+from zarnitsa.nlp.DataAugmenterNLP import DataAugmenterNLP
+from zarnitsa.stats.DataAugmenterInternally import DataAugmenterInternally
+from zarnitsa.stats.DataAugmenterExternally import DataAugmenterExternally
+from zarnitsa.series.DataAugmenterTimeSeries import DataAugmenterTimeSeries
