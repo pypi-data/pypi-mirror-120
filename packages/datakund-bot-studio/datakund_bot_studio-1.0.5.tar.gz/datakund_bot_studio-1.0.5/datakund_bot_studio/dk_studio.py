c9b522c567eaa876521170d55639e2dba0f4ae72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:29:09 +0000

    Add package datakund-bot-studio/1.0.5/datakund_bot_studio-1.0.5.tar.gz

---
+++ packages/datakund-bot-studio/datakund_bot_studio-1.0.5.tar.gz/datakund_bot_studio-1.0.5/datakund_bot_studio/dk_studio.py
@@ -0,0 +1,8 @@
+import subprocess
+import os
+from datakund_bot_studio import start_studio
+def main():
+    print("Starting Studio.....")
+    #subprocess.run(["DataKund.exe", "start"])
+    #os.system(r'DataKund.exe start')
+    start_studio()
