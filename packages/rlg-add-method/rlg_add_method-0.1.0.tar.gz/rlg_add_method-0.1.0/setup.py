3c5f8676167ae3e33953e6d8131a2d6c8f639b3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:17 +0000

    Add package rlg-add-method/0.1.0/rlg_add_method-0.1.0.tar.gz

---
+++ packages/rlg-add-method/rlg_add_method-0.1.0.tar.gz/rlg_add_method-0.1.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+	
+setup(
+   name="rlg_add_method",  # 要发布包的包名
+   version="0.1.0",  # 版本号
+   author="renligeng",  # 作者
+   author_email="renligeng@gmail.com",  # 作者邮箱
+   description="Test",  # 描述
+   license="MIT",  # 许可证
+   url="https://github.com/vmware/****",  # 项目地址
+   packages=['rlg_add_method'],  # 项目目录
+   install_requires=[],  # 需要依赖的第三方模块
+)
