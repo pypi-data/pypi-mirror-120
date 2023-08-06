d0b00c200b96b36d07cddfdf4da3fc10eb75f036
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:45:38 +0000

    Add package infapy/1.0.5.1/infapy-1.0.5.1.tar.gz

---
+++ packages/infapy/infapy-1.0.5.1.tar.gz/infapy-1.0.5.1/infapy/v3/__init__.py
@@ -0,0 +1,10 @@
+import infapy
+
+class V3():
+    def __init__(self,v3,v3SessionID,v3BaseURL):
+        print("Created V3 Class")
+        self._v3=v3
+        self._v3SessionID = v3SessionID
+        self._v3BaseURL = v3BaseURL
+     
+        
