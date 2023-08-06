80d0ef520297f5d907dcab37981aaf32ba1bd94c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:50 +0000

    Add package mxsoftpy/0.0.8/mxsoftpy-0.0.8.tar.gz

---
+++ packages/mxsoftpy/mxsoftpy-0.0.8.tar.gz/mxsoftpy-0.0.8/setup.py
@@ -0,0 +1,15 @@
+# coding: utf-8
+
+from setuptools import setup
+
+setup(
+    name='mxsoftpy',
+    version='0.0.8',
+    author='great',
+    author_email='great@mxsoft.com',
+    url='https://gitee.com/yuanhao_1998/mxsoftpy.git',
+    description=u'美信python框架',
+    packages=['mxsoftpy'],
+    install_requires=['pydantic', 'typing-extensions'],
+    entry_points={}
+)
