5a5ee32b60ea7759fb9dc3143cb79453d456b9f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:23:24 +0000

    Add package dsnd-probability-alela/1.2/dsnd_probability_alela-1.2.tar.gz

---
+++ packages/dsnd-probability-alela/dsnd_probability_alela-1.2.tar.gz/dsnd_probability_alela-1.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='dsnd_probability_alela',
+      version='1.2',
+      description='Gaussian and binomial distributions',
+      author='Alela',
+      author_email='alelasantillan@gmail.com',
+      packages=['dsnd_probability_alela'],
+      zip_safe=False)
