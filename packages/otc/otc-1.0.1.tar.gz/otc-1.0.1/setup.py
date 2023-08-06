31f6c39d79ac5ed346a16fdf2d41ecb8df86ea89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:31:35 +0000

    Add package otc/1.0.1/otc-1.0.1.tar.gz

---
+++ packages/otc/otc-1.0.1.tar.gz/otc-1.0.1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+with open("README.rst", "r") as fh:
+    long_description = fh.read().replace(".. include:: toc.rst\n\n", "")
+
+setup(
+    name="otc",
+    version="1.0.1",
+    packages=["otc",],
+    install_requires=[
+        "pynacl~=1.4.0",
+        "oblivious~=3.0.1"
+    ],
+    license="MIT",
+    url="https://github.com/nthparty/otc",
+    author="Andrei Lapets",
+    author_email="a@lapets.io",
+    description="Oblivious transfer (OT) communications protocol "+\
+                "message/response functionality implementations "+\
+                "based on Curve25519 primitives.",
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
+    test_suite="nose.collector",
+    tests_require=["nose"],
+)
