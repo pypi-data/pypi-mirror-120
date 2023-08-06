f2c64c026b7504793e9d9c967305701d67a8b8b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:22:54 +0000

    Add package mccloud-cli/0.0.6/mccloud_cli-0.0.6.tar.gz

---
+++ packages/mccloud-cli/mccloud_cli-0.0.6.tar.gz/mccloud_cli-0.0.6/src/mccloud_cli/__init__.py
@@ -0,0 +1,11 @@
+from ._version import __version__
+from .jobs import submit_job, McCloudSubmitJobFailure
+from .urls import generate_presigned_input_url, generate_presigned_output_url
+
+__all__ = [
+    __version__,
+    submit_job,
+    McCloudSubmitJobFailure,
+    generate_presigned_input_url,
+    generate_presigned_output_url,
+]
