1fe037b880c378ca335e533ce53cb27f0374deb4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:53 +0000

    Add package hub/2.0.11/hub-2.0.11.tar.gz

---
+++ packages/hub/hub-2.0.11.tar.gz/hub-2.0.11/hub/core/compute/__init__.py
@@ -0,0 +1,4 @@
+from hub.core.compute.provider import ComputeProvider
+from hub.core.compute.process import ProcessProvider
+from hub.core.compute.thread import ThreadProvider
+from hub.core.compute.serial import SerialProvider
