4a28fc9fe5e073e8c7327cdb17c8cba3fb264b1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:24:44 +0000

    Add package trie-cli-global/0.1.0/trie-cli-global-0.1.0.tar.gz

---
+++ packages/trie-cli-global/trie-cli-global-0.1.0.tar.gz/trie-cli-global-0.1.0/README.md
@@ -0,0 +1,42 @@
+# trie-cli-global
+
+A CLI that allows a user to update or see a data structure that is hosted globally.
+
+## Installation
+
+Use [pip](https://pip.pypa.io/en/stable/) to install trie-global.
+
+```bash
+pip install trie-global
+```
+
+## Usage
+
+# To List Trie:
+```
+trie-global list
+```
+
+# To add word to Trie:
+```
+trie-global add YOUR_WORD
+```
+
+# To remove word from Trie:
+```
+trie-global remove YOUR_WORD
+```
+
+# To check if word is from Trie:
+```
+trie-global check YOUR_WORD
+```
+
+# To get recommendations from a prefix:
+
+```
+trie-global recommend YOUR_PREFIX
+```
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ packages/trie-cli-global/trie-cli-global-0.1.0.tar.gz/trie-cli-global-0.1.0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as readme_file:
+    longerDesc = readme_file.read()
+
+setup(
+    name='trie-cli-global',
+    version='0.1.0',
+    author = 'c25kenneth',
+    description = 'A CLI that allows a user to update or see a data structure that is hosted globally. ',
+    long_description=longerDesc,
+    packages=find_packages(),
+    long_description_content_type='text/markdown',
+    include_package_data=True,
+    py_modules = ['trie_global', 'appFolder'],
+    python_requires='>=3.9.6',
+    install_requires=['click', 'requests'],
+    entry_points = {
+        'console_scripts': [
+            'trie-cli-global=trie_global:main',
+        ]
+    }
+)
