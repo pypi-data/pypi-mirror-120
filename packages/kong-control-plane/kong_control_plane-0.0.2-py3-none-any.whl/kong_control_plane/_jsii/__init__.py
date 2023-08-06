bacd0c1b27b219e81be7a9fe5b74e70dff0ac622
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:48:14 +0000

    Add package kong-control-plane/0.0.2/kong_control_plane-0.0.2-py3-none-any.whl

---
+++ packages/kong-control-plane/kong_control_plane-0.0.2-py3-none-any.whl/kong_control_plane/_jsii/__init__.py
@@ -0,0 +1,40 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+import aws_cdk.aws_acmpca._jsii
+import aws_cdk.aws_autoscaling._jsii
+import aws_cdk.aws_autoscaling_hooktargets._jsii
+import aws_cdk.aws_certificatemanager._jsii
+import aws_cdk.aws_ec2._jsii
+import aws_cdk.aws_eks._jsii
+import aws_cdk.aws_elasticache._jsii
+import aws_cdk.aws_elasticsearch._jsii
+import aws_cdk.aws_events._jsii
+import aws_cdk.aws_events_targets._jsii
+import aws_cdk.aws_iam._jsii
+import aws_cdk.aws_kms._jsii
+import aws_cdk.aws_logs._jsii
+import aws_cdk.aws_rds._jsii
+import aws_cdk.aws_route53._jsii
+import aws_cdk.aws_secretsmanager._jsii
+import aws_cdk.aws_sqs._jsii
+import aws_cdk.core._jsii
+import constructs._jsii
+import kong_core._jsii
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "kong-control-plane", "0.0.2", __name__[0:-6], "kong-control-plane@0.0.2.jsii.tgz"
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
