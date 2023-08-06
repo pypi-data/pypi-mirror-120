bdce8fc18219fd4e26421d8fa14f97c8266dfaea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:22:42 +0000

    Add package probability-dnsd/1/probability_dnsd-1.tar.gz

---
+++ packages/probability-dnsd/probability_dnsd-1.tar.gz/probability_dnsd-1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='probability_dnsd',
+      version='1',
+      description='Gaussian and Binomial distributions',
+      packages=['probability_dnsd'],
+      author_email= 'arghyadeepsaha17@gmail.com',
+      install_requires=['matplotlib'],
+      zip_safe=False)
