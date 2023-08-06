fc7275c2a6767cbf91c0001406a803697b042523
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:03:15 +0000

    Add package pointlessutils/0.1.5/pointlessutils-0.1.5.tar.gz

---
+++ packages/pointlessutils/pointlessutils-0.1.5.tar.gz/pointlessutils-0.1.5/README.md
@@ -0,0 +1,27 @@
+# Pointless Utils
+
+A collection of utilities that are (probably) pointless.
+
+## Decorators
+
+There are currently 6 pointless decorators available to use:
+
+- `@none` - Causes the function to always return `None`
+- `@true` - Causes the function to always return `True`
+- `@false` - Causes the function to always return `False`
+- `@maybe` - Causes the function to return a random choice of `True` or `False`
+- `@never` - The function will never return and will block indefinitely
+- `@always` - The function is now a generator that indefinitely yields `True`
+- `@alwaysv(value)` - The function is now a generator that indefinitely yields the given value
+
+These decorators can be used on either normal or async functions. For example:
+
+```py
+@maybe
+def ex_1():
+    pass
+
+@maybe
+async def ex_2():
+    pass
+```
---
+++ packages/pointlessutils/pointlessutils-0.1.5.tar.gz/pointlessutils-0.1.5/pointlessutils/decorators.py
@@ -0,0 +1,48 @@
+from asyncio import iscoroutinefunction
+from inspect import isfunction
+from random import choice
+
+
+def _deco_factory(c):
+    def deco(func):
+        if iscoroutinefunction(func):
+            async def wrapper(*args, **kwargs):  # type: ignore
+                return c() if isfunction(c) else c
+        else:
+            def wrapper(*args, **kwargs):
+                return c() if isfunction(c) else c
+        return wrapper
+    return deco
+
+
+none  = _deco_factory(None)
+true  = _deco_factory(True)
+false = _deco_factory(False)
+maybe = _deco_factory(lambda: choice([True, False]))
+
+def _never():
+    while True:
+        pass
+
+never = _deco_factory(_never)
+
+def _always():
+    while True:
+        yield True
+
+always = _deco_factory(_always)
+
+def alwaysv(v):
+    def deco(func):
+        if iscoroutinefunction(func):
+            async def wrapper(*args, **kwargs):  # type: ignore
+                def gen():
+                    while True:
+                        yield v() if isfunction(v) else v
+                return gen()
+        else:
+            def wrapper(*args, **kwargs):
+                while True:
+                    yield v() if isfunction(v) else v
+        return wrapper
+    return deco
