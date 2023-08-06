c0efee3061cd4f2f104a50b86a8c996d302cdca2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:13 +0000

    Add package 2ppy/0.1.9/2ppy-0.1.9.tar.gz

---
+++ packages/2ppy/2ppy-0.1.9.tar.gz/2ppy-0.1.9/tuprolog/solve.py
@@ -0,0 +1,11 @@
+from tuprolog import logger
+
+# noinspection PyUnresolvedReferences
+import jpype
+# noinspection PyUnresolvedReferences
+import jpype.imports
+
+# noinspection PyUnresolvedReferences
+from it.unibo.tuprolog.solve import Solver
+
+logger.debug("Loaded JVM classes from it.unibo.tuprolog.solve.*")
