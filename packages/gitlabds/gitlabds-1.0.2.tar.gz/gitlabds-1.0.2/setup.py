9e88dd4029fa8af4884b7c31857fc01423bf73de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:01:33 +0000

    Add package gitlabds/1.0.2/gitlabds-1.0.2.tar.gz

---
+++ packages/gitlabds/gitlabds-1.0.2.tar.gz/gitlabds-1.0.2/setup.py
@@ -0,0 +1,20 @@
+import setuptools, os
+
+if os.environ.get('CI_COMMIT_TAG'):
+    version = os.environ['CI_COMMIT_TAG']
+else:
+    version = os.environ['CI_JOB_ID']
+
+requires = ["pandas>=1.0.5", "numpy>=1.19.5", "scipy>=1.5.4", "scikit-learn>=0.23.1", "imbalanced-learn>=0.8.0"]
+
+setuptools.setup(
+    name='gitlabds',
+    version=version,
+    description='Gitlab Data Science and Modeling Tools',
+    author='Kevin Dietz',
+    author_email='kdietz@gitlab.com',
+    packages=setuptools.find_packages(),
+    url='https://gitlab.com/gitlab-data/gitlabds',
+    python_requires= '>=3.6',
+    install_requires=requires,
+)  
