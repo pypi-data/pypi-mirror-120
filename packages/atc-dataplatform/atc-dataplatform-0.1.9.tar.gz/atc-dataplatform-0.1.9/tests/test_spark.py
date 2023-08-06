7e6b9a0fc690135ca1974e08554de36e62030289
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:26:25 +0000

    Add package atc-dataplatform/0.1.9/atc-dataplatform-0.1.9.tar.gz

---
+++ packages/atc-dataplatform/atc-dataplatform-0.1.9.tar.gz/atc-dataplatform-0.1.9/tests/test_spark.py
@@ -0,0 +1,12 @@
+import unittest
+
+import atc
+
+
+class TestSparkImport(unittest.TestCase):
+    def test_spark(self):
+        Spark = atc.spark
+
+
+if __name__ == "__main__":
+    unittest.main()
