8d9d31db40e9895abcc0cee70eb41426ddff152b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:32:12 +0000

    Add package great-assertions/0.0.2/great-assertions-0.0.2.tar.gz

---
+++ packages/great-assertions/great-assertions-0.0.2.tar.gz/great-assertions-0.0.2/tests/test_great_assertions.py
@@ -0,0 +1,15 @@
+from great_assertions.src.great_assertions import GreatAssertions
+import pandas as pd
+import pytest
+
+
+class GreatAssertionTests(GreatAssertions):
+    def test_expect_table_row_count_to_equal(self):
+        df = pd.DataFrame({"calories": [420, 380, 390], "duration": [50, 40, 45]})
+        self.assertExpectTableRowCountToEqual(df, 3)
+
+    def test_raises_type_error(self):
+        with pytest.raises(AssertionError) as excinfo:
+            self.assertExpectTableRowCountToEqual(1, 1)
+
+        assert "Object is not type DataFrame" in str(excinfo.value)
