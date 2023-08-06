80996d5c72eedba6a4dcc9ef2db013ede88cd7d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:02:36 +0000

    Add package trading-api-wrappers/0.18.1/trading_api_wrappers-0.18.1-py3-none-any.whl

---
+++ packages/trading-api-wrappers/trading_api_wrappers-0.18.1-py3-none-any.whl/trading_api_wrappers/__init__.py
@@ -0,0 +1,16 @@
+from .bitcoinity import *  # noqa: F401
+from .bitex import *  # noqa: F401
+from .bitfinex import *  # noqa: F401
+from .bitstamp import *  # noqa: F401
+from .buda import *  # noqa: F401
+from .coindesk import *  # noqa: F401
+from .coinmarketcap import *  # noqa: F401
+from .cryptomkt import *  # noqa: F401
+from .currencylayer import *  # noqa: F401
+from .errors import *  # noqa: F401
+from .kraken import *  # noqa: F401
+from .oxr import *  # noqa: F401
+from .ripio import *  # noqa: F401
+from .sfox import *  # noqa: F401
+from .surbtc import *  # noqa: F401
+from ._version import __version__  # noqa: F40
