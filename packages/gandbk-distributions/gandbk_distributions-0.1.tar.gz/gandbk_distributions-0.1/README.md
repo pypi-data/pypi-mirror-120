49ec60c8d224ef0b63c221e6c92aab49fe38aef5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:39:31 +0000

    Add package gandbk-distributions/0.1/gandbk_distributions-0.1.tar.gz

---
+++ packages/gandbk-distributions/gandbk_distributions-0.1.tar.gz/gandbk_distributions-0.1/README.md
@@ -0,0 +1,10 @@
+ 
+# dsnd-distribution package
+
+Summary of the package
+
+# Files
+
+Explanation fo files in the package
+
+# Installation
---
+++ packages/gandbk-distributions/gandbk_distributions-0.1.tar.gz/gandbk_distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='gandbk_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['gandbk_distributions'],
+      zip_safe=False)
