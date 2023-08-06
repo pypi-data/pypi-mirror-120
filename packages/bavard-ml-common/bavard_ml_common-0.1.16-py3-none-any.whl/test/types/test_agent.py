427200a53eed7d4ed0b86689bb3f0a57f27b000c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:12:24 +0000

    Add package bavard-ml-common/0.1.16/bavard_ml_common-0.1.16-py3-none-any.whl

---
+++ packages/bavard-ml-common/bavard_ml_common-0.1.16-py3-none-any.whl/test/types/test_agent.py
@@ -0,0 +1,86 @@
+from unittest import TestCase
+
+from fastapi.encoders import jsonable_encoder
+
+from bavard_ml_common.types.agent import AgentConfig, AgentExport
+from bavard_ml_common.types.conversations.actions import UserAction
+from bavard_ml_common.types.conversations.conversation import Conversation
+from bavard_ml_common.types.conversations.dialogue_turns import UserDialogueTurn
+from test.utils import load_json_file
+
+
+class TestAgent(TestCase):
+    def test_filters_bad_training_conversations(self):
+        agent_config = AgentExport.parse_file("test/data/agents/test-agent.json").config
+
+        # Empty conversations should be filtered out.
+        num_convs = len(agent_config.trainingConversations)
+        agent_config.trainingConversations.append(Conversation(turns=[]))
+        self.assertEqual(len(agent_config.trainingConversations), num_convs + 1)
+        agent_config = AgentConfig.parse_obj(jsonable_encoder(agent_config))
+        agent_config.filter_no_agent_convs()
+
+        # The added conversation should be filtered out.
+        self.assertEqual(len(agent_config.trainingConversations), num_convs)
+
+        # Conversations with no agent actions should be filtered out.
+        agent_config.trainingConversations.append(
+            Conversation(turns=[UserDialogueTurn(userAction=UserAction(type="UTTERANCE_ACTION"))])
+        )
+        self.assertEqual(len(agent_config.trainingConversations), num_convs + 1)
+        agent_config.filter_no_agent_convs()
+        # The added conversation should be filtered out.
+        self.assertEqual(len(agent_config.trainingConversations), num_convs)
+
+    def test_adds_nlu_examples_from_training_conversations(self):
+        raw_agent = load_json_file("test/data/agents/bavard.json")["config"]
+        n_nlu_examples = sum(len(examples) for examples in raw_agent["intentExamples"].values())
+        agent = AgentConfig.parse_obj(raw_agent)
+        agent.incorporate_training_conversations()
+        # The `agent` object should now have more nlu examples than `n_nlu_examples`.
+        self.assertGreater(len(list(agent.all_nlu_examples())), n_nlu_examples)
+
+    def test_excludes_examples_with_unknown_labels(self):
+        messy_agent = AgentExport.parse_file("test/data/agents/invalid-nlu-examples.json").config
+        messy_agent.remove_unknown_intent_examples()
+
+        # Examples with unregistered intents or tag types should be filtered out.
+        self.assertEqual(len(list(messy_agent.all_nlu_examples())), 2)
+        for ex in messy_agent.all_nlu_examples():
+            self.assertIn(ex.intent, messy_agent.intent_names())
+            for tag in ex.tags or []:
+                self.assertIn(tag.tagType, messy_agent.tag_names())
+
+    def test_no_side_effects_from_conversion(self):
+        config = AgentExport.parse_file("test/data/agents/bavard.json").config
+        snapshot = config.copy(deep=True)
+        config.to_nlu_dataset()
+        config.to_conversation_dataset()
+        self.assertEqual(config, snapshot)
+
+    def test_intent_ood_examples(self):
+        config = AgentExport.parse_file("test/data/agents/ood.json").config
+        n_ood = len(config.intentOODExamples)
+        self.assertGreater(n_ood, 0)
+        without_ood = config.to_nlu_dataset()
+        dataset = config.to_nlu_dataset(include_ood=True)
+        # All the ood examples should be included in `dataset`
+        self.assertEqual(len(without_ood) + n_ood, len(dataset))
+        self.assertEqual(sum(1 for ex in dataset if ex.isOOD), n_ood)
+        # The dataset w/OOD examples should have an extra label: the `None` label.
+        self.assertEqual(len(dataset.unique_labels()), len(without_ood.unique_labels()) + 1)
+        self.assertIn(None, dataset.unique_labels())
+        self.assertNotIn(None, without_ood.unique_labels())
+
+    def test_conversation_dataset_conversion(self):
+        config = AgentExport.parse_file("test/data/agents/bavard.json").config
+        config.clean()
+        convs = config.to_conversation_dataset(expand=False)
+        reconstructed = AgentConfig.from_conversation_dataset(convs, name=config.name)
+        # The reconstructed agent should have the same conversation data.
+        self.assertEqual(len(config.trainingConversations), len(reconstructed.trainingConversations))
+        self.assertGreater(len(reconstructed.intent_names()), 0)
+        for intent in reconstructed.intent_names():
+            self.assertIn(intent, config.intent_names())
+        for i in range(len(config.trainingConversations)):
+            self.assertEqual(config.trainingConversations[i], reconstructed.trainingConversations[i])
---
+++ packages/bavard-ml-common/bavard_ml_common-0.1.16-py3-none-any.whl/test/types/test_conversation_dataset.py
@@ -0,0 +1,40 @@
+from unittest import TestCase
+
+from bavard_ml_common.types.conversations.actions import Actor
+from bavard_ml_common.types.conversations.conversation import Conversation, ConversationDataset
+
+
+class TestConversationDataset(TestCase):
+    def setUp(self):
+        self.dataset = ConversationDataset.from_conversations(
+            [Conversation.parse_file("test/data/conversations/last-turn-user.json")]
+        )
+
+    def test_make_validation_pairs(self):
+        convs, next_actions = self.dataset.make_validation_pairs()
+        self.assertEqual(len(convs), len(next_actions))
+
+        # Each validation conversation should end with a user action
+        for conv in convs:
+            self.assertEqual(conv.turns[-1].actor, Actor.USER)
+
+    def test_properties(self):
+        self.assertSetEqual(
+            self.dataset.unique_slots(),
+            {
+                "hotel-bookday",
+                "hotel-bookpeople",
+                "hotel-bookstay",
+                "hotel-internet",
+                "hotel-name",
+                "hotel-parking",
+                "hotel-pricerange",
+                "hotel-type",
+                "nomatches",
+            },
+        )
+        self.assertSetEqual(self.dataset.unique_intents(), {"Hotel-Inform", "Hotel-Request"})
+        self.assertSetEqual(self.dataset.unique_tag_types(), {"pricerange", "bookstay", "bookday", "bookpeople"})
+        self.assertSetEqual(
+            self.dataset.unique_labels(), {"Hotel-Inform", "Booking-Request", "Booking-NoBook", "general-reqmore"}
+        )
