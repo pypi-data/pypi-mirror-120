2e3c21ba94476cb91fe5936e8f5b3fee4a69e028
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:23:37 +0000

    Add package duxueyuan/1.0.16/duxueyuan-1.0.16-py3-none-any.whl

---
+++ packages/duxueyuan/duxueyuan-1.0.16-py3-none-any.whl/duxueyuan/__init__.py
@@ -0,0 +1,6 @@
+from duxueyuan.Square import Square
+from duxueyuan.duxueyuan import *
+print("你好我是duxueyuan的聊天机器人，很高兴认识你")
+print("我有许多功能，你可以来试一试")
+print("请输入duxueyuan.intro()")
+# # from duxueyuan.
