a753584af7d9aee71e8e7908dd8b2c3d245fd153
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:32 +0000

    Add package imperial.py/2.0.1/imperial.py-2.0.1.tar.gz

---
+++ packages/imperial.py/imperial.py-2.0.1.tar.gz/imperial.py-2.0.1/imperial/__init__.py
@@ -0,0 +1,23 @@
+__author__ = "Hexiro"
+from .imperialbin import *
+# not sure if i'm supposed to do use del, but i'm going to,
+# so imperialbin module doesn't exist and everything is used directly from imperial.
+# ie. imperial.create_document() and not imperial.imperialbin.create_document()
+del imperialbin
+
+__all__ = (
+    # from imperialbin
+    "Imperial",
+    "create_document",
+    "get_document",
+    "edit_document",
+    "delete_document",
+    "verify",
+    "purge_documents",
+    # from client
+    "client",
+    # from exceptions
+    "exceptions",
+    # from checks
+    "checks"
+)
