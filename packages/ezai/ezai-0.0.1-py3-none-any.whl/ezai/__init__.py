8062f001ab1ee8ee5d6f6dbab5db70a2f125bdca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:38 +0000

    Add package ezai/0.0.1/ezai-0.0.1-py3-none-any.whl

---
+++ packages/ezai/ezai-0.0.1-py3-none-any.whl/ezai/__init__.py
@@ -0,0 +1,11 @@
+# navsim_util
+
+from pathlib import Path
+
+version_file = Path(__file__).parent.joinpath('version.txt')
+with open(version_file, 'r') as vf:
+    __version__ = vf.read().strip()
+
+from . import image, env
+
+__all__ = ['image', 'env', 'dict', 'json', 'res']
