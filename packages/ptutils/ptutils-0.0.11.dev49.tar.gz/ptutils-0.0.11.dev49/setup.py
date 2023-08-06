63e72eb399c155e61975369dfdd02e5f5070cfb9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:41:04 +0000

    Add package ptutils/0.0.11.dev49/ptutils-0.0.11.dev49.tar.gz

---
+++ packages/ptutils/ptutils-0.0.11.dev49.tar.gz/ptutils-0.0.11.dev49/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#######################################################################################################################
+# (C) Copyright 2017-2019, Principled Technologies. All rights reserved.
+# This project and all associated files are proprietary copyrighted materials owned by Principled Technologies. 
+#######################################################################################################################
+import setuptools
+setuptools.setup(
+	setup_requires=['pbr'], 
+	pbr=True
+)
