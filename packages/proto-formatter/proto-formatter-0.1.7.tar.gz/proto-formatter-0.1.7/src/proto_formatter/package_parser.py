b6ad9c2542cf3f69c50aed9fd9d5decfdd77f091
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:03:20 +0000

    Add package proto-formatter/0.1.7/proto-formatter-0.1.7.tar.gz

---
+++ packages/proto-formatter/proto-formatter-0.1.7.tar.gz/proto-formatter-0.1.7/src/proto_formatter/package_parser.py
@@ -0,0 +1,28 @@
+from proto_formatter.comment import CommentParser
+from proto_formatter.proto_structures import Package
+from proto_formatter.protobuf import Protobuf
+
+
+class PackageParser():
+
+    @classmethod
+    def parse_and_add(cls, proto_obj: Protobuf, line, top_comment_list):
+        if proto_obj.package is not None:
+            raise 'multiple package detected!'
+
+        proto_obj.package = cls.parse_package(line, top_comment_list)
+
+    @classmethod
+    def parse_package(self, line, top_comment_list):
+        value = self._get_package_value(line)
+        comments = CommentParser.create_comment(line, top_comment_list)
+        package = Package(value, comments)
+        return package
+
+    @classmethod
+    def _get_package_value(self, line):
+        line = line.strip()
+        lindex = len('package ')
+        rindex = line.index(';')
+        value = line[lindex:rindex].strip()
+        return value
