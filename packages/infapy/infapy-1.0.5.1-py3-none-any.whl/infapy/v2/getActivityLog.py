ff9b72d8cd1023c597a67435fb1e3cc3663d5486
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:45:38 +0000

    Add package infapy/1.0.5.1/infapy-1.0.5.1-py3-none-any.whl

---
+++ packages/infapy/infapy-1.0.5.1-py3-none-any.whl/infapy/v2/getActivityLog.py
@@ -0,0 +1,33 @@
+import infapy
+
+class GetActivityLog:
+    def __init__(self,v2,v2BaseURL,v2icSessionID):
+        self._v2 = v2
+        self._v2BaseURL = v2BaseURL
+        self._v2icSessionID = v2icSessionID
+        
+
+    def getAllActivityLog(self):
+        """getAllAcitivityLog returns all the activity logs from IICS in dict format
+
+        Returns:
+            dict: <Acticity Log in dict Format>
+        """
+        url=self._v2BaseURL + "/api/v2/activity/activityLog"
+        headers = {'Content-Type': "application/json", 'Accept': "application/json","icSessionID":self._v2icSessionID}
+        infapy.log.info("GetAllActivityLog URL - " + url)
+        infapy.log.info("API Headers: " + str(headers))
+        infapy.log.info("Body: " + "This API requires no body")
+        # The below format is for post
+        # bodyV3={"username": userName,"password": password}
+        # r3 = re.post(url=urlV3, json=bodyV3, headers=headers)
+        try:
+            response = re.get(url=url, headers=headers)
+            infapy.log.debug(str(response))
+        except Exception as e:
+            infapy.log.exception(e)
+            raise
+        data = response.json()
+        return data
+
+        
