9ffcf0174a0179ef0f4c5572e478f2e26e66afc1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:44 +0000

    Add package edwardsserial/0.3.2/edwardsserial-0.3.2.tar.gz

---
+++ packages/edwardsserial/edwardsserial-0.3.2.tar.gz/edwardsserial-0.3.2/README.md
@@ -0,0 +1,3 @@
+# TIC
+
+Python object-oriented wrapper for the TIC Turbo- and Instrument Controller from Edwards (https://shop.edwardsvacuum.com/products/d39722000/view.aspx).
---
+++ packages/edwardsserial/edwardsserial-0.3.2.tar.gz/edwardsserial-0.3.2/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['edwardsserial', 'edwardsserial.tic']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['pyserial']
+
+setup_kwargs = {
+    'name': 'edwardsserial',
+    'version': '0.3.2',
+    'description': 'Python API implementing the serial protocoll from edwards vacuum.',
+    'long_description': '# TIC\n\nPython object-oriented wrapper for the TIC Turbo- and Instrument Controller from Edwards (https://shop.edwardsvacuum.com/products/d39722000/view.aspx).',
+    'author': 'Jan Petermann',
+    'author_email': 'jpeterma@physnet.uni-hamburg.de',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://gitlab.com/codingcoffeebean/edwardsserial',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
