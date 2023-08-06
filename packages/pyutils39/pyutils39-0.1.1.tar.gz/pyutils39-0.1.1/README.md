f12b2dd37de0d97046e3432e9b48d665a4a17a33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:41:06 +0000

    Add package pyutils39/0.1.1/pyutils39-0.1.1.tar.gz

---
+++ packages/pyutils39/pyutils39-0.1.1.tar.gz/pyutils39-0.1.1/README.md
@@ -0,0 +1,19 @@
+A small utilities package
+
+**Features:**
+
+Consoleask(message)
+Ask a question via the console
+
+reverse(text)
+Reverse a text string
+
+error(message,title)
+Show an error message (tk, but without root window)
+
+toplevelerror(message,title)
+same as error, but it stays on top.
+
+question(message,title) and toplevelquestion(message,title)
+same as error and toplevel error, but as a yes no question
+returns True for yes or False for no
---
+++ packages/pyutils39/pyutils39-0.1.1.tar.gz/pyutils39-0.1.1/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pyutils39",
+    version="0.1.1",
+    author="Enderbyte09",
+    author_email="enderbyte09@gmail.com",
+    description="A utilities package",
+    long_description=long_description,
+    py_modules=['pyutils39.py'],
+    long_description_content_type="text/markdown",
+    url="https://github.com/Enderbyte-Programs/pyutils39",
+    project_urls={
+        "Bug Tracker": "https://github.com/Enderbyte-Programs/pyutils39/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.8",
+)
