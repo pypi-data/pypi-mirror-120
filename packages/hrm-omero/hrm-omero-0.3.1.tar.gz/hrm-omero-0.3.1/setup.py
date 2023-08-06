ec54dce9e81091e217fab0a83f9b9bfb4336f7f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:01:49 +0000

    Add package hrm-omero/0.3.1/hrm-omero-0.3.1.tar.gz

---
+++ packages/hrm-omero/hrm-omero-0.3.1.tar.gz/hrm-omero-0.3.1/setup.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': 'src'}
+
+packages = \
+['hrm_omero']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['beautifulsoup4>=4.10.0,<5.0.0',
+ 'loguru>=0.5.3,<0.6.0',
+ 'omero-py>=5.9.0,<6.0.0']
+
+entry_points = \
+{'console_scripts': ['ome-hrm = hrm_omero.cli:main']}
+
+setup_kwargs = {
+    'name': 'hrm-omero',
+    'version': '0.3.1',
+    'description': 'A connector between the Huygens Remote Manager (HRM) and OMERO.',
+    'long_description': '# The HRM-OMERO connector\n\nThis project provides a connector to allow for communication between an [HRM (Huygens\nRemote Manager)][1] and an [OMERO server][2].\n\nIts purpose is to simplify the data transfer by allowing raw images to be downloaded\nfrom OMERO as well as uploading deconvolution results back to OMERO directly from within\nthe HRM web interface.\n\n## Installation\n\n### Distributions that include Python 3.6 or newer (e.g. Ubuntu 20.04)\n\n```bash\npip install hrm-omero\n```\n\n### CentOS 6\n\n[CentOS 6][co6] is EOL since 2020-11-30, so you should really consider upgrading to a newer\nrelease. However, we know that sometimes this is not easily doable due to dependencies,\nhardware support or whatever reason - so here are instructions to make the connector\nwork on that old distribution.\n\nIt\'s strongly recommended to use [pyenv][3] for installing *Python 3.6*, which is the\nabsolute minimum for using the HRM-OMERO connector (or its actual dependencies, to be\nfully correct). In case you don\'t want *pyenv* to mess with your system setup, you can\nsimply ask it to install that version somewhere and then only create a *virtual\nenvironment* from it using the `--copies` flag - this will result in a standalone\nsetup that won\'t affect anything else on the system.\n\n```bash\n# install the build-time requirements for Python 3.6 and Java 1.8 for Bio-Formats\nsudo yum install openssl-devel readline-devel gcc-c++ java-1.8.0-openjdk\n\n# get pyenv and put it into your home directory or wherever you prefer it to be\ngit clone https://github.com/pyenv/pyenv.git ~/.pyenv\n\n# activate pyenv *FOR THIS SHELL ONLY* (needs to be done whenever you want to use it)\nexport PYENV_ROOT="$HOME/.pyenv"\nexport PATH="$PYENV_ROOT/bin:$PATH"\neval "$(pyenv init --path)"\neval "$(pyenv init -)"\n\n# ask pyenv to install Python 3.6.15 (will end up in "~/.pyenv/versions/3.6.15/")\npyenv install 3.6.15  # takes a bit (compiling...)\n\n# create a bare, stand-alone Python 3.6 virtual environment\n~/.pyenv/versions/3.6.15/bin/python -m venv --copies /opt/venvs/hrm-omero\n\n# now you can install the connector into this virtual environment - please note that the\n# installation takes quite a while (~15min) as it needs to build the ZeroC Ice bindings\n/opt/venvs/hrm-omero/bin/pip install hrm-omero\n\n# from now on you can simply call the connector using its full path, there is no need\n# to pre-activate the virtual environment - you could even drop your pyenv completely:\n/opt/venvs/hrm-omero/bin/ome-hrm --help\n\n# this is even usable as a drop-in replacement for the legacy `ome_hrm.py` script:\ncd $PATH_TO_YOUR_HRM_INSTALLATION/bin\nmv "ome_hrm.py" "__old__ome_hrm.py"\nln -s "/opt/venvs/hrm-omero/bin/ome-hrm" "ome_hrm.py"\n```\n\n## Debugging\n\nBy default the connector will be rather silent as otherwise the log files will be\ncluttered up quite a bit on a production system. However, it is possible to increase the\nlog level by specifying `-v`, `-vv` and so on.\n\nCurrently it is not yet possible to ajust the log level that is being used when operated\nthrough the HRM web interface (which is the default). To do so, have a look in the\n`hrm_omero.cli.run_task()` function. Log messages produced by the connector when called\nby HRM will usually end up in the web server\'s error log (as they go to `stderr`).\n\n## Example Usage\n\nStore you username and password in variables:\n\n```bash\nread OMEROUSER\nread OMEROPW\n```\n\n### Verifying Credentials\n\n```bash\nome-hrm \\\n    --user $OMEROUSER \\\n    --password $OMEROPW \\\n    checkCredentials\n```\n\n### Fetching OMERO tree information\n\nSet the `--id` parameter according to what part of the tree should be retrieved:\n\n```bash\nOMERO_ID="ROOT"                # fetches the base tree view for the current user\nOMERO_ID="G:4:Experimenter:9"  # fetches the projects of user \'9\' in group \'4\'\nOMERO_ID="G:4:Project:12345"   # fetches the datasets of project \'12345\'\nOMERO_ID="G:4:Dataset:65432"   # lists the images of dataset \'65432\'\n```\n\nThen run the actual command to fetch the information, the result will be a JSON tree:\n\n```bash\nome-hrm \\\n    --user $OMEROUSER \\\n    --password $OMEROPW \\\n    retrieveChildren \\\n    --id "$OMERO_ID"\n```\n\nFor example this could be the output when requesting `"G:4:Dataset:65432"`:\n\n```json\n[\n    {\n        "children": [],\n        "class": "Image",\n        "id": "G:4:Image:1311448",\n        "label": "4321_mko_ctx_77.tif",\n        "owner": "somebody"\n    },\n    {\n        "children": [],\n        "class": "Image",\n        "id": "G:4:Image:1566150",\n        "label": "test-image.tif",\n        "owner": "somebody"\n    }\n]\n```\n\n### Downloading an image from OMERO\n\nThis will fetch the second image from the example tree above and store it in `/tmp/`:\n\n```bash\nome-hrm \\\n    --user $OMEROUSER \\\n    --password $OMEROPW \\\n    OMEROtoHRM \\\n    --imageid "G:4:Image:1566150" \\\n    --dest /tmp/\n```\n\n### Uploading an image from the local file system to OMERO\n\nThe command below will import a local image file into the example dataset from above:\n\n```bash\nome-hrm \\\n    --user $OMEROUSER \\\n    --password $OMEROPW \\\n    HRMtoOMERO \\\n    --dset "G:4:Dataset:65432" \\\n    --file test-image.tif\n```\n\n[1]: https://huygens-rm.org/\n[2]: https://www.openmicroscopy.org/omero/\n[3]: https://github.com/pyenv/pyenv\n[co6]: https://wiki.centos.org/About/Product\n',
+    'author': 'Niko Ehrenfeuchter',
+    'author_email': 'nikolaus.ehrenfeuchter@unibas.ch',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://pypi.org/project/hrm-omero/',
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.6.2,<4.0.0',
+}
+
+
+setup(**setup_kwargs)
---
+++ packages/hrm-omero/hrm-omero-0.3.1.tar.gz/hrm-omero-0.3.1/src/hrm_omero/cli.py
@@ -0,0 +1,211 @@
+"""Command-line interface related functions."""
+
+import argparse
+import sys
+
+from loguru import logger as log
+
+from .__init__ import __version__
+from . import formatting
+from . import hrm
+from . import omero
+from . import transfer
+
+
+def bool_to_exitstatus(value):
+    """Convert a boolean to a POSIX process exit code.
+
+    As boolean values in Python are a subset of int, `True` corresponds to the int value
+    '1', which is the opposite of a successful POSIX return code. Therefore, this
+    function simply inverts the boolean value to turn it into a proper exit code. In
+    case the provided value is not of type `bool` it will be returned unchanged.
+
+    Parameters
+    ----------
+    value : bool or int
+        The value to be converted.
+
+    Returns
+    -------
+    int
+        0 in case `value` is `True`, 1 in case `value` is `False` and `value` itself in
+        case it is not a bool.
+    """
+    if type(value) is bool:
+        return not value
+    else:
+        return value
+
+
+def parse_arguments(args):
+    """Parse the commandline arguments."""
+    # log.debug("Parsing command line arguments...")
+    argparser = argparse.ArgumentParser(
+        description=__doc__, formatter_class=argparse.RawDescriptionHelpFormatter
+    )
+    argparser.add_argument(
+        "-v",
+        "--verbose",
+        dest="verbosity",
+        action="count",
+        default=0,
+        help="verbose messages (repeat for more details)",
+    )
+
+    argparser.add_argument(
+        "--version",
+        action="version",
+        version=f"%(prog)s {__version__}",
+    )
+
+    argparser.add_argument(
+        "-c",
+        "--config",
+        default="/etc/hrm.conf",
+        help="the HRM configuration file (default: '/etc/hrm.conf')",
+    )
+
+    # required arguments group
+    req_args = argparser.add_argument_group(
+        "required arguments", "NOTE: MUST be given before any subcommand!"
+    )
+    req_args.add_argument("-u", "--user", required=True, help="OMERO username")
+    req_args.add_argument("-w", "--password", required=True, help="OMERO password")
+
+    subparsers = argparser.add_subparsers(
+        help=".",
+        dest="action",
+        description="Action to be performed, one of the following:",
+    )
+
+    # checkCredentials parser
+    subparsers.add_parser(
+        "checkCredentials", help="check if login credentials are valid"
+    )
+
+    # retrieveChildren parser
+    parser_subtree = subparsers.add_parser(
+        "retrieveChildren", help="get the children of a given node object (JSON)"
+    )
+    parser_subtree.add_argument(
+        "--id",
+        type=str,
+        required=True,
+        help='ID of the parent object, e.g. "ROOT", "G:4:Experimenter:7',
+    )
+
+    # OMEROtoHRM parser
+    parser_o2h = subparsers.add_parser(
+        "OMEROtoHRM", help="download an image from the OMERO server"
+    )
+    parser_o2h.add_argument(
+        "-i",
+        "--imageid",
+        required=True,
+        help='the OMERO ID of the image to download, e.g. "G:4:Image:42"',
+    )
+    parser_o2h.add_argument(
+        "-d",
+        "--dest",
+        type=str,
+        required=True,
+        help="the destination directory where to put the downloaded file",
+    )
+
+    # HRMtoOMERO parser
+    parser_h2o = subparsers.add_parser(
+        "HRMtoOMERO", help="upload an image to the OMERO server"
+    )
+    parser_h2o.add_argument(
+        "-d",
+        "--dset",
+        required=True,
+        dest="dset",
+        help='the ID of the target dataset in OMERO, e.g. "G:7:Dataset:23"',
+    )
+    parser_h2o.add_argument(
+        "-f",
+        "--file",
+        type=str,
+        required=True,
+        help="the image file to upload, including the full path",
+    )
+    parser_h2o.add_argument(
+        "-n",
+        "--name",
+        type=str,
+        required=False,
+        help="a label to use for the image in OMERO",
+    )
+    parser_h2o.add_argument(
+        "-a",
+        "--ann",
+        type=str,
+        required=False,
+        help="annotation text to be added to the image in OMERO",
+    )
+
+    try:
+        return argparser.parse_args(args)
+    except IOError as err:
+        argparser.error(str(err))
+
+
+def run_task(args):
+    """Parse commandline arguments and initiate the requested tasks."""
+    args = parse_arguments(args)
+
+    # one of the downsides of loguru is that the level of an existing logger can't be
+    # changed - so to adjust verbosity we actually need to remove the default logger and
+    # re-add it with the new level (unless maximum verbosity was requested anyway...)
+    # see https://github.com/Delgan/loguru/issues/138 for more information
+    log_level = "TRACE"
+    if args.verbosity < 4:  # -vvvv (4) will result in "TRACE", nothing to be done then
+        log.remove()
+        if args.verbosity == 3:  # -vvv will be "DEBUG"
+            log_level = "DEBUG"
+        elif args.verbosity == 2:  # -vv will be "INFO"
+            log_level = "INFO"
+        elif args.verbosity == 1:  # -v will be "SUCCESS"
+            log_level = "SUCCESS"
+        else:  # no verbosity flag has been provided
+            log_level = "WARNING"
+        log.add(sys.stderr, level=log_level)
+
+    # uncomment the lines below and adjust to manually set the log level until this is
+    # possible through the HRM configuration file
+    # log_level = "DEBUG"
+    # log.remove()
+    # log.add(sys.stderr, level=log_level)
+
+    log.info("Logging verbosity requested: {} ({})", args.verbosity, log_level)
+
+    hrm_config = hrm.parse_config(args.config)
+    host = hrm_config.get("OMERO_HOSTNAME", "localhost")
+    port = hrm_config.get("OMERO_PORT", 4064)
+
+    conn = omero.connect(args.user, args.password, host, port)
+
+    # TODO: implement requesting groups via cmdline option
+
+    try:
+        if args.action == "checkCredentials":
+            return omero.check_credentials(conn)
+        elif args.action == "retrieveChildren":
+            return formatting.print_children_json(conn, args.id)
+        elif args.action == "OMEROtoHRM":
+            return transfer.from_omero(conn, args.imageid, args.dest)
+        elif args.action == "HRMtoOMERO":
+            return transfer.to_omero(conn, args.dset, args.file)
+        else:
+            raise Exception("Huh, how could this happen?!")
+    finally:
+        conn.close()
+
+
+@log.catch
+def main(args=None):
+    """Wrapper to call the run_task() function and return its exit code."""
+    if not args:
+        args = sys.argv[1:]
+    sys.exit(bool_to_exitstatus(run_task(args)))
