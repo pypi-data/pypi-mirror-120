7067113fc25a103235460a05970e9401361fc6c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:37 +0000

    Add package aiobungie/0.2.5b8/aiobungie-0.2.5b8-py3-none-any.whl

---
+++ packages/aiobungie/aiobungie-0.2.5b8-py3-none-any.whl/aiobungie/__init__.pyi
@@ -0,0 +1,34 @@
+from .error import *
+from .internal.enums import *
+from .client import Client as Client
+from .rest import RESTClient as RESTClient
+
+# Names in __all__ with no definition:
+#   ActivityNotFound
+#   ClanNotFound
+#   Class
+#   Component
+#   CredentialType
+#   DamageType
+#   Dungeon
+#   Forbidden
+#   GameMode
+#   Gender
+#   GroupType
+#   HTTPException
+#   Item
+#   MembershipType
+#   MembershipTypeError
+#   MilestoneType
+#   NotFound
+#   Place
+#   Planet
+#   PlayerNotFound
+#   Race
+#   Raid
+#   ResponseError
+#   Stat
+#   Unauthorized
+#   UserNotFound
+#   Vendor
+#   WeaponType
