a2723c0b3da60f289ef64e7e0af94e46b77cf30a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:12:48 +0000

    Add package kinopoisk-api-unofficial-client/0.9.7/kinopoisk-api-unofficial-client-0.9.7.tar.gz

---
+++ packages/kinopoisk-api-unofficial-client/kinopoisk-api-unofficial-client-0.9.7.tar.gz/kinopoisk-api-unofficial-client-0.9.7/src/kinopoisk_api_client/client/reviews_api_client.py
@@ -0,0 +1,13 @@
+from kinopoisk_api_client.client.api_client import ApiClient
+from kinopoisk_api_client.request.reviews.review_details_request import ReviewDetailsRequest
+from kinopoisk_api_client.request.reviews.reviews_request import ReviewsRequest
+from kinopoisk_api_client.response.reviews.review_details_response import ReviewDetailsResponse
+from kinopoisk_api_client.response.reviews.reviews_response import ReviewsResponse
+
+
+class ReviewsApiClient(ApiClient):
+    def send_reviews_request(self, request: ReviewsRequest) -> ReviewsResponse:
+        return self._send_request(request)
+
+    def send_review_details_request(self, request: ReviewDetailsRequest) -> ReviewDetailsResponse:
+        return self._send_request(request)
