d5d582b09226e3edd5560b7f72efcbc3034a615d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:40:10 +0000

    Add package PyOOPy/0.1/PyOOPy-0.1.tar.gz

---
+++ packages/PyOOPy/PyOOPy-0.1.tar.gz/PyOOPy-0.1/setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+
+setup(
+    name='PyOOPy',
+    packages=['PyOOPy'],
+    version='0.1',
+    license='MIT',
+    description='Object-oriented programming in Python',
+    author='Bartosz Paulewicz',
+    author_email='podolce0@gmail.com',
+    url='https://gitlab.com/baton96/PyOOP',
+    download_url='https://gitlab.com/baton96/PyOOP/-/archive/0.1.1/PyOOP-0.1.1.tar.gz',
+    keywords=['OOP', 'Object-oriented programming'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3'
+    ]
+)
