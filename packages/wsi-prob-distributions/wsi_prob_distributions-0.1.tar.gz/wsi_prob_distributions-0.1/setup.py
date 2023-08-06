70a8c54d01b6831e0cd47c5ad0f30eb3da8c673a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:30 +0000

    Add package wsi-prob-distributions/0.1/wsi_prob_distributions-0.1.tar.gz

---
+++ packages/wsi-prob-distributions/wsi_prob_distributions-0.1.tar.gz/wsi_prob_distributions-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='wsi_prob_distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['wsi_prob_distributions'],
+      author = 'Walid Ismail',
+      zip_safe=False)
