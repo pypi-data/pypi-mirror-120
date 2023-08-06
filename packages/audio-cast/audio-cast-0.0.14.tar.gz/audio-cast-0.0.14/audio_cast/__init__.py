0d4fbcc82f217fb5b37ae268c6ec83bd83789b0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:24:28 +0000

    Add package audio-cast/0.0.14/audio-cast-0.0.14.tar.gz

---
+++ packages/audio-cast/audio-cast-0.0.14.tar.gz/audio-cast-0.0.14/audio_cast/__init__.py
@@ -0,0 +1,23 @@
+import os
+import click
+
+from flask import Flask
+
+def create_app(test_config=None):
+    app = Flask(__name__, instance_relative_config=True)
+
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    from audio_cast import endpoints
+
+    app.register_blueprint(endpoints.bp)
+
+    return app
+
+@click.command()
+def cli():
+    """Example script."""
+    click.echo('Hello World 2!')
