d1e8359a69eb954012bae0d90480a88ab71afb35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:17 +0000

    Add package node-py/0.0.25/node_py-0.0.25.tar.gz

---
+++ packages/node-py/node_py-0.0.25.tar.gz/node_py-0.0.25/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['node_py']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'node-py',
+    'version': '0.0.25',
+    'description': 'Easy to use, package that brings aspects of node.js to python, but improvised and with new features.',
+    'long_description': None,
+    'author': 'Ryan Cundiff',
+    'author_email': None,
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
