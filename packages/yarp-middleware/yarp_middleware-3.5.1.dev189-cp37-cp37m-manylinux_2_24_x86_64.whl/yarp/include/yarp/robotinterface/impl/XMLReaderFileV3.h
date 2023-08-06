540cc36fbd5dc2c7c0f7248711359a6b0a91de11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:29:14 +0000

    Add package yarp-middleware/3.5.1.dev189/yarp_middleware-3.5.1.dev189-cp37-cp37m-manylinux_2_24_x86_64.whl

---
+++ packages/yarp-middleware/yarp_middleware-3.5.1.dev189-cp37-cp37m-manylinux_2_24_x86_64.whl/yarp/include/yarp/robotinterface/impl/XMLReaderFileV3.h
@@ -0,0 +1,42 @@
+/*
+ * SPDX-FileCopyrightText: 2006-2021 Istituto Italiano di Tecnologia (IIT)
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef YARP_ROBOTINTERFACE_IMPL_XMLREADERFILEV3_H
+#define YARP_ROBOTINTERFACE_IMPL_XMLREADERFILEV3_H
+
+#include <yarp/robotinterface/impl/XMLReaderFileVx.h>
+
+#include <string>
+
+namespace yarp {
+namespace robotinterface {
+
+class XMLReaderResult;
+
+namespace impl {
+
+class XMLReaderFileV3 : public XMLReaderFileVx
+{
+public:
+    XMLReaderFileV3();
+    ~XMLReaderFileV3() override;
+
+    yarp::robotinterface::XMLReaderResult getRobotFromFile(const std::string& filename,
+                                                                         const yarp::os::Searchable& config,
+                                                                         bool verbose = false) override;
+    yarp::robotinterface::XMLReaderResult getRobotFromString(const std::string& xmlString,
+                                                                           const yarp::os::Searchable& config,
+                                                                           bool verbose = false) override;
+
+private:
+    class Private;
+    Private* const mPriv;
+};
+
+} // namespace impl
+} // namespace robotinterface
+} // namespace yarp
+
+#endif // YARP_ROBOTINTERFACE_XMLREADERFILEV3_H
